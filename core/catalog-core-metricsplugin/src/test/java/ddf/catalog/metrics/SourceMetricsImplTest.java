4ca5f4eeb109d65d1327bb8fd12a66dfba11f774
Hugh Rodgers <hugh.rodgers@lmco.com>
2013-06-26 14:44:53 -0700

    DDF-61: fixed CatalogMetricsTest to not cause exceptions by attempting to create metrics that already exist

--- core/catalog-core-metricsplugin/src/test/java/ddf/catalog/metrics/SourceMetricsImplTest.java
+++ core/catalog-core-metricsplugin/src/test/java/ddf/catalog/metrics/SourceMetricsImplTest.java
@@ -49,7 +49,7 @@ public class SourceMetricsImplTest {
     public static void init() {
         ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) LoggerFactory
                 .getLogger(Logger.ROOT_LOGGER_NAME);
-        root.setLevel(ch.qos.logback.classic.Level.TRACE);
+        root.setLevel(ch.qos.logback.classic.Level.INFO);
     }
 
 	@Test
