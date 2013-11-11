1109f575026f1a812cec9bf90ddfc2799aa474b2
millerw8 <william.l.miller@lmco.com>
2013-11-11 10:35:57 -0500

    DDF-236: As a user when using the KML Network link, I want to be able to view the full Metadata in a user-friendly format
    DDF-236: MORE

---
+++ transformer/service-html-transformer/src/test/java/org/codice/ddf/catalog/transformer/html/TestMetacardValueResolver.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright (c) Codice Foundation
+ * 
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ * 
+ **/
+package org.codice.ddf.catalog.transformer.html;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.junit.Test;
+
+import com.github.jknack.handlebars.ValueResolver;
+
+import ddf.catalog.data.MetacardImpl;
+import ddf.catalog.data.MetacardType;
+import ddf.catalog.data.MetacardTypeImpl;
+
+public class TestMetacardValueResolver {
+
+
+    @Test
+    public void testResolveGeometry() {
+        MetacardImpl mc = new MetacardImpl();
+        String expected = "POINT(10 5)";
+        mc.setLocation(expected);
+        
+        MetacardValueResolver mvr = new MetacardValueResolver();
+        Object actual = mvr.resolve(mc, "geometry");
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testResolveSourceId() {
+        MetacardImpl mc = new MetacardImpl();
+        String expected = "src";
+        mc.setSourceId(expected);
+        
+        MetacardValueResolver mvr = new MetacardValueResolver();
+        Object props = mvr.resolve(mc, "properties");
+        Object actual = mvr.resolve(props, "source-id");
+        assertEquals(expected, actual);        
+    }
+    
+    @Test
+    public void testResolveThumbnail() {
+        MetacardImpl mc = new MetacardImpl();
+        byte[] bytes = new byte[] {97, 98, 99};
+        String expected = DatatypeConverter.printBase64Binary(bytes);
+        mc.setThumbnail(bytes);
+        
+        MetacardValueResolver mvr = new MetacardValueResolver();
+        Object props = mvr.resolve(mc, "properties");
+        Object actual = mvr.resolve(props, "thumbnail");
+        assertEquals(expected, actual);        
+    }
+    
+    @Test
+    public void testResolveType() {
+        MetacardImpl mc = new MetacardImpl();
+        String expected = "feature";
+        MetacardType expectedType = new MetacardTypeImpl(expected, null);
+        mc.setType(expectedType);
+        
+        MetacardValueResolver mvr = new MetacardValueResolver();
+        Object props = mvr.resolve(mc, "properties");
+        Object actual = mvr.resolve(props, "type");
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testResolveProperty() {
+        MetacardImpl mc = new MetacardImpl();
+        String expectedId = "id";
+        String expectedType = "content";
+        mc.setId(expectedId);
+        mc.setContentTypeName(expectedType);
+        
+        MetacardValueResolver mvr = new MetacardValueResolver();
+        Object props = mvr.resolve(mc, "properties");
+        Object actualId = mvr.resolve(props, "id");
+        assertEquals(expectedId, actualId);
+
+        Object actualType = mvr.resolve(props, "metadata-content-type");
+        assertEquals(expectedType, actualType);
+    }
+
+    @Test
+    public void testResolveUnknown() {
+        String unknown = "unknown";
+        MetacardValueResolver mvr = new MetacardValueResolver();
+        Object actual = mvr.resolve(unknown, "anything");
+        assertEquals(ValueResolver.UNRESOLVED, actual);
+    }
+
+    @Test
+    public void testResolveUnknownProperty() {
+        String unknown = "unknownProp";
+        MetacardImpl mc = new MetacardImpl();
+        MetacardValueResolver mvr = new MetacardValueResolver();
+        Object props = mvr.resolve(mc, "properties");
+        Object actual = mvr.resolve(props, unknown);
+        assertEquals(ValueResolver.UNRESOLVED, actual);
+        
+    }
+}
