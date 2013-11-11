9863381b34ef88b016f85927ba43b6ad70cdd48e
Scott Tustison <scott.a.tustison@lmco.com>
2013-11-11 11:15:51 -0700

    DDF-231 - added moment date format handlebars helper and new js file to hold our custom helpers
     moved the result list item template into its own file because it is getting too large for the json template file

---
+++ ui/search-ui/mapsearch/src/main/webapp/js/HandlebarsHelpers.js
@@ -0,0 +1,40 @@
+// The module to be exported
+var helper, helpers = {
+    /*
+     * Handlebars Helper: Moment.js
+     * @author: https://github.com/Arkkimaagi
+     * Built for Assemble: the static site generator and
+     * component builder for Node.js, Grunt.js and Yeoman.
+     * http://assemble.io
+     *
+     * Copyright (c) 2013, Upstage
+     * Licensed under the MIT license.
+     */
+    moment: function(context, block) {
+        var date, i;
+        if (context && context.hash) {
+            block = _.cloneDeep(context);
+            context = undefined;
+        }
+        date = moment(context);
+
+        // Reset the language back to default before doing anything else
+        date.lang('en');
+
+        for (i in block.hash) {
+            if (date[i]) {
+                date = date[i](block.hash[i]);
+            } else {
+                console.log('moment.js does not support "' + i + '"');
+            }
+        }
+        return date;
+    }
+};
+
+// Export helpers
+for (helper in helpers) {
+    if (helpers.hasOwnProperty(helper)) {
+        ich.addHelper(helper, helpers[helper]);
+    }
+}
