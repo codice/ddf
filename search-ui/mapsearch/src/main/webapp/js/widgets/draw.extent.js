c31cb069acd2633a6825dc8ad73e2e45ebbcc442
Mike Macaulay <michael.macaulay@aviture.us.com>
2013-11-12 11:44:55 -0700

    start of draw widget

---
+++ search-ui/mapsearch/src/main/webapp/js/widgets/draw.extent.js
@@ -0,0 +1,43 @@
+/*global define*/
+
+define(function (require) {
+    "use strict";
+    var Marionette = require('marionette'),
+        Backbone = require('backbone'),
+        ddf = require('ddf');
+
+    var Draw = {};
+
+    Draw.Views = {};
+
+    Draw.Model = Backbone.Model.extend({
+
+    });
+
+    Draw.Controller = Marionette.Controller.extend({
+        initialize : function(options){
+            this.viewer = options.viewer;
+
+        },
+
+        method1 : function(){
+
+        }
+
+
+    });
+
+    Draw.Views.ButtonView = Backbone.View.extend({
+
+
+
+    });
+
+    Draw.Views.CesiumView = Backbone.View.extend({
+
+    });
+
+
+
+    return Draw;
+});
