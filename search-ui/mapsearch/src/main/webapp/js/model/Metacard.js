742cb43ce09cf951d6892cbf9d4232ebd605d551
Scott Tustison <scott.a.tustison@lmco.com>
2013-11-15 10:15:06 -0700

    DDF-231 - pushed the default query parameters down to the model

--- search-ui/mapsearch/src/main/webapp/js/model/Metacard.js
+++ search-ui/mapsearch/src/main/webapp/js/model/Metacard.js
@@ -10,59 +10,75 @@ define(function (require) {
     require('backbonerelational');
     MetaCard.Geometry = Backbone.RelationalModel.extend({
 
-});
+    });
 
     MetaCard.Properties = Backbone.RelationalModel.extend({
 
-});
+    });
 
     MetaCard.Metacard = Backbone.RelationalModel.extend({
-    relations: [
-        {
-            type: Backbone.HasOne,
-            key: 'geometry',
-            relatedModel: MetaCard.Geometry
-        },
-        {
-            type: Backbone.HasOne,
-            key: 'properties',
-            relatedModel: MetaCard.Properties
-        }
-    ]
-});
+        relations: [
+            {
+                type: Backbone.HasOne,
+                key: 'geometry',
+                relatedModel: MetaCard.Geometry
+            },
+            {
+                type: Backbone.HasOne,
+                key: 'properties',
+                relatedModel: MetaCard.Properties
+            }
+        ]
+    });
 
     MetaCard.MetacardResult = Backbone.RelationalModel.extend({
-    relations: [{
-        type: Backbone.HasOne,
-        key: 'metacard',
-        relatedModel: MetaCard.Metacard
-    }]
-});
+        relations: [{
+            type: Backbone.HasOne,
+            key: 'metacard',
+            relatedModel: MetaCard.Metacard
+        }]
+    });
 
     MetaCard.MetacardList = Backbone.Collection.extend({
-    model: MetaCard.MetacardResult
-});
+        model: MetaCard.MetacardResult
+    });
 
     MetaCard.SearchResult = Backbone.RelationalModel.extend({
-    relations: [{
-        type: Backbone.HasMany,
-        key: 'results',
-        relatedModel: MetaCard.MetacardResult,
-        collectionType: MetaCard.MetacardList
-    }],
-    url: "/services/catalog/query",
-    loadMoreResults: function() {
-        var queryParams = this.get("queryParams");
-        this.set("count", this.get("count") + this.get("itemsPerPage"));
-        queryParams = queryParams.replace(new RegExp("&count=\\d*", "g"), "&count="+this.get("count"));
-        this.fetch({
-            url: this.url,
-            data: queryParams,
-            dataType: "jsonp",
-            timeout: 300000
-        });
-    }
-});
+        defaults: {
+            count: 100,
+            itemsPerPage: 100,
+            startIndex: 1,
+            format: "geojson",
+            queryParamDefaults: {
+                count: "&count=",
+                format: "&format=",
+                start: "&start="
+            }
+        },
+        relations: [{
+            type: Backbone.HasMany,
+            key: 'results',
+            relatedModel: MetaCard.MetacardResult,
+            collectionType: MetaCard.MetacardList
+        }],
+        url: "/services/catalog/query",
+        loadMoreResults: function() {
+            var queryParams;
+            this.set("count", this.get("count") + this.get("itemsPerPage"));
+            queryParams = this.getQueryParams();
+            this.fetch({
+                url: this.url,
+                data: queryParams,
+                dataType: "jsonp",
+                timeout: 300000
+            });
+        },
+        getQueryParams: function() {
+            return this.get("queryParams")+this.get("queryParamDefaults").count+this.get("count")+
+                this.get("queryParamDefaults").start+this.get("startIndex")+
+                this.get("queryParamDefaults").format+this.get("format");
+        }
+    });
     return MetaCard;
 
 });
