733599d3daf64907b05c9e49d24730cfabb51874
Scott Tustison <scott.a.tustison@lmco.com>
2014-01-22 10:58:18 -0700

    DDF-249 turned off cometd debugging

--- search-ui/standard/src/main/webapp/js/cometd.js
+++ search-ui/standard/src/main/webapp/js/cometd.js
@@ -11,8 +11,8 @@ define(function (require) {
     Cometd.Comet = $.cometd;
     var path = location.protocol + '//' + location.hostname+(location.port ? ':' + location.port : '') + '/cometd';
     Cometd.Comet.configure({
-        url: path,
-        logLevel: 'debug'
+        url: path
+//        logLevel: 'debug'
     });
     //TODO: we need some way to unsub/disconnect when we know it is finished
     Cometd.Comet.handshake({});
