418498168d40e6ccbc71e2c34e593bfb691fbebb
Jesse Kim <jesse.s.kim@lmco.com>
2014-02-26 13:28:57 -0700

    DDF-335: Branching off Admin Ui code

--- opensearch/catalog-opensearch-source/src/main/java/ddf/catalog/source/opensearch/CddaOpenSearchSite.java
+++ opensearch/catalog-opensearch-source/src/main/java/ddf/catalog/source/opensearch/CddaOpenSearchSite.java
@@ -67,17 +67,20 @@ import ddf.catalog.operation.SourceResponse;
 import ddf.catalog.operation.impl.SourceResponseImpl;
 import ddf.catalog.resource.ResourceNotFoundException;
 import ddf.catalog.resource.ResourceNotSupportedException;
+import ddf.catalog.service.ConfiguredService;
 import ddf.catalog.source.FederatedSource;
 import ddf.catalog.source.SourceMonitor;
 import ddf.catalog.source.UnsupportedQueryException;
 import ddf.util.XPathHelper;
+
+
 
 /**
  * Federated site that talks via OpenSearch to the DDF platform. Communication is usually performed
  * via https which requires a keystore and trust store to be provided.
  * 
  */
-public final class CddaOpenSearchSite implements FederatedSource {
+public final class CddaOpenSearchSite implements FederatedSource, ConfiguredService {
     private boolean isInitialized = false;
 
     // service properties
@@ -142,6 +145,7 @@ public final class CddaOpenSearchSite implements FederatedSource {
     private Configuration siteSecurityConfig;
 
     private SecureRemoteConnection connection;
+    private String configurationPid;
 
     /**
      * Creates an OpenSearch Site instance. Sets an initial default endpointUrl that can be
@@ -903,4 +907,15 @@ public final class CddaOpenSearchSite implements FederatedSource {
         logger.exit(methodName + ":   endpointUrl = " + endpointUrl);
     }
 
+    @Override
+    public String getConfigurationPid()
+    {
+        return configurationPid;
+    }
+
+    @Override
+    public void setConfigurationPid(String configurationPid)
+    {
+        this.configurationPid = configurationPid;
+    }
 }
