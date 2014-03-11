2c940de0bbc617de85161a5697c7bc04a4571a4b
Damon Jones <damon.jones@lmco.com>
2014-03-10 21:17:51 -0700

    DDF-344: Added code to read proxy from system settings and apply to Camel route

--- proxy-camel-route/src/main/java/org/codice/proxy/http/HttpProxyServiceImpl.java
+++ proxy-camel-route/src/main/java/org/codice/proxy/http/HttpProxyServiceImpl.java
@@ -14,13 +14,12 @@
  **/
 package org.codice.proxy.http;
 
-import java.math.BigInteger;
-import java.security.SecureRandom;
+import java.util.ArrayList;
 
-import org.apache.camel.Component;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.servlet.ServletComponent;
 import org.apache.camel.core.osgi.OsgiDefaultCamelContext;
+import org.apache.commons.lang.StringUtils;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,6 +39,17 @@ public class HttpProxyServiceImpl extends OsgiDefaultCamelContext implements Htt
 	private static final String SERVLET_COMPONENT = "servlet";
 	public static final String SERVLET_PATH = "/proxy";
 	public static final String GENERIC_ENDPOINT_NAME = "endpoint";
+	
+	public static final String HTTP_PROXY_KEY = "http.";
+	public static final String HTTPS_PROXY_KEY = "https.";
+	public static final String HTTP_PROXY_HOST_KEY = "proxyHost";
+	public static final String HTTP_PROXY_PORT_KEY = "proxyPort";
+	public static final String HTTP_PROXY_AUTH_METHOD_KEY = "proxyAuthMethod";
+	public static final String HTTP_PROXY_AUTH_USERNAME_KEY = "proxyAuthUsername";
+	public static final String HTTP_PROXY_AUTH_PASSWORD_KEY = "proxyAuthPassword";
+	public static final String HTTP_PROXY_AUTH_DOMAIN_KEY = "proxyAuthDomain";
+	public static final String HTTP_PROXY_AUTH_HOST_KEY = "proxyAuthHost";
+	
 	int incrementer = 0;
 
     public HttpProxyServiceImpl(final BundleContext bundleContext) throws Exception {
@@ -61,8 +71,10 @@ public class HttpProxyServiceImpl extends OsgiDefaultCamelContext implements Htt
     	return endpointName;
     }
     
-    public String start(final String endpointName, final String targetUri) throws Exception{
-
+    public String start(final String endpointName, final String targetUri) throws Exception {
+    	
+    	enableProxySettings();
+    	
     	this.targetUri = targetUri;
 	    routeBuilder = new RouteBuilder() {
 	        @Override
@@ -73,12 +85,45 @@ public class HttpProxyServiceImpl extends OsgiDefaultCamelContext implements Htt
 	    };
 	    this.addRoutes(routeBuilder);
 	    this.start();
-	    LOGGER.debug("Started proxy route at servlet endpoint: " + endpointName + ", routing to: " + targetUri);
+	    LOGGER.debug("Started proxy route at servlet endpoint: {}, routing to: {}", endpointName, targetUri);
 	    return endpointName;
     }
     
+    
+    /**
+     * Enable external proxy settings
+     */
+    private void enableProxySettings(){
+    	
+    	//Fetch all proxy settings and add settings to Camel context if not null, whitespace or empty
+    	ArrayList<String> sysProxyConfigs = new ArrayList<String>();
+    	sysProxyConfigs.add(HTTP_PROXY_KEY + HTTP_PROXY_HOST_KEY);
+    	sysProxyConfigs.add(HTTP_PROXY_KEY + HTTP_PROXY_PORT_KEY);
+    	sysProxyConfigs.add(HTTP_PROXY_KEY + HTTP_PROXY_AUTH_METHOD_KEY);
+    	sysProxyConfigs.add(HTTP_PROXY_KEY + HTTP_PROXY_AUTH_USERNAME_KEY);
+    	sysProxyConfigs.add(HTTP_PROXY_KEY + HTTP_PROXY_AUTH_PASSWORD_KEY);
+    	sysProxyConfigs.add(HTTP_PROXY_KEY + HTTP_PROXY_AUTH_DOMAIN_KEY);
+    	sysProxyConfigs.add(HTTP_PROXY_KEY + HTTP_PROXY_AUTH_HOST_KEY);
+    	sysProxyConfigs.add(HTTPS_PROXY_KEY + HTTP_PROXY_HOST_KEY);
+    	sysProxyConfigs.add(HTTPS_PROXY_KEY + HTTP_PROXY_PORT_KEY);
+    	sysProxyConfigs.add(HTTPS_PROXY_KEY + HTTP_PROXY_AUTH_METHOD_KEY);
+    	sysProxyConfigs.add(HTTPS_PROXY_KEY + HTTP_PROXY_AUTH_USERNAME_KEY);
+    	sysProxyConfigs.add(HTTPS_PROXY_KEY + HTTP_PROXY_AUTH_PASSWORD_KEY);
+    	sysProxyConfigs.add(HTTPS_PROXY_KEY + HTTP_PROXY_AUTH_DOMAIN_KEY);
+    	sysProxyConfigs.add(HTTPS_PROXY_KEY + HTTP_PROXY_AUTH_HOST_KEY);
+    	
+    	String prop = null;
+    	for(int i=0; i < sysProxyConfigs.size(); ++i){
+    		prop = System.getProperty(sysProxyConfigs.get(i));
+        	if(StringUtils.isNotBlank(prop)){
+        		LOGGER.debug("Property: {} = {}", sysProxyConfigs.get(i), prop);
+        		this.getProperties().put(sysProxyConfigs.get(i), prop);
+        	}
+    	}
+    }
+    
     public void stop(String endpointName) throws Exception{
-    	LOGGER.debug("Stopping proxy route at endpoint: " + endpointName);
+    	LOGGER.debug("Stopping proxy route at endpoint: {}", endpointName);
     	this.removeRoute(endpointName);
     }
     
