968f250ae85fb39502aea29c315ff2b461a3ad81
Bruce Beyeler <bruce.beyeler@lmco.com>
2014-03-26 09:44:25 -0700

    DDF-373 Started on updating the application models.

---
+++ modules/admin-modules-installer/src/main/webapp/js/models/Applications.js
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ **/
+/*global define*/
+define(function (require) {
+
+    var Backbone = require('backbone');
+    var Applications = {};
+    var Application = {};
+
+    Application.Model = Backbone.Model.extend({
+        defaults: {
+            name: "Application",
+            version: "1.0",
+            description: "An application to be installed.",
+            selected: false
+        }
+    });
+
+    Applications.Model = Backbone.Model.extend({
+        defaults: {
+
+            hasNext: true,
+            hasPrevious: false,
+            totalSteps: 3,
+            stepNumber: 0,
+            percentComplete: 0
+        },
+        nextStep: function() {
+            this.set(this._step(1));
+        },
+        previousStep: function() {
+            this.set(this._step(-1));
+        },
+        _step: function(direction) {
+            var changeObj = {};
+            changeObj.stepNumber = this.get('stepNumber') + direction;
+            if(changeObj.stepNumber < this.get('totalSteps')) {
+                changeObj.hasNext = true;
+            } else {
+                changeObj.stepNumber = this.get('totalSteps');
+                changeObj.hasNext = false;
+            }
+
+            if(changeObj.stepNumber > 0) {
+                changeObj.hasPrevious = true;
+            } else {
+                changeObj.stepNumber = 0;
+                changeObj.hasPrevious = false;
+            }
+
+            changeObj.percentComplete = Math.round(changeObj.stepNumber / this.get('totalSteps') * 100);
+
+            return changeObj;
+        }
+    });
+
+    return Applications;
+
+});
