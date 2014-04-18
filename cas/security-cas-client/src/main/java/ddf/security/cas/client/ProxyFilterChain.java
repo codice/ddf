3b3542ae222f85c1828ee5e90906a4909c264bd0
Dave Willison <dave.willison@lmco.com>
2014-04-18 12:52:38 -0700

    DDF-419: Updates to personalize standard search UI based on cas userid
    This update is to spport updates to ddf-ui

--- cas/security-cas-client/src/main/java/ddf/security/cas/client/ProxyFilterChain.java
+++ cas/security-cas-client/src/main/java/ddf/security/cas/client/ProxyFilterChain.java
@@ -1,36 +1,36 @@
 /**
  * Copyright (c) Codice Foundation
- * 
+ *
  * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
  * is distributed along with this program and can be found at
  * <http://www.gnu.org/licenses/lgpl.html>.
- * 
+ *
  **/
 package ddf.security.cas.client;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import org.apache.log4j.Logger;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-
-import org.apache.log4j.Logger;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
- * 
+ *
  * Proxy filter chain used to call of the required CAS filters in a specific order.
- * 
+ *
  */
 public class ProxyFilterChain implements FilterChain {
     private static final Logger LOGGER = Logger.getLogger(ProxyFilterChain.class);
@@ -83,7 +83,7 @@ public class ProxyFilterChain implements FilterChain {
 
     /**
      * Calls the next filter in the chain.
-     * 
+     *
      * @see javax.servlet.FilterChain#doFilter(javax.servlet.ServletRequest,
      *      javax.servlet.ServletResponse)
      */
@@ -95,12 +95,15 @@ public class ProxyFilterChain implements FilterChain {
 
         if (iterator.hasNext()) {
             Filter filter = iterator.next();
-            LOGGER.debug("Calling " + filter.getClass().getName() + ".doFilter(" + servletRequest
+            LOGGER.debug("Calling filter " + filter.getClass().getName() + ".doFilter(" + servletRequest
                     + ", " + servletResponse + ", " + this + ")");
             filter.doFilter(servletRequest, servletResponse, this);
         } else {
-            LOGGER.debug("Calling " + filterChain.getClass().getName() + ".doFilter("
+            LOGGER.debug("Calling filterChain " + filterChain.getClass().getName() + ".doFilter("
                     + servletRequest + ", " + servletResponse + ")");
+            servletRequest.setAttribute("org.codice.ddf.ui.searchui.standard.properties.user",
+                    ((HttpServletRequestWrapper)servletRequest).getRemoteUser());
+            LOGGER.debug("User: " + servletRequest.getAttribute("org.codice.ddf.ui.searchui.standard.properties.user"));
             filterChain.doFilter(servletRequest, servletResponse);
         }
     }
