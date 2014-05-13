c84209d77fded0096f81d9463f195b95386d3414
Scott Tustison <scott.a.tustison@lmco.com>
2014-05-13 14:14:43 -0700

    DDF-462 modified security servlet filter to grab all filters and add in the correct order

--- search-ui/search-securityservletfilter/src/main/java/org/codice/ddf/ui/searchui/security/SecurityServletFilter.java
+++ search-ui/search-securityservletfilter/src/main/java/org/codice/ddf/ui/searchui/security/SecurityServletFilter.java
@@ -14,7 +14,12 @@
  **/
 package org.codice.ddf.ui.searchui.security;
 
-import java.io.IOException;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -22,13 +27,10 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.framework.ServiceReference;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * {@link SecurityServletFilter} is meant to detect any Security ServletFilters and call their
@@ -63,9 +65,15 @@ public class SecurityServletFilter implements Filter {
 
         if (serviceRefs != null && serviceRefs.length != 0) {
             LOGGER.debug("Found filter, now filtering...");
-            Filter proxyFilter = (Filter) ctx.getService(serviceRefs[0]);
-            LOGGER.debug("Filter class: {}", proxyFilter.getClass().getName());
-            proxyFilter.doFilter(servletRequest, servletResponse, filterChain);
+
+            ProxyFilterChain chain = new ProxyFilterChain(filterChain);
+
+            for(int i=serviceRefs.length - 1; i>=0; i--) {
+                chain.addFilter((Filter) ctx.getService(serviceRefs[i]));
+            }
+
+//            LOGGER.debug("Filter class: {}", proxyFilter.getClass().getName());
+            chain.doFilter(servletRequest, servletResponse);
         } else {
             LOGGER.debug("Did not find filter");
             filterChain.doFilter(servletRequest, servletResponse);
@@ -74,6 +82,81 @@ public class SecurityServletFilter implements Filter {
 
     @Override
     public void init(FilterConfig arg0) throws ServletException {
+    }
+
+    public static class ProxyFilterChain implements FilterChain {
+        private static final Logger LOGGER = LoggerFactory.getLogger(ProxyFilterChain.class);
+
+        private FilterChain filterChain;
+
+        private List<Filter> filters;
+
+        private Iterator<Filter> iterator;
+
+        /**
+         * @param filterChain
+         *            The filter chain from the web container.
+         */
+        public ProxyFilterChain(FilterChain filterChain) {
+            this.filterChain = filterChain;
+            filters = new ArrayList<Filter>();
+        }
+
+        /**
+         * @param filter
+         *            The servlet filter to add.
+         */
+        public void addFilter(Filter filter) {
+            if (iterator != null) {
+                throw new IllegalStateException();
+            }
+
+            LOGGER.debug("Adding filter " + filter.getClass().getName() + " to filter chain.");
+            filters.add(0, filter);
+        }
+
+        /**
+         * @param filters
+         *            The servlet filters to add.
+         */
+        public void addFilters(List<Filter> filters) {
+            if (iterator != null) {
+                throw new IllegalStateException();
+            }
+
+            this.filters.addAll(filters);
+
+            if (LOGGER.isDebugEnabled()) {
+                for (Filter filter : filters) {
+                    LOGGER.debug("Added filter " + filter.getClass().getName() + " to filter chain.");
+                }
+            }
+        }
+
+        /**
+         * Calls the next filter in the chain.
+         *
+         * @see javax.servlet.FilterChain#doFilter(javax.servlet.ServletRequest,
+         *      javax.servlet.ServletResponse)
+         */
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse)
+                throws IOException, ServletException {
+            if (iterator == null) {
+                iterator = filters.iterator();
+            }
+
+            if (iterator.hasNext()) {
+                Filter filter = iterator.next();
+                LOGGER.debug("Calling filter " + filter.getClass().getName() + ".doFilter(" + servletRequest
+                        + ", " + servletResponse + ", " + this + ")");
+                filter.doFilter(servletRequest, servletResponse, this);
+            } else {
+                LOGGER.debug("Calling filterChain " + filterChain.getClass().getName() + ".doFilter("
+                        + servletRequest + ", " + servletResponse + ")");
+                filterChain.doFilter(servletRequest, servletResponse);
+            }
+        }
+
     }
 
 }
