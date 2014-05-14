697156f065c56cba5e52d3863bac7bf9725fde0e
Bruce Beyeler <bruce.beyeler@lmco.com>
2014-05-13 17:10:56 -0700

    DDF-463 Continuation of refactoring.

---
+++ filter/security-filter-anonymous/src/main/java/org/codice/security/filter/anonymous/AnonymousHandler.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ **/
+package org.codice.security.filter.anonymous;
+
+
+import org.codice.security.filter.api.AuthenticationHandler;
+import org.codice.security.filter.api.FilterResult;
+import org.apache.cxf.sts.QNameConstants;
+import org.apache.cxf.ws.security.sts.provider.model.secext.AttributedString;
+import org.apache.cxf.ws.security.sts.provider.model.secext.PasswordString;
+import org.apache.cxf.ws.security.sts.provider.model.secext.UsernameTokenType;
+import org.apache.ws.security.WSConstants;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.xml.bind.JAXB;
+import javax.xml.bind.JAXBElement;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AnonymousHandler implements AuthenticationHandler {
+    public static final Logger logger = LoggerFactory.getLogger(AnonymousHandler.class.getName());
+
+    @Override
+    public FilterResult getNormalizedToken(ServletRequest request, ServletResponse response, FilterChain chain, boolean resolve) {
+        FilterResult result = new FilterResult();
+        result.setStatus(FilterResult.FilterStatus.NO_ACTION);
+
+        // For anonymous - always generate authentication credentials as 'guest'
+        UsernameTokenType usernameTokenType = new UsernameTokenType();
+        AttributedString user = new AttributedString();
+        user.setValue("guest");
+        usernameTokenType.setUsername(user);
+
+        // Add a password
+        PasswordString password = new PasswordString();
+        password.setValue("guest");
+        password.setType(WSConstants.PASSWORD_TEXT);
+        JAXBElement<PasswordString> passwordType = new JAXBElement<PasswordString>(QNameConstants.PASSWORD, PasswordString.class, password);
+        usernameTokenType.getAny().add(passwordType);
+
+        Writer writer = new StringWriter();
+        JAXB.marshal(usernameTokenType, writer);
+
+        String usernameSecurityToken = writer.toString();
+        logger.debug("Security token returned: {}", usernameSecurityToken);
+
+        result.setAuthCredentials(usernameSecurityToken);
+        result.setStatus(FilterResult.FilterStatus.COMPLETED);
+        return result;
+    }
+}
---
+++ filter/security-filter-web-sso/src/main/java/org/codice/security/filter/websso/FilterUtils.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ **/
+package org.codice.security.filter.websso;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FilterUtils {
+    /**
+     * Returns a mapping of cookies from the incoming request. Key is the cookie name, while the
+     * value is the Cookie object itself.
+     *
+     * @param req Servlet request for this call
+     * @return map of Cookie objects present in the current request - always returns a map
+     */
+    public static Map<String, Cookie> getCookieMap(HttpServletRequest req) {
+        HashMap<String, Cookie> map = new HashMap<String, Cookie>();
+
+        Cookie[] cookies = req.getCookies();
+        for (Cookie cookie : cookies) {
+            map.put(cookie.getName(), cookie);
+        }
+
+        return map;
+    }
+
+}
