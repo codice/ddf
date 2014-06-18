02fbc101f63202e9af34fa8a638917a31dde427d
Hugh Rodgers <hugh.rodgers@lmco.com>
2014-06-18 15:08:09 -0700

    DDF-527  removed client code from CassandraEmbeddedServer, it is no longer a service and only a server

--- platform-cassandra-embedded-server/src/main/java/org/codice/ddf/platform/cassandra/embedded/CassandraEmbeddedServer.java
+++ platform-cassandra-embedded-server/src/main/java/org/codice/ddf/platform/cassandra/embedded/CassandraEmbeddedServer.java
@@ -19,10 +19,7 @@ import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 import java.net.InetAddress;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
@@ -54,27 +51,15 @@ import org.apache.cassandra.tracing.Tracing;
 import org.apache.cassandra.utils.CLibrary;
 import org.apache.cassandra.utils.Mx4jTool;
 import org.apache.cassandra.utils.Pair;
-import org.codice.ddf.persistentstorage.PersistentStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.KeyspaceMetadata;
-import com.datastax.driver.core.Metadata;
-import com.datastax.driver.core.ProtocolOptions.Compression;
-import com.datastax.driver.core.Row;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.core.TableMetadata;
-import com.datastax.driver.core.exceptions.NoHostAvailableException;
-import com.datastax.driver.core.exceptions.QueryExecutionException;
-import com.datastax.driver.core.policies.Policies;
 import com.google.common.collect.Iterables;
 
-public class CassandraEmbeddedServer implements PersistentStore {
+public class CassandraEmbeddedServer {
 
     private static Logger LOGGER = LoggerFactory.getLogger(CassandraEmbeddedServer.class);
-    
-    private static final String TABLE_NAME = "catalog";
     
     // Have a dedicated thread to call exit to avoid deadlock in the case where the thread that wants to invoke exit
     // belongs to an executor that our shutdown hook wants to wait to exit gracefully. See CASSANDRA-5273.
@@ -171,15 +156,6 @@ public class CassandraEmbeddedServer implements PersistentStore {
             LOGGER.error("Timeout starting Cassandra embedded", e);
             throw new IllegalStateException("Timeout starting Cassandra embedded", e);
         }
-        
-        this.cluster = createCluster("localhost", config);
-        
-        LOGGER.info("Creating {} keyspace", keyspaceName);
-        createKeyspace(keyspaceName, true);
-        // Sets the keyspace to be used for all subsequent CQL statements
-        setKeyspace(keyspaceName);
-        createTable(keyspaceName, TABLE_NAME, 
-                "id uuid PRIMARY KEY, type text, properties map");
     }
     
     // Based on CassadraDaemon.setup()
@@ -441,174 +417,11 @@ public class CassandraEmbeddedServer implements PersistentStore {
             thriftServer.start();
         else
             LOGGER.info("Not starting RPC server as requested. Use JMX (StorageService->startRPCServer()) or nodetool (enablethrift) to start it");
-    }
-    
-    private Cluster createCluster(String hostname, CassandraConfig config) {
-        return Cluster.builder()
-                .addContactPoint(hostname)
-                .withPort(config.getCqlPort())
-                .withClusterName(config.getClusterName())
-                .withCompression(Compression.SNAPPY)
-                .withLoadBalancingPolicy(Policies.defaultLoadBalancingPolicy())
-                .withRetryPolicy(Policies.defaultRetryPolicy())
-                .withReconnectionPolicy(Policies.defaultReconnectionPolicy())
-                .build();
-    }
-    
-    public void createKeyspace(String keyspaceName) {
-        createKeyspace(keyspaceName, true);
-    }
-    
-    public void createKeyspace(String keyspaceName, boolean keyspaceDurableWrite) {
-        final Session session = cluster.connect("system");
-        final Row row = session.execute(
-                "SELECT count(1) FROM schema_keyspaces WHERE keyspace_name='" + keyspaceName + "'").one();
-        if (row.getLong(0) != 1) {
-            StringBuilder createKeyspaceStatement = new StringBuilder("CREATE keyspace ");
-            createKeyspaceStatement.append(keyspaceName);
-            createKeyspaceStatement.append(" WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor':1}");
-            if (!keyspaceDurableWrite) {
-                createKeyspaceStatement.append(" AND DURABLE_WRITES=false");
-            }
-            session.execute(createKeyspaceStatement.toString());
-        }
-        session.close();
-    }
-    
-    
-    //TODO:  Pass in Java object to be morphed into CQL column descriptions (see Achilles TableBuilder, etc. code for ideas)   
-    public void createTable(String keyspaceName, String tableName, String cqlColumnDescriptions) {        
-        try {
-            if (!hasTable(tableName, keyspaceName)) {               
-                String query = "CREATE TABLE " + keyspaceName + "." + tableName + " (" + cqlColumnDescriptions + ")";
-                LOGGER.info("Executing query: {}", query);
-                getSession(keyspaceName).execute(query);
-            } else {
-                LOGGER.info("Table {} already exists", tableName);
-            }
-        } catch (QueryExecutionException e) {
-            LOGGER.info("QueryExecutionException: ", e);
-        } catch (NoHostAvailableException e) {
-            LOGGER.info("NoHostAvailableException: ", e);
-        }
-    }
-    
-    public Session getSession(String keyspaceName) {
-        return cluster.connect(keyspaceName);
-    }
-    
-    public void setKeyspace(String keyspaceName) {
-        getSession(keyspaceName).execute("USE " + keyspaceName);
-    }
-    
-    public List<String> getKeyspaceNames() {
-        Metadata metadata = cluster.getMetadata();
-        List<KeyspaceMetadata> keyspaceMetadataList = metadata.getKeyspaces();
-        List<String> keyspaceNames = new ArrayList<String>();
-        
-        for (KeyspaceMetadata ksMetadata : keyspaceMetadataList) {
-            keyspaceNames.add(ksMetadata.getName());
-        }
-        
-        return keyspaceNames;
-    }
-    
-    public boolean hasKeyspace(String keyspaceName) {
-        return getKeyspaceNames().contains(keyspaceName);
-    }
-    
-    public Map<String, TableMetadata> getTables(String keyspaceName) {
-        Metadata metadata = cluster.getMetadata();
-        List<KeyspaceMetadata> keyspaceMetadataList = metadata.getKeyspaces();
-        Map<String, TableMetadata> tables = new HashMap<String, TableMetadata>();
-        LOGGER.info("keyspaceMetadataList.size() = {}", keyspaceMetadataList.size());
-        
-        for (KeyspaceMetadata ksMetadata : keyspaceMetadataList) {
-            LOGGER.info("keyspace name = {}", ksMetadata.getName());
-            if (ksMetadata.getName().equals(keyspaceName)) {               
-                for (TableMetadata tableMetadata : ksMetadata.getTables()) {
-                    LOGGER.info("table name = {}", tableMetadata.getName());
-                    tables.put(tableMetadata.getName(), tableMetadata);
-                }
-            }
-        }
-        
-        return tables;
-    }
-    
-    public TableMetadata getTable(String keyspaceName, String tableName) {
-        Metadata metadata = cluster.getMetadata();
-        List<KeyspaceMetadata> keyspaceMetadataList = metadata.getKeyspaces();
-        Map<String, TableMetadata> tables = new HashMap<String, TableMetadata>();
-        LOGGER.info("keyspaceMetadataList.size() = {}", keyspaceMetadataList.size());
-        
-        for (KeyspaceMetadata ksMetadata : keyspaceMetadataList) {
-            LOGGER.info("keyspace name = {}", ksMetadata.getName());
-            if (ksMetadata.getName().equals(keyspaceName)) {               
-                for (TableMetadata tableMetadata : ksMetadata.getTables()) {
-                    if (tableMetadata.getName().equals(tableName)) {
-                        return tableMetadata;
-                    }
-                }
-            }
-        }
-        
-        return null;
-    }
-    
-    public boolean hasTable(String tableName, String keyspaceName) {
-        Map<String, TableMetadata> tables = getTables(keyspaceName);
-        
-        return tables.containsKey(tableName);
-    }
-    
-    public void addEntry(String keyspaceName, String tableName, String cql) {
-        LOGGER.info("Executing CQL:  {}", cql);
-        Session session = getSession(keyspaceName);
-        session.execute(cql);
-    }
+    }    
     
     public void shutdown() {
         LOGGER.info("Embedded Cassandra shutdown() invoked ...");
         //TODO ...
         //session.close();   //will this do it???
-    }
-
-    @Override
-    public void addEntry(String type, Map<String, Object> properties) {
-        if (type.equalsIgnoreCase("saved_query")) {
-            addSavedQuery(properties);
-        } else {
-            LOGGER.info("Unsupported entry type {}", type);
-        }
-        
-    }
-
-    @Override
-    public void getEntry(String type, String cql) {
-        LOGGER.info("Not yet implemented ...");
-    }
-    
-    private void addSavedQuery(Map<String, Object> properties) {
-        LOGGER.info("ENTERING: addSavedQuery()");
-        
-        String cql = "INSERT INTO " + keyspaceName + "." + TABLE_NAME + "(id, type, properties) VALUES (" + 
-        		UUID.randomUUID().toString() + ", 'saved_query', {";
-        int count = 1;
-        for (String name : properties.keySet()) {
-            cql += "'" + name + "' : '" + properties.get(name);
-            if (count == properties.size()) {
-                cql += "'";
-            } else {
-                cql += "',";
-            }
-            count++;
-        }
-        cql += "})";
-        LOGGER.info("cql = {}", cql);
-        Session session = getSession(keyspaceName);
-        session.execute(cql);
-        
-        LOGGER.info("EXITING: addSavedQuery()");
-    }
+    }   
 }
