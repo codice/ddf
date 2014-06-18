6f0b81ee7b0e559e934dee61e1f67e4c8f86745d
Hugh Rodgers <hugh.rodgers@lmco.com>
2014-06-18 14:59:57 -0700

    DDF-527  added update chain to solrconfig.xml to persist all Solr entries into Cassandra for backup

---
+++ solr-cassandra/src/main/java/org/codice/solr/cassandra/CassandraUpdateRequestProcessor.java
@@ -0,0 +1,173 @@
+/**
+ * Copyright (c) Codice Foundation
+ * 
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ * 
+ **/
+package org.codice.solr.cassandra;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.SolrInputField;
+import org.apache.solr.update.AddUpdateCommand;
+import org.apache.solr.update.CommitUpdateCommand;
+import org.apache.solr.update.DeleteUpdateCommand;
+import org.apache.solr.update.RollbackUpdateCommand;
+import org.apache.solr.update.processor.UpdateRequestProcessor;
+import org.codice.ddf.cassandra.CassandraClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CassandraUpdateRequestProcessor extends UpdateRequestProcessor {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(CassandraUpdateRequestProcessor.class);
+    
+    private static final String CASSANDRA_KEYSPACE_NAME = "ddf";
+    
+    // The minimum schema/columns that every Cassandra table has
+    private static final String CASSANDRA_BASE_TABLE_SCHEMA = 
+            "id_txt uuid PRIMARY KEY, createddate_tdt timestamp";
+    
+    private String storeName;
+    private CassandraClient cassandraClient;
+
+    public CassandraUpdateRequestProcessor(String storeName, CassandraClient cassandraClient, UpdateRequestProcessor next) {
+        super(next);
+        LOGGER.info("INSIDE: CassandraUpdateRequestProcessor constructor");
+        this.storeName = storeName;
+        this.cassandraClient = cassandraClient;
+    }
+    
+    @Override
+    public void processAdd(AddUpdateCommand cmd) throws IOException {
+        LOGGER.info("ENTERING: processAdd()");
+        try {
+            // Add SolrInputDocument's contents into Cassandra table corresponding to SolrCore
+            SolrInputDocument solrInputDocument = cmd.getSolrInputDocument();
+            persistToCassandra(solrInputDocument);
+            
+            // Call next UpdateRequestProcessor in the chain
+            super.processAdd(cmd);
+        } catch (Exception e) {
+            LOGGER.info("Need to do rollback here");
+            throw new RuntimeException(e);
+        }
+    }
+    
+    @Override
+    public void processCommit(CommitUpdateCommand cmd) throws IOException {
+        LOGGER.info("ENTERING: processCommit()");
+        super.processCommit(cmd);
+    }
+    
+    @Override
+    public void processDelete(DeleteUpdateCommand cmd) throws IOException {
+        LOGGER.info("ENTERING: processDelete()");
+        super.processDelete(cmd);
+    }
+    
+    @Override
+    public void processRollback(RollbackUpdateCommand cmd) throws IOException {
+        LOGGER.info("ENTERING: processRollback()");
+        super.processRollback(cmd);
+    }
+
+    private void persistToCassandra(SolrInputDocument solrInputDocument) {
+        LOGGER.info("ENTERING: persistToCassandra()");
+        
+        String columnNamesClause = "";
+        String valuesClause = "";
+
+        int count = 1;
+        Collection<String> fieldNames = solrInputDocument.getFieldNames();
+        for (String fieldName : fieldNames) {
+            LOGGER.info("Working on field name {}", fieldName);
+            SolrInputField fieldValue = (SolrInputField) solrInputDocument.getField(fieldName);
+
+            boolean validColumn = false;
+            if (fieldName.equals("id_txt")) {
+                String value = (String) fieldValue.getValue();
+                valuesClause += CassandraClient.normalizeUuid(value);  // do not quote UUID value
+                validColumn = true;
+            } else if (fieldName.endsWith("_set_txt")) {
+                Collection<Object> values = fieldValue.getValues();
+                if (values != null && !values.isEmpty()) {
+                    valuesClause += "{";
+                    int setCount = 1;
+                    for (Object value : values) {
+                        valuesClause += "'" + (String) value + "'";
+                        if (setCount != values.size()) {
+                            valuesClause += ", ";
+                        }
+                        setCount++;
+                    }
+                    valuesClause += "}";
+                    validColumn = true;
+                }
+            } else if (fieldName.endsWith("_xml") || fieldName.endsWith("_txt")) {
+                String value = (String) fieldValue.getValue();
+                valuesClause += "'" + value + "'";
+                validColumn = true;
+            } else if (fieldName.endsWith("_lng")) {
+                Long value = Long.valueOf((String) fieldValue.getValue());
+                valuesClause += value;
+                validColumn = true;
+            } else if (fieldName.endsWith("_int")) {
+                Integer value = Integer.valueOf((String) fieldValue.getValue());
+                valuesClause += value;
+                validColumn = true;
+            }
+
+            if (validColumn) {
+                columnNamesClause += CassandraClient.normalizeCqlName(fieldName);
+                if (count != fieldNames.size()) {
+                    columnNamesClause += ", ";
+                    valuesClause += ", ";
+                }
+            } else {
+                LOGGER.info("Column {} was deemed invalid and not added to the INSERT", fieldName);
+            }
+
+            count++;
+        }
+            
+        String cql = "INSERT INTO " + storeName + "(";
+        cql += columnNamesClause + ") VALUES (" + valuesClause + ")";
+        LOGGER.info("cql = {}", cql);
+        try {
+            cassandraClient.addEntry(CASSANDRA_KEYSPACE_NAME, cql);
+        } catch (Exception e) {
+            LOGGER.info("Exception trying to INSERT entry for table {}. Will attempt to create table/schema and retry INSERT", storeName);
+            // Assume exception is because Cassandra table or its columns do not exist yet.
+            // Create and execute the CQL commands to create the table with a schema based on
+            // the field types in the SolrInputDoument (the suffixes on the field names will indicate
+            // the table schema to generate, e.g., "_txt" suffix indicates a "text" column type).
+            createTable(storeName, fieldNames);
+            
+            // Re-execute CQL to insert entry
+            LOGGER.info("Re-trying INSERT with cql = {}", cql);
+            cassandraClient.addEntry(CASSANDRA_KEYSPACE_NAME, cql);
+        }
+    }
+    
+    private void createTable(String tableName, Collection<String> columnNames) {
+        cassandraClient.createTable(CASSANDRA_KEYSPACE_NAME, tableName, CASSANDRA_BASE_TABLE_SCHEMA);
+        
+        for (String columnName : columnNames) {
+            cassandraClient.addColumn(tableName, columnName);
+        }
+    }
+
+}
---
+++ solr-cassandra/src/main/java/org/codice/solr/cassandra/CassandraUpdateRequestProcessorFactory.java
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) Codice Foundation
+ * 
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ * 
+ **/
+package org.codice.solr.cassandra;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.core.PluginInfo;
+import org.apache.solr.core.SolrCore;
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.response.SolrQueryResponse;
+import org.apache.solr.update.processor.UpdateRequestProcessor;
+import org.apache.solr.update.processor.UpdateRequestProcessorChain;
+import org.apache.solr.update.processor.UpdateRequestProcessorFactory;
+import org.apache.solr.util.plugin.SolrCoreAware;
+import org.codice.ddf.cassandra.CassandraClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CassandraUpdateRequestProcessorFactory extends UpdateRequestProcessorFactory implements SolrCoreAware {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CassandraUpdateRequestProcessorFactory.class);
+       
+    private static final String CHAIN_NAME = "lux-update-chain";
+    
+    private SolrCore solrCore;
+    private CassandraClient cassandraClient;
+    
+            
+    @Override
+    public void inform(SolrCore solrCore) {
+        LOGGER.info("ENTERING: inform()");
+        this.solrCore = solrCore;
+        NamedList<?> initArgs = null;
+        for (PluginInfo info : solrCore.getSolrConfig().getPluginInfos(UpdateRequestProcessorChain.class.getName())) {
+            
+            if (info.name.equals(CHAIN_NAME)) {
+                initArgs = info.initArgs;
+                LOGGER.info("initArgs = {}", initArgs);
+                if (initArgs != null) {
+                    this.cassandraClient = getCassandraClient(initArgs);
+                }
+            } else {
+                LOGGER.info("Skipping info.name = {}", info.name);
+            }
+        }
+    }
+    
+    /**
+     * Hierarchical check for definition of Cassandra hostname and port to connect to.
+     * Order of checking is:
+     *     system properties cassandra.host and cassandra.port, usually defined in setenv script
+     *     <INSTALL_DIR>/data/solr/cassandra_connection.properties file
+     *     defined as arguments in the solrconfig.xml definition of the Solr update chain
+     *     
+     * @param args
+     * @return
+     */
+    private CassandraClient getCassandraClient(NamedList<?> args) {
+        CassandraClient cassandraClient = null;
+        int cassandraCqlPort = -1;
+        
+        LOGGER.info("Checking if any system properties set for Cassandra host/port connection");
+        String cassandraHost = System.getProperty("cassandra.host");
+        String port = System.getProperty("cassandra.port");
+        if (StringUtils.isNotBlank(cassandraHost) && StringUtils.isNotBlank(port)) {
+            cassandraCqlPort = Integer.valueOf(port);
+            try {
+                cassandraClient = new CassandraClient(cassandraHost, cassandraCqlPort);
+                LOGGER.info("Successfully connected to Cassandra at host={}, port={} using system properties",
+                        cassandraHost, cassandraCqlPort);    
+                return cassandraClient;
+            } catch (Exception e) {
+                LOGGER.info("Exception trying to get CassandraClient connection using system properties cassandra.host={}, cassandra.port={}", 
+                        cassandraHost, cassandraCqlPort);
+            }
+        } else {
+            LOGGER.info("No system properties set for cassandra.host and cassandra.port");
+        }
+        
+        String cassandraConnectionPropertiesFilename = System.getProperty("karaf.home") + "/data/solr/cassandra_connection.properties";
+        File f = new File(cassandraConnectionPropertiesFilename);
+        if (f.exists()) {
+            LOGGER.info("Attempting Cassandra connection using host/port from properties file {}", 
+                    cassandraConnectionPropertiesFilename);
+            try {
+                InputStream is = new FileInputStream(f);
+                Properties props = new Properties();
+                props.load(is);
+                cassandraHost = props.getProperty("cassandra.host");
+                port = props.getProperty("cassandra.port");
+                if (StringUtils.isNotBlank(cassandraHost) && StringUtils.isNotBlank(port)) {
+                    cassandraCqlPort = Integer.valueOf(port);
+                    try {
+                        cassandraClient = new CassandraClient(cassandraHost, cassandraCqlPort);
+                        LOGGER.info("Successfully connected to Cassandra at host={}, port={} using properties file {}", 
+                                cassandraHost, cassandraCqlPort, cassandraConnectionPropertiesFilename);
+                        return cassandraClient;
+                    } catch (Exception e) {
+                        LOGGER.info("Exception trying to get CassandraClient connection using cassandra_connection.properties file with cassandra.host={}, cassandra.port={}", 
+                                cassandraHost, cassandraCqlPort);
+                    }
+                } else {
+                    LOGGER.info("Cassandra properties file's values for cassandra.host and cassandra.port were either null or empty");
+                }
+            } catch (FileNotFoundException e) {
+                LOGGER.info("FileNotFoundException trying to connect to Cassandra using properties file", e);
+            } catch (IOException e) {
+                LOGGER.info("IOException trying to connect to Cassandra using properties file", e);
+            }
+        } else {
+            LOGGER.info("Cassandra connection properties file {} did not exist", cassandraConnectionPropertiesFilename);
+        }
+        
+        LOGGER.info("Attempting Cassandra connection using host/port from solrconfig.xml");
+        cassandraHost = (String) args.get("cassandra-host");
+        port = (String) args.get("cassandra-cql-port");
+        if (StringUtils.isNotBlank(port)) {
+            cassandraCqlPort = Integer.valueOf(port);
+        }
+        try {
+            LOGGER.info("Attempting Cassandra connection using host={}, port={} from solrconfig.xml", 
+                    cassandraHost, cassandraCqlPort);
+            cassandraClient = new CassandraClient(cassandraHost, cassandraCqlPort);
+            return cassandraClient;
+        } catch (Exception e) {
+            LOGGER.info("Exception trying to get CassandraClient connection using args host={}, port={} from solrconfig.xml", 
+                    cassandraHost, cassandraCqlPort);
+        }
+        
+        return null;
+    }
+
+    @Override
+    public UpdateRequestProcessor getInstance(SolrQueryRequest solrQueryRequest, SolrQueryResponse solrQueryResponse,
+            UpdateRequestProcessor next) {
+        LOGGER.info("ENTERING: getInstance()");
+        return new CassandraUpdateRequestProcessor(solrCore.getName(), cassandraClient, next);
+    }
+
+}
