5108d63929cc12da829021d275bb7f966b96eb00
Hugh Rodgers <hugh.rodgers@lmco.com>
2014-07-14 09:24:37 -0700

    DDF-579  added persistence of ID for Notification; changed to Use ddf-libs Notification constants vs. PersistentNotification's

--- notifications/core/platform-core-notifications/src/main/java/org/codice/ddf/notifications/impl/NotificationListener.java
+++ notifications/core/platform-core-notifications/src/main/java/org/codice/ddf/notifications/impl/NotificationListener.java
@@ -15,7 +15,7 @@
 package org.codice.ddf.notifications.impl;
 
 import org.apache.commons.lang.StringUtils;
-import org.codice.ddf.notifications.store.PersistentNotification;
+import org.codice.ddf.notifications.Notification;
 import org.codice.ddf.persistence.PersistenceException;
 import org.codice.ddf.persistence.PersistentItem;
 import org.codice.ddf.persistence.PersistentStore;
@@ -39,29 +39,31 @@ public class NotificationListener implements EventHandler {
     public void handleEvent(Event event) throws IllegalArgumentException {
         LOGGER.debug("Received notification on topic {}", event.getTopic());
 
-        String application = (String) event.getProperty(PersistentNotification.NOTIFICATION_KEY_APPLICATION);
-        String message = (String) event.getProperty(PersistentNotification.NOTIFICATION_KEY_MESSAGE);
-        String timestamp = (String) event.getProperty(PersistentNotification.NOTIFICATION_KEY_TIMESTAMP);
-        String title = (String) event.getProperty(PersistentNotification.NOTIFICATION_KEY_TITLE);
-        String userId = (String) event.getProperty(PersistentNotification.NOTIFICATION_KEY_USER_ID);
+        String id = (String) event.getProperty(Notification.NOTIFICATION_KEY_ID);
+        String application = (String) event.getProperty(Notification.NOTIFICATION_KEY_APPLICATION);
+        String message = (String) event.getProperty(Notification.NOTIFICATION_KEY_MESSAGE);
+        String timestamp = (String) event.getProperty(Notification.NOTIFICATION_KEY_TIMESTAMP);
+        String title = (String) event.getProperty(Notification.NOTIFICATION_KEY_TITLE);
+        String userId = (String) event.getProperty(Notification.NOTIFICATION_KEY_USER_ID);
         if (StringUtils.isBlank(userId)) {
-            throw new IllegalArgumentException("Event \"" + PersistentNotification.NOTIFICATION_KEY_USER_ID
+            throw new IllegalArgumentException("Event \"" + Notification.NOTIFICATION_KEY_USER_ID
                     + "\" property is null or empty");
         }
         
         //TODO: Do we need to get extra properties out of event for Notification, i.e., STATUS and BYTES?
         PersistentItem item = new PersistentItem();
-        item.addProperty(PersistentNotification.NOTIFICATION_KEY_USER_ID, userId);
-        item.addProperty(PersistentNotification.NOTIFICATION_KEY_TIMESTAMP, timestamp);
-        item.addProperty(PersistentNotification.NOTIFICATION_KEY_APPLICATION, application);
-        item.addProperty(PersistentNotification.NOTIFICATION_KEY_TITLE,  title);
-        item.addProperty(PersistentNotification.NOTIFICATION_KEY_MESSAGE, message);
+        item.addProperty(Notification.NOTIFICATION_KEY_ID, id);
+        item.addProperty(Notification.NOTIFICATION_KEY_USER_ID, userId);
+        item.addProperty(Notification.NOTIFICATION_KEY_TIMESTAMP, timestamp);
+        item.addProperty(Notification.NOTIFICATION_KEY_APPLICATION, application);
+        item.addProperty(Notification.NOTIFICATION_KEY_TITLE,  title);
+        item.addProperty(Notification.NOTIFICATION_KEY_MESSAGE, message);
 //        Map<String, Object> item = new HashMap<String, Object>();
-//        item.put(PersistentNotification.NOTIFICATION_KEY_USER_ID, userId);
-//        item.put(PersistentNotification.NOTIFICATION_KEY_TIMESTAMP, timestamp);
-//        item.put(PersistentNotification.NOTIFICATION_KEY_APPLICATION, application);
-//        item.put(PersistentNotification.NOTIFICATION_KEY_TITLE,  title);
-//        item.put(PersistentNotification.NOTIFICATION_KEY_MESSAGE, message);
+//        item.put(Notification.NOTIFICATION_KEY_USER_ID, userId);
+//        item.put(Notification.NOTIFICATION_KEY_TIMESTAMP, timestamp);
+//        item.put(Notification.NOTIFICATION_KEY_APPLICATION, application);
+//        item.put(Notification.NOTIFICATION_KEY_TITLE,  title);
+//        item.put(Notification.NOTIFICATION_KEY_MESSAGE, message);
         try {
             persistentStore.add(PersistentStore.NOTIFICATION_TYPE, item);
         } catch (PersistenceException e) {
