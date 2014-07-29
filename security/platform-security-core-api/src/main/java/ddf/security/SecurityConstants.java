569ece9276fd177ca47865c9f42055a28fe536f4
Bruce Beyeler <bruce.beyeler@lmco.com>
2014-07-29 15:24:08 -0700

    DDF-551 Code cleanup from reviews.
    Used maven dependency management for slf4j.
    Updated platform app versions from merged code to match snapshot version.
    Logged null user in CAS authentication handler.
    Added SAML cookie constants to SecurityConstants - referenced them vs. local constants.
    LoginFilter uses common SecurityConstants, checks for specific types before casting.
    Removed transient from loggers.
    Updated LoginFilter test to work properly with refactored code.
    Updated anonymous handler test to check values in expected order.
    Removed commented out code in main and test branches.
    Removed dual licenses from various files.

--- security/platform-security-core-api/src/main/java/ddf/security/SecurityConstants.java
+++ security/platform-security-core-api/src/main/java/ddf/security/SecurityConstants.java
@@ -1,16 +1,16 @@
 /**
  * Copyright (c) Codice Foundation
- * 
+ *
  * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
  * is distributed along with this program and can be found at
  * <http://www.gnu.org/licenses/lgpl.html>.
- * 
+ *
  **/
 package ddf.security;
 
@@ -35,6 +35,16 @@ public final class SecurityConstants {
      * response.
      */
     public static final String SAML_ASSERTION = "saml.assertion";
+
+    /**
+     * Property key to obtain the saml cookie from an incoming HTTP request.
+     */
+    public static final String SAML_COOKIE_NAME = "org.codice.websso.saml.token";
+
+    /**
+     * Property key to obtain the saml cookie reference from an incoming HTTP request.
+     */
+    public static final String SAML_COOKIE_REF = "org.codice.websso.saml.ref";
 
     private SecurityConstants() {
 
