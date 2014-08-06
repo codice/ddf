2dab6205c7faed1b01af67240b7b2ee1f49c4414
Michael Nguyen <michael.1.nguyen@lmco.com>
2014-08-06 08:54:45 -0700

    Revert IntelliJ IDEA automatic import condensation
    Reverted IntelliJ IDEA's replacement of explicit imports with "java.io".

--- persistence/platform-persistence-core-impl/src/main/java/org/codice/ddf/persistence/internal/PersistentStoreImpl.java
+++ persistence/platform-persistence-core-impl/src/main/java/org/codice/ddf/persistence/internal/PersistentStoreImpl.java
@@ -38,7 +38,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PersistentStoreImpl implements PersistentStore {
