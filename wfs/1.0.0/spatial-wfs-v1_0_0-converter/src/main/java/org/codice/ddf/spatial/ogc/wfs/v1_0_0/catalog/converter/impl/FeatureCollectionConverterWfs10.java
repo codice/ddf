e602eea8bbe868b0b69e2fe6743c2eb208072e99
Damon Jones <damon.jones@lmco.com>
2014-08-08 11:56:22 -0700

    DDF-642: Code not shared anymore so moved code to WFS 1.0

--- wfs/1.0.0/spatial-wfs-v1_0_0-converter/src/main/java/org/codice/ddf/spatial/ogc/wfs/v1_0_0/catalog/converter/impl/FeatureCollectionConverterWfs10.java
+++ wfs/1.0.0/spatial-wfs-v1_0_0-converter/src/main/java/org/codice/ddf/spatial/ogc/wfs/v1_0_0/catalog/converter/impl/FeatureCollectionConverterWfs10.java
@@ -15,26 +15,159 @@
 
 package org.codice.ddf.spatial.ogc.wfs.v1_0_0.catalog.converter.impl;
 
-import javax.xml.XMLConstants;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
+import javax.xml.XMLConstants;
+import javax.xml.namespace.QName;
+
+import org.codice.ddf.spatial.ogc.catalog.common.converter.XmlNode;
+import org.codice.ddf.spatial.ogc.wfs.catalog.common.WfsConstants;
 import org.codice.ddf.spatial.ogc.wfs.catalog.common.WfsFeatureCollection;
-import org.codice.ddf.spatial.ogc.wfs.catalog.converter.impl.FeatureCollectionConverter;
+import org.codice.ddf.spatial.ogc.wfs.catalog.common.WfsQnameBuilder;
+import org.codice.ddf.spatial.ogc.wfs.catalog.converter.FeatureConverter;
 import org.codice.ddf.spatial.ogc.wfs.v1_0_0.catalog.common.Wfs10Constants;
 
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryCollection;
+import com.vividsolutions.jts.geom.GeometryFactory;
+
+import ddf.catalog.data.Metacard;
+import ddf.catalog.data.impl.MetacardImpl;
+
 /**
- * This class works in conjunction with XStream to convert a {@link WfsFeatureCollection} v2.0 to XML
- * according to the GML 2.1.2 spec. It will also convert respective XML into a
+ * This class works in conjunction with XStream to convert a {@link WfsFeatureCollection} to XML
+ * according to the GML 3.2.1 spec. It will also convert respective XML into a
  * {@link WfsFeatureCollection}.
  * 
  */
 
-public class FeatureCollectionConverterWfs10 extends FeatureCollectionConverter {
+public class FeatureCollectionConverterWfs10 implements Converter {
+
+    protected String featureMember = "";
+
+    private String contextRoot;
+
+    private Map<String, FeatureConverter> featureConverterMap = new HashMap<String, FeatureConverter>();
+    
+    protected Map<String, String> prefixToUriMapping = new HashMap<String, String>();
     
     public FeatureCollectionConverterWfs10() {
-        super();
         featureMember = "featureMember";
         prefixToUriMapping.put(Wfs10Constants.XSI_PREFIX, XMLConstants.W3C_XML_SCHEMA_INSTANCE_NS_URI);
         prefixToUriMapping.put(Wfs10Constants.WFS_NAMESPACE_PREFIX, Wfs10Constants.WFS_NAMESPACE);
         prefixToUriMapping.put(Wfs10Constants.GML_PREFIX, Wfs10Constants.GML_NAMESPACE);
+    }
+    
+    @Override
+    public boolean canConvert(Class clazz) {
+        return WfsFeatureCollection.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void marshal(Object value, HierarchicalStreamWriter writer, MarshallingContext context) {
+        WfsFeatureCollection wfc = (WfsFeatureCollection) value;
+
+        String schemaLoc = generateSchemaLocationFromMetacards(wfc.getFeatureMembers(),
+                prefixToUriMapping);
+
+        for (Entry<String, String> entry : prefixToUriMapping.entrySet()) {
+            writer.addAttribute(XMLConstants.XMLNS_ATTRIBUTE + ":" + entry.getKey(), entry.getValue());
+        }
+        writer.addAttribute(WfsConstants.ATTRIBUTE_SCHEMA_LOCATION, schemaLoc);
+
+        Geometry allGeometry = getBounds(wfc.getFeatureMembers());
+        if (!allGeometry.isEmpty()) {
+            XmlNode.writeEnvelope(WfsConstants.GML_PREFIX + ":" + "boundedBy", context, writer,
+                    allGeometry.getEnvelopeInternal());
+        }
+
+        for (Metacard mc : wfc.getFeatureMembers()) {
+            writer.startNode(WfsConstants.GML_PREFIX + ":"
+                    + featureMember);
+            context.convertAnother(mc);
+            writer.endNode();
+        }
+    }
+
+    public void setContextRoot(String contextRoot) {
+        if (null != contextRoot) {
+            this.contextRoot = contextRoot;
+        }
+    }
+
+    private String generateSchemaLocationFromMetacards(List<Metacard> metacards,
+            Map<String, String> prefixToUriMapping) {
+
+        StringBuilder descFeatureService = new StringBuilder();
+        descFeatureService.append(contextRoot).append(
+                "/wfs?service=wfs&request=DescribeFeatureType&version=1.0.0&typeName=");
+
+        StringBuilder schemaLocation = new StringBuilder();
+        Set<QName> qnames = new HashSet<QName>();
+        for (Metacard metacard : metacards) {
+            qnames.add(WfsQnameBuilder.buildQName(metacard.getMetacardType().getName(),
+                    metacard.getContentTypeName()));
+        }
+        for (QName qname : qnames) {
+            prefixToUriMapping.put(qname.getPrefix(), qname.getNamespaceURI());
+            schemaLocation.append(qname.getNamespaceURI()).append(" ")
+                    .append(descFeatureService).append(qname.getPrefix())
+                    .append(":").append(qname.getLocalPart())
+                    .append(" ");
+
+        }
+        return schemaLocation.toString();
+    }
+
+    private Geometry getBounds(List<Metacard> metacards) {
+        List<Geometry> geometries = new ArrayList<Geometry>();
+        for (Metacard card : metacards) {
+            if (null != card.getLocation()) {
+                Geometry geo = XmlNode.readGeometry(card.getLocation());
+                if (null != geo) {
+                    geometries.add(geo);
+                }
+            }
+        }
+
+        Geometry allGeometry = new GeometryCollection(geometries.toArray(new Geometry[0]),
+                new GeometryFactory());
+        return allGeometry;
+    }
+
+    @Override
+    public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+        WfsFeatureCollection featureCollection = new WfsFeatureCollection();
+        while (reader.hasMoreChildren()) {
+            reader.moveDown();
+            String nodeName = reader.getNodeName();
+            // Its important to note that the reader appears to drop the
+            // namespace.
+            if (featureMember.equals(nodeName)) {
+                reader.moveDown();
+                // lookup the converter for this featuretype
+                featureCollection.getFeatureMembers().add(
+                        (Metacard) context.convertAnother(null, MetacardImpl.class,
+                                featureConverterMap.get(reader.getNodeName())));
+                reader.moveUp();
+            }
+            reader.moveUp();
+        }
+        return featureCollection;
+    }
+
+    public void setFeatureConverterMap(Map<String, FeatureConverter> featureConverterMap) {
+        this.featureConverterMap = featureConverterMap;
     }
 }
--- wfs/1.0.0/spatial-wfs-v1_0_0-source/src/main/java/org/codice/ddf/spatial/ogc/wfs/v1_0_0/catalog/source/reader/FeatureCollectionMessageBodyReaderWfs10.java
+++ wfs/1.0.0/spatial-wfs-v1_0_0-source/src/main/java/org/codice/ddf/spatial/ogc/wfs/v1_0_0/catalog/source/reader/FeatureCollectionMessageBodyReaderWfs10.java
@@ -14,21 +14,112 @@
  **/
 package org.codice.ddf.spatial.ogc.wfs.v1_0_0.catalog.source.reader;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.Provider;
 
-import org.codice.ddf.spatial.ogc.wfs.catalog.source.reader.FeatureCollectionMessageBodyReader;
+import org.apache.commons.io.IOUtils;
+import org.codice.ddf.spatial.ogc.wfs.catalog.common.WfsFeatureCollection;
+import org.codice.ddf.spatial.ogc.wfs.catalog.converter.FeatureConverter;
+import org.codice.ddf.spatial.ogc.wfs.catalog.converter.impl.GmlEnvelopeConverter;
+import org.codice.ddf.spatial.ogc.wfs.catalog.converter.impl.GmlGeometryConverter;
 import org.codice.ddf.spatial.ogc.wfs.v1_0_0.catalog.converter.impl.FeatureCollectionConverterWfs10;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Consumes({MediaType.TEXT_XML, MediaType.APPLICATION_XML})
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.XStreamException;
+import com.thoughtworks.xstream.io.xml.WstxDriver;
+
+import ddf.catalog.data.Metacard;
+
+@Consumes({ MediaType.TEXT_XML, MediaType.APPLICATION_XML })
 @Provider
-public class FeatureCollectionMessageBodyReaderWfs10 extends FeatureCollectionMessageBodyReader{
-	
-	public FeatureCollectionMessageBodyReaderWfs10(){
-		super();
-		featureCollectionConverter = new FeatureCollectionConverterWfs10();
+public class FeatureCollectionMessageBodyReaderWfs10 implements MessageBodyReader<WfsFeatureCollection> {
+
+    protected XStream xstream;
+
+    protected FeatureCollectionConverterWfs10 featureCollectionConverter;
+
+    protected Map<String, FeatureConverter> featureConverterMap = new HashMap<String, FeatureConverter>();
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FeatureCollectionMessageBodyReaderWfs10.class);
+
+    public FeatureCollectionMessageBodyReaderWfs10() {
+        xstream = new XStream(new WstxDriver());
+        xstream.setClassLoader(this.getClass().getClassLoader());
+        xstream.registerConverter(new GmlGeometryConverter());
+        xstream.registerConverter(new GmlEnvelopeConverter());
+        xstream.alias("FeatureCollection", WfsFeatureCollection.class);
+        
+        featureCollectionConverter = new FeatureCollectionConverterWfs10();
         featureCollectionConverter.setFeatureConverterMap(featureConverterMap);
         xstream.registerConverter(featureCollectionConverter);
-	}
+    }
+
+    @Override
+    public boolean isReadable(Class<?> clazz, Type type, Annotation[] annotations,
+            MediaType mediaType) {
+        return WfsFeatureCollection.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public WfsFeatureCollection readFrom(Class<WfsFeatureCollection> clazz, Type type,
+            Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> headers,
+            InputStream inStream) throws IOException, WebApplicationException {
+
+        // Save original input stream for any exception message that might need to be
+        // created
+        String originalInputStream = IOUtils.toString(inStream, "UTF-8");
+        
+        // Re-create the input stream (since it has already been read for potential
+        // exception message creation)
+        inStream = new ByteArrayInputStream(originalInputStream.getBytes("UTF-8"));
+
+        WfsFeatureCollection featureCollection = null;
+
+        try {
+            featureCollection = (WfsFeatureCollection) xstream.fromXML(inStream);
+        } catch (XStreamException e) {
+            // If a ServiceExceptionReport is sent from the remote WFS site it will be sent with an
+            // JAX-RS "OK" status, hence the ErrorResponse exception mapper will not fire.
+            // Instead the ServiceExceptionReport will come here and be treated like a GetFeature
+            // response, resulting in an XStreamException since ExceptionReport cannot be
+            // unmarshalled. So this catch clause is responsible for catching that XStream
+            // exception and creating a JAX-RS response containing the original stream
+            // (with the ExceptionReport) and rethrowing it as a WebApplicationException,
+            // which CXF will wrap as a ClientException that the WfsSource catches, converts
+            // to a WfsException, and logs.
+            LOGGER.error("Exception unmarshalling {}", e);
+            ByteArrayInputStream bis = new ByteArrayInputStream(originalInputStream.getBytes());
+            ResponseBuilder responseBuilder = Response.ok(bis);
+            responseBuilder.type("text/xml");
+            Response response = responseBuilder.build();
+            throw new WebApplicationException(e, response);
+        } finally {
+            IOUtils.closeQuietly(inStream);
+        }
+
+        return featureCollection;
+    }
+
+    public void registerConverter(FeatureConverter converter) {
+        featureConverterMap.put(converter.getMetacardType().getName(), converter);
+        xstream.registerConverter(converter);
+        xstream.alias(converter.getMetacardType().getName(), Metacard.class);
+    }
+
 }
