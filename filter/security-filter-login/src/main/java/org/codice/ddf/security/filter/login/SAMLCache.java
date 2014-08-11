afb681f0efb330ca6c2f82f6f6ab958ec28f2e3d
Matt Ramey <matthew.ramey@lmco.com>
2014-08-11 11:38:42 -0700

    DDF-651: Added cache expiration and tests.

--- filter/security-filter-login/src/main/java/org/codice/ddf/security/filter/login/SAMLCache.java
+++ filter/security-filter-login/src/main/java/org/codice/ddf/security/filter/login/SAMLCache.java
@@ -14,20 +14,30 @@
  **/
 package org.codice.ddf.security.filter.login;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
 import org.apache.cxf.ws.security.tokenstore.SecurityToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class SAMLCache {
     private static final Logger LOGGER = LoggerFactory.getLogger(SAMLCache.class);
 
-    private ConcurrentHashMap<String, SecurityToken> cache = new ConcurrentHashMap<String, SecurityToken>();
+    private static final int DEFAULT_EXPIRATION_MINUTES = 31;
+
+    private int currentExpiration = DEFAULT_EXPIRATION_MINUTES;
+
+    private Cache<String, SecurityToken> cache = CacheBuilder.newBuilder()
+            .expireAfterWrite(DEFAULT_EXPIRATION_MINUTES,
+                    TimeUnit.MINUTES).removalListener(new RemovalListenerLogger()).build();
 
     public void clearCache() {
-        cache.clear();
+        cache.invalidateAll();
     }
 
     /**
@@ -38,9 +48,8 @@ public class SAMLCache {
      * @return the key for this element in the cache
      */
     public String put(String realm, SecurityToken token) {
-        UUID uuid = UUID.randomUUID();
-        String id = uuid.toString();
-        LOGGER.trace("Adding security token to cache for realm {} and id {}", realm , id);
+        String id = UUID.randomUUID().toString();
+        LOGGER.trace("Adding security token to cache for realm {} and id {}", realm, id);
         cache.put(realm + id, token);
         return id;
     }
@@ -49,14 +58,51 @@ public class SAMLCache {
      * Retrieves the SecurityToken associated with the provided realm and unique identifier.
      *
      * @param realm the realm that this request is associated with
-     * @param id the reference id for a SAML assertion
+     * @param id    the reference id for a SAML assertion
      * @return the SecurityToken object associated with the reference, or null
      */
     public SecurityToken get(String realm, String id) {
-        SecurityToken token = cache.get(realm + id);
+        SecurityToken token = cache.getIfPresent(realm + id);
         LOGGER.trace("Retrieving security token with realm {} and id {} - {}.",
-            realm, id, token == null ? "not found" : "found");
+                realm, id, token == null ? "not found" : "found");
         return token;
+    }
+
+    /**
+     * Set the expiration time for the cache. <br/><br/>
+     * Note: This will also reset the expiration times of any items currently in the cache.
+     *
+     * @param expirationMinutes Value (in minutes) to set the cache expiration to.
+     */
+    public synchronized void setExpirationTime(int expirationMinutes) {
+        if (expirationMinutes != currentExpiration) {
+            LOGGER.debug(
+                    "New expiration value passed in. Changing cache to expire every {} minutes instead of every {}.",
+                    expirationMinutes, currentExpiration);
+            Cache<String, SecurityToken> tmpCache = CacheBuilder.newBuilder()
+                    .expireAfterWrite(expirationMinutes, TimeUnit.MINUTES)
+                    .removalListener(new RemovalListenerLogger()).build();
+            LOGGER.debug("Adding old cache items to cache with updated expiration.");
+            tmpCache.putAll(cache.asMap());
+            cache = tmpCache;
+            currentExpiration = expirationMinutes;
+        } else {
+            LOGGER.debug("Incoming time of {} matches current expiration time. Not updating cache.",
+                    expirationMinutes);
+        }
+    }
+
+    /**
+     * Listens for removal notifications from the cache and logs each time a removal is performed.
+     */
+    private class RemovalListenerLogger implements RemovalListener<String, SecurityToken> {
+
+        @Override
+        public void onRemoval(RemovalNotification<String, SecurityToken> notification) {
+            LOGGER.debug("Expiring SAML ref:assertion {}:{} due to {}.",
+                    notification.getKey(),
+                    notification.getValue(), notification.getCause().toString());
+        }
     }
 
 }
