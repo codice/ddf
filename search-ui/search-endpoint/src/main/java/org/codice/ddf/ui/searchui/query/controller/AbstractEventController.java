b93addfda25dba88b94e97bf6b3f9950ec58eece
Shaun Morris <shaun.d.morris@lmco.com>
2014-08-18 14:48:11 -0700

    DDF-656: removed commented out code

--- search-ui/search-endpoint/src/main/java/org/codice/ddf/ui/searchui/query/controller/AbstractEventController.java
+++ search-ui/search-endpoint/src/main/java/org/codice/ddf/ui/searchui/query/controller/AbstractEventController.java
@@ -14,21 +14,8 @@
  **/
 package org.codice.ddf.ui.searchui.query.controller;
 
-import java.security.Principal;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
+import ddf.security.assertion.SecurityAssertion;
 import net.minidev.json.JSONObject;
-
-import org.apache.commons.lang.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
@@ -51,7 +38,17 @@ import org.osgi.service.event.EventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ddf.security.assertion.SecurityAssertion;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * The {@code AbstractEventController} handles the processing and routing of events.
@@ -122,10 +119,9 @@ public abstract class AbstractEventController implements EventHandler {
     /**
      * Obtains the {@link ServerSession} associated with a given user id.
      *
-     * @param userId
-     *            The id of the user associated with the {@code ServerSession} to be retrieved.
+     * @param userId The id of the user associated with the {@code ServerSession} to be retrieved.
      * @return The {@code ServerSession} associated with the received userId or null if the user
-     *         does not have an established {@code ServerSession}
+     * does not have an established {@code ServerSession}
      */
     public ServerSession getSessionByUserId(String userId) {
         return userSessionMap.get(userId);
@@ -136,11 +132,9 @@ public abstract class AbstractEventController implements EventHandler {
      * and deregisters the user. This should be invoked in order to remove
      * {@code AbstractEventController} references to invalid {@link ServerSession}s.
      *
-     * @param serverSession
-     *            The {@code ServerSession} object associated with the client that is disconnecting
-     * @param serverMessage
-     *            The {@link ServerMessage} that was sent from the client on the /meta/disconnect
-     *            Channel
+     * @param serverSession The {@code ServerSession} object associated with the client that is disconnecting
+     * @param serverMessage The {@link ServerMessage} that was sent from the client on the /meta/disconnect
+     *                      Channel
      */
     @Listener("/meta/disconnect")
     public void deregisterUserSession(ServerSession serverSession, ServerMessage serverMessage) {
@@ -170,52 +164,6 @@ public abstract class AbstractEventController implements EventHandler {
         }
     }
 
-//    @Listener("/service/action")
-//    public void actionSession(final ServerSession serverSession, ServerMessage serverMessage) {
-//        LOGGER.debug("\nServerSession: {}\nServerMessage: {}", serverSession, serverMessage);
-//
-//        if (null == serverSession) {
-//            throw new IllegalArgumentException("ServerSession is null");
-//        }
-//
-//        if (null == serverMessage) {
-//            throw new IllegalArgumentException("ServerMessage is null");
-//        }
-//
-//        Map<String, Object> actionMessage = serverMessage.getDataAsMap();
-//        String actionName = (String) actionMessage.get("action");
-//        String downloadIdentifier = (String) actionMessage.get("id");
-//        LOGGER.debug("\nAction: {}", actionName);
-//
-//        if (StringUtils.equalsIgnoreCase(actionName, "cancel")) {
-//            Subject subject = null;
-//            try {
-//                subject = SecurityUtils.getSubject();
-//            } catch (Exception e) {
-//                LOGGER.debug("Couldn't grab user subject from Shiro.", e);
-//            }
-//
-//            String userId = getUserId(serverSession, subject);
-//
-//            if (null == userId) {
-//                throw new IllegalArgumentException("User ID is null");
-//            }
-//            if (null == downloadIdentifier) {
-//                throw new IllegalArgumentException("Metadata ID is null");
-//            }
-//
-//            String downloadId = userId + downloadIdentifier;
-//
-//            JSONObject jsonPropMap = new JSONObject();
-//            jsonPropMap.put(ActivityEvent.DOWNLOAD_ID_KEY, downloadId);
-//
-//            Event event = new Event(ActivityEvent.EVENT_TOPIC_DOWNLOAD_CANCEL, jsonPropMap);
-//            eventAdmin.postEvent(event);
-//
-//        }
-//
-//    }
-
     /**
      * Called by {@link  ddf.catalog.event.retrievestatus.DownloadStatusInfoImpl.cancelDownload} to fire a
      * cancel event.
@@ -240,17 +188,14 @@ public abstract class AbstractEventController implements EventHandler {
      * >Obtaining user and session information for private message delivery</a> for more
      * information.
      *
-     * @param serverSession
-     *            The {@link ServerSession} on which to deliver messages to the user for the user.
-     * @param serverMessage
-     *            The {@link ServerMessage} containing the userId property with which to associate
-     *            the {@code ServerSession}.
-     * @throws IllegalArgumentException
-     *             when the received {@code ServerSession} or the {@code ServerSession}'s id is
-     *             null.
+     * @param serverSession The {@link ServerSession} on which to deliver messages to the user for the user.
+     * @param serverMessage The {@link ServerMessage} containing the userId property with which to associate
+     *                      the {@code ServerSession}.
+     * @throws IllegalArgumentException when the received {@code ServerSession} or the {@code ServerSession}'s id is
+     *                                  null.
      */
     public void registerUserSession(final ServerSession serverSession, ServerMessage serverMessage)
-        throws IllegalArgumentException {
+            throws IllegalArgumentException {
 
         LOGGER.debug("ServerSession: {}\nServerMessage: {}", serverSession, serverMessage);
 
