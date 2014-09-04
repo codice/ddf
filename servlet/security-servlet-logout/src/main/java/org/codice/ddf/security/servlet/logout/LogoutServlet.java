d0a50e3cb7cace3254efc63c522d8a9289c1b5c9
Scott Tustison <scott.a.tustison@lmco.com>
2014-09-04 09:09:12 -0700

    DDF-712 added log out servlet, updated search ui log out button to hit the log out servlet

---
+++ servlet/security-servlet-logout/src/main/java/org/codice/ddf/security/servlet/logout/LogoutServlet.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ **/
+package org.codice.ddf.security.servlet.logout;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setHeader("Cache-Control", "no-cache, no-store");
+        response.setHeader("Pragma", "no-cache");
+        response.setContentType("text/html");
+
+        request.getSession().invalidate();
+
+        //This is just here to avoid a blank screen
+        //A user would most likely never see this as they will be redirected to some other
+        //login page by a filter or just returned back to the same screen they were already viewing
+        response.getWriter().print("You have successfully logged out. Please close your browser or tab.");
+
+        response.getWriter().close();
+    }
+}
