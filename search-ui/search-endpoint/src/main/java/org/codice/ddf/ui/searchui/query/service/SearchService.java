4adc694609bea7143284dfb7fe9f9d2245b6e4a6
Phillip Klinefelter <phillip.klinefelter@lmco.com>
2014-09-25 16:12:37 -0700

    DDF-772 Changed CometD endpoint to use CQL filter

--- search-ui/search-endpoint/src/main/java/org/codice/ddf/ui/searchui/query/service/SearchService.java
+++ search-ui/search-endpoint/src/main/java/org/codice/ddf/ui/searchui/query/service/SearchService.java
@@ -41,10 +41,8 @@ import org.cometd.bayeux.server.ConfigurableServerChannel;
 import org.cometd.bayeux.server.ServerMessage;
 import org.cometd.bayeux.server.ServerSession;
 import org.cometd.server.ServerMessageImpl;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.DateTimeFormatterBuilder;
-import org.joda.time.format.DateTimeParser;
-import org.joda.time.format.ISODateTimeFormat;
+import org.geotools.filter.text.cql2.CQLException;
+import org.geotools.filter.text.ecql.ECQL;
 import org.opengis.filter.Filter;
 import org.opengis.filter.sort.SortBy;
 import org.opengis.filter.sort.SortOrder;
@@ -52,11 +50,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -70,8 +65,6 @@ public class SearchService {
 
     private static final String GUID = "guid";
 
-    private static final String PHRASE = "q";
-
     private static final String MAX_RESULTS = "mr";
 
     private static final String SOURCES = "src";
@@ -82,35 +75,9 @@ public class SearchService {
 
     private static final String COUNT = "count";
 
-    private static final String BBOX = "bbox";
-
-    private static final String POLYGON = "polygon";
-
-    private static final String GEOMETRY = "geometry";
-
-    private static final String LAT = "lat";
-
-    private static final String LON = "lon";
-
-    private static final String RADIUS = "radius";
-
-    private static final String DATE_START = "dtstart";
-
-    private static final String DATE_END = "dtend";
-
-    private static final String DATE_OFFSET = "dtoffset";
-
-    private static final String TIME_TYPE = "timeType";
-
-    private static final String TYPE = "type";
-
-    private static final String VERSION = "version";
-
-    private static final String SELECTOR = "selector";
+    private static final String CQL_FILTER = "cql";
 
     private static final String SORT = "sort";
-
-    private static final String FORMAT = "format";
 
     private static final String DEFAULT_SORT_ORDER = "desc";
 
@@ -121,16 +88,6 @@ public class SearchService {
     private static final int DEFAULT_START_INDEX = 1;
 
     public static final String LOCAL_SOURCE = "local";
-
-    private static DateTimeFormatter dateFormatter;
-
-    static {
-        DateTimeParser[] parsers = {ISODateTimeFormat.dateTime().getParser(),
-                ISODateTimeFormat.dateTimeNoMillis().getParser(),
-                ISODateTimeFormat.basicDateTime().getParser(),
-                ISODateTimeFormat.basicDateTimeNoMillis().getParser()};
-        dateFormatter = new DateTimeFormatterBuilder().append(null, parsers).toFormatter();
-    }
 
     private final FilterBuilder filterBuilder;
 
@@ -225,31 +182,14 @@ public class SearchService {
      *            - JSON message received from cometd
      */
     public void executeQuery(Map<String, Object> queryMessage, Subject subject) {
-        final String methodName = "executeQuery";
-        LOGGER.debug("ENTERING {}", methodName);
-
-        String searchTerms = castObject(String.class, queryMessage.get(PHRASE));
         Long maxResults = castObject(Long.class, queryMessage.get(MAX_RESULTS));
         String sources = castObject(String.class, queryMessage.get(SOURCES));
         Long maxTimeout = castObject(Long.class, queryMessage.get(MAX_TIMEOUT));
         Long startIndex = castObject(Long.class, queryMessage.get(START_INDEX));
         Long count = castObject(Long.class, queryMessage.get(COUNT));
-        String geometry = castObject(String.class, queryMessage.get(GEOMETRY));
-        String bbox = castObject(String.class, queryMessage.get(BBOX));
-        String polygon = castObject(String.class, queryMessage.get(POLYGON));
-        String lat = castObject(String.class, queryMessage.get(LAT));
-        String lon = castObject(String.class, queryMessage.get(LON));
-        Double radius = castObject(Double.class, queryMessage.get(RADIUS));
-        String dateStart = castObject(String.class, queryMessage.get(DATE_START));
-        String dateEnd = castObject(String.class, queryMessage.get(DATE_END));
-        Long dateOffset = castObject(Long.class, queryMessage.get(DATE_OFFSET));
+        String cql = castObject(String.class, queryMessage.get(CQL_FILTER));
         String sort = castObject(String.class, queryMessage.get(SORT));
-        String format = castObject(String.class, queryMessage.get(FORMAT));
-        String selector = castObject(String.class, queryMessage.get(SELECTOR));
-        String type = castObject(String.class, queryMessage.get(TYPE));
-        String versions = castObject(String.class, queryMessage.get(VERSION));
         String guid = castObject(String.class, queryMessage.get(GUID));
-        String timeType = castObject(String.class, queryMessage.get(TIME_TYPE));
 
         Long localCount = count;
 
@@ -262,14 +202,16 @@ public class SearchService {
         }
 
         Set<String> sourceIds = getSourceIds(sources);
-        List<Filter> filters = new ArrayList<Filter>();
 
-        addContextualFilter(filters, searchTerms);
-        addTemporalFilter(filters, dateStart, dateEnd, dateOffset, timeType);
-        addSpatialFilter(filters, bbox, radius, lat, lon);
-        addTypeFilters(filters, type);
+        Filter filter = null;
+        try {
+            filter = ECQL.toFilter(cql);
+        } catch (CQLException e) {
+            LOGGER.warn("Unable to parse CQL filter", e);
+            return;
+        }
 
-        Query query = createQuery(andFilters(filters), startIndex, localCount, sort, maxTimeout);
+        Query query = createQuery(filter, startIndex, localCount, sort, maxTimeout);
         SearchRequest searchRequest = new SearchRequest(sourceIds, query, guid);
 
         try {
@@ -278,8 +220,6 @@ public class SearchService {
         } catch (RuntimeException re) {
             LOGGER.warn("Exception while executing a query", re);
         }
-
-        LOGGER.debug("EXITING {}", methodName);
 
     }
 
@@ -318,140 +258,6 @@ public class SearchService {
             }
         }
         return sourceIds;
-    }
-
-    private Filter andFilters(List<Filter> filters) {
-        if (filters.size() > 1) {
-            return filterBuilder.allOf(filters);
-        } else if (filters.size() == 1) {
-            return filters.get(0);
-        } else {
-            LOGGER.warn("Unable to create filter for given input");
-            return null;
-        }
-    }
-
-    /**
-     * Create temporal filter
-     *
-     * @param filters
-     * @param dateStart
-     * @param dateEnd
-     * @param dateOffset
-     */
-    private void addTemporalFilter(List<Filter> filters, String dateStart, String dateEnd,
-            Long dateOffset, String timeType) {
-        if ((Metacard.MODIFIED).equals(timeType) || (Metacard.EFFECTIVE).equals(timeType)) {
-            if (StringUtils.isNotBlank(dateStart) && StringUtils.isNotBlank(dateEnd)) {
-                filters.add(filterBuilder.attribute(timeType).is().during().dates(
-                        parseDate(dateStart), parseDate(dateEnd)));
-            } else if (StringUtils.isNotBlank(dateStart)) {
-                filters.add(filterBuilder.attribute(timeType).is().after().date(parseDate(dateStart)));
-            } else if (StringUtils.isNotBlank(dateEnd)) {
-                filters.add(filterBuilder.attribute(timeType).is().before().date(parseDate(dateEnd)));
-            } else if (dateOffset != null) {
-                filters.add(filterBuilder.attribute(timeType).is().during().last(dateOffset));
-            }
-        }
-        else {
-            LOGGER.warn("Invalid timeType passed. Expect {} {} instead got {}", Metacard.MODIFIED, Metacard.EFFECTIVE, timeType);
-        }
-        
-    }
-
-    public static Date parseDate(String date) {
-        Date parsedDate = null;
-        if (StringUtils.isNotBlank(date)) {
-            try {
-                parsedDate = dateFormatter.parseDateTime(date).toDate();
-            } catch (IllegalArgumentException iae) {
-                LOGGER.warn("Could not parse given date: {}", date);
-            }
-        }
-        return parsedDate;
-    }
-
-    /**
-     * Create contextual filter
-     *
-     * @param filters
-     * @param searchTerms
-     */
-    private void addContextualFilter(List<Filter> filters, String searchTerms) {
-        if (StringUtils.isNotBlank(searchTerms)) {
-            filters.add(filterBuilder.attribute(Metacard.ANY_TEXT).is().like().text(searchTerms));
-        }
-    }
-
-    /**
-     * Creates SpatialCriterion based on the input parameters, any null values will be ignored
-     *
-     * @param filters
-     * @param bbox
-     *            - the bounding box to search over
-     * @param radius
-     *            - the radius for a point radius search
-     * @param lat
-     *            - the latitude of the point.
-     * @param lon
-     *            - the longitude of the point.
-     * @return - the spatialCriterion created, can be null
-     */
-    private void addSpatialFilter(List<Filter> filters, String bbox, Double radius, String lat,
-            String lon) {
-        if (StringUtils.isNotBlank(bbox)) {
-            String wkt = getBboxWkt(bbox);
-            if (wkt != null) {
-                filters.add(filterBuilder.attribute(Metacard.ANY_GEO).intersecting().wkt(wkt));
-            }
-        } else if (StringUtils.isNotBlank(lat) && StringUtils.isNotBlank(lon) && StringUtils
-                .isNotBlank(radius.toString())) {
-            String wkt = getPointWkt(lat, lon);
-            filters.add(filterBuilder.attribute(Metacard.ANY_GEO).withinBuffer().wkt(wkt, radius));
-        }
-    }
-
-    private String getPointWkt(String lat, String lon) {
-        return "POINT(" + lon + " " + lat + ")";
-    }
-
-    private String getBboxWkt(String bbox) {
-        String wkt = null;
-        String[] bboxParts = bbox.split(",");
-
-        if (bboxParts.length == 4) {
-            double minX = Double.parseDouble(bboxParts[0]);
-            double minY = Double.parseDouble(bboxParts[1]);
-            double maxX = Double.parseDouble(bboxParts[2]);
-            double maxY = Double.parseDouble(bboxParts[3]);
-
-            wkt = "POLYGON((" +
-                    minX + " " + minY +
-                    "," + minX + " " + maxY +
-                    "," + maxX + " " + maxY +
-                    "," + maxX + " " + minY +
-                    "," + minX + " " + minY +
-                    "))";
-        }
-
-        return wkt;
-    }
-
-    private void addTypeFilters(List<Filter> queryFilters, String types) {
-        if (StringUtils.isNotBlank(types)) {
-            LOGGER.debug("Received Types: {}", types);
-            
-            List<Filter> contentTypeFilters = new ArrayList<Filter>();
-            
-            for (String type : StringUtils.stripAll(types.split(","))) {
-                contentTypeFilters.add(filterBuilder.attribute(
-                        Metacard.CONTENT_TYPE).is().text(type));
-            }   
-            
-            queryFilters.add(filterBuilder.anyOf(contentTypeFilters));
-        } else {
-            LOGGER.debug("Received empty content types list");
-        }
     }
 
     /**
--- search-ui/standard/src/main/webapp/js/model/Query.js
+++ search-ui/standard/src/main/webapp/js/model/Query.js
@@ -23,6 +23,8 @@ define([
         "use strict";
         var Query = {};
 
+        var CQL_DATE_FORMAT = 'YYYY-MM-DD[T]HH:mm:ss[Z]';
+
         Query.Model = Backbone.AssociatedModel.extend({
             relations: [
                 {
@@ -45,7 +47,7 @@ define([
                 radius: 0,
                 radiusValue: 0,
                 count: properties.resultCount,
-                startIndex: 1,
+                start: 1,
                 format: "geojson"
             },
 
@@ -54,16 +56,116 @@ define([
                 this.listenTo(this, 'change:north change:south change:east change:west',this.setBBox);
                 this.listenTo(this, 'change:scheduled change:scheduleValue change:scheduleUnits', this.startScheduledSearch);
 
-                if(this.get('scheduled')) {
+                if (this.get('scheduled')) {
                     this.startSearch();
                 }
 
                 this.startScheduledSearch();
             },
 
+            getCql: function () {
+                var filters = [];
+
+                // contextual
+                var q = this.get('q');
+                if (q) {
+                    filters.push('anyText ILIKE ' + this.getValue(q));
+                }
+
+                // temporal
+                var start = this.get('dtstart'),
+                    end = this.get('dtend'),
+                    offset = this.get('dtoffset'),
+                    timeType = this.get('timeType');
+                if (start && end) {
+                    filters.push(timeType + ' DURING ' + this.getValue(new Date(start)) + '/' +
+                        this.getValue(new Date(end)));
+                } else if (start) {
+                    filters.push(timeType + ' AFTER ' + this.getValue(new Date(start)));
+                } else if (end) {
+                    filters.push(timeType + ' BEFORE ' + this.getValue(new Date(end)));
+                } else if (offset) {
+                    filters.push(timeType + ' AFTER ' +
+                        moment().subtract(offset, 'milliseconds').format(CQL_DATE_FORMAT));
+                }
+
+                // spatial
+                var north = this.get('north'),
+                    south = this.get('south'),
+                    west = this.get('west'),
+                    east = this.get('east'),
+                    lat = this.get('lat'),
+                    lon = this.get('lon'),
+                    radius = this.get('radius');
+                if (north && south && east && west) {
+                    var bbox = 'POLYGON ((' +
+                        west + ' ' + south +
+                        ', ' + west + ' ' + north +
+                        ', ' + east + ' ' + north +
+                        ', ' + east + ' ' + south +
+                        ', ' + west + ' ' + south +
+                        '))';
+                    filters.push('INTERSECTS(anyGeo, ' + bbox + ')');
+                } else if (lat && lon && radius) {
+                    var point = 'POINT(' + lon + ' ' + lat + ')';
+                    filters.push('DWITHIN(anyGeo, ' + point + ', ' + radius + ', meters)');
+                }
+
+                // type
+                var types = this.get('type');
+                if (types) {
+                    var typeFilters = [];
+                    _.each(types.split(','), function (type) {
+                        typeFilters.push('"metadata-content-type" = ' + this.getValue(type));
+                    }, this);
+
+                    filters.push(this.logicalCql(typeFilters, 'OR'));
+                }
+
+                return this.logicalCql(filters, 'AND');
+            },
+
+            logicalCql: function (filters, operator) {
+                var cql = '';
+                if (filters.length === 1) {
+                    cql = filters[0];
+                } else if (filters.length > 1) {
+                    cql = _.map(filters, function (filter) {
+                        return '(' + filter + ')';
+                    }).join(' ' + operator + ' ');
+                }
+                return cql;
+            },
+
+            getValue: function(value) {
+                switch (typeof value) {
+                    case 'string':
+                        return "'" + value.replace(/'/g, "''") + "'";
+                    case 'number':
+                        return String(value);
+                    case 'object':
+                        if (_.isDate(value)) {
+                            return moment(value).format(CQL_DATE_FORMAT);
+                        } else {
+                            throw new Error("Can't write object to CQL: " + value);
+                        }
+                        break;
+                    default:
+                        throw new Error("Can't write value to CQL: " + value);
+                }
+            },
+
+            toJSON: function() {
+                var json = _.clone(this.attributes);
+
+                json.cql = this.getCql();
+
+                return json;
+            },
+
             startScheduledSearch: function() {
                 var model = this;
-                if(this.get('scheduled')) {
+                if (this.get('scheduled')) {
                     var scheduleDelay = this.getScheduleDelay();
                     this.stopScheduledSearch();
                     this.timeoutId = setInterval(function () {
@@ -75,14 +177,14 @@ define([
             },
 
             stopScheduledSearch: function() {
-                if(this.timeoutId) {
+                if (this.timeoutId) {
                     clearInterval(this.timeoutId);
                 }
             },
 
             getScheduleDelay: function() {
                 var val;
-                switch(this.get('scheduleUnits')) {
+                switch (this.get('scheduleUnits')) {
                     case 'minutes':
                         val = (this.get('scheduleValue') || 5) * 60 * 1000;
                         break;
@@ -94,7 +196,7 @@ define([
             },
 
             clearSearch: function() {
-                if(this.get('result')) {
+                if (this.get('result')) {
                     this.get('result').cleanup();
                 }
                 this.set({result: undefined});
@@ -102,7 +204,7 @@ define([
 
             startSearch:function(progressFunction) {
                 var result;
-                if(this.get('result')) {
+                if (this.get('result')) {
                     result = this.get('result');
                 } else {
                     result = new Metacard.SearchResult();
@@ -122,7 +224,7 @@ define([
 
                 return result.fetch({
                     progress: progress,
-                    data: this.getQueryParams(),
+                    data: this.toJSON(),
                     dataType: "json",
                     timeout: 300000,
                     error : function(){
@@ -145,14 +247,6 @@ define([
                 }
             },
 
-            getQueryParams: function () {
-                var queryParams = this.toJSON();
-                queryParams.count = this.get("count");
-                queryParams.start = this.get("startIndex");
-                queryParams.format = this.get("format");
-                return queryParams;
-            },
-
             setDefaults : function() {
                 var model = this;
                 _.each(_.keys(model.defaults), function(key) {
@@ -165,17 +259,17 @@ define([
                     south = this.get('south'),
                     west = this.get('west'),
                     east = this.get('east');
-                if(north && south && east && west){
+                if (north && south && east && west){
                     this.set('bbox', [west,south,east,north].join(','));
                 }
             },
 
             swapDatesIfNeeded : function() {
                 var model = this;
-                if(model.get('dtstart') && model.get('dtend')){
+                if (model.get('dtstart') && model.get('dtend')){
                     var start = new Date(model.get('dtstart'));
                     var end = new Date(model.get('dtend'));
-                    if(start > end){
+                    if (start > end){
                         this.set({
                             dtstart : end.toISOString(),
                             dtend : start.toISOString()
