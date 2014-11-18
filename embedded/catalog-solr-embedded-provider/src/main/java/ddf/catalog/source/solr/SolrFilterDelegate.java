c2bb6ac5ed28178aeca19dc5899e163e3edde990
Hugh Rodgers <hugh.rodgers@lmco.com>
2014-11-18 06:35:09 -0700

    DDF-886  fixed PropertyIsLessThan and PropertyIsEqualTo implementations

--- embedded/catalog-solr-embedded-provider/src/main/java/ddf/catalog/source/solr/SolrFilterDelegate.java
+++ embedded/catalog-solr-embedded-provider/src/main/java/ddf/catalog/source/solr/SolrFilterDelegate.java
@@ -684,7 +684,7 @@ public class SolrFilterDelegate extends FilterDelegate<SolrQuery> {
         String mappedPropertyName = getMappedPropertyName(propertyName, format, true);
 
         SolrQuery query = new SolrQuery();
-        query.setQuery(" " + mappedPropertyName + ":{ " + literal.toString() + TO + "* ] ");
+        query.setQuery(" " + mappedPropertyName + ":" + literal.toString());
 
         return query;
     }
@@ -724,7 +724,7 @@ public class SolrFilterDelegate extends FilterDelegate<SolrQuery> {
         String mappedPropertyName = getMappedPropertyName(propertyName, format, true);
 
         SolrQuery query = new SolrQuery();
-        query.setQuery(" " + mappedPropertyName + ":{ * TO " + literal.toString() + " ] ");
+        query.setQuery(" " + mappedPropertyName + ":[ * TO " + literal.toString() + " } ");
 
         return query;
     }
