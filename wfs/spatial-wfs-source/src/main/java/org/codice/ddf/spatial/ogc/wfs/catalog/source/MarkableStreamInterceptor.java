c775e15cd145e3fabdf45f47e57351c4a62feeef
Scott Tustison <scott.tustison@gmail.com>
2014-12-11 13:45:24 -0700

    DDF-938 updated content catalogger to not load create and update requests into memory
    - updated stream interceptor to not load all responses into memory
    - changed thumbnail size on ui thumbnailer to 200px

--- wfs/spatial-wfs-source/src/main/java/org/codice/ddf/spatial/ogc/wfs/catalog/source/MarkableStreamInterceptor.java
+++ wfs/spatial-wfs-source/src/main/java/org/codice/ddf/spatial/ogc/wfs/catalog/source/MarkableStreamInterceptor.java
@@ -14,10 +14,7 @@
  **/
 package org.codice.ddf.spatial.ogc.wfs.catalog.source;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
+import com.google.common.io.FileBackedOutputStream;
 import org.apache.commons.io.IOUtils;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.message.Message;
@@ -25,6 +22,9 @@ import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MarkableStreamInterceptor extends AbstractPhaseInterceptor<Message> {
 
@@ -38,9 +38,16 @@ public class MarkableStreamInterceptor extends AbstractPhaseInterceptor<Message>
     public void handleMessage(Message message) throws Fault {
         LOGGER.debug("Converting message input stream to a buffered stream");
         InputStream is = message.getContent(InputStream.class);
+        FileBackedOutputStream fileBackedOutputStream = new FileBackedOutputStream(1000000);
+
+        try {
+            IOUtils.copy(is, fileBackedOutputStream);
+        } catch (IOException e) {
+            LOGGER.warn("Could not copy bytes of content message.", e);
+        }
          
         try {
-            message.setContent(InputStream.class, new ByteArrayInputStream(IOUtils.toByteArray(is)));
+            message.setContent(InputStream.class, fileBackedOutputStream.asByteSource().openStream());
         } catch (IOException e) {
             LOGGER.warn("Failed to convert buffered stream");
         } catch (NullPointerException e) {
