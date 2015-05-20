619bbffc295b45f42576b2dd41d5cf73c7c05647
Andrew Fiedler <andrewkfiedler@gmail.com>
2015-05-20 15:16:12 -0700

    DDF-1194 Adding to javadoc
     - Added clarification of the th param, and the return

--- util/src/main/java/org/codice/ddf/platform/util/Exceptions.java
+++ util/src/main/java/org/codice/ddf/platform/util/Exceptions.java
@@ -27,8 +27,8 @@ public final class Exceptions {
     /**
      * Given a throwable, this traces back through the causes to construct a full message.
      *
-     * @param th
-     * @return
+     * @param   th  meant to be a nested throwable, but doesn't have to be
+     * @return      all messages in reverse order (i.e. root cause message is first!)
      */
     public static String getFullMessage(Throwable th) {
         StringBuilder message = new StringBuilder(th.getMessage());
