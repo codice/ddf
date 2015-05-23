67d15ad26572e4f00748c9c011166ab17f0cc34f
elessard <eric.lessard@connexta.com>
2015-05-22 17:35:59 -0700

    DDF-1210: Reformatted code.

--- test/itests/test-itests-common/src/test/java/ddf/common/test/KarafConsole.java
+++ test/itests/test-itests-common/src/test/java/ddf/common/test/KarafConsole.java
@@ -11,7 +11,7 @@
  * is distributed along with this program and can be found at
  * <http://www.gnu.org/licenses/lgpl.html>.
  *
- */
+ **/
 package ddf.common.test;
 
 import java.security.Principal;
