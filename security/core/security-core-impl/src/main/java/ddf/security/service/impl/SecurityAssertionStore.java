bd51aa6fd4ffa9c26c02322283f77b4744b5ffeb
Scott Tustison <scott.tustison@gmail.com>
2015-06-01 08:03:38 -0700

    DDF-1223 fixed the security assertion store so that it creates security tokens with a created and expires date

--- security/core/security-core-impl/src/main/java/ddf/security/service/impl/SecurityAssertionStore.java
+++ security/core/security-core-impl/src/main/java/ddf/security/service/impl/SecurityAssertionStore.java
@@ -1,50 +1,47 @@
 /**
  * Copyright (c) Codice Foundation
- * 
+ * <p/>
  * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or any later version.
- * 
+ * <p/>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
  * is distributed along with this program and can be found at
  * <http://www.gnu.org/licenses/lgpl.html>.
- * 
  **/
 package ddf.security.service.impl;
 
 import ddf.security.assertion.SecurityAssertion;
 import ddf.security.assertion.impl.SecurityAssertionImpl;
-import org.apache.cxf.endpoint.Endpoint;
 import org.apache.cxf.message.Message;
-import org.apache.cxf.service.model.EndpointInfo;
-import org.apache.cxf.ws.security.SecurityConstants;
 import org.apache.cxf.ws.security.tokenstore.SecurityToken;
 import org.apache.cxf.ws.security.tokenstore.TokenStore;
-import org.apache.cxf.ws.security.tokenstore.TokenStoreFactory;
 import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.cxf.ws.security.wss4j.WSS4JUtils;
 import org.apache.wss4j.common.principal.SAMLTokenPrincipal;
+import org.apache.wss4j.common.saml.SamlAssertionWrapper;
 import org.apache.wss4j.dom.WSSecurityEngineResult;
 import org.apache.wss4j.dom.handler.WSHandlerConstants;
 import org.apache.wss4j.dom.handler.WSHandlerResult;
 
 import java.security.Principal;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Creates a SecurityAssertion object by extracting the token id from the message and using that id
  * to retrieve the SecurityToken object from the TokenStore instance.
- * 
+ *
  * @author tustisos
- * 
  */
 public final class SecurityAssertionStore {
     /**
      * Return the SecurityAssertion wrapper associated with the provided message
-     * 
-     * @param message
-     *            Message
+     *
+     * @param message Message
      * @return SecurityAssertion
      */
     public static SecurityAssertion getSecurityAssertion(Message message) {
@@ -54,32 +51,50 @@ public final class SecurityAssertionStore {
             if (!(principal instanceof SAMLTokenPrincipal)) {
                 // Try to find the SAMLTokenPrincipal if it exists
                 List<?> wsResults = List.class.cast(message.get(WSHandlerConstants.RECV_RESULTS));
-                if (wsResults != null)
-                {
+                if (wsResults != null) {
                     for (Object wsResult : wsResults) {
                         if (wsResult instanceof WSHandlerResult) {
                             List<WSSecurityEngineResult> wsseResults = ((WSHandlerResult) wsResult)
                                     .getResults();
 
-                                for (WSSecurityEngineResult wsseResult : wsseResults) {
-                                    Object principalResult = wsseResult
-                                            .get(WSSecurityEngineResult.TAG_PRINCIPAL);
-                                    if (principalResult instanceof SAMLTokenPrincipal) {
-                                        principal = (SAMLTokenPrincipal) principalResult;
-                                        break;
-                                    }
+                            for (WSSecurityEngineResult wsseResult : wsseResults) {
+                                Object principalResult = wsseResult
+                                        .get(WSSecurityEngineResult.TAG_PRINCIPAL);
+                                if (principalResult instanceof SAMLTokenPrincipal) {
+                                    principal = (SAMLTokenPrincipal) principalResult;
+                                    break;
                                 }
+                            }
 
                         }
                     }
                 }
             }
-            if (tokenStore != null && principal != null && principal instanceof SAMLTokenPrincipal) {
+            if (tokenStore != null && principal != null
+                    && principal instanceof SAMLTokenPrincipal) {
                 String id = ((SAMLTokenPrincipal) principal).getId();
+                SamlAssertionWrapper samlAssertionWrapper = ((SAMLTokenPrincipal) principal)
+                        .getToken();
                 SecurityToken token = tokenStore.getToken(id);
                 if (token == null) {
-                    token = new SecurityToken(id);
-                    token.setToken(((SAMLTokenPrincipal) principal).getToken().getElement());
+                    if (samlAssertionWrapper.getSaml2().getIssueInstant() != null
+                            && samlAssertionWrapper.getSaml2().getConditions() != null
+                            && samlAssertionWrapper.getSaml2().getConditions().getNotOnOrAfter()
+                            != null) {
+                        token = new SecurityToken(id, samlAssertionWrapper.getElement(),
+                                samlAssertionWrapper.getSaml2().getIssueInstant().toDate(),
+                                samlAssertionWrapper.getSaml2().getConditions().getNotOnOrAfter()
+                                        .toDate());
+                    } else {
+                        // we don't know how long this should last or when it was created, so just
+                        // set it to 1 minute
+                        // This shouldn't happen unless someone sets up a third party STS with weird
+                        // settings.
+                        Date date = new Date();
+                        token = new SecurityToken(id, samlAssertionWrapper.getElement(), date,
+                                new Date(date.getTime() + TimeUnit.MINUTES.toMillis(1)));
+                    }
+                    tokenStore.add(token);
                 }
 
                 return new SecurityAssertionImpl(token);
@@ -90,26 +105,11 @@ public final class SecurityAssertionStore {
 
     /**
      * Return the TokenStore associated with this message.
-     * 
+     *
      * @param message
      * @return TokenStore
      */
-    private static TokenStore getTokenStore(Message message) {
-        EndpointInfo info = message.getExchange().get(Endpoint.class).getEndpointInfo();
-        synchronized (info) {
-            TokenStore tokenStore = (TokenStore) message
-                    .getContextualProperty(SecurityConstants.TOKEN_STORE_CACHE_INSTANCE);
-            if (tokenStore == null) {
-                tokenStore = (TokenStore) info
-                        .getProperty(SecurityConstants.TOKEN_STORE_CACHE_INSTANCE);
-            }
-            if (tokenStore == null) {
-                TokenStoreFactory tokenStoreFactory = TokenStoreFactory.newInstance();
-                tokenStore = tokenStoreFactory.newTokenStore(
-                        SecurityConstants.TOKEN_STORE_CACHE_INSTANCE, message);
-                info.setProperty(SecurityConstants.TOKEN_STORE_CACHE_INSTANCE, tokenStore);
-            }
-            return tokenStore;
-        }
+    public static TokenStore getTokenStore(Message message) {
+        return WSS4JUtils.getTokenStore(message);
     }
 }
