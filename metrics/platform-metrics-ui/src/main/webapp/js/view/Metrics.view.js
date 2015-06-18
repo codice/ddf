ec55a5705f29acadf8543c5c3696e71b6d5c80d2
Brendan Hofmann <brendan.hofmann@connexta.com>
2015-06-18 15:05:55 -0700

    DDF-1244 Add Metrics tab to Platform App.
     - First Metric table implemented
     - todo: summaries

---
+++ metrics/platform-metrics-ui/src/main/webapp/js/view/Metrics.view.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ **/
+/*global define*/
+define([
+        'backbone',
+        'jquery',
+        'underscore',
+        'marionette',
+        'handlebars',
+        'icanhaz',
+        'text!templates/metrics.handlebars'
+    ],
+    function (Backbone, $, _, Marionette, Handlebars, ich, metricsTemplate) {
+
+        var MetricsView = {};
+        ich.addTemplate('metricsTemplate', metricsTemplate);
+
+        Handlebars.registerHelper('titleizeCamelCase', function (camelCase) {
+            // insert a space before all caps
+            return camelCase.replace(/([A-Z])/g, ' $1')
+                // uppercase the first character
+                .replace(/^./, function (str) {
+                    return str.toUpperCase();
+                });
+        });
+
+        MetricsView.MetricsPage = Marionette.Layout.extend({
+            template: 'metricsTemplate',
+            initialize: function () {
+                _.bindAll(this);
+            }
+        });
+
+        return MetricsView;
+    });
