564ce251508ba51b3aed45630b1290022f1bbc05
Joseph North <joernorth@gmail.com>
2015-06-23 10:52:55 -0700

    DDF-1248: Redesigned DDF GitHub site

--- js/script.js
+++ js/script.js
@@ -1,18 +1,48 @@
-$(document).ready(function() {
-	var day=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
-	   month=['January','February','March','April','May','June','July','August','September','October','Novermber','December'];
-   SetData();
-   function SetData() {
-	   var now = new Date();
-	   $('.date').html(day[now.getDay()]+', ');
-	   $('.date').append(' '+month[now.getMonth()]+' ');
-	   $('.date').append(now.getDate()+', ');
-	   $('.date').append(now.getFullYear()+' &nbsp; &nbsp; ');
-	   hour=now.getHours();
-	   minutes=now.getMinutes();
-	   if (minutes<10) {minutes='0'+minutes};
-	   $('.date').append(hour+':'+minutes);
-	}
-  	setInterval(SetData,60);
+var getLatestStableVersion = function(callback) {
+    var versionContent = $("#stable-version");
 
+    $.ajax({
+        url: "https://api.github.com/repos/codice/ddf/releases/latest",
+        success: function(data) {
+            var dateStr = data.published_at.slice(0, 10);
+            var html = "<a href='" + data.html_url + "' target='_blank'>" + data.tag_name + "</a>" +
+                " released on " + dateStr;
+            versionContent.html(html);
+            
+            if (callback !== undefined) {
+                callback(data);
+            }
+        },
+        error: function() {
+            versionContent.html("<a href='https://github.com/codice/ddf/releases/latest' target='_blank'>Link to the latest release</a>");
+            
+            if (callback !== undefined) {
+                callback();
+            }
+        },
+        dataType: "json"
+    });
+};
+
+var updateDownloadButton = function(data) {
+    var downloadButton = $("#download-button"), href, html;
+    
+    if (data !== undefined) {
+        href = data.assets[0].browser_download_url;
+        html = "Download .zip <span class='glyphicon glyphicon-download'></span>";
+    } else {
+        href = "https://github.com/codice/ddf/releases/latest";
+        html = "Download from GitHub";
+    }
+    
+    downloadButton.attr("href", href);
+    downloadButton.html(html);
+};
+
+$(function() {
+    if (window.location.href.indexOf("Downloads") > -1) { // We're on the downloads page.
+        getLatestStableVersion(updateDownloadButton);
+    } else {
+        getLatestStableVersion();
+    }
 });
