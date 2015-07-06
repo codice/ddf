06cd1bf34231f76feabf96094ec93de7b7c35e61
Scott Tustison <scott.tustison@gmail.com>
2015-07-06 09:26:05 -0700

    DDF-1308 fixed issues with bower.json in sources admin module

--- admin/module/catalog-admin-module-sources/Gruntfile.js
+++ admin/module/catalog-admin-module-sources/Gruntfile.js
@@ -16,6 +16,9 @@
 
 module.exports = function (grunt) {
 
+    require('load-grunt-tasks')(grunt);
+    grunt.loadTasks('src/main/grunt/tasks');
+
     grunt.initConfig({
        
         pkg: grunt.file.readJSON('package.json'),
@@ -113,37 +116,4 @@ module.exports = function (grunt) {
 
     grunt.registerTask('build', buildTasks);
     grunt.registerTask('default', ['build','express', 'watch']);
-
-    grunt.registerTask('bower-offline-install', 'Bower offline install work-around', function() {
-        var bower = require('bower');
-        var done = this.async();
-        grunt.log.writeln("Trying to install bower packages OFFline.");
-        bower.commands
-            .install([], {save: true}, { offline: true })
-            .on('data', function(data){
-                grunt.log.write(data);
-            })
-            .on('error', function(data){
-                grunt.log.writeln(data);
-                grunt.log.writeln("Trying to install bower packages ONline.");
-                bower.commands
-                    .install()
-                    .on('data', function(data){
-                        grunt.log.write(data);
-                    })
-                    .on('error', function(data){
-                        grunt.log.write(data);
-                        done(false);
-                    })
-                    .on('end', function () {
-                        grunt.log.write("Bower installed online.");
-                        done();
-                    });
-            })
-            .on('end', function () {
-
-                grunt.log.writeln("Bower installed offline.");
-                done();
-            });
-    });
 };
