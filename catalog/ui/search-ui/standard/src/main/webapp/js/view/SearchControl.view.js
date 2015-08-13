6f559f9bc6ccd469197585aa92a4d4d7d0ea96e5
Travis McMahon <azgoalie@gmail.com>
2015-08-13 11:05:27 -0700

    DDF-1385: Fixed enhanced search querying
    Reset the query object's filter when going back to the original search panel and added functional tests.

--- catalog/ui/search-ui/standard/src/main/webapp/js/view/SearchControl.view.js
+++ catalog/ui/search-ui/standard/src/main/webapp/js/view/SearchControl.view.js
@@ -150,6 +150,7 @@ define([
                         break;
                     case 'results':
                         if(id === 'Search') {
+                            wreqr.vent.trigger('query:clearFilters');
                             wreqr.vent.trigger('search:show', dir.backward);
                         }
                         if(id === 'Save') {
