29046a4bf070abdff2fc292c21beb2182e1a9e89
Don Cruver <donald.cruver@connexta.com>
2015-09-15 08:34:58 -0700

    DDF-1493 Remove gazetteer feature from search-ui-app feature.
    Prior to this change, the webservice-gazetteer was being loaded as part of the
    search-ui feature.  While the search-ui needs a gazetteer, the selection of a
    specific gazetteer implementation should be left to configurations that are
    loaded at a later time.  This change adds a ServiceSelector which determines
    which GeoCoder implementation should be used based on the service ranking of
    each bound implementation of GeoCoder.  It also adds new unit tests for the
    ServiceSelector and the GeoCoderEndpoint.

---
+++ catalog/core/catalog-core-api-impl/src/main/java/ddf/catalog/util/impl/ServiceSelectorConverter.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p/>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+
+package ddf.catalog.util.impl;
+
+import org.osgi.service.blueprint.container.Converter;
+import org.osgi.service.blueprint.container.ReifiedType;
+
+/**
+ * This converter is used to allow {@link ServiceSelector} objects to pass through for
+ * {@link java.util.List} implementations. This was originally intended to allow plugins to be automatically
+ * sorted in the list. Without this converter, blueprint will copy the list and lose the reference.
+ *
+ */
+public class ServiceSelectorConverter implements Converter {
+
+    /**
+     * @parameter sourceObject object considering to be converted
+     * @parameter targetType
+     *
+     * @return true if sourceObject is an instance of ServiceSelector; false otherwise
+     */
+
+    @Override
+    public boolean canConvert(Object sourceObject, ReifiedType targetType) {
+        return (sourceObject instanceof ServiceSelector);
+    }
+
+    /**
+     * Converts (casts) the sourceObject to a ServiceSelector.
+     *
+     * @parameter sourceObject object being converted
+     * @parameter targetType
+     *
+     * @return sourceObject cast to a ServiceSelector
+     */
+
+    @Override
+    public Object convert(Object sourceObject, ReifiedType targetType) throws Exception {
+        return sourceObject;
+    }
+}
--- catalog/spatial/geocoding/spatial-geocoding-geocoder/src/main/java/org/codice/ddf/spatial/geocoder/endpoint/GeoCoderEndpoint.java
+++ catalog/spatial/geocoding/spatial-geocoding-geocoder/src/main/java/org/codice/ddf/spatial/geocoder/endpoint/GeoCoderEndpoint.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (c) Codice Foundation
- *
+ * <p/>
  * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or any later version.
- *
+ * <p/>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
  * is distributed along with this program and can be found at
  * <http://www.gnu.org/licenses/lgpl.html>.
- *
  **/
+
 package org.codice.ddf.spatial.geocoder.endpoint;
 
 import java.util.List;
@@ -26,21 +26,42 @@ import org.codice.ddf.spatial.geocoder.GeoResult;
 import org.opengis.geometry.DirectPosition;
 import org.opengis.geometry.primitive.Point;
 
+import ddf.catalog.util.impl.ServiceSelector;
+
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 
 @Path("/")
 public class GeoCoderEndpoint {
 
-    private GeoCoder geoCoder;
+    private ServiceSelector<GeoCoder> geoCoderFactory;
 
-    public GeoCoderEndpoint(GeoCoder geoCoder) {
-        this.geoCoder = geoCoder;
+    public GeoCoderEndpoint(ServiceSelector<GeoCoder> geoCoderFactory) {
+
+        if (geoCoderFactory == null) {
+            throw new IllegalArgumentException(
+                    "GeoCoderEndpoint(): constructor argument 'geoCoderFactory' may not be null.");
+        }
+
+        this.geoCoderFactory = geoCoderFactory;
     }
 
     @GET
     public Response getLocation(@QueryParam("jsonp") String jsonp,
             @QueryParam("query") String query) {
+
+        JSONObject jsonObject = doQuery(query);
+        return Response.ok(jsonp + "(" + jsonObject.toJSONString() + ")").build();
+    }
+
+    JSONObject doQuery(String query) {
+        GeoCoder geoCoder = geoCoderFactory.getService();
+        GeoResult geoResult = null;
+
+        if (geoCoder != null) {
+            geoResult = geoCoder.getLocation(query);
+        }
+
         JSONObject jsonObject = new JSONObject();
         JSONArray resourceSets = new JSONArray();
         JSONObject resourceSet = new JSONObject();
@@ -49,36 +70,39 @@ public class GeoCoderEndpoint {
         JSONArray resources = new JSONArray();
         resourceSet.put("resources", resources);
 
-        GeoResult geoResult = geoCoder.getLocation(query);
-
         if (geoResult != null) {
-            DirectPosition directPosition = geoResult.getPoint().getDirectPosition();
-            double[] coords = directPosition.getCoordinate();
-
-            double longitude = coords[0];
-            double latitude = coords[1];
-
-            JSONObject resource = new JSONObject();
-            JSONArray bbox = new JSONArray();
-            List<Point> points = geoResult.getBbox();
-            DirectPosition upperCorner = points.get(0).getDirectPosition();
-            DirectPosition lowerCorner = points.get(1).getDirectPosition();
-            bbox.add(upperCorner.getCoordinate()[1]);
-            bbox.add(upperCorner.getCoordinate()[0]);
-            bbox.add(lowerCorner.getCoordinate()[1]);
-            bbox.add(lowerCorner.getCoordinate()[0]);
-            resource.put("bbox", bbox);
-            JSONObject point = new JSONObject();
-            point.put("type", "Point");
-            JSONArray coordinates = new JSONArray();
-            coordinates.add(latitude);
-            coordinates.add(longitude);
-            point.put("coordinates", coordinates);
-            resource.put("point", point);
-            resource.put("name", geoResult.getFullName());
-            resources.add(resource);
+            transformGeoResult(geoResult, resources);
         }
 
-        return Response.ok(jsonp + "(" + jsonObject.toJSONString() + ")").build();
+        return jsonObject;
     }
+
+    void transformGeoResult(GeoResult geoResult, JSONArray resources) {
+        DirectPosition directPosition = geoResult.getPoint().getDirectPosition();
+        double[] coords = directPosition.getCoordinate();
+
+        double longitude = coords[0];
+        double latitude = coords[1];
+
+        JSONObject resource = new JSONObject();
+        JSONArray bbox = new JSONArray();
+        List<Point> points = geoResult.getBbox();
+        DirectPosition upperCorner = points.get(0).getDirectPosition();
+        DirectPosition lowerCorner = points.get(1).getDirectPosition();
+        bbox.add(upperCorner.getCoordinate()[1]);
+        bbox.add(upperCorner.getCoordinate()[0]);
+        bbox.add(lowerCorner.getCoordinate()[1]);
+        bbox.add(lowerCorner.getCoordinate()[0]);
+        resource.put("bbox", bbox);
+        JSONObject point = new JSONObject();
+        point.put("type", "Point");
+        JSONArray coordinates = new JSONArray();
+        coordinates.add(latitude);
+        coordinates.add(longitude);
+        point.put("coordinates", coordinates);
+        resource.put("point", point);
+        resource.put("name", geoResult.getFullName());
+        resources.add(resource);
+    }
+
 }
