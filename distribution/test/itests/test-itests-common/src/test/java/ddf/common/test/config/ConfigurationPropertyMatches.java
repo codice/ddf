9a38422f56e6a76e8e5ba71750b19232f6b69324
elessard <eric.lessard@connexta.com>
2015-09-21 14:17:58 -0700

    DDF-1505: Add methods in integration test common classes to restart bundles and wait for configuration updates
    Added a method to ServiceManager to restart one or more bundles. Also added a
    waitForConfiguration() method to AdminConfig along with a ConfigurationPredicate
    class that wait for a property value to match a regular expression.

--- distribution/test/itests/test-itests-common/src/test/java/ddf/common/test/config/ConfigurationPropertyMatches.java
+++ distribution/test/itests/test-itests-common/src/test/java/ddf/common/test/config/ConfigurationPropertyMatches.java
@@ -1,15 +1,36 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p/>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ **/
 package ddf.common.test.config;
 
 import org.osgi.service.cm.Configuration;
 
 /**
- * Created by elessard on 9/21/15.
+ * Configuration predicate class that checks to see if a {@link Configuration} property exists and
+ * matches a specific regular expression.
  */
 public class ConfigurationPropertyMatches implements ConfigurationPredicate {
     private String propertyName;
 
     private String valueRegex;
 
+    /**
+     * Constructor.
+     *
+     * @param propertyName name of the property this predicate will use
+     * @param valueRegex   regular expression the property value needs to match for this predicate
+     *                     to return {@code true}.
+     */
     public ConfigurationPropertyMatches(String propertyName, String valueRegex) {
         this.propertyName = propertyName;
         this.valueRegex = valueRegex;
@@ -23,5 +44,10 @@ public class ConfigurationPropertyMatches implements ConfigurationPredicate {
         }
 
         return ((String) configuration.getProperties().get(propertyName)).matches(valueRegex);
+    }
+
+    public String toString() {
+        return String
+                .format("property [%s] matches regular expression [%s]", propertyName, valueRegex);
     }
 }
