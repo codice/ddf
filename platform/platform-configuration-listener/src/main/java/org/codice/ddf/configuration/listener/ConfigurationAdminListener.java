4b3ada93fe08902b769f235c51ba477d6fb16775
Dan Figliola <daniel.figliola@connexta.com>
2015-09-30 13:31:01 -0700

    DDF-1555: Implement solution for cardinality issues that cause unpredictable configuration
     - Added a configuration admin listener that reads and writes custom configuration
       files that properly support cardinality. The current implementation uses
       Felix' ConfigurationHandler class to read and write the files.
     - Added a file watcher to configuration directory. Previously, there
       wasn't a way to get changes that were made directly in the
       filesystem as opposed to the given administration ui or commands.
       With this change, ddf will pick up any changes to config files that are
       made (created, modified, deleted) and propagate them to the Config Admin.
     - Listeners added as a feature that installs at Karaf start up.
     - Added ConfigurationPropertiesComparator class in platform-util to compare
       OSGi Configuration properties and removed equal() method from FileHandlerImpl
       class.
     - Removed cardinality workaround in policy manager.
     - Added property replacement using PropertyResolver where needed.
     - Addresses code review comments.
     - Added documentation.

---
+++ platform/platform-configuration-listener/src/main/java/org/codice/ddf/configuration/listener/ConfigurationAdminListener.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p/>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.codice.ddf.configuration.listener;
+
+import java.io.IOException;
+import java.util.Dictionary;
+
+import org.codice.ddf.configuration.store.FileHandler;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.cm.ConfigurationEvent;
+import org.osgi.service.cm.ConfigurationListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class listens for changes in configAdmin and calls FileHandler to write out changes to a
+ * configuration file.
+ */
+public class ConfigurationAdminListener implements ConfigurationListener {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigurationAdminListener.class);
+
+    private BundleContext bundleContext;
+
+    private FileHandler fileHandler;
+
+    public ConfigurationAdminListener(BundleContext bundleContext, FileHandler fileHandler) {
+        this.bundleContext = bundleContext;
+        this.fileHandler = fileHandler;
+        LOGGER.debug("{} is using the {} file handler.", ConfigurationAdminListener.class.getName(),
+                fileHandler.getClass().getName());
+    }
+
+    /**
+     * This method is invoked when a configuration event occurs (modification of configAdmin)
+     *
+     * @param event object containing configuration event data
+     */
+    @Override
+    public void configurationEvent(ConfigurationEvent event) {
+        if (event == null) {
+            return;
+        }
+
+        String pid = event.getPid();
+        int eventType = event.getType();
+        String factoryPid = event.getFactoryPid();
+        LOGGER.debug("Factory pid: {}", factoryPid);
+        LOGGER.debug("Event type: {}", eventType);
+        LOGGER.debug("Received configuration event for bundle with pid [{}].", pid);
+        try {
+            Configuration configuration = getConfiguration(pid, event);
+            Dictionary<String, Object> properties = configuration.getProperties();
+            switch (eventType) {
+            case ConfigurationEvent.CM_UPDATED:
+                fileHandler.write(pid, properties);
+                break;
+            case ConfigurationEvent.CM_DELETED:
+                fileHandler.delete(pid);
+                break;
+            default:
+                LOGGER.debug("Unsupported ConfigurationEvent [{}]. No action taken for pid [{}].",
+                        eventType, pid);
+            }
+        } catch (RuntimeException | IOException e) {
+            LOGGER.error("Failed to process ConfigurationEvent [{}] for pid [{}].", eventType, pid,
+                    e);
+        }
+    }
+
+    /*
+     * Returns the configuration object associated with a specific PID and configuration event.
+     */
+    private Configuration getConfiguration(String pid, ConfigurationEvent event)
+            throws IOException {
+        ServiceReference<ConfigurationAdmin> configAdminServiceReference = event.getReference();
+        ConfigurationAdmin configAdmin = bundleContext.getService(configAdminServiceReference);
+        return configAdmin.getConfiguration(pid, null);
+    }
+}
