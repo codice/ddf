29b390216a2c8c48d1358e5c1dff2271b3c11276
Eric Irwin <Eric.Irwin1124@gmail.com>
2015-09-30 12:44:18 -0700

    DDF-1530: Replaced URLConnection with CFX WebClient in GeocoderEndpoint
    The Geocoder endpoint previously used URLConnection, but this had a lot
    of issues with response encoding. In order to avoid having to mask these
    issues, it was replaced with CXF which doesn't have any of the issues.

--- catalog/spatial/geocoding/spatial-geocoding-websearch/src/main/java/org/codice/ddf/spatial/geocoder/geonames/GeoNamesWebService.java
+++ catalog/spatial/geocoding/spatial-geocoding-websearch/src/main/java/org/codice/ddf/spatial/geocoder/geonames/GeoNamesWebService.java
@@ -14,13 +14,10 @@
  **/
 package org.codice.ddf.spatial.geocoder.geonames;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
 
+import org.apache.cxf.jaxrs.client.WebClient;
 import org.codice.ddf.spatial.geocoder.GeoCoder;
 import org.codice.ddf.spatial.geocoder.GeoResult;
 import org.codice.ddf.spatial.geocoder.GeoResultCreator;
@@ -36,6 +33,8 @@ public class GeoNamesWebService implements GeoCoder {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GeoNamesWebService.class);
 
+    private  static  final  String CHARACTER_ENCODING = "UTF-8";
+
     //geonames requires an application username, this is the default name for DDF
     protected String username = "ddf_ui";
 
@@ -45,70 +44,47 @@ public class GeoNamesWebService implements GeoCoder {
 
     @Override
     public GeoResult getLocation(String location) {
-        String urlStr;
         String method = "search";
         String term = "q=";
 
         try {
-            location = URLEncoder.encode(location, "UTF-8");
+            location = URLEncoder.encode(location, CHARACTER_ENCODING);
         } catch (UnsupportedEncodingException e) {
             LOGGER.error("Unable to encode location.", e);
         }
-        urlStr = geoNamesProtocol + "://" + geoNamesApiServer + "/" + method + "JSON" + "?" + term
-                + location + "&username=" + username;
 
-        URL url = null;
-        try {
-            url = new URL(urlStr);
-        } catch (MalformedURLException e) {
-            LOGGER.error("Geonames URL is invalid: {}", urlStr, e);
-        }
+        String urlStr = geoNamesProtocol + "://" + geoNamesApiServer + "/" + method + "JSON" + "?" + term
+                + location + "&username=" + username;
+        WebClient client = WebClient.create(urlStr);
+        String response = client.acceptEncoding(CHARACTER_ENCODING).accept("application/json").get(
+                String.class);
 
         GeoResult geoResult = new GeoResult();
-        if (url != null) {
-            URLConnection urlConnection = null;
-            try {
-                urlConnection = url.openConnection();
-            } catch (IOException e) {
-                LOGGER.error("Unable to open connection to Geonames service.", e);
-            }
+        Object result = null;
+        try {
+            JSONParser parser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
+            result = parser.parse(response);
+        } catch (ParseException e) {
+            LOGGER.error("Error while parsing JSON message from Geonames service.", e);
+        }
 
-            if (urlConnection != null) {
-                try {
-                    urlConnection.connect();
-                } catch (IOException e) {
-                    LOGGER.error("Unable to connect to Geonames service", e);
-                }
 
-                JSONParser parser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
+        if (result != null) {
+            if (result instanceof JSONObject) {
+                JSONObject jsonResult = (JSONObject) result;
+                JSONArray geonames = (JSONArray) jsonResult.get("geonames");
+                if (geonames != null && geonames.size() > 0) {
+                    JSONObject firstResult = (JSONObject) geonames.get(0);
+                    if (firstResult != null) {
+                        double lat = Double.valueOf((String) firstResult.get("lat"));
+                        double lon = Double.valueOf((String) firstResult.get("lng"));
 
-                Object result = null;
-                try {
-                    result = parser.parse(urlConnection.getInputStream());
-                } catch (ParseException e) {
-                    LOGGER.error("Error while parsing JSON message from Geonames service.", e);
-                } catch (IOException e) {
-                    LOGGER.error("Error reading input stream from Geonames service.", e);
-                }
+                        Long population = (Long) firstResult.get("population");
+                        String adminCode = (String) firstResult.get("fcode");
 
-                if (result != null) {
-                    if (result instanceof JSONObject) {
-                        JSONObject jsonResult = (JSONObject) result;
-                        JSONArray geonames = (JSONArray) jsonResult.get("geonames");
-                        if (geonames != null && geonames.size() > 0) {
-                            JSONObject firstResult = (JSONObject) geonames.get(0);
-                            if (firstResult != null) {
-                                double lat = Double.valueOf((String) firstResult.get("lat"));
-                                double lon = Double.valueOf((String) firstResult.get("lng"));
 
-                                Long population = (Long) firstResult.get("population");
-                                String adminCode = (String) firstResult.get("fcode");
-
-                                return GeoResultCreator
-                                        .createGeoResult((String) firstResult.get("name"), lat, lon,
-                                                adminCode, population);
-                            }
-                        }
+                        return  GeoResultCreator
+                                .createGeoResult((String)firstResult.get("name"), lat, lon, adminCode, population);
                     }
                 }
             }
