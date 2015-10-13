809fa615d6e07d53f690fb36302a0ce6fa70508b
Garrett Freibott <garrett.freibott@connexta.com>
2015-10-13 14:34:41 -0700

    DDF-1506: Unit Tests, Refactoring, and Documentation
     - Added unit tests to cover CRL additions
     - Minor code refactoring to support unit testing
     - Modified code coverage values due to the addition of error
       handling cases.
     - Documentation was added to explain how to enable and disable
       CRL revocation for the PKIHandler, as well as how to set up CRL
       revocation in the web context policy manager

--- platform/security/handler/security-handler-pki/src/test/java/org/codice/ddf/security/handler/pki/PKIHandlerTest.java
+++ platform/security/handler/security-handler-pki/src/test/java/org/codice/ddf/security/handler/pki/PKIHandlerTest.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (c) Codice Foundation
- * <p/>
+ * <p>
  * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or any later version.
- * <p/>
+ * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
@@ -13,8 +13,12 @@
  */
 package org.codice.ddf.security.handler.pki;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -22,12 +26,14 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.Properties;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.codice.ddf.security.handler.api.BaseAuthenticationToken;
 import org.codice.ddf.security.handler.api.HandlerResult;
 import org.codice.ddf.security.handler.api.PKIAuthenticationTokenFactory;
 import org.jasypt.contrib.org.apache.commons.codec_1_3.binary.Base64;
@@ -40,7 +46,7 @@ public class PKIHandlerTest {
      * getNormalizedToken(), when given a valid HTTPServletRequest.
      */
     @Test
-    public void testGetNormalizedTokenSuccess()
+    public void testGetNormalizedTokenSuccessNoCRL()
             throws java.security.cert.CertificateException, ServletException {
         for (AbstractPKIHandler handler : new AbstractPKIHandler[] {new PKIHandler(),
                 new WssPKIHandler()}) {
@@ -53,21 +59,7 @@ public class PKIHandlerTest {
             HttpServletResponse response = mock(HttpServletResponse.class);
             FilterChain chain = mock(FilterChain.class);
 
-            String certificateString =
-                    "MIIC5DCCAk2gAwIBAgIJAKj7ROPHjo1yMA0GCSqGSIb3DQEBCwUAMIGKMQswCQYDVQQGEwJVUzEQ"
-                            + "MA4GA1UECAwHQXJpem9uYTERMA8GA1UEBwwIR29vZHllYXIxGDAWBgNVBAoMD0xvY2toZWVkIE1h"
-                            + "cnRpbjENMAsGA1UECwwESTRDRTEPMA0GA1UEAwwGY2xpZW50MRwwGgYJKoZIhvcNAQkBFg1pNGNl"
-                            + "QGxtY28uY29tMB4XDTEyMDYyMDE5NDMwOVoXDTIyMDYxODE5NDMwOVowgYoxCzAJBgNVBAYTAlVT"
-                            + "MRAwDgYDVQQIDAdBcml6b25hMREwDwYDVQQHDAhHb29keWVhcjEYMBYGA1UECgwPTG9ja2hlZWQg"
-                            + "TWFydGluMQ0wCwYDVQQLDARJNENFMQ8wDQYDVQQDDAZjbGllbnQxHDAaBgkqhkiG9w0BCQEWDWk0"
-                            + "Y2VAbG1jby5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAIpHxCBLYE7xfDLcITS9SsPG"
-                            + "4Q04Z6S32/+TriGsRgpGTj/7GuMG7oJ98m6Ws5cTYl7nyunyHTkZuP7rBzy4esDIHheyx18EgdSJ"
-                            + "vvACgGVCnEmHndkf9bWUlAOfNaxW+vZwljUkRUVdkhPbPdPwOcMdKg/SsLSNjZfsQIjoWd4rAgMB"
-                            + "AAGjUDBOMB0GA1UdDgQWBBQx11VLtYXLvFGpFdHnhlNW9+lxBDAfBgNVHSMEGDAWgBQx11VLtYXL"
-                            + "vFGpFdHnhlNW9+lxBDAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4GBAHYs2OI0K6yVXzyS"
-                            + "sKcv2fmfw6XCICGTnyA7BOdAjYoqq6wD+33dHJUCFDqye7AWdcivuc7RWJt9jnlfJZKIm2BHcDTR"
-                            + "Hhk6CvjJ14Gf40WQdeMHoX8U8b0diq7Iy5Ravx+zRg7SdiyJUqFYjRh/O5tywXRT1+freI3bwAN0"
-                            + "L6tQ";
+            String certificateString = getLocalhostCert();
 
             InputStream stream = new ByteArrayInputStream(
                     Base64.decodeBase64(certificateString.getBytes()));
@@ -146,4 +138,212 @@ public class PKIHandlerTest {
         assertNotNull(result);
         assertEquals(HandlerResult.Status.NO_ACTION, result.getStatus());
     }
+
+    /**
+     * Tests that CRL checking becomes enabled when given a valid location
+     */
+    @Test
+    public void testCRLEnabledWhenPropertyIsSpecified() {
+        PKIHandler handler = new PKIHandler();
+        assertThat(handler.getIsEnabled(), is(false));
+        Properties prop = handler.loadProperties("encryption-crl-valid.properties");
+        String crlRelativePath = "/" + prop.getProperty(handler.CRL_PROPERTY_KEY);
+        String crlAbsolutePath = PKIHandlerTest.class.getResource(crlRelativePath).getPath();
+        handler.setCrlLocation(crlAbsolutePath);
+        assertThat(handler.getIsEnabled(), is(true));
+    }
+
+    /**
+     * Test that CRL checking is not enabled when the CRL property is not specified
+     */
+    @Test
+    public void testCRLDisabledWhenPropertyIsNotSpecified() {
+        PKIHandler handler = new PKIHandler();
+        assertThat(handler.getIsEnabled(), is(false));
+        Properties prop = handler.loadProperties("encryption-crl-none.properties");
+        String crlRelativePath = prop.getProperty(handler.CRL_PROPERTY_KEY);
+        assertNull(crlRelativePath);
+        handler.setCrlLocation(crlRelativePath);
+        assertThat(handler.getIsEnabled(), is(false));
+    }
+
+    /**
+     * Tests that the certificate gets through when CRL checking is enabled but
+     * the cert is not listed in the CRL
+     *
+     * @throws java.security.cert.CertificateException
+     * @throws ServletException
+     */
+    @Test
+    public void testCertPassesCRLCheckWhenNotListedInCRL()
+            throws java.security.cert.CertificateException, ServletException {
+
+        PKIHandler handler = configurePKIHandlerWithCRL("signature.properties",
+                "encryption-crl-valid.properties");
+
+        String certificateString = getLocalhostCert();
+
+        InputStream stream = new ByteArrayInputStream(
+                Base64.decodeBase64(certificateString.getBytes()));
+        CertificateFactory factory = CertificateFactory.getInstance("X.509");
+        X509Certificate cert = (X509Certificate) factory.generateCertificate(stream);
+        X509Certificate[] certs = new X509Certificate[1];
+        certs[0] = cert;
+
+        assertThat(handler.passesCRL(certs), is(true));
+    }
+
+    /**
+     * Tests that the certificate is not let through when it is listed in the CRL
+     *
+     * @throws java.security.cert.CertificateException
+     * @throws ServletException
+     */
+    @Test
+    public void testCertFailsCRLCheckWhenListedInCRL()
+            throws java.security.cert.CertificateException, ServletException {
+
+        PKIHandler handler = configurePKIHandlerWithCRL("signature.properties",
+                "encryption-crl-revoked.properties");
+
+        String certificateString = getLocalhostCert();
+
+        InputStream stream = new ByteArrayInputStream(
+                Base64.decodeBase64(certificateString.getBytes()));
+        CertificateFactory factory = CertificateFactory.getInstance("X.509");
+        X509Certificate cert = (X509Certificate) factory.generateCertificate(stream);
+        X509Certificate[] certs = new X509Certificate[1];
+        certs[0] = cert;
+
+        assertThat(handler.passesCRL(certs), is(false));
+    }
+
+    @Test
+    public void testHandlerCompletedWhenPassingCRL()
+            throws java.security.cert.CertificateException, ServletException {
+
+        PKIHandler handler = configurePKIHandlerWithCRL("signature.properties",
+                "encryption-crl-valid.properties");
+
+        String certificateString = getLocalhostCert();
+
+        InputStream stream = new ByteArrayInputStream(
+                Base64.decodeBase64(certificateString.getBytes()));
+        CertificateFactory factory = CertificateFactory.getInstance("X.509");
+        X509Certificate cert = (X509Certificate) factory.generateCertificate(stream);
+        X509Certificate[] certs = new X509Certificate[1];
+        certs[0] = cert;
+
+        HandlerResult handlerResult = new HandlerResult();
+        BaseAuthenticationToken token = new BaseAuthenticationToken(new Object(), "test",
+                new Object());
+
+        HttpServletResponse httpResponse = mock(HttpServletResponse.class);
+
+        handlerResult = handler.checkAgainstCRL(httpResponse, token, certs, handlerResult);
+        assertThat(handlerResult.getStatus(), equalTo(HandlerResult.Status.COMPLETED));
+    }
+
+    @Test
+    public void testHandlerRedirectedWhenFailingCRL()
+            throws java.security.cert.CertificateException, ServletException {
+
+        PKIHandler handler = configurePKIHandlerWithCRL("signature.properties",
+                "encryption-crl-revoked.properties");
+
+        String certificateString = getLocalhostCert();
+
+        InputStream stream = new ByteArrayInputStream(
+                Base64.decodeBase64(certificateString.getBytes()));
+        CertificateFactory factory = CertificateFactory.getInstance("X.509");
+        X509Certificate cert = (X509Certificate) factory.generateCertificate(stream);
+        X509Certificate[] certs = new X509Certificate[1];
+        certs[0] = cert;
+
+        HandlerResult handlerResult = new HandlerResult();
+        BaseAuthenticationToken token = new BaseAuthenticationToken(new Object(), "test",
+                new Object());
+
+        HttpServletResponse httpResponse = mock(HttpServletResponse.class);
+
+        handlerResult = handler.checkAgainstCRL(httpResponse, token, certs, handlerResult);
+        assertThat(handlerResult.getStatus(), equalTo(HandlerResult.Status.REDIRECTED));
+    }
+
+    @Test
+    public void testPassesCRLCheckWhenNoCertsArePresent()
+            throws java.security.cert.CertificateException, ServletException {
+
+        PKIHandler handler = configurePKIHandlerWithCRL("signature.properties",
+                "encryption-crl-revoked.properties");
+
+        X509Certificate[] certs = null;
+
+        assertThat(handler.passesCRL(certs), is(true));
+    }
+
+    @Test
+    public void testPassesCRLCheckWhenNoCRLIsDefined()
+            throws java.security.cert.CertificateException, ServletException {
+
+        PKIHandler handler = new PKIHandler();
+        PKIAuthenticationTokenFactory tokenFactory = new PKIAuthenticationTokenFactory();
+        tokenFactory.setSignaturePropertiesPath("signature.properties");
+        tokenFactory.init();
+        handler.setTokenFactory(tokenFactory);
+
+        // handler CRL is not set -- it will default to null and isEnabled to false
+        assertThat(handler.getIsEnabled(), is(false));
+
+        String certificateString = getLocalhostCert();
+
+        InputStream stream = new ByteArrayInputStream(
+                Base64.decodeBase64(certificateString.getBytes()));
+        CertificateFactory factory = CertificateFactory.getInstance("X.509");
+        X509Certificate cert = (X509Certificate) factory.generateCertificate(stream);
+        X509Certificate[] certs = new X509Certificate[1];
+        certs[0] = cert;
+
+        assertThat(handler.passesCRL(certs), is(true));
+    }
+
+    private PKIHandler configurePKIHandlerWithCRL(String signatureProperties,
+            String encryptionProperties) {
+        PKIHandler handler = new PKIHandler();
+        PKIAuthenticationTokenFactory tokenFactory = new PKIAuthenticationTokenFactory();
+        tokenFactory.setSignaturePropertiesPath(signatureProperties);
+        tokenFactory.init();
+        handler.setTokenFactory(tokenFactory);
+
+        Properties prop = handler.loadProperties(encryptionProperties);
+        String crlRelativePath = "/" + prop.getProperty(handler.CRL_PROPERTY_KEY);
+        String crlAbsolutePath = PKIHandlerTest.class.getResource(crlRelativePath).getPath();
+        handler.setCrlLocation(crlAbsolutePath);
+        assertThat(handler.getIsEnabled(), is(true));
+
+        return handler;
+    }
+
+    private String getLocalhostCert() {
+        String certificateString =
+                "MIIDEzCCAnygAwIBAgIJAIzc4FYrIp9mMA0GCSqGSIb3DQEBBQUAMHcxCzAJBgNV\n"
+                        + "BAYTAlVTMQswCQYDVQQIDAJBWjEMMAoGA1UECgwDRERGMQwwCgYDVQQLDANEZXYx\n"
+                        + "GTAXBgNVBAMMEERERiBEZW1vIFJvb3QgQ0ExJDAiBgkqhkiG9w0BCQEWFWRkZnJv\n"
+                        + "b3RjYUBleGFtcGxlLm9yZzAeFw0xNDEyMTAyMTU4MThaFw0xNTEyMTAyMTU4MTha\n"
+                        + "MIGDMQswCQYDVQQGEwJVUzELMAkGA1UECAwCQVoxETAPBgNVBAcMCEdvb2R5ZWFy\n"
+                        + "MQwwCgYDVQQKDANEREYxDDAKBgNVBAsMA0RldjESMBAGA1UEAwwJbG9jYWxob3N0\n"
+                        + "MSQwIgYJKoZIhvcNAQkBFhVsb2NhbGhvc3RAZXhhbXBsZS5vcmcwgZ8wDQYJKoZI\n"
+                        + "hvcNAQEBBQADgY0AMIGJAoGBAMeCyNZbCTZphHQfB5g8FrgBq1RYzV7ikVw/pVGk\n"
+                        + "z8gx3l3A99s8WtA4mRAeb6n0vTR9yNBOekW4nYOiEOq//YTi/frI1kz0QbEH1s2c\n"
+                        + "I5nFButabD3PYGxUSuapbc+AS7+Pklr0TDI4MRzPPkkTp4wlORQ/a6CfVsNr/mVg\n"
+                        + "L2CfAgMBAAGjgZkwgZYwCQYDVR0TBAIwADAnBglghkgBhvhCAQ0EGhYYRk9SIFRF\n"
+                        + "U1RJTkcgUFVSUE9TRSBPTkxZMB0GA1UdDgQWBBSA95QIMyBAHRsd0R4s7C3BreFr\n"
+                        + "sDAfBgNVHSMEGDAWgBThVMeX3wrCv6lfeF47CyvkSBe9xjAgBgNVHREEGTAXgRVs\n"
+                        + "b2NhbGhvc3RAZXhhbXBsZS5vcmcwDQYJKoZIhvcNAQEFBQADgYEAtRUp7fAxU/E6\n"
+                        + "JD2Kj/+CTWqu8Elx13S0TxoIqv3gMoBW0ehyzEKjJi0bb1gUxO7n1SmOESp5sE3j\n"
+                        + "GTnh0GtYV0D219z/09n90cd/imAEhknJlayyd0SjpnaL9JUd8uYxJexy8TJ2sMhs\n"
+                        + "GAZ6EMTZCfT9m07XduxjsmDz0hlSGV0";
+        return certificateString;
+    }
+
 }
