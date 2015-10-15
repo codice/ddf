f9d717006461f6c3ed399e69648cfa558dde5bd0
Garrett Freibott <garrett.freibott@connexta.com>
2015-10-15 16:55:43 -0700

    DDF-1506: Refactored CrlChecker
     - Refactored CRL check into its own new class.
     - Rewrote unit tests for new class
     - Updated documentation

--- platform/security/handler/security-handler-pki/src/main/java/org/codice/ddf/security/handler/pki/WssPKIHandler.java
+++ platform/security/handler/security-handler-pki/src/main/java/org/codice/ddf/security/handler/pki/WssPKIHandler.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (c) Codice Foundation
- * <p/>
+ * <p>
  * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or any later version.
- * <p/>
+ * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
@@ -29,7 +29,7 @@ public class WssPKIHandler extends AbstractPKIHandler {
      * WS-Security compliant PKI type to use when configuring context policy.
      */
     public static final String AUTH_TYPE = "WSSPKI";
-
+    
     @Override
     public String getAuthenticationType() {
         return AUTH_TYPE;
