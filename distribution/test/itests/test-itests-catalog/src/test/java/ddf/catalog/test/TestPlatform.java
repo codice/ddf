fec399dba0e9c0f363217954857d406673b85981
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2015-10-19 15:37:20 -0400

    DDF-1555: Implement solution for cardinality issues that cause unpredictable configuration
    Added a configuration file watcher that reads in any .config file located in the /etc directory
    and provisions DDF accordingly.  Configuration files are moved to /etc/processed or /etc/failed
    based on whether they were successfully processed or not. The .config files use Felix' configuration
    file format. The file watcher also reads in any new file that gets added to the /etc directory
    after DDF is started.
    
    Added the ConfigurationPropertiesComparator class in platform-util to compare OSGi Configuration
    properties.
    
    Added a new integration test class (TestPlatform) and required utility classes and resource
    files to test the new feature.
    
    Updated documentation explaining how the new configuration files can be used.

---
+++ distribution/test/itests/test-itests-catalog/src/test/java/ddf/catalog/test/TestPlatform.java
@@ -0,0 +1,282 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p/>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package ddf.catalog.test;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static ddf.common.test.WaitCondition.expect;
+import static ddf.common.test.matchers.ConfigurationPropertiesEqualTo.equalToConfigurationProperties;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Dictionary;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.felix.cm.file.ConfigurationHandler;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ddf.common.test.BeforeExam;
+import ddf.common.test.callables.GetConfigurationProperties;
+import ddf.common.test.matchers.ConfigurationPropertiesEqualTo;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class TestPlatform extends AbstractIntegrationTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestPlatform.class);
+
+    /**
+     * Class that provides utility and assertion methods for a Managed Service Felix configuration
+     * file.
+     */
+    private static class ManagedServiceConfigFile {
+        protected String pid;
+
+        public ManagedServiceConfigFile(String pid) {
+            this.pid = pid;
+        }
+
+        public String getResourcePath() {
+            return String.format("/%s.config", pid);
+        }
+
+        public void addConfigurationFile() throws IOException {
+            FileUtils.copyInputStreamToFile(getResourceAsStream(), getFile());
+        }
+
+        public void addConfigurationFileAndWait(ConfigurationAdmin configAdmin) throws IOException {
+            addConfigurationFile();
+
+            expect("Configuration properties for PID " + pid + " to be set").within(20, SECONDS)
+                    .until(new GetConfigurationProperties(configAdmin, "id", pid),
+                            equalToConfigurationProperties(getProperties()));
+        }
+
+        public void assertConfigurationPropertiesSet(ConfigurationAdmin configAdmin)
+                throws Exception {
+            Dictionary<String, Object> properties = new GetConfigurationProperties(configAdmin,
+                    "id", pid).call();
+            assertThat("No Configuration object exist for PID " + pid, properties,
+                    is(notNullValue()));
+            assertPropertiesMatch(properties, getProperties());
+        }
+
+        public void assertFileMovedToProcessedDirectory() {
+            expect("File to be moved to /etc/processed directory").within(20, SECONDS)
+                    .until(() -> getProcessedFile().exists());
+            assertThat(String.format("Configuration file %s has not been removed",
+                    getFile().getAbsolutePath()), getFile().exists(), is(false));
+        }
+
+        public void assertFileMovedToFailedDirectory() {
+            expect("Waiting for file to be moved to /etc/failed directory").within(20, SECONDS)
+                    .until(() -> getFailedFile().exists());
+            assertThat(String.format("Configuration file %s has not been removed",
+                    getFile().getAbsolutePath()), getFile().exists(), is(false));
+        }
+
+        protected void assertPropertiesMatch(Dictionary<String, Object> actualProperties,
+                Dictionary<String, Object> expectedProperties) throws IOException {
+            assertThat("Configuration properties do not match for PID " + pid, actualProperties,
+                    equalToConfigurationProperties(expectedProperties));
+        }
+
+        @SuppressWarnings("unchecked")
+        protected Dictionary<String, Object> getProperties() throws IOException {
+            return ConfigurationHandler.read(getResourceAsStream());
+        }
+
+        private File getProcessedFile() {
+            return new File(String.format("%s/etc/processed%s", ddfHome, getResourcePath()));
+        }
+
+        private File getFailedFile() {
+            return new File(String.format("%s/etc/failed%s", ddfHome, getResourcePath()));
+        }
+
+        private File getFile() {
+            return new File(String.format("%s/etc%s", ddfHome, getResourcePath()));
+        }
+
+        private InputStream getResourceAsStream() {
+            return getClass().getResourceAsStream(getResourcePath());
+        }
+    }
+
+    /**
+     * Class that provides utility and assertion methods for a Managed Service Factory Felix
+     * configuration file.
+     */
+    private static class ManagedServiceFactoryConfigFile extends ManagedServiceConfigFile {
+
+        private final String factoryPid;
+
+        public ManagedServiceFactoryConfigFile(String pid) {
+            super(pid);
+            this.factoryPid = pid.substring(0, pid.lastIndexOf('.'));
+        }
+
+        protected void assertPropertiesMatch(Dictionary<String, Object> actualProperties,
+                Dictionary<String, Object> expectedProperties) throws IOException {
+            assertThat("Configuration properties do not match for PID " + pid, actualProperties,
+                    new ManagedServiceFactoryConfigurationPropertiesEqualTo(factoryPid,
+                            expectedProperties));
+        }
+
+        @Override
+        public void addConfigurationFileAndWait(ConfigurationAdmin configAdmin) throws IOException {
+            addConfigurationFile();
+
+            final Dictionary<String, Object> expectedProperties = getProperties();
+
+            expect("Waiting for Configuration expectedProperties for PID " + pid + " to be set")
+                    .within(20, SECONDS)
+                    .until(new GetConfigurationProperties(configAdmin, "id", pid),
+                            new ManagedServiceFactoryConfigurationPropertiesEqualTo(factoryPid,
+                                    expectedProperties));
+        }
+    }
+
+    /**
+     * Hamcrest {@link org.hamcrest.Matcher} class for Managed Service Factory properties. Compares
+     * all the attributes except for the {@code service.pid} one.
+     */
+    private static class ManagedServiceFactoryConfigurationPropertiesEqualTo
+            extends ConfigurationPropertiesEqualTo {
+        private String factoryPid;
+
+        public ManagedServiceFactoryConfigurationPropertiesEqualTo(String factoryPid,
+                Dictionary<String, Object> expectedProperties) {
+            super(expectedProperties);
+            this.factoryPid = factoryPid;
+        }
+
+        @Override
+        public boolean matches(Object object) {
+            if ((object == null) || !(object instanceof Dictionary)) {
+                return false;
+            }
+
+            @SuppressWarnings("unchecked")
+            Dictionary<String, Object> properties = (Dictionary<String, Object>) object;
+
+            if (properties.get("service.pid") == null) {
+                return false;
+            }
+
+            if (!((String) properties.get("service.pid")).startsWith(factoryPid)) {
+                return false;
+            }
+
+            properties.remove("service.pid");
+
+            return super.matches(properties);
+        }
+    }
+
+    private static ManagedServiceConfigFile managedServiceStartupConfig = new ManagedServiceConfigFile(
+            "ddf.catalog.test.TestPlatform.startup");
+
+    private static ManagedServiceConfigFile managedServiceNewConfig = new ManagedServiceConfigFile(
+            "ddf.catalog.test.TestPlatform.new");
+
+    private static ManagedServiceConfigFile managedServiceFactoryStartupConfig = new ManagedServiceFactoryConfigFile(
+            "ddf.catalog.test.TestPlatform.msf.1");
+
+    private static ManagedServiceConfigFile managedServiceFactoryNewConfig = new ManagedServiceFactoryConfigFile(
+            "ddf.catalog.test.TestPlatform.msf.2");
+
+    private static ManagedServiceConfigFile configWithNoPid = new ManagedServiceConfigFile(
+            "ddf.catalog.test.TestPlatform.nopid");
+
+    private static ManagedServiceConfigFile invalidConfig = new ManagedServiceConfigFile(
+            "ddf.catalog.test.TestPlatform.invalid");
+
+    @BeforeExam
+    @SuppressWarnings("unchecked")
+    public void beforeExam() throws Exception {
+        getAdminConfig().setLogLevels();
+        getServiceManager().waitForAllBundles();
+    }
+
+    @Override
+    protected Option[] configureCustom() {
+        try {
+            return options(installStartupFile(getClass().getResourceAsStream(
+                                    managedServiceStartupConfig.getResourcePath()),
+                            "/etc" + managedServiceStartupConfig.getResourcePath()),
+                    installStartupFile(getClass().getResourceAsStream(
+                                    managedServiceFactoryStartupConfig.getResourcePath()),
+                            "/etc" + managedServiceFactoryStartupConfig.getResourcePath()));
+        } catch (Exception e) {
+            LOGGER.error("Could not copy config files {} and {} to /etc directory",
+                    managedServiceStartupConfig.getResourcePath(),
+                    managedServiceFactoryStartupConfig.getResourcePath());
+            return null;
+        }
+    }
+
+    @Test
+    public void testStartUpWithExistingManagedServiceConfigurationFile() throws Exception {
+        managedServiceStartupConfig.assertConfigurationPropertiesSet(configAdmin);
+        managedServiceStartupConfig.assertFileMovedToProcessedDirectory();
+    }
+
+    @Test
+    public void testCreateNewManagedServiceConfigurationFile() throws IOException {
+        managedServiceNewConfig.addConfigurationFileAndWait(configAdmin);
+        managedServiceNewConfig.assertFileMovedToProcessedDirectory();
+    }
+
+    @Test
+    public void testStartUpWithExistingManagedServiceFactoryConfigurationFile() throws Exception {
+        managedServiceFactoryStartupConfig.assertConfigurationPropertiesSet(configAdmin);
+        managedServiceFactoryStartupConfig.assertFileMovedToProcessedDirectory();
+    }
+
+    @Test
+    public void testCreateNewManagedServiceFactoryConfigurationFile() throws IOException {
+        managedServiceFactoryNewConfig.addConfigurationFileAndWait(configAdmin);
+        managedServiceFactoryNewConfig.assertFileMovedToProcessedDirectory();
+    }
+
+    @Test
+    public void testStartUpWithInvalidFile() {
+        // TODO - Make sure things still come up when one of the startup files is invalid.
+    }
+
+    @Test
+    public void testConfigurationFileWithNoFactoryOrServicePid() throws IOException {
+        configWithNoPid.addConfigurationFile();
+        configWithNoPid.assertFileMovedToFailedDirectory();
+    }
+
+    @Test
+    public void testConfigurationFileWithInvalidFormat() throws IOException {
+        invalidConfig.addConfigurationFile();
+        invalidConfig.assertFileMovedToFailedDirectory();
+    }
+}
