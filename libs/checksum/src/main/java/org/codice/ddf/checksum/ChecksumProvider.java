974965f0aeef37b4d8f82d366969c7b0b4b2154c
Eric Irwin <Eric.Irwin1124@gmail.com>
2015-10-20 14:09:25 -0700

    Implemented CheckSumProvider implementations for MD5 and CRC32 that can be used by a CheckSumPlugin. CheckSumPlugin is an implementation of a newly created PreCreateStoragePlugin

---
+++ libs/checksum/src/main/java/org/codice/ddf/checksum/ChecksumProvider.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.codice.ddf.checksum;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.NoSuchAlgorithmException;
+
+public interface ChecksumProvider {
+
+    /**
+     * Calculates checksum from {@link InputStream}
+     * @param {@link InputStream} to process
+     * @return calculated checksum value
+     */
+    String calculateChecksum(InputStream inputStream) throws IOException, NoSuchAlgorithmException;
+
+    /**
+     * Returns identifier for check sum algorithm
+     * @return checksum algorithm identifier
+     */
+    String getChecksumAlgorithm();
+}
