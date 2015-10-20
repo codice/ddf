b83cca36a5214d5cbb335e9e9638c753c04e62c7
Andrew Fiedler <andrewkfiedler@gmail.com>
2015-10-19 17:15:59 -0700

    DDF-1555 Adds integration testing for the listeners
     - TestPlatform provides integration tests for the listeners.
     - Updates AdminConfig to use getConfig(XX,null) instead of getConfig(XX)
     - Added a few different config files to the resources folder for the integration tests.

--- distribution/test/itests/test-itests-catalog/src/test/java/ddf/catalog/test/TestPlatform.java
+++ distribution/test/itests/test-itests-catalog/src/test/java/ddf/catalog/test/TestPlatform.java
@@ -13,7 +13,16 @@
  */
 package ddf.catalog.test;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.replaceConfigurationFile;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Files;
 import java.util.Dictionary;
 import java.util.concurrent.TimeUnit;
 
@@ -24,6 +33,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
@@ -40,11 +50,24 @@ public class TestPlatform extends AbstractIntegrationTest {
 
     private static final XLogger LOGGER = new XLogger(LoggerFactory.getLogger(TestPlatform.class));
 
-    private static final String testConfig = "/itest.platform.config";
+    private static final String TRACKED_PID = "ddf.catalog.test.TestPlatform";
 
-    private static final String testConfig2 = "/itest.platform.modified.config";
+    private static final String UNTRACKED_PID = TRACKED_PID + ".untracked";
 
-    private static final long POLLER_WAIT_TIME = TimeUnit.SECONDS.toMillis(20);
+    private static final String TRACKED_CONFIG_FILE = "/" + TRACKED_PID + ".config";
+
+    private static final String STARTUP_CONFIG_FILE = "/" + TRACKED_PID + ".startup.config";
+
+    private static final String MODIFIED_TRACKED_CONFIG_FILE =
+            "/" + TRACKED_PID + ".modified.config";
+
+    private static final String UNTRACKED_CONFIG_FILE = "/" + UNTRACKED_PID + ".config";
+
+    /* This value is derived from SensitivityWatchEventModifier class.
+       The default sensitivity for the watch service is Medium, so we should
+       use the medium wait time (10 seconds) + 2 second buffer which is 12 seconds total.
+     */
+    private static final long POLLER_WAIT_TIME = TimeUnit.SECONDS.toMillis(12);
 
     private static final ConfigurationPropertiesComparator CONFIGURATION_PROPERTIES_COMPARATOR = new ConfigurationPropertiesComparator();
 
@@ -52,57 +75,108 @@ public class TestPlatform extends AbstractIntegrationTest {
 
     private static Dictionary<String, Object> modifiedConfigProperties;
 
+    private static Dictionary<String, Object> untrackedConfigProperties;
+
+    private static Dictionary<String, Object> startupConfigProperties;
+
     @BeforeExam
     @SuppressWarnings("unchecked")
     public void beforeExam() throws Exception {
-        configProperties = ConfigurationHandler.read(getClass().getResourceAsStream(testConfig));
+        configProperties = ConfigurationHandler
+                .read(getClass().getResourceAsStream(TRACKED_CONFIG_FILE));
         modifiedConfigProperties = ConfigurationHandler
-                .read(getClass().getResourceAsStream(testConfig2));
+                .read(getClass().getResourceAsStream(MODIFIED_TRACKED_CONFIG_FILE));
+        untrackedConfigProperties = ConfigurationHandler
+                .read(getClass().getResourceAsStream(UNTRACKED_CONFIG_FILE));
+        startupConfigProperties = ConfigurationHandler
+                .read(getClass().getResourceAsStream(STARTUP_CONFIG_FILE));
         getAdminConfig().setLogLevels();
         getServiceManager().waitForAllBundles();
     }
 
+    @Before
+    public void setup() throws Exception {
+        copyConfigToFileSystem();
+
+        getAdminConfig().waitForConfiguration(TRACKED_PID,
+                new ConfigurationPropertiesMatch(configProperties), POLLER_WAIT_TIME);
+    }
+
+    @Override
+    protected Option[] configureCustom() {
+        String fileName = String.format("/%s.startup.config", TRACKED_PID);
+
+        try {
+            File tempFile = Files.createTempFile(TRACKED_PID, ".config").toFile();
+            tempFile.deleteOnExit();
+            FileUtils.copyInputStreamToFile(getClass().getResourceAsStream(fileName), tempFile);
+            return options(replaceConfigurationFile("/etc" + fileName, tempFile));
+        } catch (Exception e) {
+            LOGGER.error("Could not copy file [{}]", fileName);
+            return null;
+        }
+
+    }
+
     @Test
     public void testStartUpWithExistingConfigFile() throws Exception {
-        // TODO
+        assertThat("Did not startup with config",
+                configAdmin.getConfiguration(TRACKED_PID + ".startup", null), is(notNullValue()));
+        assertThat("Config properties did not match", CONFIGURATION_PROPERTIES_COMPARATOR
+                .equal(configAdmin.getConfiguration(TRACKED_PID + ".startup", null).getProperties(),
+                        startupConfigProperties), is(true));
     }
 
     @Test
     public void testModifyConfigFileFromAdmin() throws Exception {
-
+        LOGGER.debug("Starting test");
+        getAdminConfig().getConfiguration(TRACKED_PID, null).update(modifiedConfigProperties);
+        waitForConfigurationFilePoller();
+        LOGGER.debug("Checking file properties");
+        Dictionary<String, Object> fileProperties = ConfigurationHandler.read(new FileInputStream(
+                String.format("%s/etc%s", DDF_HOME, TRACKED_CONFIG_FILE)));
+        assertThat("File does not match modification from config admin",
+                CONFIGURATION_PROPERTIES_COMPARATOR.equal(fileProperties, modifiedConfigProperties),
+                is(true));
     }
 
     @Test
     public void testDeleteConfigFileFromAdmin() throws Exception {
-
+        getAdminConfig().getConfiguration(TRACKED_PID, null).delete();
+        waitForConfigurationFilePoller();
+        assertThat("Files was not deleted ",
+                new File(String.format("%s/etc%s", DDF_HOME, TRACKED_CONFIG_FILE)).exists(),
+                is(false));
     }
 
     @Test
     public void testCreateUntrackedConfigFileFromAdmin() throws Exception {
-
+        getAdminConfig().getConfiguration(UNTRACKED_PID, null).update(untrackedConfigProperties);
+        waitForConfigurationFilePoller();
+        assertThat("File was created for untracked pid",
+                new File(String.format("%s/etc%s", DDF_HOME, UNTRACKED_CONFIG_FILE)).exists(),
+                is(false));
     }
 
     @Test
     public void testRecreationOfTrackedFile() throws Exception {
         deleteConfigFileFromFileSystem();
 
-        getAdminConfig().waitForConfiguration("itest.platform", new ConfigurationDeleted(),
-                POLLER_WAIT_TIME);
+        getAdminConfig()
+                .waitForConfiguration(TRACKED_PID, new ConfigurationDeleted(), POLLER_WAIT_TIME);
         LOGGER.debug("Configuration deleted!");
-        /*The next line (the one updating properties) will cause the test to fail.
-          For some reason it appears to generate a CM_LOCATION_CHANGED event, which
-          the ConfigurationAdminListener fails on.  I tried to add a case for this event and debug
-          into the issue.  It looks like there is a config admin with the proper itest.platform config,
-          and one without it. */
-        //getAdminConfig().getConfiguration("itest.platform").update(configProperties);
-        /*long timeoutLimit = System.currentTimeMillis() + POLLER_WAIT_TIME;
-        while (!(new File(DDF_HOME + "/etc/itest.platform.config").exists())) {
+        getAdminConfig().getConfiguration(TRACKED_PID, null).update(configProperties);
+        long timeoutLimit = System.currentTimeMillis() + POLLER_WAIT_TIME;
+        while (!(new File(String.format("%s/etc%s", DDF_HOME, TRACKED_CONFIG_FILE)).exists())) {
             Thread.sleep(1000);
             if (System.currentTimeMillis() > timeoutLimit) {
                 fail(String.format("Configuration file wasn't recreated within %d seconds.",
                         TimeUnit.MILLISECONDS.toSeconds(POLLER_WAIT_TIME)));
             }
-        }*/
+        }
+        LOGGER.debug("Wait to Delete!");
+        waitForConfigurationFilePoller();
+        LOGGER.debug("Start the Delete!");
     }
 
     @Test
@@ -110,7 +184,7 @@ public class TestPlatform extends AbstractIntegrationTest {
 
         overwriteExistingConfigOnFileSystem();
 
-        getAdminConfig().waitForConfiguration("itest.platform",
+        getAdminConfig().waitForConfiguration(TRACKED_PID,
                 new ConfigurationPropertiesMatch(modifiedConfigProperties), POLLER_WAIT_TIME);
     }
 
@@ -124,35 +198,32 @@ public class TestPlatform extends AbstractIntegrationTest {
         // this is accomplished by setup
     }
 
-    public void overwriteExistingConfigOnFileSystem() throws Exception {
-        FileUtils.copyInputStreamToFile(getClass().getResourceAsStream(testConfig2),
-                new File(DDF_HOME + "/etc/itest.platform.config"));
-    }
-
-    public void copyConfigToFileSystem() throws Exception {
-        FileUtils.copyInputStreamToFile(getClass().getResourceAsStream(testConfig),
-                new File(DDF_HOME + "/etc/itest.platform.config"));
-    }
-
-    public void deleteConfigFileFromFileSystem() throws Exception {
-        (new File(DDF_HOME + "/etc/itest.platform.config")).delete();
-    }
-
-    @Before
-    public void setup() throws Exception {
-        copyConfigToFileSystem();
-
-        getAdminConfig().waitForConfiguration("itest.platform",
-                new ConfigurationPropertiesMatch(configProperties), POLLER_WAIT_TIME);
-    }
-
     @After
     public void tearDown() throws Exception {
         deleteConfigFileFromFileSystem();
 
-        getAdminConfig().waitForConfiguration("itest.platform", new ConfigurationDeleted(),
-                POLLER_WAIT_TIME);
-        getAdminConfig().getConfiguration("itest.platform").delete();
+        getAdminConfig()
+                .waitForConfiguration(TRACKED_PID, new ConfigurationDeleted(), POLLER_WAIT_TIME);
+        getAdminConfig().getConfiguration(TRACKED_PID, null).delete();
+    }
+
+    public void overwriteExistingConfigOnFileSystem() throws Exception {
+        FileUtils
+                .copyInputStreamToFile(getClass().getResourceAsStream(MODIFIED_TRACKED_CONFIG_FILE),
+                        new File(String.format("%s/etc%s", DDF_HOME, TRACKED_CONFIG_FILE)));
+    }
+
+    public void copyConfigToFileSystem() throws Exception {
+        FileUtils.copyInputStreamToFile(getClass().getResourceAsStream(TRACKED_CONFIG_FILE),
+                new File(String.format("%s/etc%s", DDF_HOME, TRACKED_CONFIG_FILE)));
+    }
+
+    public void deleteConfigFileFromFileSystem() throws Exception {
+        new File(String.format("%s/etc%s", DDF_HOME, TRACKED_CONFIG_FILE)).delete();
+    }
+
+    private void waitForConfigurationFilePoller() throws Exception {
+        Thread.sleep(POLLER_WAIT_TIME);
     }
 
     private static class ConfigurationDeleted implements ConfigurationPredicate {
