95f6e40b930d2307be3209249cb8ae5e197b60c4
Armand Roelens <armand.roelens@connexta.com>
2015-10-26 15:29:42 -0700

    DDF-1597 Authenticating against one realm allows access to only that realm
    DDF-1579 Added unit tests for coverage of SecurityTokenHolder
    
    DDF-1597 Fix realm validation in UsernameTokenValidator and UPBSTValidator
    
    DDF-1597 Remove caching in UsernameTokenValidator to fix integration tests
    
    DDF-1597 Remove all references of usernameTokenRealmCodec

--- platform/security/sts/security-sts-upbstvalidator/src/test/java/org/codice/ddf/security/validator/uname/UPBSTValidatorTest.java
+++ platform/security/sts/security-sts-upbstvalidator/src/test/java/org/codice/ddf/security/validator/uname/UPBSTValidatorTest.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (c) Codice Foundation
- * <p/>
+ * <p>
  * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or any later version.
- * <p/>
+ * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
@@ -135,7 +135,7 @@ public class UPBSTValidatorTest {
             @Override
             public SecurityToken getToken(String identifier) {
                 SecurityToken securityToken = new SecurityToken();
-                securityToken.setTokenHash(-1432225335);
+                securityToken.setTokenHash(584149325);
                 return securityToken;
             }
         });
--- platform/security/sts/security-sts-usernametokenvalidator/src/test/java/org/codice/ddf/security/validator/username/TestUsernameTokenValidator.java
+++ platform/security/sts/security-sts-usernametokenvalidator/src/test/java/org/codice/ddf/security/validator/username/TestUsernameTokenValidator.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (c) Codice Foundation
- * <p/>
+ * <p>
  * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or any later version.
- * <p/>
+ * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
@@ -132,6 +132,8 @@ public class TestUsernameTokenValidator {
         doCallRealMethod().when(receivedToken).getState();
         when(receivedToken.isUsernameToken()).thenReturn(true);
         when(tokenValidatorParameters.getToken()).thenReturn(receivedToken);
+        //when(usernameTokenRealmCodec.getRealmFromToken(any(UsernameToken.class)))
+        //.thenReturn("myrealm");
 
         Set<Class<?>> classes = new HashSet<>();
         classes.add(ObjectFactory.class);
