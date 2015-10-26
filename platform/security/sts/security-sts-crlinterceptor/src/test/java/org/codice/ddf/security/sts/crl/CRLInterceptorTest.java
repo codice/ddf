0b556c39fe2b5e71baf521a33e2e50ce0c68c760
Garrett Freibott <garrett.freibott@connexta.com>
2015-10-26 14:29:47 -0700

    DDF-1573: Generalize CrlChecker and Refactor CrlInterceptor
     - Modified the CrlChecker to instead take only certs as a parameter and
       return true or false. This was done so that the CrlInterceptor could
       make use of the CrlChecker.
     - Error throwing logic has been moved out of the CrlChecker and into the
       AbstractPKIHandler. This improved readability for both the CrlChacker
       and the AbstractPKIHandler
     - Refactored CRLInterceptor to call the CrlChecker. Unit tests were
       rewreitten to reflect these changes.
     - Removed the Admin UI Configuration for the STS CRL
     - AbstractPKIHandler unit tests were modified to reflect the changes
       made.
    
    Now that the STS CRL has been modified to use the CrlChecker class, the
    Admin UI configuration is not how the CRL is set or activated. It follows
    the same process for setting up as the AbstractPKIHandler's CRL, using the
    CRL file listed in the encryption.properties file.

--- platform/security/sts/security-sts-crlinterceptor/src/test/java/org/codice/ddf/security/sts/crl/CRLInterceptorTest.java
+++ platform/security/sts/security-sts-crlinterceptor/src/test/java/org/codice/ddf/security/sts/crl/CRLInterceptorTest.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (c) Codice Foundation
- * <p/>
+ * <p>
  * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or any later version.
- * <p/>
+ * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
@@ -13,23 +13,23 @@
  */
 package org.codice.ddf.security.sts.crl;
 
-import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.cxf.interceptor.security.AccessDeniedException;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.transport.http.AbstractHTTPDestination;
+import org.jasypt.contrib.org.apache.commons.codec_1_3.binary.Base64;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Tests the CRL Interceptor by using use cases where a certificate gets revoked
@@ -39,152 +39,58 @@ import org.slf4j.LoggerFactory;
  */
 public class CRLInterceptorTest {
 
-    private static final String REVOKED_CERT = "/certs/ia-bad.crt";
-
-    private static final String VALID_CERT = "/certs/ia.crt";
-
-    private static final String CRL_LOCATION = CRLInterceptorTest.class
-            .getResource("/certs/root.crl").getFile();
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(CRLInterceptorTest.class);
-
-    // START Error cases (calling interceptor should throw AccessDeniedException)
-
     /**
-     * Verifies that a certificate designated as revoked in the CRL is properly
-     * caught in the interceptor.
-     *
-     */
-    @Test(expected = AccessDeniedException.class)
-    public void testDenyCertificate() {
-        Message message = null;
-        try {
-            message = createMockMessageWithCert(REVOKED_CERT);
-        } catch (CertificateException ce) {
-            LOGGER.error(
-                    "Could not create certificate objects from files located in test, failing test.",
-                    ce);
-            fail();
-        }
-
-        callNewInterceptor(CRL_LOCATION, true, message);
-
-    }
-
-    /**
-     * Tests that the interceptor will deny access if it is enabled and there is
-     * no CRL file set.
-     */
-    @Test(expected = AccessDeniedException.class)
-    public void testDenyNoCRL() {
-        Message message = null;
-        try {
-            message = createMockMessageWithCert(VALID_CERT);
-        } catch (CertificateException ce) {
-            LOGGER.error(
-                    "Could not create certificate objects from files located in test, failing test.",
-                    ce);
-            fail();
-        }
-
-        // create and call interceptor manually (without setting the CRL)
-        CRLInterceptor interceptor = new CRLInterceptor();
-        interceptor.setIsEnabled(true);
-        interceptor.handleMessage(message);
-    }
-
-    /**
-     * Tests that the interceptor will deny access if it is enabled and there is
-     * a bad CRL file set.
-     */
-    @Test(expected = AccessDeniedException.class)
-    public void testDenyBadCRL() {
-        Message message = null;
-        try {
-            message = createMockMessageWithCert(VALID_CERT);
-        } catch (CertificateException ce) {
-            LOGGER.error(
-                    "Could not create certificate objects from files located in test, failing test.",
-                    ce);
-            fail();
-        }
-
-        callNewInterceptor(VALID_CERT, true, message);
-    }
-
-    // END Error Cases
-
-    // START Success Cases
-
-    /**
-     * Tests that a certificate not designated as revoked in the CRL is properly
-     * let through the interceptor.
-     *
+     * Tests that the interceptor will NOT throw an error if CRL is disabled
      */
     @Test
-    public void testPassCertificate() {
-        Message message = null;
-        try {
-            message = createMockMessageWithCert(VALID_CERT);
-        } catch (CertificateException ce) {
-            LOGGER.error(
-                    "Could not create certificate objects from files located in test, failing test.",
-                    ce);
-            fail();
-        }
+    public void testRevokedCertWithCrlDisabledPasses() throws CertificateException {
+        Message message = createMockMessageWithCert(getRevokedCert());
+        CRLInterceptor crlInterceptor = new CRLInterceptor();
+        crlInterceptor.crlChecker.setCrlLocation(null);
 
-        callNewInterceptor(CRL_LOCATION, true, message);
+        crlInterceptor.handleMessage(message);
     }
 
     /**
-     * Tests that the interceptor does not error out if no certificate is in the
-     * incoming message.
-     *
+     * Tests that the interceptor will NOT throw an error if the cert is not in the CRL and it is enabled
      */
     @Test
-    public void testPassNoCertificate() {
+    public void testUnrevokedCertWithCrlEnabledPasses() throws CertificateException {
+        Message message = createMockMessageWithCert(getUnrevokedCert());
+        CRLInterceptor crlInterceptor = getCrlInterceptorWithConfiguredCrl(
+                "encryption-crl-revoked.properties");
 
-        Message message = mock(Message.class);
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        when(message.get(AbstractHTTPDestination.HTTP_REQUEST)).thenReturn(request);
-
-        callNewInterceptor(CRL_LOCATION, true, message);
-
+        crlInterceptor.handleMessage(message);
     }
 
     /**
-     * Tests that the interceptor will allow access if it is disabled, even if
-     * the certificate is bad or the crl file location is incorrect.
+     * Tests that the interceptor will throw an error if the cert is in the CRL and it is enabled
      */
-    @Test
-    public void testPassBadFilesDisabledCheck() {
-        Message message = null;
-        try {
-            message = createMockMessageWithCert(REVOKED_CERT);
-        } catch (CertificateException ce) {
-            LOGGER.error(
-                    "Could not create certificate objects from files located in test, failing test.",
-                    ce);
-            fail();
-        }
+    @Test(expected = AccessDeniedException.class)
+    public void testRevokedCertWithCrlEnabledFails() throws CertificateException {
+        Message message = createMockMessageWithCert(getRevokedCert());
+        CRLInterceptor crlInterceptor = getCrlInterceptorWithConfiguredCrl(
+                "encryption-crl-revoked.properties");
 
-        // bad certificate
-        callNewInterceptor(CRL_LOCATION, false, message);
-
-        // bad crl file (points to cert instead of crl)
-        callNewInterceptor(VALID_CERT, false, message);
+        crlInterceptor.handleMessage(message);
     }
 
-    // END Success Cases
-
-    private Message createMockMessageWithCert(String certificateFile) throws CertificateException {
+    /**
+     * Creates a mock message with a cert attached
+     * @param certificateString The string of the certificate to attach
+     * @return A message object to be passed to the CRLInterceptor for testing
+     * @throws CertificateException
+     */
+    private Message createMockMessageWithCert(String certificateString)
+            throws CertificateException {
         // create mock objects
         Message message = mock(Message.class);
         HttpServletRequest request = mock(HttpServletRequest.class);
         when(message.get(AbstractHTTPDestination.HTTP_REQUEST)).thenReturn(request);
 
         // add in certificate
-        InputStream stream = getClass().getResourceAsStream(certificateFile);
+        InputStream stream = new ByteArrayInputStream(
+                Base64.decodeBase64(certificateString.getBytes()));
         CertificateFactory factory = CertificateFactory.getInstance("X.509");
         X509Certificate cert = (X509Certificate) factory.generateCertificate(stream);
         X509Certificate[] certs = new X509Certificate[] {cert};
@@ -193,12 +99,67 @@ public class CRLInterceptorTest {
         return message;
     }
 
-    private void callNewInterceptor(String crlLocation, boolean isEnabled, Message message) {
-        // create and call interceptor
-        CRLInterceptor interceptor = new CRLInterceptor();
-        interceptor.setCrlLocation(crlLocation);
-        interceptor.setIsEnabled(isEnabled);
-        interceptor.handleMessage(message);
+    /**
+     * Creates a CRLInterceptor with the CrlChecker inside of it pointing to the specified file
+     * @param encryptionProperties the name of the encryption.properties file that points to the CRL file
+     * @return a configured CRLInterceptor
+     */
+    private CRLInterceptor getCrlInterceptorWithConfiguredCrl(String encryptionProperties) {
+        CRLInterceptor crlInterceptor = new CRLInterceptor();
+        Properties prop = crlInterceptor.crlChecker.loadProperties(encryptionProperties);
+        String crlRelativePath = "/" + prop.getProperty(crlInterceptor.crlChecker.CRL_PROPERTY_KEY);
+        String crlAbsolutePath = CRLInterceptorTest.class.getResource(crlRelativePath).getPath();
+        crlInterceptor.crlChecker.setCrlLocation(crlAbsolutePath);
+
+        return crlInterceptor;
     }
 
+    /**
+     * Returns a string of a cert that is listed in the crl-revoked.pem CRL
+     * @return Cert String
+     */
+    private String getRevokedCert() {
+        String certificateString =
+                "MIIDEzCCAnygAwIBAgIJAIzc4FYrIp9mMA0GCSqGSIb3DQEBBQUAMHcxCzAJBgNV\n"
+                        + "BAYTAlVTMQswCQYDVQQIDAJBWjEMMAoGA1UECgwDRERGMQwwCgYDVQQLDANEZXYx\n"
+                        + "GTAXBgNVBAMMEERERiBEZW1vIFJvb3QgQ0ExJDAiBgkqhkiG9w0BCQEWFWRkZnJv\n"
+                        + "b3RjYUBleGFtcGxlLm9yZzAeFw0xNDEyMTAyMTU4MThaFw0xNTEyMTAyMTU4MTha\n"
+                        + "MIGDMQswCQYDVQQGEwJVUzELMAkGA1UECAwCQVoxETAPBgNVBAcMCEdvb2R5ZWFy\n"
+                        + "MQwwCgYDVQQKDANEREYxDDAKBgNVBAsMA0RldjESMBAGA1UEAwwJbG9jYWxob3N0\n"
+                        + "MSQwIgYJKoZIhvcNAQkBFhVsb2NhbGhvc3RAZXhhbXBsZS5vcmcwgZ8wDQYJKoZI\n"
+                        + "hvcNAQEBBQADgY0AMIGJAoGBAMeCyNZbCTZphHQfB5g8FrgBq1RYzV7ikVw/pVGk\n"
+                        + "z8gx3l3A99s8WtA4mRAeb6n0vTR9yNBOekW4nYOiEOq//YTi/frI1kz0QbEH1s2c\n"
+                        + "I5nFButabD3PYGxUSuapbc+AS7+Pklr0TDI4MRzPPkkTp4wlORQ/a6CfVsNr/mVg\n"
+                        + "L2CfAgMBAAGjgZkwgZYwCQYDVR0TBAIwADAnBglghkgBhvhCAQ0EGhYYRk9SIFRF\n"
+                        + "U1RJTkcgUFVSUE9TRSBPTkxZMB0GA1UdDgQWBBSA95QIMyBAHRsd0R4s7C3BreFr\n"
+                        + "sDAfBgNVHSMEGDAWgBThVMeX3wrCv6lfeF47CyvkSBe9xjAgBgNVHREEGTAXgRVs\n"
+                        + "b2NhbGhvc3RAZXhhbXBsZS5vcmcwDQYJKoZIhvcNAQEFBQADgYEAtRUp7fAxU/E6\n"
+                        + "JD2Kj/+CTWqu8Elx13S0TxoIqv3gMoBW0ehyzEKjJi0bb1gUxO7n1SmOESp5sE3j\n"
+                        + "GTnh0GtYV0D219z/09n90cd/imAEhknJlayyd0SjpnaL9JUd8uYxJexy8TJ2sMhs\n"
+                        + "GAZ6EMTZCfT9m07XduxjsmDz0hlSGV0";
+        return certificateString;
+    }
+
+    /**
+     * Returns a string of a cert that is NOT listed in the crl-revoked.pem CRL
+     * @return Cert String
+     */
+    private String getUnrevokedCert() {
+        String certificateString =
+                "MIIC5DCCAk2gAwIBAgIJAKj7ROPHjo1yMA0GCSqGSIb3DQEBCwUAMIGKMQswCQYDVQQGEwJVUzEQ"
+                        + "MA4GA1UECAwHQXJpem9uYTERMA8GA1UEBwwIR29vZHllYXIxGDAWBgNVBAoMD0xvY2toZWVkIE1h"
+                        + "cnRpbjENMAsGA1UECwwESTRDRTEPMA0GA1UEAwwGY2xpZW50MRwwGgYJKoZIhvcNAQkBFg1pNGNl"
+                        + "QGxtY28uY29tMB4XDTEyMDYyMDE5NDMwOVoXDTIyMDYxODE5NDMwOVowgYoxCzAJBgNVBAYTAlVT"
+                        + "MRAwDgYDVQQIDAdBcml6b25hMREwDwYDVQQHDAhHb29keWVhcjEYMBYGA1UECgwPTG9ja2hlZWQg"
+                        + "TWFydGluMQ0wCwYDVQQLDARJNENFMQ8wDQYDVQQDDAZjbGllbnQxHDAaBgkqhkiG9w0BCQEWDWk0"
+                        + "Y2VAbG1jby5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAIpHxCBLYE7xfDLcITS9SsPG"
+                        + "4Q04Z6S32/+TriGsRgpGTj/7GuMG7oJ98m6Ws5cTYl7nyunyHTkZuP7rBzy4esDIHheyx18EgdSJ"
+                        + "vvACgGVCnEmHndkf9bWUlAOfNaxW+vZwljUkRUVdkhPbPdPwOcMdKg/SsLSNjZfsQIjoWd4rAgMB"
+                        + "AAGjUDBOMB0GA1UdDgQWBBQx11VLtYXLvFGpFdHnhlNW9+lxBDAfBgNVHSMEGDAWgBQx11VLtYXL"
+                        + "vFGpFdHnhlNW9+lxBDAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4GBAHYs2OI0K6yVXzyS"
+                        + "sKcv2fmfw6XCICGTnyA7BOdAjYoqq6wD+33dHJUCFDqye7AWdcivuc7RWJt9jnlfJZKIm2BHcDTR"
+                        + "Hhk6CvjJ14Gf40WQdeMHoX8U8b0diq7Iy5Ravx+zRg7SdiyJUqFYjRh/O5tywXRT1+freI3bwAN0"
+                        + "L6tQ";
+        return certificateString;
+    }
 }
