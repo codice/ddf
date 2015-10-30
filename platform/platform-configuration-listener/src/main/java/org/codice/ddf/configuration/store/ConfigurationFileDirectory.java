993a6ca37b9819f2793ad4d5756ced166515026b
elessard <eric.lessard@connexta.com>
2015-10-30 16:39:56 -0700

    DDF-1555: Added unit tests for ConfigurationFileDirectory class.

--- platform/platform-configuration-listener/src/main/java/org/codice/ddf/configuration/store/ConfigurationFileDirectory.java
+++ platform/platform-configuration-listener/src/main/java/org/codice/ddf/configuration/store/ConfigurationFileDirectory.java
@@ -13,18 +13,17 @@
  */
 package org.codice.ddf.configuration.store;
 
-import static org.apache.commons.lang.Validate.isTrue;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static org.apache.commons.lang.Validate.notNull;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import org.slf4j.Logger;
@@ -33,21 +32,16 @@ import org.slf4j.LoggerFactory;
 /**
  * Class that provides utility methods to access the configuration files in the configuration
  * directory.
- * <p/>
- * Note: Since this class is meant to only be used by {@link FileHandlerImpl} and is package
- * private, it assumes that all input validation has already been performed.
  */
 public class ConfigurationFileDirectory implements ChangeListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigurationFileDirectory.class);
 
-    private Path configurationDirectory;
+    private DirectoryStream<Path> directoryStream;
 
     private Path processedDirectory;
 
     private Path failedDirectory;
-
-    private String fileExtension;
 
     private ConfigurationFileFactory configurationFileFactory;
 
@@ -56,44 +50,57 @@ public class ConfigurationFileDirectory implements ChangeListener {
     /**
      * Constructor.
      *
-     * @param configurationDirectory
-     *            directory that contains the configuration files. The directory must exist and be
-     *            readable and writable.
-     * @param fileExtension
-     *            configuration files extension with preceding period, e.g., ".cfg"
-     * @throws IllegalArgumentException
-     *             thrown if any of the arguments is invalid
+     * @param directoryStream          reference to a {@link DirectoryStream} that will return the
+     *                                 configuration files to read in upon startup
+     * @param processedDirectory       directory where configuration files will be moved to after
+     *                                 being successfully processed
+     * @param failedDirectory          directory where configuration files will be moved when they
+     *                                 failed to be processed
+     * @param configurationFileFactory factory object used to create {@link ConfigurationFile}
+     *                                 instances based on their type
+     * @param poller                   object to register with to know when new configuration
+     *                                 files have been created
+     * @throws IllegalArgumentException thrown if any of the arguments is invalid
      */
-    public ConfigurationFileDirectory(@NotNull Path configurationDirectory,
-            Path processedDirectory, Path failedDirectory, @NotNull @Min(2) String fileExtension,
-            ConfigurationFileFactory configurationFileFactory, ConfigurationFilesPoller poller) {
-        notNull(configurationDirectory, "Configuration directory cannot be null");
-        notNull(fileExtension, "File extension is required");
-        isTrue(fileExtension.length() >= 2, "Invalid file extension: ", fileExtension);
-        isTrue(configurationDirectory.toFile().exists()
-                && configurationDirectory.toFile().canRead()
-                && configurationDirectory.toFile().canWrite(),
-                "Directory does not exist or is not readable/writable: ", configurationDirectory);
+    public ConfigurationFileDirectory(@NotNull DirectoryStream<Path> directoryStream,
+            @NotNull Path processedDirectory, @NotNull Path failedDirectory,
+            @NotNull ConfigurationFileFactory configurationFileFactory,
+            @NotNull ConfigurationFilesPoller poller) {
 
-        this.configurationDirectory = configurationDirectory;
+        notNull(directoryStream, "Directory stream cannot be null");
+        notNull(processedDirectory, "Processed directory cannot be null");
+        notNull(failedDirectory, "Failed directory cannot be null");
+        notNull(configurationFileFactory, "Configuration file factory cannot be null");
+        notNull(poller, "Directory poller cannot be null");
+
+        this.directoryStream = directoryStream;
         this.processedDirectory = processedDirectory;
         this.failedDirectory = failedDirectory;
-        this.fileExtension = fileExtension;
         this.configurationFileFactory = configurationFileFactory;
         this.poller = poller;
     }
 
+    /**
+     * Loads all the configuration files located in the configuration directory specified in the
+     * constructor. Files that have been successfully processed will be moved to the
+     * {@code processedDirectory} and files that failed to be processed will be moved to the
+     * {@code failedDirectory}. It will also register for file creation events which will
+     * be handled by the {@link #notify(Path)} method.
+     *
+     * @throws IOException thrown if an I/O error occurs during initialization
+     */
     public void init() throws IOException {
-        createProcessedDirectory();
-        createFailedDirectory();
-        Collection<ConfigurationFile> configFiles = null;
-        configFiles = getConfigurationFiles();
+        createDirectory(processedDirectory);
+        createDirectory(failedDirectory);
+
+        Collection<ConfigurationFile> configFiles = getConfigurationFiles();
+
         for (ConfigurationFile configFile : configFiles) {
             try {
                 configFile.createConfig();
-                moveToProcessedDirectory(configFile);
+                moveConfigurationFile(configFile.getConfigFilePath(), processedDirectory);
             } catch (ConfigurationFileException e) {
-                moveToFailedDirectory(configFile.getConfigFilePath());
+                moveConfigurationFile(configFile.getConfigFilePath(), failedDirectory);
             }
         }
 
@@ -101,110 +108,76 @@ public class ConfigurationFileDirectory implements ChangeListener {
         poller.register(this);
     }
 
-    /**
-     * Gets the list of configuration file PIDs in the configuration directory.
-     *
-     * @return list of configuration file PIDs
-     * @throws IOException
-     * @throws FileNotFoundException
-     */
+    @Override
+    public void notify(Path file) {
+        try {
+            ConfigurationFile configFile = configurationFileFactory.createConfigurationFile(file);
+            configFile.createConfig();
+            moveConfigurationFile(file, processedDirectory);
+        } catch (ConfigurationFileException e) {
+            moveConfigurationFile(file, failedDirectory);
+        }
+    }
+
+    void moveFile(Path source, Path destination) throws IOException {
+        Files.move(source, destination.resolve(source.getFileName()), REPLACE_EXISTING);
+    }
+
+    private void moveConfigurationFile(Path source, Path destination) {
+        try {
+            moveFile(source, destination);
+        } catch (IOException e) {
+            LOGGER.warn(String.format("Failed to move %s to %s directory", source.toString(),
+                    destination.toString()), e);
+        }
+    }
+
     private Collection<ConfigurationFile> getConfigurationFiles() throws IOException {
         Collection<Path> files = listFiles();
         Collection<ConfigurationFile> configurationFiles = new ArrayList<>(files.size());
+
         for (Path file : files) {
-            ConfigurationFile configFile = null;
+            ConfigurationFile configFile;
+
             try {
                 configFile = configurationFileFactory.createConfigurationFile(file);
                 configurationFiles.add(configFile);
             } catch (ConfigurationFileException e) {
                 LOGGER.error(e.getMessage(), e);
-                moveToFailedDirectory(file);
+                moveConfigurationFile(file, failedDirectory);
             }
         }
+
         return configurationFiles;
     }
 
     private Collection<Path> listFiles() throws IOException {
         Collection<Path> fileNames = new ArrayList<>();
-        if (Files.isDirectory(configurationDirectory)) {
-            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(
-                    configurationDirectory, "*" + fileExtension)) {
-                for (Path path : directoryStream) {
-                    fileNames.add(path);
-                }
-            } catch (IOException e) {
-                throw new IOException("Unable to list files with extension " + fileExtension
-                        + " in directory " + configurationDirectory, e);
-            }
+
+        for (Path path : directoryStream) {
+            fileNames.add(path);
         }
+
         return fileNames;
     }
 
-    private void createProcessedDirectory() throws IOException {
-        if (!Files.exists(processedDirectory)) {
-            LOGGER.debug(
-                    "Creating directory [{}] to move configuration files to after processing.",
-                    processedDirectory.toString());
-            try {
-                Files.createDirectory(processedDirectory);
-            } catch (IOException e) {
-                throw new IOException("Unable to create failed directory [" + processedDirectory.toString() + "].", e);
+    private void createDirectory(Path path) throws IOException {
+        File file = path.toFile();
+
+        if (file.exists()) {
+            if (!file.isDirectory()) {
+                LOGGER.error(
+                        "Failed to create directory [{}]. A file with the same name already exists.",
+                        path.toString());
+                throw new IOException(String.format(
+                        "Failed to create %s. A file with the same name already exists.",
+                        path.toString()));
             }
-        }
-    }
-
-    private void createFailedDirectory() throws IOException {
-        if (!Files.exists(failedDirectory)) {
-            LOGGER.debug(
-                    "Creating directory [{}] to move configuration files to after processing failure.",
-                    failedDirectory.toString());
-            try {
-                Files.createDirectory(failedDirectory);
-            } catch (IOException e) {
-                throw new IOException("Unable to create failed directory [" + failedDirectory.toString() + "].", e);
+        } else {
+            if (!file.mkdir()) {
+                LOGGER.error("Failed to create directory [{}].", path.toString());
+                throw new IOException(String.format("Failed to create %s.", path.toString()));
             }
-        }
-    }
-
-    private void moveToFailedDirectory(Path file) {
-        Path source = null;
-        Path destination = null;
-        try {
-            source = Paths.get(file.toFile().getCanonicalPath());
-            destination = Paths.get(failedDirectory.resolve(file.getFileName()).toFile()
-                    .getCanonicalPath());
-            LOGGER.debug("Moving [{}] to [{}].", source, destination);
-            Files.move(source, destination);
-        } catch (IOException e) {
-            LOGGER.error("Unable to move file [{}] to the failed directory [{}].", source,
-                    destination, e);
-        }
-    }
-
-    protected void moveToProcessedDirectory(ConfigurationFile configFile) {
-        Path source = null;
-        Path destination = null;
-        try {
-            source = Paths.get(configFile.getConfigFilePath().toFile().getCanonicalPath());
-            destination = Paths.get(processedDirectory
-                    .resolve(configFile.getConfigFilePath().getFileName()).toFile()
-                    .getCanonicalPath());
-            Files.move(source, destination);
-        } catch (IOException e) {
-            LOGGER.error("Unable to move file [{}] to the processed directory [{}].", source,
-                    destination, e);
-        }
-    }
-
-    @Override
-    public void notify(Path file) {
-        ConfigurationFile configFile = null;
-        try {
-            configFile = configurationFileFactory.createConfigurationFile(file);
-            configFile.createConfig();
-            moveToProcessedDirectory(configFile);
-        } catch (ConfigurationFileException e) {
-            moveToFailedDirectory(file);
         }
     }
 }
