687f4f0773db8f3d3eac828a2056ce0713ab62be
Benjamin Deininger <bdeininger@Benjamins-MacBook-Pro.local>
2015-11-05 13:59:12 -0700

    DDF-1635 Refactored catalog-core-standardframework          Removed generic exception catches 	 Made logging more consistent 	 Refactored test cases to catch IllegalArgumentExceptions when appropriate

--- catalog/core/catalog-core-standardframework/src/main/java/ddf/catalog/cache/ResourceCacheInterface.java
+++ catalog/core/catalog-core-standardframework/src/main/java/ddf/catalog/cache/ResourceCacheInterface.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (c) Codice Foundation
- * <p/>
+ * <p>
  * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or any later version.
- * <p/>
+ * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
@@ -77,7 +77,7 @@ public interface ResourceCacheInterface {
      * This can help when multiple clients may be interacting with the same cache in order to
      * prevent multiple copies of the same resource being cached.
      *
-     * @param cacheKey
+     * @param reliableResource
      */
     void addPendingCacheEntry(ReliableResource reliableResource);
 
