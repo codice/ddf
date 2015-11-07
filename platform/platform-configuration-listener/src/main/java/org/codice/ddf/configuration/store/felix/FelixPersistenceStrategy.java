1384086574fdbfb9e8a2bf2f0b025618af6666d0
elessard <eric.lessard@connexta.com>
2015-11-06 17:31:48 -0700

    DDF-1636: Completed code that handles floats and doubles in .config files
    Added all the code to properly handle floats and doubles in .config files,
    as well as arrays and vectors. Also added unit tests and updated integration
    tests accordingly.
    
    Added delay after file creation event received in ConfigurationFilePoller
    class to make sure configuration file doesn't get read before it's
    completely written. That problem was observed when running the itests on
    Bamboo.

--- platform/platform-configuration-listener/src/main/java/org/codice/ddf/configuration/store/felix/FelixPersistenceStrategy.java
+++ platform/platform-configuration-listener/src/main/java/org/codice/ddf/configuration/store/felix/FelixPersistenceStrategy.java
@@ -1,39 +1,67 @@
 /**
  * Copyright (c) Codice Foundation
- * <p>
+ * <p/>
  * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or any later version.
- * <p>
+ * <p/>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
  * is distributed along with this program and can be found at
  * <http://www.gnu.org/licenses/lgpl.html>.
  */
+package org.codice.ddf.configuration.store.felix;
 
-package org.codice.ddf.configuration.store;
+import static org.apache.commons.lang.Validate.notNull;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.Dictionary;
 
 import org.apache.felix.cm.file.ConfigurationHandler;
+import org.codice.ddf.configuration.store.PersistenceStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Class that persists configuration properties using the Felix' file format.
  */
 public class FelixPersistenceStrategy implements PersistenceStrategy {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FelixPersistenceStrategy.class);
+
     @Override
     @SuppressWarnings("unchecked")
     public Dictionary<String, Object> read(InputStream inputStream) throws IOException {
-        return ConfigurationHandler.read(inputStream);
+        notNull(inputStream, "InputStream cannot be null");
+
+        final StringBuilder filteredOutput = new StringBuilder();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+        PropertyConverter propertyConverter = createPropertyConverter(filteredOutput);
+        reader.lines().forEach(propertyConverter);
+
+        LOGGER.debug("Calling ConfigurationHandler with {}", filteredOutput.toString());
+
+        return ConfigurationHandler
+                .read(new ByteArrayInputStream(filteredOutput.toString().getBytes()));
     }
 
     @Override
     public void write(OutputStream outputStream, Dictionary<String, Object> properties)
             throws IOException {
+        notNull(outputStream, "OutputStream cannot be null");
+        notNull(properties, "Properties cannot be null");
+
         ConfigurationHandler.write(outputStream, properties);
+    }
+
+    PropertyConverter createPropertyConverter(StringBuilder filteredOutput) {
+        return new PropertyConverter(filteredOutput);
     }
 }
