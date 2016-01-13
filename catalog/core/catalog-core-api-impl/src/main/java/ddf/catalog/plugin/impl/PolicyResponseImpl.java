e73ca6c289d37836cff829a76916abe9fc835135
Scott Tustison <scott.tustison@gmail.com>
2016-01-13 13:44:22 -0700

    DDF-1799 created new api for catalog security plugins

---
+++ catalog/core/catalog-core-api-impl/src/main/java/ddf/catalog/plugin/impl/PolicyResponseImpl.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p/>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package ddf.catalog.plugin.impl;
+
+import java.util.Map;
+import java.util.Set;
+
+import ddf.catalog.plugin.PolicyResponse;
+
+/**
+ * Generic implementation of {@link PolicyResponse}
+ */
+public class PolicyResponseImpl implements PolicyResponse {
+
+    private Map<String, Set<String>> operationPolicy;
+
+    private Map<String, Set<String>> itemPolicy;
+
+    public PolicyResponseImpl(Map<String, Set<String>> operationPolicy, Map<String, Set<String>> itemPolicy) {
+        this.operationPolicy = operationPolicy;
+        this.itemPolicy = itemPolicy;
+    }
+
+    @Override
+    public Map<String, Set<String>> operationPolicy() {
+        return operationPolicy;
+    }
+
+    @Override
+    public Map<String, Set<String>> itemPolicy() {
+        return itemPolicy;
+    }
+}
---
+++ catalog/core/catalog-core-api/src/main/java/ddf/catalog/plugin/PolicyResponse.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p/>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package ddf.catalog.plugin;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Response from {@link PolicyPlugin} containing policy information for {@link ddf.catalog.operation.Operation}
+ * and individual items, such as {@link ddf.catalog.data.Metacard} or {@link ddf.catalog.data.Result}
+ */
+public interface PolicyResponse {
+
+    /**
+     * Policy to be applied to the catalog operation being attempted.
+     *
+     * The {@link PolicyResponse} object contains 1 or more policy objects of the type: Map<String, Set<String>>.
+     * Where the key is some attribute that you wish to assert against a Subject and Set<String> would be the values
+     * associated with that key.
+     *
+     * @return Map containing policy information pertaining to this operation.
+     */
+    Map<String, Set<String>> operationPolicy();
+
+    /**
+     * Policy to be applied to item being worked on by the catalog.
+     *
+     * The {@link PolicyResponse} object contains 1 or more policy objects of the type: Map<String, Set<String>>.
+     * Where the key is some attribute that you wish to assert against a Subject and Set<String> would be the values
+     * associated with that key.
+     *
+     * @return Map containing policy information pertaining to this item.
+     */
+    Map<String, Set<String>> itemPolicy();
+}
