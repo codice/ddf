71696ad331bd1a17afe1c3e619ad94c4dbfe177a
Dan Figliola <daniel.figliola@connexta.com>
2016-01-26 16:57:39 -0700

    DDF-1827: Create Migratable Utility module

---
+++ platform/migration/platform-migratable-util/src/main/java/org/codice/ddf/migration/util/MigratableUtil.java
@@ -0,0 +1,275 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p/>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.codice.ddf.migration.util;
+
+import static org.apache.commons.lang.Validate.notEmpty;
+import static org.apache.commons.lang.Validate.notNull;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Properties;
+import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
+
+import javax.validation.constraints.NotNull;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.codice.ddf.migration.MigrationException;
+import org.codice.ddf.migration.MigrationWarning;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Utility class for services that implement {@link org.codice.ddf.migration.Migratable}.
+ * <p/>
+ * <p>
+ * <b>This code is experimental. While this interface is functional and tested, it may change or be
+ * removed in a future version of the library.</b>
+ * </p>
+ */
+public class MigratableUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MigratableUtil.class);
+
+    private static final String UNREAL_PATH_WARNING =
+            "The value for property [%s] is set to a path [%s] that could not coerced into a real path; "
+                    + "therefore, the file will not be included in the export.  "
+                    + "Check that the file exists on the system you're migrating to "
+                    + "or update the property value and export again.";
+
+    private static final String DDF_HOME_SYSTEM_PROP = "ddf.home";
+
+    private final Path ddfHome;
+
+    /**
+     * Constructor.
+     *
+     * @throws MigrationException if the utility class fails to initialize
+     */
+    public MigratableUtil() throws MigrationException {
+        this.ddfHome = Paths.get(getSystemProperty(DDF_HOME_SYSTEM_PROP));
+    }
+
+    /**
+     * Copies a file to a destination directory. The file to copy must be a relative path under
+     * {@code ddf.home}, and its path must not contain any symbolic link, otherwise the file will
+     * not be copied and a {@link MigrationWarning} will be returned.
+     *
+     * @param sourceFile      relative path to the file to copy
+     * @param exportDirectory root directory where the file will be copied. If the file'
+     *                        relative path included any directories, those will be re-created
+     *                        under this directory.
+     * @param warnings        any warnings generated during this operation (e.g., source file
+     *                        outside of {@code ddf.home}) will be added to this collection
+     * @throws MigrationException thrown if file system error prevents the file to be copied
+     */
+    public void copyFile(@NotNull Path sourceFile, @NotNull Path exportDirectory,
+            @NotNull Collection<MigrationWarning> warnings) throws MigrationException {
+        copy(sourceFile,
+                exportDirectory,
+                warnings,
+                () -> isSourceMigratable(sourceFile,
+                        () -> new PathMigrationWarning(sourceFile, "is absolute"),
+                        () -> new PathMigrationWarning(sourceFile, "contains a symbolic link"),
+                        () -> new PathMigrationWarning(sourceFile,
+                                String.format("is outside [%s]", ddfHome)),
+                        warnings));
+    }
+
+    /**
+     * Recursively copies a directory to a destination directory. The directory to
+     * copy must be a relative path under {@code ddf.home}, and its path must not contain any
+     * symbolic link, otherwise the directory will not be copied and a {@link MigrationWarning}
+     * will be returned.
+     *
+     * @param source          relative path to the directory to copy
+     * @param exportDirectory root directory where the file will be copied. If the file'
+     *                        relative path included any directories, those will be re-created
+     *                        under this directory.
+     * @param warnings        any warnings generated during this operation (e.g., source file outside
+     *                        of {@code ddf.home}) will be added to this collection
+     * @throws MigrationException thrown if file system error prevents the directory to be copied
+     */
+    public void copyDirectory(@NotNull Path source, @NotNull Path exportDirectory,
+            @NotNull Collection<MigrationWarning> warnings) throws MigrationException {
+        copyFile(source, exportDirectory, warnings);
+    }
+
+    /**
+     * Copies a file, whose path is taken from a {@link System} property value, to a destination
+     * directory. The file to copy must be a relative path under {@code ddf.home}, and its path
+     * must not contain any symbolic link, otherwise the file will not be copied and a
+     * {@link MigrationWarning} will be returned.
+     *
+     * @param systemProperty  name of the {@link System} property that contains the path to the
+     *                        source file
+     * @param exportDirectory root directory where the file will be copied. If the file'
+     *                        relative path included any directories, those will be re-created
+     *                        under this directory.
+     * @param warnings        any warnings generated during this operation (e.g., source file outside
+     *                        of {@code ddf.home}) will be added to this collection
+     */
+    public void copyFileFromSystemPropertyValue(@NotNull String systemProperty,
+            @NotNull Path exportDirectory, @NotNull Collection<MigrationWarning> warnings) {
+        String source = System.getProperty(systemProperty);
+        notEmpty(source,
+                String.format("Source path property [%s] is invalid: [%s]",
+                        systemProperty,
+                        source));
+
+        Path sourcePath = Paths.get(source);
+
+        copy(sourcePath,
+                exportDirectory,
+                warnings,
+                () -> isSourceMigratable(sourcePath,
+                        () -> new PathMigrationWarning(systemProperty, sourcePath, "is absolute"),
+                        () -> new PathMigrationWarning(systemProperty,
+                                sourcePath,
+                                "contains a symbolic link"),
+                        () -> new PathMigrationWarning(systemProperty,
+                                sourcePath,
+                                String.format("is outside [%s]", ddfHome)),
+                        warnings));
+    }
+
+    /**
+     * Copies a file, whose path is taken from the value a Java properties file, to a destination.
+     * directory. The file to copy must be a relative path under {@code ddf.home}, and its path
+     * must not contain any symbolic link, otherwise the file will not be copied and a
+     * {@link MigrationWarning} will be returned.
+     *
+     * @param propertyFilePath path to the Java properties file that contains the path to the
+     *                         source file to copy
+     * @param javaProperty     name of the property inside the Java properties file that contains
+     *                         the path to the source file
+     * @param exportDirectory  path to the destination
+     * @param warnings         any warnings generated during this operation (e.g., source file
+     *                         outside of {@code ddf.home}) will be added to this collection
+     */
+    public void copyFileFromJavaPropertyValue(@NotNull Path propertyFilePath,
+            @NotNull String javaProperty, @NotNull Path exportDirectory,
+            @NotNull Collection<MigrationWarning> warnings) {
+        notNull(propertyFilePath, "Java properties file cannot be null");
+        Properties properties = readPropertiesFile(ddfHome.resolve(propertyFilePath));
+        String source = (String) properties.get(javaProperty);
+        notEmpty(source,
+                String.format("Source path property [%s] is invalid: [%s]", javaProperty, source));
+
+        Path sourcePath = Paths.get(source);
+
+        copy(sourcePath,
+                exportDirectory,
+                warnings,
+                () -> isSourceMigratable(sourcePath,
+                        () -> new PathMigrationWarning(propertyFilePath,
+                                javaProperty,
+                                sourcePath,
+                                "is absolute"),
+                        () -> new PathMigrationWarning(propertyFilePath,
+                                javaProperty,
+                                sourcePath,
+                                "contains a symbolic link"),
+                        () -> new PathMigrationWarning(propertyFilePath,
+                                javaProperty,
+                                sourcePath,
+                                String.format("is outside [%s]", ddfHome)),
+                        warnings));
+    }
+
+    private String getSystemProperty(String property) throws MigrationException {
+        String prop = System.getProperty(property);
+
+        if (StringUtils.isBlank(prop)) {
+            String message = String.format("System property %s is not set.", property);
+            LOGGER.error(message);
+            throw new MigrationException(message);
+        }
+
+        return prop;
+    }
+
+    private void copy(Path sourceFile, Path exportDirectory, Collection<MigrationWarning> warnings,
+            BooleanSupplier isSourceMigratable) {
+        notNull(sourceFile, "Source file cannot be null");
+        notNull(exportDirectory, "Destination cannot be null");
+        notNull(warnings, "Warning collection cannot be null");
+
+        try {
+            if (isSourceMigratable.getAsBoolean()) {
+                FileUtils.copyFile(sourceFile.toFile(),
+                        exportDirectory.resolve(sourceFile)
+                                .toFile());
+            }
+        } catch (IOException e) {
+            String message = String.format("Unable to copy [%s] to [%s].",
+                    sourceFile.toString(),
+                    exportDirectory.toString());
+            LOGGER.error(message, e);
+            throw new MigrationException(message, e);
+        }
+    }
+
+    private Properties readPropertiesFile(Path propertiesFile) throws MigrationException {
+        Properties properties = new Properties();
+
+        try (InputStream inputStream = new FileInputStream(propertiesFile.toString())) {
+            properties.load(inputStream);
+            return properties;
+        } catch (IOException e) {
+            String message = String.format("Unable to read properties file [%s].",
+                    propertiesFile.toString());
+            LOGGER.error(message, e);
+            throw new MigrationException(message, e);
+        }
+    }
+
+    private boolean isSourceMigratable(Path source,
+            Supplier<PathMigrationWarning> absolutePathWarning,
+            Supplier<PathMigrationWarning> symbolicLinkWarning,
+            Supplier<PathMigrationWarning> outsidePathWarning,
+            Collection<MigrationWarning> warnings) {
+
+        if (source.isAbsolute()) {
+            warnings.add(absolutePathWarning.get());
+            return false;
+        }
+
+        if (Files.isSymbolicLink(source)) {
+            warnings.add(symbolicLinkWarning.get());
+            return false;
+        }
+
+        try {
+            if (!ddfHome.resolve(source)
+                    .toRealPath()
+                    .startsWith(ddfHome)) {
+                warnings.add(outsidePathWarning.get());
+                return false;
+            }
+        } catch (IOException e) {
+            String message = String.format("Source file [%s] does not exist.", source.toString());
+            LOGGER.error(message, e);
+            throw new MigrationException(message, e);
+        }
+
+        return true;
+    }
+}
