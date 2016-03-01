abccd17acfe068b3c3e94fd38bca08156c77f8ae
Joseph North <joernorth@gmail.com>
2016-03-01 15:55:12 -0700

    DDF-1911 PDF input transformer adds document metadata to metacard - Update PDFBox version to 1.8.11

--- catalog/transformer/catalog-transformer-pdf/src/test/groovy/ddf/catalog/transformer/input/pdf/PdfInputTransformerSpecTest.groovy
+++ catalog/transformer/catalog-transformer-pdf/src/test/groovy/ddf/catalog/transformer/input/pdf/PdfInputTransformerSpecTest.groovy
@@ -16,7 +16,6 @@ package ddf.catalog.transformer.input.pdf
 import ddf.catalog.data.Metacard
 import spock.lang.Specification
 
-
 class PdfInputTransformerSpecTest extends Specification {
     PdfInputTransformer pdfInputTransformer;
 
@@ -28,12 +27,31 @@ class PdfInputTransformerSpecTest extends Specification {
         PdfInputTransformerSpecTest.class.getResourceAsStream("/sample.pdf")
     }
 
+    def getSampleInputStreamWithCatalogMetadata = {
+        PdfInputTransformerSpecTest.class.getResourceAsStream("/sample_with_catalog_metadata.pdf")
+    }
+
     void setup() {
         pdfInputTransformer = new PdfInputTransformer();
     }
 
-    void cleanup() {
-        // Function for post test cleanup
+    void verifySamplePdfMetacard(Metacard metacard) {
+        verifyThumbnailSize(metacard.thumbnail)
+        assert metacard.contentTypeName == 'pdf'
+        assert metacard.title == 'Microsoft Word - Document1'
+        assert metacard.createdDate.getTime() == 1456150156000
+        assert metacard.modifiedDate.getTime() == 1456150156000
+
+        assert metacard.metadata.contains('<title>Microsoft Word - Document1</title>')
+        assert metacard.metadata.contains('<producer>Mac OS X 10.11.3 Quartz PDFContext</producer>')
+        assert metacard.metadata.contains('<creationDate>2016-02-22T14:09:16+00:00</creationDate>')
+        assert metacard.metadata.contains('<modificationDate>2016-02-22T14:09:16+00:00</modificationDate>')
+        assert metacard.metadata.contains('<pageCount>1</pageCount>')
+    }
+
+    void verifyThumbnailSize(byte[] thumbnail) {
+        assert thumbnail.length > 0
+        assert thumbnail.length < 128 * 1024
     }
 
     def "Transform"() {
@@ -44,10 +62,8 @@ class PdfInputTransformerSpecTest extends Specification {
         Metacard metacard = pdfInputTransformer.transform(getSampleInputStream(), id)
 
         then:
-        metacard.thumbnail.length < 128 * 1024
-        metacard.contentTypeName == 'pdf'
         metacard.id == id
-        metacard.title == 'Microsoft Word - Document1'
+        verifySamplePdfMetacard(metacard)
     }
 
     def "Transform without id"() {
@@ -55,9 +71,27 @@ class PdfInputTransformerSpecTest extends Specification {
         Metacard metacard = pdfInputTransformer.transform(getSampleInputStream())
 
         then:
-        metacard.thumbnail.length < 128 * 1024
+        verifySamplePdfMetacard(metacard)
+    }
+
+    def "Transform with catalog metadata"() {
+        when:
+        Metacard metacard = pdfInputTransformer.transform(getSampleInputStreamWithCatalogMetadata())
+
+        then:
+        verifyThumbnailSize(metacard.thumbnail)
         metacard.contentTypeName == 'pdf'
-        metacard.title == 'Microsoft Word - Document1'
+        metacard.title == 'Untitled'
+        metacard.createdDate.getTime() == 1253116122000
+        metacard.modifiedDate.getTime() == 1253116509000
+
+        metacard.metadata.contains('<xap:MetadataDate>2009-09-16T10:55:09-05:00</xap:MetadataDate>')
+        metacard.metadata.contains('<title>Untitled</title>')
+        metacard.metadata.contains('<creator>Acrobat Editor 8.0</creator>')
+        metacard.metadata.contains('<producer>Adobe Acrobat 8.1.6</producer>')
+        metacard.metadata.contains('<creationDate>2009-09-16T10:48:42-05:00</creationDate>')
+        metacard.metadata.contains('<modificationDate>2009-09-16T10:55:09-05:00</modificationDate>')
+        metacard.metadata.contains('<pageCount>12</pageCount>')
     }
 
     def "Attempt transform encrypted pdf"() {
@@ -67,6 +101,7 @@ class PdfInputTransformerSpecTest extends Specification {
         then:
         metacard.thumbnail == null
         metacard.title == null
+        metacard.metadata == null
         metacard.contentTypeName == 'pdf'
     }
 
@@ -76,12 +111,12 @@ class PdfInputTransformerSpecTest extends Specification {
 
         then:
         thumbnail != null
-        thumbnail.length > 0
+        verifyThumbnailSize(thumbnail)
     }
 
     def "Generate thumbnail only encrypted"() {
         when:
-        byte[] thumbnail = pdfInputTransformer.generatePdfThumbnail(getEncryptedInputStream())
+        pdfInputTransformer.generatePdfThumbnail(getEncryptedInputStream())
 
         then:
         thrown(IOException)
