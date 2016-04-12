23d87325e87fd6aae304ca9c18e0325bbf7a0f4a
harrison-tarr <harrison.tarr@connexta.com>
2016-04-12 15:13:18 -0700

    DDF-1998 Fix Metacard Type bug (#664)
    DDF-1998 Further fix metacard type
    
    DDF-1998 Edit CswQueryBuilder so it can be configurable
    
    DDF-1998 Fix InputTransformer metacard type
    
    and add test
    
    DDF-1998 Fix Xml Reconstruction namespace bug
    
    There was a bug where a namespace could go out of scope
    but not get redeclared. This is fixed now.
    
    DDF-1998 Handle namespace edge cases better
    
    DDF-1998 Add all Basic metacard attributes
    
    More attributes will be getting added to basic metacard type
    It's easier to add them programmatically in the XmlInputTransformer
    than adding them by hand to a specific handler every time a new one gets added
    
    DDF-1998 Address thread non safety, remove old code
    
    DDF-1998 Abstract logic from public methods
    
    DDF-1998 Remove init & destroy methods
    
    DDF-1998 Split test that tested too much

--- distribution/test/itests/test-itests-ddf/src/test/java/ddf/test/itests/common/CswQueryBuilder.java
+++ distribution/test/itests/test-itests-ddf/src/test/java/ddf/test/itests/common/CswQueryBuilder.java
@@ -42,6 +42,17 @@ public class CswQueryBuilder {
                 + "    </ns10:Query>" + "</csw:GetRecords>";
     }
 
+    public String getQuery(String outputFormat, String outputSchema) {
+        return "<csw:GetRecords resultType=\"results\" outputFormat=\"" + outputFormat
+                + "\" outputSchema=\"" + outputSchema
+                + "\" startPosition=\"1\" maxRecords=\"10\" service=\"CSW\" version=\"2.0.2\" xmlns:ns2=\"http://www.opengis.net/ogc\" xmlns:csw=\"http://www.opengis.net/cat/csw/2.0.2\" xmlns:ns4=\"http://www.w3.org/1999/xlink\" xmlns:ns3=\"http://www.opengis.net/gml\" xmlns:ns9=\"http://www.w3.org/2001/SMIL20/Language\" xmlns:ns5=\"http://www.opengis.net/ows\" xmlns:ns6=\"http://purl.org/dc/elements/1.1/\" xmlns:ns7=\"http://purl.org/dc/terms/\" xmlns:ns8=\"http://www.w3.org/2001/SMIL20/\">"
+                + "    <ns10:Query typeNames=\"csw:Record\" xmlns=\"\" xmlns:ns10=\"http://www.opengis.net/cat/csw/2.0.2\">"
+                + "        <ns10:ElementSetName>full</ns10:ElementSetName>"
+                + "        <ns10:Constraint version=\"1.1.0\">" + "            <ns2:Filter>"
+                + internalRepresentation + "</ns2:Filter>" + "        </ns10:Constraint>"
+                + "    </ns10:Query>" + "</csw:GetRecords>";
+    }
+
     public CswQueryBuilder addAttributeFilter(String filter, String propertyName,
             String literalValue) {
         internalRepresentation = internalRepresentation + "                    <ns2:" + filter
