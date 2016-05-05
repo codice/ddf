34cc141300b391ac26e7c8210ccd6e010036780c
William <william.miller@connexta.com>
2016-05-05 00:09:31 -0400

    DDF-2097: Added logic to compare Sources based on attributes instead of Object.hashCode()

--- catalog/core/catalog-core-api-impl/src/test/java/ddf/catalog/util/impl/SourcePollerRunnerTest.java
+++ catalog/core/catalog-core-api-impl/src/test/java/ddf/catalog/util/impl/SourcePollerRunnerTest.java
@@ -14,6 +14,7 @@
 package ddf.catalog.util.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -25,15 +26,19 @@ import java.util.Set;
 
 import org.junit.Test;
 
+
 import ddf.catalog.data.ContentType;
 import ddf.catalog.data.impl.ContentTypeImpl;
 import ddf.catalog.source.Source;
 
 public class SourcePollerRunnerTest {
 
-    private Source createDefaultFederatedSource(boolean avail, Set<ContentType> types) {
+    private Source createDefaultFederatedSource(boolean avail, Set<ContentType> types,
+            String src, String version) {
         Source source = mock(Source.class);
 
+        when(source.getTitle()).thenReturn(src);
+        when(source.getVersion()).thenReturn(version);
         when(source.isAvailable()).thenReturn(avail);
         when(source.getContentTypes()).thenReturn(types);
 
@@ -52,7 +57,7 @@ public class SourcePollerRunnerTest {
     public void testDoesntUpdateUnexpiredCachedValuesOnAvailableSource() {
         SourcePollerRunner runner = new SourcePollerRunner();
         Set<ContentType> types = createContentTypes();
-        Source source = createDefaultFederatedSource(true, types);
+        Source source = createDefaultFederatedSource(true, types, "src", "1");
         CachedSource cached = null;
         runner.bind(source);
 
@@ -81,17 +86,13 @@ public class SourcePollerRunnerTest {
 
         verify(source, times(1)).isAvailable();
         verify(source, times(1)).getContentTypes();
-        verify(source, times(1)).getVersion();
-        verify(source, times(1)).getTitle();
-        verify(source, times(1)).getOrganization();
-        verify(source, times(1)).getDescription();
     }
 
     @Test
     public void testDoesntUpdateUnexpiredCachedValuesOnUnAvailableSource() {
         SourcePollerRunner runner = new SourcePollerRunner();
         Set<ContentType> types = createContentTypes();
-        Source source = createDefaultFederatedSource(false, types);
+        Source source = createDefaultFederatedSource(false, types, "src", "1");
         CachedSource cached;
         runner.bind(source);
 
@@ -119,10 +120,132 @@ public class SourcePollerRunnerTest {
 
         verify(source, times(1)).isAvailable();
         verify(source, never()).getContentTypes();
-        verify(source, never()).getVersion();
-        verify(source, never()).getTitle();
-        verify(source, never()).getOrganization();
-        verify(source, never()).getDescription();
     }
+
+    @Test
+    public void testCorrectSourceGetsIdentified() {
+        SourcePollerRunner runner = new SourcePollerRunner();
+        Set<ContentType> types = createContentTypes();
+        Source source = createDefaultFederatedSource(true, types, "src", "1");
+        Source source2 = createDefaultFederatedSource(true, types, "src2", "2");
+        CachedSource cached;
+        CachedSource cached2;
+        runner.bind(source);
+        runner.bind(source2);
+
+        SourceStatus status = null;
+        SourceStatus status2 = null;
+        do {
+            Thread.yield();
+            cached = runner.getCachedSource(source);
+            cached2 = runner.getCachedSource(source2);
+            if (cached != null) {
+                status = cached.getSourceStatus();
+            }
+            if (cached2 != null) {
+                status2 = cached2.getSourceStatus();
+            }
+        } while (status == null || status == SourceStatus.UNCHECKED ||
+                status2 == null || status2 == SourceStatus.UNCHECKED);
+
+        for (int i = 0; i < 10; i++) {
+            cached.isAvailable();
+            cached.getContentTypes();
+            cached2.isAvailable();
+            cached2.getContentTypes();
+        }
+        assertEquals(SourceStatus.AVAILABLE, cached.getSourceStatus());
+        assertEquals(true, cached.isAvailable());
+
+        assertEquals(SourceStatus.AVAILABLE, cached2.getSourceStatus());
+        assertEquals(true, cached2.isAvailable());
+
+        assertEquals(source.getTitle(), cached.getTitle());
+        assertEquals(source2.getTitle(), cached2.getTitle());
+
+        verify(source, times(1)).isAvailable();
+        verify(source, times(1)).getContentTypes();
+        verify(source2, times(1)).isAvailable();
+        verify(source2, times(1)).getContentTypes();
+    }
+
+    @Test
+    public void testKnownSourceRepresentedByDifferentObjectDiscovered() {
+        SourcePollerRunner runner = new SourcePollerRunner();
+        Set<ContentType> types = createContentTypes();
+        Source source = createDefaultFederatedSource(false, types, "src1", "1");
+        Source source2 = createDefaultFederatedSource(false, types, "src2", "1");
+        CachedSource cached;
+        runner.bind(source);
+
+        SourceStatus status = null;
+        do {
+            Thread.yield();
+            cached = runner.getCachedSource(source);
+            if (cached != null) {
+                status = cached.getSourceStatus();
+            }
+        } while (status == null || status == SourceStatus.UNCHECKED);
+
+        for (int i = 0; i < 10; i++) {
+            cached.isAvailable();
+            cached.getSourceStatus();
+            cached.getContentTypes();
+            cached.getVersion();
+            cached.getTitle();
+            cached.getOrganization();
+            cached.getId();
+            cached.getDescription();
+        }
+        assertEquals(SourceStatus.UNAVAILABLE, cached.getSourceStatus());
+        assertEquals(false, cached.isAvailable());
+
+        verify(source, times(1)).isAvailable();
+        verify(source, never()).getContentTypes();
+    }
+
+
+    @Test
+    public void testNonExistantWithUnknownTitleIsntFound() {
+        SourcePollerRunner runner = new SourcePollerRunner();
+        Set<ContentType> types = createContentTypes();
+        Source source = createDefaultFederatedSource(true, types, "src", "1");
+        Source source2 = createDefaultFederatedSource(true, types, "src2", "1");
+        CachedSource cached = null;
+        runner.bind(source);
+
+        SourceStatus status = null;
+        do {
+            Thread.yield();
+            cached = runner.getCachedSource(source);
+            if (cached != null) {
+                status = cached.getSourceStatus();
+            }
+        } while (status == null || status == SourceStatus.UNCHECKED);
+
+        assertNull(runner.getCachedSource(source2));
+    }
+
+    @Test
+    public void testNonExistantWithDuplicateTitleButWrongVersionIsntFound() {
+        SourcePollerRunner runner = new SourcePollerRunner();
+        Set<ContentType> types = createContentTypes();
+        Source source = createDefaultFederatedSource(true, types, "src", "1");
+        Source source2 = createDefaultFederatedSource(true, types, "src", "2");
+        CachedSource cached = null;
+        runner.bind(source);
+
+        SourceStatus status = null;
+        do {
+            Thread.yield();
+            cached = runner.getCachedSource(source);
+            if (cached != null) {
+                status = cached.getSourceStatus();
+            }
+        } while (status == null || status == SourceStatus.UNCHECKED);
+
+        assertNull(runner.getCachedSource(source2));
+    }
+
 
 }
