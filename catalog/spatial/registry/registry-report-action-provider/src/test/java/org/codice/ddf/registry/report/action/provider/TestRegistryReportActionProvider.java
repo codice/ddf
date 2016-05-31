4d350eb8ca565dd9ec24fb4f1343ba0df177f4a8
Matthew Calcote <matthew.calcote@connexta.com>
2016-05-31 12:33:19 -0700

    DDF-2008 Update RegistryReportActionProvider based on design refinement (#822)

--- catalog/spatial/registry/registry-report-action-provider/src/test/java/org/codice/ddf/registry/report/action/provider/TestRegistryReportActionProvider.java
+++ catalog/spatial/registry/registry-report-action-provider/src/test/java/org/codice/ddf/registry/report/action/provider/TestRegistryReportActionProvider.java
@@ -19,21 +19,31 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Dictionary;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Set;
 
 import org.codice.ddf.registry.common.RegistryConstants;
+import org.codice.ddf.registry.common.metacard.RegistryObjectMetacardType;
 import org.junit.Before;
 import org.junit.Test;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 
 import ddf.action.Action;
 import ddf.catalog.data.impl.MetacardImpl;
+import ddf.catalog.source.Source;
 
-public class TestRegistryMetacardActionProvider {
+public class TestRegistryReportActionProvider {
 
     private static final String SAMPLE_PATH = "/registries/";
 
@@ -51,6 +61,8 @@ public class TestRegistryMetacardActionProvider {
 
     private static final String SAMPLE_ID = "abcdef1234567890abdcef1234567890";
 
+    private static final String SAMPLE_REGISTRY_ID = "sample_reg_id";
+
     private static final String ACTION_PROVIDER_ID = "catalog.view.metacard";
 
     private static final Set<String> SAMPLE_REGISTRY_TAGS = new HashSet<>(Collections.singletonList(
@@ -62,28 +74,38 @@ public class TestRegistryMetacardActionProvider {
 
     private static final String SOURCE_ID_QUERY_PARAM = "?sourceId=";
 
-    private RegistryMetacardActionProvider actionProvider;
+    private RegistryReportActionProvider actionProvider;
+
+    private ConfigurationAdmin configurationAdmin;
+
+    private Configuration configuration;
 
     private MetacardImpl metacard;
+
+    private Source source;
 
     @Before
     public void setup() {
         metacard = new MetacardImpl();
-        actionProvider = new RegistryMetacardActionProvider(ACTION_PROVIDER_ID);
+        source = mock(Source.class);
+        configurationAdmin = mock(ConfigurationAdmin.class);
+        configuration = mock(Configuration.class);
+        actionProvider = new RegistryReportActionProvider(ACTION_PROVIDER_ID);
         metacard.setId(SAMPLE_ID);
         metacard.setTags(SAMPLE_REGISTRY_TAGS);
         metacard.setSourceId(SAMPLE_SOURCE_ID);
+        metacard.setAttribute(RegistryObjectMetacardType.REGISTRY_ID, SAMPLE_REGISTRY_ID);
     }
 
     @Test
     public void testMetacardNull() {
-        assertThat(new RegistryMetacardActionProvider(ACTION_PROVIDER_ID).getActions(null),
+        assertThat(new RegistryReportActionProvider(ACTION_PROVIDER_ID).getActions(null),
                 hasSize(0));
     }
 
     @Test
     public void testUriSyntaxException() {
-        this.configureActionProvider(SAMPLE_PROTOCOL, "23^&*#", SAMPLE_PORT, SAMPLE_SERVICES_ROOT);
+        configureActionProvider(SAMPLE_PROTOCOL, "23^&*#", SAMPLE_PORT, SAMPLE_SERVICES_ROOT);
 
         assertThat("A bad url should have been caught and an empty list returned.",
                 actionProvider.getActions(metacard),
@@ -93,9 +115,9 @@ public class TestRegistryMetacardActionProvider {
 
     @Test
     public void testMetacardIdUrlEncodedSpace() {
-        metacard.setId("abd ef");
+        metacard.setAttribute(RegistryObjectMetacardType.REGISTRY_ID, "abd ef");
 
-        this.configureActionProvider();
+        configureActionProvider();
 
         String url = actionProvider.getActions(metacard)
                 .get(0)
@@ -108,9 +130,9 @@ public class TestRegistryMetacardActionProvider {
 
     @Test
     public void testMetacardIdUrlEncodedAmpersand() {
-        metacard.setId("abd&ef");
+        metacard.setAttribute(RegistryObjectMetacardType.REGISTRY_ID, "abd&ef");
 
-        this.configureActionProvider();
+        configureActionProvider();
 
         String url = actionProvider.getActions(metacard)
                 .get(0)
@@ -122,20 +144,19 @@ public class TestRegistryMetacardActionProvider {
     }
 
     @Test
-    public void testMetacardIdNull() {
-        metacard.setId(null);
+    public void testRegistryIdNull() {
+        metacard.setAttribute(RegistryObjectMetacardType.REGISTRY_ID, null);
 
-        this.configureActionProvider();
+        configureActionProvider();
 
         assertThat("An action should not have been created when no id is provided.",
                 actionProvider.getActions(metacard),
                 hasSize(0));
-
     }
 
     @Test
     public void testIpNull() {
-        this.configureActionProvider(SAMPLE_PROTOCOL, null, SAMPLE_PORT, SAMPLE_SERVICES_ROOT);
+        configureActionProvider(SAMPLE_PROTOCOL, null, SAMPLE_PORT, SAMPLE_SERVICES_ROOT);
 
         assertThat(actionProvider.getActions(metacard)
                 .get(0)
@@ -145,17 +166,8 @@ public class TestRegistryMetacardActionProvider {
     }
 
     @Test
-    public void testIpUnknown() {
-        this.configureActionProvider(SAMPLE_PROTOCOL, "0.0.0.0", SAMPLE_PORT, SAMPLE_SERVICES_ROOT);
-
-        assertThat("An action should not have been created when ip is unknown (0.0.0.0).",
-                actionProvider.getActions(metacard),
-                hasSize(0));
-    }
-
-    @Test
     public void testPortNull() {
-        this.configureActionProvider(SAMPLE_PROTOCOL, SAMPLE_IP, null, SAMPLE_SERVICES_ROOT);
+        configureActionProvider(SAMPLE_PROTOCOL, SAMPLE_IP, null, SAMPLE_SERVICES_ROOT);
 
         assertThat(actionProvider.getActions(metacard)
                 .get(0)
@@ -165,7 +177,7 @@ public class TestRegistryMetacardActionProvider {
 
     @Test
     public void testContextRootNull() {
-        this.configureActionProvider(SAMPLE_PROTOCOL, SAMPLE_IP, SAMPLE_PORT, null);
+        configureActionProvider(SAMPLE_PROTOCOL, SAMPLE_IP, SAMPLE_PORT, null);
 
         assertThat(actionProvider.getActions(metacard)
                 .get(0)
@@ -175,7 +187,7 @@ public class TestRegistryMetacardActionProvider {
 
     @Test
     public void testNonMetacard() {
-        this.configureActionProvider();
+        configureActionProvider();
 
         assertThat("An action when metacard was not provided.",
                 actionProvider.getActions(new Date()),
@@ -185,15 +197,18 @@ public class TestRegistryMetacardActionProvider {
 
     @Test
     public void testMetacard() {
-        this.configureActionProvider();
+        configureActionProvider();
 
         Action action = actionProvider.getActions(metacard)
                 .get(0);
 
-        assertEquals(RegistryMetacardActionProvider.TITLE, action.getTitle());
-        assertEquals(RegistryMetacardActionProvider.DESCRIPTION, action.getDescription());
+        assertEquals(actionProvider.getTitle(), action.getTitle());
+        assertEquals(actionProvider.getDescription(), action.getDescription());
         assertThat(action.getUrl()
-                .toString(), is(expectedDefaultAddressWith(metacard.getId())));
+                .toString(), is(expectedDefaultAddressWith(metacard.getAttribute(
+                RegistryObjectMetacardType.REGISTRY_ID)
+                .getValue()
+                .toString())));
         assertEquals(ACTION_PROVIDER_ID, actionProvider.getId());
 
     }
@@ -205,7 +220,7 @@ public class TestRegistryMetacardActionProvider {
         String newSourceName = "newSource";
         metacard.setSourceId(newSourceName);
 
-        this.configureActionProvider();
+        configureActionProvider();
 
         List<Action> action = actionProvider.getActions(metacard);
 
@@ -220,12 +235,14 @@ public class TestRegistryMetacardActionProvider {
         Action action = actionProvider.getActions(metacard)
                 .get(0);
 
-        assertEquals(RegistryMetacardActionProvider.TITLE, action.getTitle());
-        assertEquals(RegistryMetacardActionProvider.DESCRIPTION, action.getDescription());
+        assertEquals(actionProvider.getTitle(), action.getTitle());
+        assertEquals(actionProvider.getDescription(), action.getDescription());
         assertEquals(
                 SAMPLE_SECURE_PROTOCOL + SAMPLE_IP + ":" + SAMPLE_SECURE_PORT + SAMPLE_SERVICES_ROOT
-                        + SAMPLE_PATH + metacard.getId() + PATH_AND_FORMAT + SOURCE_ID_QUERY_PARAM
-                        + SAMPLE_SOURCE_ID,
+                        + SAMPLE_PATH
+                        + metacard.getAttribute(RegistryObjectMetacardType.REGISTRY_ID)
+                        .getValue()
+                        .toString() + PATH_AND_FORMAT + SOURCE_ID_QUERY_PARAM + SAMPLE_SOURCE_ID,
                 action.getUrl()
                         .toString());
         assertEquals(ACTION_PROVIDER_ID, actionProvider.getId());
@@ -234,7 +251,7 @@ public class TestRegistryMetacardActionProvider {
 
     @Test
     public void testNullProtocol() {
-        this.configureActionProvider(null, SAMPLE_IP, SAMPLE_SECURE_PORT, SAMPLE_SERVICES_ROOT);
+        configureActionProvider(null, SAMPLE_IP, SAMPLE_SECURE_PORT, SAMPLE_SERVICES_ROOT);
 
         Action action = actionProvider.getActions(metacard)
                 .get(0);
@@ -246,17 +263,134 @@ public class TestRegistryMetacardActionProvider {
 
     @Test
     public void testSourceIdPresent() {
-        this.configureActionProvider();
+        configureActionProvider();
 
         Action action = actionProvider.getActions(metacard)
                 .get(0);
 
-        assertEquals(RegistryMetacardActionProvider.TITLE, action.getTitle());
-        assertEquals(RegistryMetacardActionProvider.DESCRIPTION, action.getDescription());
+        assertEquals(actionProvider.getTitle(), action.getTitle());
+        assertEquals(actionProvider.getDescription(), action.getDescription());
         assertThat(action.getUrl()
-                .toString(), is(expectedDefaultAddressWith(metacard.getId())));
+                .toString(), is(expectedDefaultAddressWith(metacard.getAttribute(
+                RegistryObjectMetacardType.REGISTRY_ID)
+                .getValue()
+                .toString())));
         assertEquals(ACTION_PROVIDER_ID, actionProvider.getId());
 
+    }
+
+    @Test
+    public void testBlankSourceId() {
+        metacard.setSourceId("");
+        configureActionProvider();
+
+        Action action = actionProvider.getActions(metacard)
+                .get(0);
+
+        assertThat(action.getUrl()
+                        .toString(),
+                is(SAMPLE_PROTOCOL + SAMPLE_IP + ":" + SAMPLE_PORT + SAMPLE_SERVICES_ROOT
+                        + SAMPLE_PATH
+                        + metacard.getAttribute(RegistryObjectMetacardType.REGISTRY_ID)
+                        .getValue()
+                        .toString() + PATH_AND_FORMAT));
+    }
+
+    @Test
+    public void testNullSourceId() {
+        metacard.setSourceId(null);
+        configureActionProvider();
+
+        Action action = actionProvider.getActions(metacard)
+                .get(0);
+
+        assertThat(action.getUrl()
+                        .toString(),
+                is(SAMPLE_PROTOCOL + SAMPLE_IP + ":" + SAMPLE_PORT + SAMPLE_SERVICES_ROOT
+                        + SAMPLE_PATH
+                        + metacard.getAttribute(RegistryObjectMetacardType.REGISTRY_ID)
+                        .getValue()
+                        .toString() + PATH_AND_FORMAT));
+    }
+
+    @Test
+    public void testUnsupportedType() {
+        configureActionProvider();
+
+        List<Action> actions = actionProvider.getActions(SAMPLE_PATH);
+
+        assertThat(actions.size(), is(0));
+    }
+
+    @Test
+    public void testBlankSource() {
+        configureActionProvider();
+
+        List<Action> actions = actionProvider.getActions(source);
+
+        assertThat(actions.size(), is(0));
+    }
+
+    @Test
+    public void testEmptyConfigurationsSource() throws IOException, InvalidSyntaxException {
+        configureActionProvider();
+        actionProvider.setConfigurationAdmin(configurationAdmin);
+        Configuration[] configurations = new Configuration[0];
+
+        when(source.getId()).thenReturn(SAMPLE_SOURCE_ID);
+        when(configurationAdmin.listConfigurations(String.format("(id=%s)",
+                SAMPLE_SOURCE_ID))).thenReturn(configurations);
+
+        List<Action> actions = actionProvider.getActions(source);
+
+        assertThat(actions.size(), is(0));
+    }
+
+    @Test
+    public void testNullRegistryIdConfiguration() {
+        configureActionProvider();
+
+        when(configuration.getProperties()).thenReturn(new Hashtable<>());
+
+        List<Action> actions = actionProvider.getActions(configuration);
+
+        assertThat(actions.size(), is(0));
+    }
+
+    @Test
+    public void testValidConfiguration() {
+        configureActionProvider();
+        Dictionary<String, Object> testDictionary = new Hashtable<>();
+
+        testDictionary.put(RegistryObjectMetacardType.REGISTRY_ID, SAMPLE_REGISTRY_ID);
+
+        when(configuration.getProperties()).thenReturn(testDictionary);
+
+        List<Action> actions = actionProvider.getActions(configuration);
+
+        assertThat(actions.size(), is(1));
+        assertThat(actions.get(0)
+                        .getUrl()
+                        .toString(),
+                is(SAMPLE_PROTOCOL + SAMPLE_IP + ":" + SAMPLE_PORT + SAMPLE_SERVICES_ROOT
+                        + SAMPLE_PATH
+                        + metacard.getAttribute(RegistryObjectMetacardType.REGISTRY_ID)
+                        .getValue()
+                        .toString() + PATH_AND_FORMAT));
+    }
+
+    @Test
+    public void testConfigurationBadUri() {
+        configureActionProvider(SAMPLE_PROTOCOL, "23^&*#", SAMPLE_PORT, SAMPLE_SERVICES_ROOT);
+        Dictionary<String, Object> testDictionary = new Hashtable<>();
+
+        testDictionary.put(RegistryObjectMetacardType.REGISTRY_ID, SAMPLE_REGISTRY_ID);
+
+        when(configuration.getProperties()).thenReturn(testDictionary);
+
+        List<Action> actions = actionProvider.getActions(configuration);
+
+        assertThat(actions.size(), is(0));
     }
 
     private String expectedDefaultAddressWith(String id) {
