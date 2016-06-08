ac56db84703dfc4aadd38d4c93a2b80f48bfb094
Matthew Calcote <matthew.calcote@connexta.com>
2016-06-08 15:55:14 -0700

    DDF-2109 Merge RegistryStoreImpl from ddf-registry to ddf (#809) (cherry picked from commit f63f6c5)

--- catalog/spatial/registry/registry-api-impl/src/main/java/org/codice/ddf/registry/api/impl/RegistryStoreImpl.java
+++ catalog/spatial/registry/registry-api-impl/src/main/java/org/codice/ddf/registry/api/impl/RegistryStoreImpl.java
@@ -15,9 +15,12 @@ package org.codice.ddf.registry.api.impl;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,9 +37,15 @@ import org.codice.ddf.parser.ParserException;
 import org.codice.ddf.registry.api.RegistryStore;
 import org.codice.ddf.registry.common.RegistryConstants;
 import org.codice.ddf.registry.common.metacard.RegistryObjectMetacardType;
+import org.codice.ddf.registry.schemabindings.EbrimConstants;
 import org.codice.ddf.spatial.ogc.csw.catalog.common.CswSourceConfiguration;
 import org.codice.ddf.spatial.ogc.csw.catalog.common.source.AbstractCswStore;
+import org.opengis.filter.Filter;
 import org.osgi.framework.BundleContext;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Charsets;
 import com.thoughtworks.xstream.converters.Converter;
@@ -44,15 +53,20 @@ import com.thoughtworks.xstream.converters.Converter;
 import ddf.catalog.Constants;
 import ddf.catalog.data.Attribute;
 import ddf.catalog.data.Metacard;
+import ddf.catalog.data.Result;
 import ddf.catalog.data.impl.AttributeImpl;
 import ddf.catalog.filter.delegate.TagsFilterDelegate;
 import ddf.catalog.operation.OperationTransaction;
+import ddf.catalog.operation.Query;
 import ddf.catalog.operation.QueryRequest;
 import ddf.catalog.operation.SourceResponse;
 import ddf.catalog.operation.UpdateRequest;
 import ddf.catalog.operation.UpdateResponse;
+import ddf.catalog.operation.impl.QueryImpl;
+import ddf.catalog.operation.impl.QueryRequestImpl;
 import ddf.catalog.operation.impl.SourceResponseImpl;
 import ddf.catalog.source.IngestException;
+import ddf.catalog.source.SourceMonitor;
 import ddf.catalog.source.UnsupportedQueryException;
 import ddf.security.encryption.EncryptionService;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.ExternalIdentifierType;
@@ -60,19 +74,29 @@ import oasis.names.tc.ebxml_regrep.xsd.rim._3.RegistryObjectType;
 
 public class RegistryStoreImpl extends AbstractCswStore implements RegistryStore {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegistryStoreImpl.class);
+
     public static final String PUSH_ALLOWED_PROPERTY = "pushAllowed";
 
     public static final String PULL_ALLOWED_PROPERTY = "pullAllowed";
 
+    public static final String REMOTE_NAME = "remoteName";
+
     private boolean pushAllowed = true;
 
     private boolean pullAllowed = true;
+
+    private String registryId = "";
+
+    private String remoteName = "";
 
     private Parser parser;
 
     private ParserConfigurator marshalConfigurator;
 
     private ParserConfigurator unmarshalConfigurator;
+
+    private ConfigurationAdmin configAdmin;
 
     public RegistryStoreImpl(BundleContext context, CswSourceConfiguration cswSourceConfiguration,
             Converter provider, SecureCxfClientFactory factory,
@@ -89,6 +113,7 @@ public class RegistryStoreImpl extends AbstractCswStore implements RegistryStore
         Map<String, Consumer<Object>> map = new HashMap<>();
         map.put(PUSH_ALLOWED_PROPERTY, value -> setPushAllowed((Boolean) value));
         map.put(PULL_ALLOWED_PROPERTY, value -> setPullAllowed((Boolean) value));
+        map.put(RegistryObjectMetacardType.REGISTRY_ID, value -> setRegistryId((String) value));
         return map;
     }
 
@@ -138,6 +163,31 @@ public class RegistryStoreImpl extends AbstractCswStore implements RegistryStore
             return new SourceResponseImpl(request, Collections.emptyList());
         }
 
+        SourceResponse registryQueryResponse = queryResponse(request);
+        for (Result singleResult : registryQueryResponse.getResults()) {
+            if (singleResult.getMetacard()
+                    .getAttribute(RegistryObjectMetacardType.REGISTRY_ID)
+                    .getValue()
+                    .toString()
+                    .equals(registryId)) {
+                String metacardTitle = singleResult.getMetacard().getTitle();
+                if (metacardTitle != null && !remoteName.equals(metacardTitle)) {
+                    remoteName = metacardTitle;
+                    updateConfiguration();
+                }
+                break;
+            }
+        }
+        return registryQueryResponse;
+    }
+
+    /*
+    * After reviewing the various ways to test the query method above, it was decided
+    * that moving the super.query method call into its own method was the least offensive option.
+    * This is due to the inability to mock or spy an abstract super class method that is being
+    * overwritten without resorting to... Power Mock.
+    */
+    SourceResponse queryResponse(QueryRequest request) throws UnsupportedQueryException {
         return super.query(request);
     }
 
@@ -147,6 +197,14 @@ public class RegistryStoreImpl extends AbstractCswStore implements RegistryStore
 
     public void setPullAllowed(boolean pullAllowed) {
         this.pullAllowed = pullAllowed;
+    }
+
+    public void setRegistryId(String registryId) {
+        this.registryId = registryId;
+    }
+
+    public void setRemoteName(String remoteName) {
+        this.remoteName = remoteName;
     }
 
     private String getRegistryId(Metacard mcard) {
@@ -192,9 +250,11 @@ public class RegistryStoreImpl extends AbstractCswStore implements RegistryStore
     public void setParser(Parser parser) {
         List<String> contextPath = Arrays.asList(RegistryObjectType.class.getPackage()
                         .getName(),
-                net.opengis.ogc.ObjectFactory.class.getPackage()
+                EbrimConstants.OGC_FACTORY.getClass()
+                        .getPackage()
                         .getName(),
-                net.opengis.gml.v_3_1_1.ObjectFactory.class.getPackage()
+                EbrimConstants.GML_FACTORY.getClass()
+                        .getPackage()
                         .getName());
         ClassLoader classLoader = this.getClass()
                 .getClassLoader();
@@ -202,5 +262,81 @@ public class RegistryStoreImpl extends AbstractCswStore implements RegistryStore
         this.marshalConfigurator = parser.configureParser(contextPath, classLoader);
         this.marshalConfigurator.addProperty(Marshaller.JAXB_FRAGMENT, true);
         this.parser = parser;
+    }
+
+    public void init() {
+
+        SourceMonitor registrySourceMonitor = new SourceMonitor() {
+            @Override
+            public void setAvailable() {
+                try {
+                    registryInfoQuery();
+                } catch (UnsupportedQueryException e) {
+                    LOGGER.error("Unable to query registry configurations, ", e);
+                }
+            }
+
+            @Override
+            public void setUnavailable() {
+            }
+        };
+
+        addSourceMonitor(registrySourceMonitor);
+        super.init();
+        isAvailable();
+    }
+
+    void registryInfoQuery() throws UnsupportedQueryException {
+        List<Filter> filters = new ArrayList<>();
+        filters.add(filterBuilder.attribute(Metacard.TAGS)
+                .is()
+                .like()
+                .text(RegistryConstants.REGISTRY_TAG));
+        filters.add(filterBuilder.not(filterBuilder.attribute(RegistryObjectMetacardType.REGISTRY_IDENTITY_NODE)
+                .empty()));
+        Filter filter = filterBuilder.allOf(filters);
+        Query newQuery = new QueryImpl(filter);
+        QueryRequest queryRequest = new QueryRequestImpl(newQuery);
+        SourceResponse identityMetacard = query(queryRequest);
+        if (identityMetacard.getResults()
+                .size() > 0) {
+            remoteName = identityMetacard.getResults()
+                    .get(0)
+                    .getMetacard()
+                    .getTitle();
+            registryId = identityMetacard.getResults()
+                    .get(0)
+                    .getMetacard()
+                    .getAttribute(RegistryObjectMetacardType.REGISTRY_ID)
+                    .getValue()
+                    .toString();
+        }
+        updateConfiguration();
+    }
+
+    private void updateConfiguration() {
+        String currentPid = getConfigurationPid();
+        try {
+            Configuration currentConfig = configAdmin.getConfiguration(currentPid);
+            Dictionary<String, Object> currentProperties = currentConfig.getProperties();
+            currentProperties.put(REMOTE_NAME, remoteName);
+            currentProperties.put(RegistryObjectMetacardType.REGISTRY_ID, registryId);
+            currentConfig.update(currentProperties);
+        } catch (IOException e) {
+            LOGGER.error("Unable to update registry configurations, ", e);
+        }
+    }
+
+    public void setConfigAdmin(ConfigurationAdmin config) {
+        this.configAdmin = config;
+    }
+
+    @Override
+    public String getRegistryId() {
+        return registryId;
+    }
+
+    public String getRemoteName() {
+        return remoteName;
     }
 }
