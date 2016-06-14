f43f91be29171dd8ca4f7968fec945ce8f11da37
Joseph North <joernorth@gmail.com>
2016-06-14 10:48:36 -0700

    DDF-2125 Add the ability to inject default values into metacards during ingest (#777)
    - Move the validation JSON parser into its own bundle
    - Update the validation JSON parser to handle default attribute values
    - Add a default attribute value API
    - Modify the catalog framework to inject default values for missing attributes on creates and updates
    - Increase Felix file installer log level so validation parser errors are logged

--- catalog/core/catalog-core-federationstrategy/src/test/java/ddf/catalog/federation/impl/FederationStrategyTest.java
+++ catalog/core/catalog-core-federationstrategy/src/test/java/ddf/catalog/federation/impl/FederationStrategyTest.java
@@ -54,6 +54,7 @@ import org.slf4j.LoggerFactory;
 import ddf.catalog.data.ContentType;
 import ddf.catalog.data.Metacard;
 import ddf.catalog.data.Result;
+import ddf.catalog.data.defaultvalues.DefaultAttributeValueRegistryImpl;
 import ddf.catalog.data.impl.MetacardImpl;
 import ddf.catalog.federation.FederationException;
 import ddf.catalog.federation.base.AbstractFederationStrategy;
@@ -141,6 +142,7 @@ public class FederationStrategyTest {
         props.setSourcePoller(poller);
         props.setQueryResponsePostProcessor(mock(QueryResponsePostProcessor.class));
         props.setFilterBuilder(new GeotoolsFilterBuilder());
+        props.setDefaultAttributeValueRegistry(new DefaultAttributeValueRegistryImpl());
         CatalogFrameworkImpl framework = new CatalogFrameworkImpl(props);
         framework.bind(provider);
 
