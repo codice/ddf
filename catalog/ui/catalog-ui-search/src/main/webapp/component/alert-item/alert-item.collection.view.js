f102f19ac713488b1d3eac5f1338cfcebca4cd79
Andrew Fiedler <andrewkfiedler@gmail.com>
2016-06-23 16:08:05 -0700

    DDF-2644 Adds an empty view for the alert-item.collection.view
     - Displays "No Recent Alerts"

--- catalog/ui/catalog-ui-search/src/main/webapp/component/alert-item/alert-item.collection.view.js
+++ catalog/ui/catalog-ui-search/src/main/webapp/component/alert-item/alert-item.collection.view.js
@@ -23,6 +23,7 @@ define([
 ], function (Marionette, _, $, childView, CustomElements, store) {
 
     return Marionette.CollectionView.extend({
+        emptyView: Marionette.ItemView.extend({className: 'alert-empty', template: 'No Recent Alerts'}),
         className: 'is-list',
         setDefaultCollection: function(){
             this.collection = store.get('user').get('user').get('preferences').get('alerts');
