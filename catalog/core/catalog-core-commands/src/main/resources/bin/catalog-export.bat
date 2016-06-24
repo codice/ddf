474b259c85c6a7e15044aeb491cc3082f196dd08
Leah Reese <leah.reese@connexta.com>
2016-06-24 10:45:03 -0600

    DDF-2233 Added CQL filter expression support for the catalog-export scripts to dump filtered metacards

--- catalog/core/catalog-core-commands/src/main/resources/bin/catalog-export.bat
+++ catalog/core/catalog-core-commands/src/main/resources/bin/catalog-export.bat
@@ -19,24 +19,28 @@ set HOST=127.0.0.1
 
 :GETOPTS
 if %1 == -d (
-	setlocal
-	set DIR=%~2& shift
+    setlocal
+    set DIR=%~2& shift
 )
 if %1 == -z (
-	setlocal
-	set FILENAME=%~2& shift
+    setlocal
+    set FILENAME=%~2& shift
 )
 if %1 == -k (
-	setlocal
-	set KEYSTORE=%~2& shift
+    setlocal
+    set KEYSTORE=%~2& shift
 )
 if %1 == -a (
-	setlocal
-	set ALIAS=%~2& shift
+    setlocal
+    set ALIAS=%~2& shift
 )
 if %1 == -h (
-	setlocal
-	set HOST=%~2& shift
+    setlocal
+    set HOST=%~2& shift
+)
+if %1 == -c (
+    setlocal
+    set CQL=%~2& shift
 )
 shift
 if not (%1)==() goto GETOPTS
@@ -45,17 +49,42 @@ if "%FILENAME%"=="" goto USAGE
 if "%KEYSTORE%"=="" goto USAGE
 
 echo Connecting to running DDF and exporting catalog to %FILENAME%...
-call client.bat -h %HOST% "catalog:dump --include-content=%FILENAME% %DIR% "
+
+if "%CQL%"=="" (
+    echo Calling client with command:  catalog:dump --include-content %FILENAME% %DIR%
+    call client.bat -h %HOST% "catalog:dump --include-content %FILENAME% %DIR% "
+) else (
+    echo Calling client with command:  catalog:dump --include-content %FILENAME% --cql ^"%CQL%^" %DIR%
+    call client.bat -h %HOST% "catalog:dump --include-content %FILENAME% --cql ""%CQL%"" %DIR% "
+)
 
 where %JARSIGNER% >nul 2>nul
 if %ERRORLEVEL% NEQ 0 (
-	echo Unable to find %JARSIGNER%, ensure that $JAVA_HOME and $JAVA_HOME/bin are set on the path.
-	exit /b 1
+    echo Unable to find %JARSIGNER%, ensure that $JAVA_HOME and $JAVA_HOME/bin are set on the path.
+    exit /b 1
 ) else (
-	echo Running %JARSIGNER% on %FILENAME%, which will prompt you to enter the keystore password to sign the zip file...
-	call %JARSIGNER% -keystore %KEYSTORE% %DIR%/%FILENAME% %ALIAS% -signedJar %DIR%signed%FILENAME%
+    echo Running %JARSIGNER% on %FILENAME%, which will prompt you to enter the keystore password to sign the zip file...
+    call %JARSIGNER% -keystore %KEYSTORE% %DIR%/%FILENAME% %ALIAS% -signedJar %DIR%signed%FILENAME%
 )
 goto :eof
 
 :USAGE
-echo Usage: ^<-d directory^> ^<-z zipname^> ^<-k keystore location^> [-a alias] [-h hostname]
+echo SYNTAX
+echo    ^<-d directory^> ^<-z zipname^> ^<-k keystore location^> [-a alias] [-h hostname] [-c cql-expr]
+
+echo ARGUMENTS
+echo    -d directory : The absolute path of the directory to export the metacard
+echo                   archive. Directory path must exist.
+echo    -z zipname   : The name of the archive file to be generated. The command
+echo                   will fail if this name is not unique in the directory.
+echo    -k keystore  : The absolute path to the keystore directory.
+
+echo OPTIONS
+echo    -a alias     : Name of alias of the private key entry in the keystore.
+echo                   Typically matches the Common Name (CN) for the host in
+echo                   the keystore.
+echo    -c cql-expr  : Search using CQL Filter expressions.
+echo                   CQL Examples:
+echo                   {Textual}  -c "title like 'some text'"
+echo                   {Temporal} -c "modified before 2012-09-01T12:30:00Z"
+echo                   {Spatial}  -c "DWITHIN(location, POINT (1 2) , 10, kilometers)"
