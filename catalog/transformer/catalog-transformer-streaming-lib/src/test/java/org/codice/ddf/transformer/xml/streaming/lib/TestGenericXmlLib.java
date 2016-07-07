08fd0ffd150daacd9d21274f19a5d7878c2824a2
harrison-tarr <harrison.tarr@connexta.com>
2016-07-07 11:16:01 -0700

    DDF-2250 Make SaxEventToXmlConverter handle edge cases (#969)
    specifically nested redeclaration of a namespace prefix
    
    DDF-2250 Remove reliance on commons-lang3
    
    DDF-2250 Add lots of better unit tests
    
    DDF-2250 Improve unit tests
    
    DDF-2250 Add unit tests and fix bugs
    
    DDF-2250 Reformat unit test resources
    
    DDF-2250 Add another corner case unit test
    
    DDF-2250 Another edge case covered

--- catalog/transformer/catalog-transformer-streaming-lib/src/test/java/org/codice/ddf/transformer/xml/streaming/lib/TestGenericXmlLib.java
+++ catalog/transformer/catalog-transformer-streaming-lib/src/test/java/org/codice/ddf/transformer/xml/streaming/lib/TestGenericXmlLib.java
@@ -19,7 +19,6 @@ import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -27,18 +26,14 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.xml.stream.XMLStreamException;
-
 import org.codice.ddf.transformer.xml.streaming.SaxEventHandler;
 import org.codice.ddf.transformer.xml.streaming.SaxEventHandlerFactory;
 import org.junit.Test;
-import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 
 import ddf.catalog.data.Attribute;
@@ -200,84 +195,6 @@ public class TestGenericXmlLib {
         assertThat(inputTransformer.getOrganization(), is("foo"));
         assertThat(inputTransformer.getTitle(), is("foo"));
         assertThat(inputTransformer.getVersion(), is("foo"));
-    }
-
-    @Test
-    public void testSaxEventToXmlElementConverter()
-            throws UnsupportedEncodingException, XMLStreamException {
-        SaxEventToXmlElementConverter saxEventToXmlElementConverter =
-                new SaxEventToXmlElementConverter();
-        saxEventToXmlElementConverter.addNamespace("foo", "bar");
-        saxEventToXmlElementConverter.addNamespace("barfoo", "foobar");
-        Attributes attrs = mock(Attributes.class);
-        when(attrs.getLength()).thenReturn(2);
-        when(attrs.getLocalName(0)).thenReturn("foo");
-        when(attrs.getLocalName(1)).thenReturn("bar");
-        when(attrs.getURI(0)).thenReturn("foobar");
-        when(attrs.getURI(1)).thenReturn("");
-        when(attrs.getValue(anyInt())).thenReturn("test");
-        saxEventToXmlElementConverter.toElement("bar", "test", attrs);
-        saxEventToXmlElementConverter.toElement("&lt;chara&gt;cters".toCharArray(), 0, 16);
-        saxEventToXmlElementConverter.toElement("bar", "test");
-        saxEventToXmlElementConverter.toElement("bar", "test", attrs);
-        saxEventToXmlElementConverter.toElement("characters".toCharArray(), 0, 8);
-        saxEventToXmlElementConverter.toElement("bar", "test");
-        assertThat(saxEventToXmlElementConverter.toString(),
-                is("<foo:test xmlns:foo=\"bar\" xmlns:barfoo=\"foobar\" barfoo:foo=\"test\" bar=\"test\">&amp;lt;chara&amp;gt;cte</foo:test><foo:test xmlns:foo=\"bar\" xmlns:barfoo=\"foobar\" barfoo:foo=\"test\" bar=\"test\">characte</foo:test>"));
-        saxEventToXmlElementConverter.reset();
-        assertThat(saxEventToXmlElementConverter.toString(), is(""));
-    }
-
-    @Test
-    public void testSaxEventToXmlElementConverterEdgeCase()
-            throws UnsupportedEncodingException, XMLStreamException {
-        // set up mock Attribute object
-        Attributes attrs = mock(Attributes.class);
-        when(attrs.getLength()).thenReturn(2);
-        when(attrs.getLocalName(0)).thenReturn("foo");
-        when(attrs.getLocalName(1)).thenReturn("bar");
-        when(attrs.getURI(0)).thenReturn("ns1uri");
-        when(attrs.getURI(1)).thenReturn("");
-        when(attrs.getValue(anyInt())).thenReturn("test");
-        // inner attrs
-        Attributes attrs2 = mock(Attributes.class);
-        when(attrs2.getLength()).thenReturn(2);
-        when(attrs2.getLocalName(0)).thenReturn("foo");
-        when(attrs2.getLocalName(1)).thenReturn("bar");
-        when(attrs2.getURI(0)).thenReturn("ns1uri.v2");
-        when(attrs2.getURI(1)).thenReturn("");
-        when(attrs2.getValue(anyInt())).thenReturn("test");
-
-        SaxEventToXmlElementConverter converter = new SaxEventToXmlElementConverter();
-
-        // Simulate begin reading
-        converter.addNamespace("ns1", "ns1uri");
-        converter.addNamespace("ns2", "ns2uri");
-
-        // Write parent element and characters
-        converter.toElement("ns1uri", "element1", attrs);
-        converter.toElement("0123456789ABCDEF".toCharArray(), 0, 16);
-
-        // Declare new namespace, using same prefix as previous
-        converter.addNamespace("ns1", "ns1uri.v2");
-        converter.toElement("ns1uri.v2", "nestedElement", attrs2);
-        converter.toElement("ns1uri.v2", "nestedElement");
-
-        // Pop prefix
-        converter.removeNamespace("ns1");
-
-        converter.toElement("ns1uri", "element1", attrs);
-        converter.toElement("0123456789ABCDEF".toCharArray(), 0, 16);
-        converter.toElement("ns1uri", "element1");
-
-        // Finish writing parent
-        converter.toElement("ns1uri", "element1");
-
-        converter.toElement("ns1uri", "element1", attrs);
-        converter.toElement("0123456789ABCDEF".toCharArray(), 0, 16);
-        converter.toElement("ns1uri", "element1");
-        assertThat(converter.toString(),
-                is("<ns1:element1 xmlns:ns1=\"ns1uri\" ns1:foo=\"test\" bar=\"test\">0123456789ABCDEF<ns1:nestedElement xmlns:ns1=\"ns1uri.v2\" ns1:foo=\"test\" bar=\"test\"></ns1:nestedElement><ns1:element1 ns1:foo=\"test\" bar=\"test\">0123456789ABCDEF</ns1:element1></ns1:element1><ns1:element1 xmlns:ns1=\"ns1uri\" ns1:foo=\"test\" bar=\"test\">0123456789ABCDEF</ns1:element1>"));
     }
 
     @Test
