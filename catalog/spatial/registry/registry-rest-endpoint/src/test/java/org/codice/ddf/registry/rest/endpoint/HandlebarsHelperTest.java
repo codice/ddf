8ec1934bd1ee6cdd3699903aa9c7df0d6e1b96f9
Anisha Gupta <anisha.gupta@connexta.com>
2016-07-08 16:37:40 -0700

    DDF-2251 Refactor Registry Report Builder to be able to generate 'summaries' of parts of the overall report

---
+++ catalog/spatial/registry/registry-rest-endpoint/src/test/java/org/codice/ddf/registry/rest/endpoint/HandlebarsHelperTest.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.codice.ddf.registry.rest.endpoint;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+
+public class HandlebarsHelperTest {
+
+    private HandlebarsHelper handlebarsHelper;
+
+    @Before
+    public void setup() {
+        handlebarsHelper = new HandlebarsHelper();
+    }
+
+    @Test
+    public void testHelperSameValue() throws IOException {
+        HashMap map = new HashMap();
+        map.put("String1", "someString");
+        map.put("String2", "someString");
+        assertEquals("True", inlineTemplate().apply(map));
+    }
+
+    @Test
+    public void testHelperDifferentValue() throws IOException {
+        HashMap map = new HashMap();
+        map.put("String1", "someString");
+        map.put("String2", "differentString");
+        assertEquals("False", inlineTemplate().apply(map));
+    }
+
+    @Test
+    public void testHelperNullValue() throws IOException {
+        HashMap map = new HashMap();
+        map.put("String1", "someString");
+        map.put("String2", null);
+        assertEquals("False", inlineTemplate().apply(map));
+    }
+
+    @Test
+    public void testHelperNullValues() throws IOException {
+        HashMap map = new HashMap();
+        map.put("String1", null);
+        map.put("String2", null);
+        assertEquals("True", inlineTemplate().apply(map));
+    }
+
+    private Template inlineTemplate() throws IOException {
+        Handlebars handlebars = new Handlebars();
+        HandlebarsHelper helper = new HandlebarsHelper();
+        handlebars.registerHelpers(helper);
+        return handlebars.compileInline("{{#ifeq String1 String2 }}True{{else}}False{{/ifeq}}");
+    }
+
+}
--- catalog/spatial/registry/registry-rest-endpoint/src/test/java/org/codice/ddf/registry/rest/endpoint/RegistryRestEndpointTest.java
+++ catalog/spatial/registry/registry-rest-endpoint/src/test/java/org/codice/ddf/registry/rest/endpoint/RegistryRestEndpointTest.java
@@ -16,6 +16,7 @@ package org.codice.ddf.registry.rest.endpoint;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyList;
 import static org.mockito.Matchers.anySet;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doThrow;
@@ -23,8 +24,10 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.List;
 
 import javax.ws.rs.core.Response;
 
@@ -32,49 +35,49 @@ import org.codice.ddf.parser.ParserException;
 import org.codice.ddf.registry.federationadmin.service.FederationAdminException;
 import org.codice.ddf.registry.federationadmin.service.FederationAdminService;
 import org.codice.ddf.registry.federationadmin.service.RegistryPublicationService;
-import org.codice.ddf.registry.rest.endpoint.report.RegistryReportMapBuilder;
+import org.codice.ddf.registry.rest.endpoint.report.RegistryReportBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
+import ddf.catalog.data.Metacard;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.RegistryPackageType;
 
 public class RegistryRestEndpointTest {
+
+    private static final String REGISTRY_ID = "REGISTRY_ID";
+
+    private static final String DESTINATION_ID = "DESTINATION_ID";
 
     private FederationAdminService federationAdminService = mock(FederationAdminService.class);
 
     private RegistryRestEndpoint restEndpoint;
 
-    private RegistryReportMapBuilder reportMapBuilder = mock(RegistryReportMapBuilder.class);
+    private RegistryReportBuilder registryReportBuilder = mock(RegistryReportBuilder.class);
 
     private RegistryPublicationService registryPublicationService =
             mock(RegistryPublicationService.class);
 
     private RegistryPackageType registryPackage = mock(RegistryPackageType.class);
 
-    private static final String REGISTRY_ID = "REGISTRY_ID";
-
-    private static final String DESTINATION_ID = "DESTINATION_ID";
-
     @Before
     public void setup() {
         restEndpoint = new RegistryRestEndpoint();
         restEndpoint.setFederationAdminService(federationAdminService);
-        restEndpoint.setRegistryReportMapBuilder(reportMapBuilder);
+        restEndpoint.setRegistryReportBuilder(registryReportBuilder);
         restEndpoint.setRegistryPublicationService(registryPublicationService);
 
-        when(reportMapBuilder.buildRegistryMap(any(RegistryPackageType.class))).thenReturn(new HashMap<>());
     }
 
     @Test
-    public void testReportWithBlankMetacard() {
-        RegistryRestEndpoint reportViewer = new RegistryRestEndpoint();
-        Response response = reportViewer.viewRegInfoHtml(null, null);
+    public void testFullReportWithBlankMetacard() throws IOException {
+        Response response = restEndpoint.viewRegInfoHtml(null, null);
         assertThat(response.getStatusInfo()
                 .getStatusCode(), is(Response.Status.BAD_REQUEST.getStatusCode()));
     }
 
     @Test
-    public void testReportWithFederationAdminException() throws FederationAdminException {
+    public void testFullReportWithFederationAdminException()
+            throws FederationAdminException, IOException {
         when(federationAdminService.getRegistryObjectByRegistryId(anyString(), anySet())).thenThrow(
                 new FederationAdminException());
         Response response = restEndpoint.viewRegInfoHtml("metacardId", Arrays.asList("sourceIds"));
@@ -83,7 +86,8 @@ public class RegistryRestEndpointTest {
     }
 
     @Test
-    public void testReportRegistryPackageNotFound() throws FederationAdminException {
+    public void testFullReportRegistryPackageNotFound()
+            throws FederationAdminException, IOException {
         when(federationAdminService.getRegistryObjectByRegistryId(anyString(),
                 anySet())).thenReturn(null);
         Response response = restEndpoint.viewRegInfoHtml("metacardId", Arrays.asList("sourceIds"));
@@ -92,7 +96,8 @@ public class RegistryRestEndpointTest {
     }
 
     @Test
-    public void testReportWithRegistryPackage() throws ParserException, FederationAdminException {
+    public void testFullReportWithRegistryPackage()
+            throws ParserException, FederationAdminException, IOException {
         when(federationAdminService.getRegistryObjectByRegistryId(anyString(),
                 anySet())).thenReturn(registryPackage);
 
@@ -100,6 +105,141 @@ public class RegistryRestEndpointTest {
         assertThat(response.getStatusInfo()
                 .getStatusCode(), is(Response.Status.OK.getStatusCode()));
 
+    }
+
+    @Test
+    public void testFullReportWithIOException() throws IOException, FederationAdminException {
+
+        when(registryReportBuilder.getHtmlFromRegistryPackage(any(RegistryPackageType.class),
+                anyString())).thenThrow(new IOException());
+        when(federationAdminService.getRegistryObjectByRegistryId(anyString(),
+                anySet())).thenReturn(registryPackage);
+        Response response = restEndpoint.viewRegInfoHtml("metacardId", Arrays.asList("sourceIds"));
+        assertThat(response.getStatusInfo()
+                .getStatusCode(), is(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()));
+    }
+
+    @Test
+    public void testSectionReportWithBlankMetacard() throws IOException {
+        Response response = restEndpoint.viewSectionInfoHtml(null, null, null);
+        assertThat(response.getStatusInfo()
+                .getStatusCode(), is(Response.Status.BAD_REQUEST.getStatusCode()));
+    }
+
+    @Test
+    public void testSectionReportWithFederationAdminException()
+            throws FederationAdminException, IOException {
+        when(federationAdminService.getRegistryObjectByRegistryId(anyString(), anySet())).thenThrow(
+                new FederationAdminException());
+        Response response = restEndpoint.viewSectionInfoHtml("metacardId",
+                Arrays.asList("sourceIds"),
+                "organizations");
+        assertThat(response.getStatusInfo()
+                .getStatusCode(), is(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()));
+    }
+
+    @Test
+    public void testSectionReportWithUnknownSectionName()
+            throws FederationAdminException, IOException {
+        when(federationAdminService.getRegistryObjectByRegistryId(anyString(),
+                anySet())).thenReturn(registryPackage);
+        Response response = restEndpoint.viewSectionInfoHtml("metacardId",
+                Arrays.asList("sourceIds"),
+                "unknownSectionName");
+        assertThat(response.getStatusInfo()
+                .getStatusCode(), is(Response.Status.BAD_REQUEST.getStatusCode()));
+    }
+
+    @Test
+    public void testSectionReportWithSectionName()
+            throws ParserException, FederationAdminException, IOException {
+        when(federationAdminService.getRegistryObjectByRegistryId(anyString(),
+                anySet())).thenReturn(registryPackage);
+
+        Response response = restEndpoint.viewSectionInfoHtml("metacardId",
+                Arrays.asList("sourceIds"),
+                "organizations");
+        assertThat(response.getStatusInfo()
+                .getStatusCode(), is(Response.Status.OK.getStatusCode()));
+
+    }
+
+    @Test
+    public void testSectionReportWithIOException() throws IOException, FederationAdminException {
+
+        when(registryReportBuilder.getHtmlFromRegistryPackage(any(RegistryPackageType.class),
+                anyString())).thenThrow(new IOException());
+        when(federationAdminService.getRegistryObjectByRegistryId(anyString(),
+                anySet())).thenReturn(registryPackage);
+        Response response = restEndpoint.viewSectionInfoHtml("metacardId",
+                Arrays.asList("sourceIds"),
+                "organizations");
+        assertThat(response.getStatusInfo()
+                .getStatusCode(), is(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()));
+    }
+
+    @Test
+    public void testSectionReportRegistryPackageNotFound()
+            throws FederationAdminException, IOException {
+        when(federationAdminService.getRegistryObjectByRegistryId(anyString(),
+                anySet())).thenReturn(null);
+        Response response = restEndpoint.viewSectionInfoHtml("metacardId",
+                Arrays.asList("sourceIds"),
+                "organizations");
+        assertThat(response.getStatusInfo()
+                .getStatusCode(), is(Response.Status.NOT_FOUND.getStatusCode()));
+    }
+
+    @Test
+    public void testSummaryReportWithBlankRegistryID() throws IOException {
+        Response response = restEndpoint.viewSummaryInfoHtml(null);
+        assertThat(response.getStatusInfo()
+                .getStatusCode(), is(Response.Status.BAD_REQUEST.getStatusCode()));
+    }
+
+    @Test
+    public void testSummaryReportWithBlankMetacard() throws FederationAdminException, IOException {
+        Response response = restEndpoint.viewSummaryInfoHtml("metacardId");
+        when(federationAdminService.getRegistryMetacardsByRegistryIds(any(Collections.class).singletonList(
+                "metcardId"))).thenReturn(null);
+        assertThat(response.getStatusInfo()
+                .getStatusCode(), is(Response.Status.NOT_FOUND.getStatusCode()));
+    }
+
+    @Test
+    public void testSummaryReportWithMetacard()
+            throws ParserException, FederationAdminException, IOException {
+
+        List<Metacard> metacardList = mock(List.class);
+        when(federationAdminService.getRegistryMetacardsByRegistryIds(anyList())).thenReturn(
+                metacardList);
+        Response response = restEndpoint.viewSummaryInfoHtml("metacardId");
+        assertThat(response.getStatusInfo()
+                .getStatusCode(), is(Response.Status.OK.getStatusCode()));
+
+    }
+
+    @Test
+    public void testSummaryReportWithFederationAdminException()
+            throws FederationAdminException, IOException {
+        when(federationAdminService.getRegistryMetacardsByRegistryIds(anyList())).thenThrow(new FederationAdminException());
+
+        Response response = restEndpoint.viewSummaryInfoHtml("metacardId");
+        assertThat(response.getStatusInfo()
+                .getStatusCode(), is(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()));
+    }
+
+    @Test
+    public void testSummaryReportWithIOException() throws FederationAdminException, IOException {
+        List<Metacard> metacardList = mock(List.class);
+
+        when(federationAdminService.getRegistryMetacardsByRegistryIds(anyList())).thenReturn(
+                metacardList);
+        when(registryReportBuilder.getSummaryHtmlFromMetacard(any(Metacard.class))).thenThrow(new IOException());
+
+        Response response = restEndpoint.viewSummaryInfoHtml("metacardId");
+        assertThat(response.getStatusInfo()
+                .getStatusCode(), is(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()));
     }
 
     @Test
