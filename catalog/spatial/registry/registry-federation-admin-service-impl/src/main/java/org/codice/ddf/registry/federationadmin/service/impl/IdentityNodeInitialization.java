connexta/ddf-registry

DDF-2271 Updated registry components to retry catalog failed catalog operations on initialization.   - Three different components try to access the catalog on initialization which fails if the catalog provider hasn't come up yet. This can occur now that the registry can be started in the installation profile. Added retry logic so the registry will still function properly.

catalog/spatial/registry/registry-federation-admin-service-impl/src/main/java/org/codice/ddf/registry/federationadmin/service/impl/IdentityNodeInitialization.java
@@ -22,6 +22,8 @@ import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.util.UUID;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.xml.datatype.DatatypeConstants;
 
@@ -60,6 +62,8 @@ public class IdentityNodeInitialization {
 
     private static final String UNKNOWN_SITE_NAME = "Unknown Site Name";
 
+    private static final int RETRY_INTERVAL = 30;
+
     private FederationAdminService federationAdminService;
 
     private InputTransformer registryTransformer;
@@ -68,18 +72,27 @@ public class IdentityNodeInitialization {
 
     private SlotTypeHelper slotTypeHelper = new SlotTypeHelper();
 
+    private ScheduledExecutorService executorService;
+
     private InternationalStringTypeHelper internationalStringTypeHelper =
             new InternationalStringTypeHelper();
 
-    public void init() throws PrivilegedActionException {
-        Security.runAsAdminWithException(() -> {
-            if (!federationAdminService.getLocalRegistryIdentityMetacard()
-                    .isPresent()) {
-                createIdentityNode();
-            }
-            return null;
-        });
+    public void init() {
+        try {
+            Security.runAsAdminWithException(() -> {
+                if (!federationAdminService.getLocalRegistryIdentityMetacard().isPresent()) {
+                    createIdentityNode();
+                }
+                return null;
+            });
+        } catch (PrivilegedActionException e) {
+            LOGGER.warn("Error checking for local registry identity node. Will try again later");
+            executorService.schedule(this::init, RETRY_INTERVAL, TimeUnit.SECONDS);
+        }
+    }
 
+    public void destroy() {
+        executorService.shutdown();
     }
 
     private void createIdentityNode() throws FederationAdminException {
@@ -149,6 +162,7 @@ public class IdentityNodeInitialization {
 
             federationAdminService.addRegistryEntry(identityMetacard);
         }
+        LOGGER.info("Successfully create registry identity node: {}", registryPackageId);
     }
 
     private Metacard getRegistryMetacardFromRegistryPackage(RegistryPackageType registryPackage)
@@ -178,5 +192,9 @@ public class IdentityNodeInitialization {
 
     public void setFederationAdminService(FederationAdminService federationAdminService) {
         this.federationAdminService = federationAdminService;
+    }
+
+    public void setExecutorService(ScheduledExecutorService executorService) {
+        this.executorService = executorService;
     }
 }

catalog/spatial/registry/registry-publication-action-provider/src/test/java/org/codice/ddf/registry/publication/manager/RegistryPublicationManagerTest.java
@@ -16,6 +16,9 @@ package org.codice.ddf.registry.publication.manager;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -24,10 +27,13 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.StringUtils;
 import org.codice.ddf.registry.common.RegistryConstants;
 import org.codice.ddf.registry.common.metacard.RegistryObjectMetacardType;
+import org.codice.ddf.registry.federationadmin.service.FederationAdminException;
 import org.codice.ddf.registry.federationadmin.service.FederationAdminService;
 import org.junit.Before;
 import org.junit.Test;
@@ -56,12 +62,16 @@ public class RegistryPublicationManagerTest {
     @Mock
     private FederationAdminService federationAdmin;
 
+    @Mock
+    private ScheduledExecutorService executorService;
+
     private RegistryPublicationManager publicationManager;
 
     @Before
     public void setup() throws Exception {
         publicationManager = new RegistryPublicationManager();
         publicationManager.setFederationAdminService(federationAdmin);
+        publicationManager.setExecutorService(executorService);
     }
 
     @Test
@@ -84,6 +94,16 @@ public class RegistryPublicationManagerTest {
         assertThat(publications.get("regId1"), equalTo(locations));
         assertThat(publications.get("regId2")
                 .size(), is(0));
+    }
+
+    @Test
+    public void tstInitFederationAdminException() throws Exception {
+        when(federationAdmin.getRegistryMetacards())
+                .thenThrow(new FederationAdminException("Test error"));
+        publicationManager.init();
+        verify(executorService).schedule(any(Runnable.class), anyLong(), any(TimeUnit.class));
+        publicationManager.destroy();
+        verify(executorService).shutdown();
     }
 
     @Test

codice/ddf

catalog/spatial/registry/registry-federation-admin-service-impl/src/main/java/org/codice/ddf/registry/federationadmin/service/impl/IdentityNodeInitialization.java
@@ -22,6 +22,8 @@ import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.util.UUID;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.xml.datatype.DatatypeConstants;
 
@@ -60,6 +62,8 @@ public class IdentityNodeInitialization {
 
     private static final String UNKNOWN_SITE_NAME = "Unknown Site Name";
 
+    private static final int RETRY_INTERVAL = 30;
+
     private FederationAdminService federationAdminService;
 
     private InputTransformer registryTransformer;
@@ -68,18 +72,27 @@ public class IdentityNodeInitialization {
 
     private SlotTypeHelper slotTypeHelper = new SlotTypeHelper();
 
+    private ScheduledExecutorService executorService;
+
     private InternationalStringTypeHelper internationalStringTypeHelper =
             new InternationalStringTypeHelper();
 
-    public void init() throws PrivilegedActionException {
-        Security.runAsAdminWithException(() -> {
-            if (!federationAdminService.getLocalRegistryIdentityMetacard()
-                    .isPresent()) {
-                createIdentityNode();
-            }
-            return null;
-        });
+    public void init() {
+        try {
+            Security.runAsAdminWithException(() -> {
+                if (!federationAdminService.getLocalRegistryIdentityMetacard().isPresent()) {
+                    createIdentityNode();
+                }
+                return null;
+            });
+        } catch (PrivilegedActionException e) {
+            LOGGER.warn("Error checking for local registry identity node. Will try again later");
+            executorService.schedule(this::init, RETRY_INTERVAL, TimeUnit.SECONDS);
+        }
+    }
 
+    public void destroy() {
+        executorService.shutdown();
     }
 
     private void createIdentityNode() throws FederationAdminException {
@@ -149,6 +162,7 @@ public class IdentityNodeInitialization {
 
             federationAdminService.addRegistryEntry(identityMetacard);
         }
+        LOGGER.info("Successfully created registry identity node: {}", registryPackageId);
     }
 
     private Metacard getRegistryMetacardFromRegistryPackage(RegistryPackageType registryPackage)
@@ -178,5 +192,9 @@ public class IdentityNodeInitialization {
 
     public void setFederationAdminService(FederationAdminService federationAdminService) {
         this.federationAdminService = federationAdminService;
+    }
+
+    public void setExecutorService(ScheduledExecutorService executorService) {
+        this.executorService = executorService;
     }
 }

catalog/spatial/registry/registry-publication-action-provider/src/test/java/org/codice/ddf/registry/publication/manager/RegistryPublicationManagerTest.java
@@ -16,6 +16,9 @@ package org.codice.ddf.registry.publication.manager;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -24,10 +27,13 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.StringUtils;
 import org.codice.ddf.registry.common.RegistryConstants;
 import org.codice.ddf.registry.common.metacard.RegistryObjectMetacardType;
+import org.codice.ddf.registry.federationadmin.service.FederationAdminException;
 import org.codice.ddf.registry.federationadmin.service.FederationAdminService;
 import org.junit.Before;
 import org.junit.Test;
@@ -56,12 +62,16 @@ public class RegistryPublicationManagerTest {
     @Mock
     private FederationAdminService federationAdmin;
 
+    @Mock
+    private ScheduledExecutorService executorService;
+
     private RegistryPublicationManager publicationManager;
 
     @Before
     public void setup() throws Exception {
         publicationManager = new RegistryPublicationManager();
         publicationManager.setFederationAdminService(federationAdmin);
+        publicationManager.setExecutorService(executorService);
     }
 
     @Test
@@ -84,6 +94,16 @@ public class RegistryPublicationManagerTest {
         assertThat(publications.get("regId1"), equalTo(locations));
         assertThat(publications.get("regId2")
                 .size(), is(0));
+    }
+
+    @Test
+    public void testInitFederationAdminException() throws Exception {
+        when(federationAdmin.getRegistryMetacards())
+                .thenThrow(new FederationAdminException("Test error"));
+        publicationManager.init();
+        verify(executorService).schedule(any(Runnable.class), anyLong(), any(TimeUnit.class));
+        publicationManager.destroy();
+        verify(executorService).shutdown();
     }
 
     @Test