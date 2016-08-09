b70b6db8cf843ac5e5c14029b7761f09428e9515
Joseph North <joernorth@gmail.com>
2016-08-09 06:03:33 -0700

    DDF-2333 Add a command for seeding the metacard and product caches (#1054)
    * DDF-2333 Add a command for seeding the metacard and product caches
    
    * Switch to new karaf command annotations
    
    * Separate description details into detailedDescription
    
    * Address code review comments

---
+++ catalog/core/catalog-core-commands/src/main/java/org/codice/ddf/commands/catalog/SeedCommand.java
@@ -0,0 +1,241 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.codice.ddf.commands.catalog;
+
+import static java.util.AbstractMap.SimpleEntry;
+import static java.util.Map.Entry;
+import static java.util.stream.Collectors.toList;
+import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
+import static org.apache.commons.lang.StringUtils.isNotBlank;
+import static ddf.catalog.data.Metacard.ANY_TEXT;
+import static ddf.catalog.filter.FilterDelegate.WILDCARD_CHAR;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Option;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.geotools.filter.text.cql2.CQL;
+import org.opengis.filter.Filter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ddf.catalog.CatalogFramework;
+import ddf.catalog.data.Attribute;
+import ddf.catalog.data.Metacard;
+import ddf.catalog.data.Result;
+import ddf.catalog.filter.FilterBuilder;
+import ddf.catalog.operation.QueryResponse;
+import ddf.catalog.operation.ResourceRequest;
+import ddf.catalog.operation.ResourceResponse;
+import ddf.catalog.operation.impl.QueryImpl;
+import ddf.catalog.operation.impl.QueryRequestImpl;
+import ddf.catalog.operation.impl.ResourceRequestById;
+import ddf.catalog.resource.ResourceNotFoundException;
+import ddf.catalog.resource.ResourceNotSupportedException;
+
+//@formatter:off
+@Service
+@Command(
+        scope = CatalogCommands.NAMESPACE,
+        name = "seed",
+        description = "Seeds the local metacard and resource caches from the enterprise or from specific federated sources.",
+        detailedDescription = "This command will query the enterprise or the specified sources for metacards in increments "
+                + "the size of the `--resource-limit` argument (default 20) until that number of resource downloads "
+                + "have started or until there are no more results. Local resources will not be added to the cache. "
+                + "This command will not re-download resources that are up-to-date in the cache, so subsequent runs "
+                + "of the command will attempt to cache metacards and resources that have not already been cached.\n\n"
+                + "Note: this command will trigger resource downloads in the background and they may continue after "
+                + "control is returned to the console. Also, resource caching must be enabled in the catalog framework "
+                + "for this command to seed the resource cache.")
+//@formatter:on
+public class SeedCommand extends SubjectCommands implements Action {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SeedCommand.class);
+
+    private static final Map<String, Serializable> CACHE_UPDATE_PROPERTIES =
+            Collections.singletonMap("mode", "update");
+
+    private static final String RESOURCE_CACHE_STATUS = "internal.local-resource";
+
+    private static final ThreadPoolExecutor EXECUTOR;
+
+    static {
+        EXECUTOR = new ThreadPoolExecutor(20,
+                20,
+                30,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>());
+        EXECUTOR.allowCoreThreadTimeOut(true);
+    }
+
+    @Option(name = "--source", multiValued = true, aliases = {
+            "-s"}, description = "Source(s) to query (e.g., -s source1 -s source2). Default is to query the entire enterprise.")
+    List<String> sources;
+
+    @Option(name = "--cql", description = "CQL expression specifying the metacards to seed.\n"
+            + "CQL Examples:\n" + "\tTextual:   seed --cql \"title like 'some text'\"\n"
+            + "\tTemporal:  seed --cql \"modified before 2012-09-01T12:30:00Z\"\n"
+            + "\tSpatial:   seed --cql \"DWITHIN(location, POINT (1 2), 10, kilometers)\"\n"
+            + "\tComplex:   seed --cql \"title like 'some text' AND modified before 2012-09-01T12:30:00Z\"")
+    String cql;
+
+    @Option(name = "--resource-limit", aliases = {"-rl"}, description =
+            "The maximum number of resources to download to the cache. The number of metacards cached "
+                    + "might not be equal to this number because some metacards may not have associated "
+                    + "resources.")
+    int resourceLimit = 20;
+
+    @Reference
+    CatalogFramework framework;
+
+    @Reference
+    FilterBuilder filterBuilder;
+
+    private final Predicate<Metacard> isNonCachedResource = metacard -> {
+        Attribute cached = metacard.getAttribute(RESOURCE_CACHE_STATUS);
+        return cached == null || !((Boolean) cached.getValue());
+    };
+
+    private Filter createFilter() throws Exception {
+        if (isNotBlank(cql)) {
+            return CQL.toFilter(cql);
+        } else {
+            return filterBuilder.attribute(ANY_TEXT)
+                    .is()
+                    .like()
+                    .text(WILDCARD_CHAR);
+        }
+    }
+
+    @Override
+    public Object execute() throws Exception {
+        return doExecute();
+    }
+
+    @Override
+    protected Object executeWithSubject() throws Exception {
+        if (resourceLimit <= 0) {
+            printErrorMessage("A limit of " + resourceLimit
+                    + " was supplied. The limit must be greater than 0.");
+            return null;
+        }
+
+        final Filter filter = createFilter();
+
+        final long start = System.currentTimeMillis();
+        int resourceDownloads = 0;
+        int downloadErrors = 0;
+        int pageCount = 0;
+
+        while (resourceDownloads < resourceLimit) {
+            final QueryImpl query = new QueryImpl(filter);
+            query.setPageSize(resourceLimit);
+            query.setStartIndex(pageCount * resourceLimit + 1);
+            ++pageCount;
+
+            final QueryRequestImpl queryRequest;
+            if (isNotEmpty(sources)) {
+                queryRequest = new QueryRequestImpl(query, sources);
+            } else {
+                queryRequest = new QueryRequestImpl(query, true);
+            }
+            queryRequest.setProperties(new HashMap<>(CACHE_UPDATE_PROPERTIES));
+
+            final QueryResponse queryResponse = framework.query(queryRequest);
+
+            if (queryResponse.getResults()
+                    .isEmpty()) {
+                break;
+            }
+
+            final List<Entry<? extends ResourceRequest, String>> resourceRequests =
+                    queryResponse.getResults()
+                            .stream()
+                            .map(Result::getMetacard)
+                            .filter(isNonCachedResource)
+                            .map(metacard -> new SimpleEntry<>(new ResourceRequestById(metacard.getId()),
+                                    metacard.getSourceId()))
+                            .collect(toList());
+
+            for (Entry<? extends ResourceRequest, String> requestAndSourceId : resourceRequests) {
+                final ResourceRequest request = requestAndSourceId.getKey();
+                try {
+                    ResourceResponse response = framework.getResource(request,
+                            requestAndSourceId.getValue());
+                    ++resourceDownloads;
+                    EXECUTOR.execute(new ResourceCloseHandler(response));
+                } catch (IOException | ResourceNotFoundException | ResourceNotSupportedException e) {
+                    ++downloadErrors;
+                    LOGGER.warn("Could not download resource for metacard [id={}]",
+                            request.getAttributeValue(),
+                            e);
+                }
+
+                printProgressAndFlush(start, resourceLimit, resourceDownloads);
+
+                if (resourceDownloads == resourceLimit) {
+                    break;
+                }
+            }
+        }
+
+        printProgressAndFlush(start, resourceDownloads, resourceDownloads);
+
+        console.println();
+        if (downloadErrors > 0) {
+            printErrorMessage(downloadErrors
+                    + " resource download(s) had errors. Check the logs for details.");
+        }
+        printSuccessMessage(
+                "Done seeding. " + resourceDownloads + " resource download(s) started.");
+
+        return null;
+    }
+
+    private static class ResourceCloseHandler implements Runnable {
+        private final InputStream resourceStream;
+
+        private ResourceCloseHandler(ResourceResponse response) {
+            resourceStream = response.getResource()
+                    .getInputStream();
+        }
+
+        @Override
+        public void run() {
+            byte[] buffer = new byte[4096];
+            try {
+                while (resourceStream.read(buffer, 0, buffer.length) != -1) {
+                    if (Thread.interrupted()) {
+                        return;
+                    }
+                }
+                resourceStream.close();
+            } catch (IOException e) {
+                LOGGER.debug("Error reading resource input stream.", e);
+            }
+        }
+    }
+}
---
+++ catalog/core/catalog-core-commands/src/test/java/org/codice/ddf/commands/catalog/TestSeedCommand.java
@@ -0,0 +1,302 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.codice.ddf.commands.catalog;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import org.geotools.filter.text.cql2.CQL;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatcher;
+
+import ddf.catalog.CatalogFramework;
+import ddf.catalog.data.Metacard;
+import ddf.catalog.data.Result;
+import ddf.catalog.data.impl.MetacardImpl;
+import ddf.catalog.filter.proxy.builder.GeotoolsFilterBuilder;
+import ddf.catalog.operation.Query;
+import ddf.catalog.operation.QueryRequest;
+import ddf.catalog.operation.QueryResponse;
+import ddf.catalog.operation.ResourceRequest;
+import ddf.catalog.operation.ResourceResponse;
+import ddf.catalog.resource.Resource;
+
+public class TestSeedCommand extends TestAbstractCommand {
+    private SeedCommand seedCommand;
+
+    private ConsoleOutput consoleOutput;
+
+    private CatalogFramework framework;
+
+    @Before
+    public void setUp() throws Exception {
+        consoleOutput = new ConsoleOutput();
+        consoleOutput.interceptSystemOut();
+
+        seedCommand = new SeedCommand();
+
+        framework = mock(CatalogFramework.class);
+
+        doReturn(mockResourceResponse()).when(framework)
+                .getResource(any(ResourceRequest.class), anyString());
+
+        seedCommand.framework = framework;
+
+        seedCommand.filterBuilder = new GeotoolsFilterBuilder();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        consoleOutput.resetSystemOut();
+        consoleOutput.closeBuffer();
+    }
+
+    @Test
+    public void testBadResourceLimit() throws Exception {
+        seedCommand.resourceLimit = 0;
+        seedCommand.executeWithSubject();
+        assertThat(consoleOutput.getOutput(), containsString("The limit must be greater than 0."));
+    }
+
+    @Test
+    public void testEnterprise() throws Exception {
+        mockQueryResponse(1, new String[0], new boolean[0]);
+        runCommandAndVerifyQueryRequest(request -> assertThat(request.isEnterprise(), is(true)));
+    }
+
+    @Test
+    public void testSources() throws Exception {
+        final String source1 = "source1";
+        final String source2 = "source2";
+        seedCommand.sources = newArrayList(source1, source2);
+
+        mockQueryResponse(1, new String[0], new boolean[0]);
+
+        runCommandAndVerifyQueryRequest(request -> {
+            assertThat(request.isEnterprise(), is(false));
+            assertThat(request.getSourceIds(), is(newHashSet(source1, source2)));
+        });
+    }
+
+    @Test
+    public void testCql() throws Exception {
+        final String cql = "modified AFTER 2016-07-21T00:00:00Z";
+        seedCommand.cql = cql;
+
+        mockQueryResponse(1, new String[0], new boolean[0]);
+
+        runCommandAndVerifyQueryRequest(request -> {
+            Query query = request.getQuery();
+            assertThat(CQL.toCQL(query), is(cql));
+        });
+    }
+
+    @Test
+    public void testNoCql() throws Exception {
+        mockQueryResponse(1, new String[0], new boolean[0]);
+
+        runCommandAndVerifyQueryRequest(request -> {
+            Query query = request.getQuery();
+            assertThat(CQL.toCQL(query), is("anyText ILIKE '*'"));
+        });
+    }
+
+    @Test
+    public void testCacheMetacards() throws Exception {
+        mockQueryResponse(1, new String[0], new boolean[0]);
+
+        runCommandAndVerifyQueryRequest(request -> assertThat(request.getProperties(),
+                hasEntry("mode", "update")));
+    }
+
+    private void runCommandAndVerifyQueryRequest(Consumer<QueryRequest> queryRequestAssertions)
+            throws Exception {
+        seedCommand.executeWithSubject();
+
+        ArgumentCaptor<QueryRequest> queryCaptor = ArgumentCaptor.forClass(QueryRequest.class);
+        verify(framework).query(queryCaptor.capture());
+
+        QueryRequest request = queryCaptor.getValue();
+        queryRequestAssertions.accept(request);
+    }
+
+    @Test
+    public void testResourceLimit() throws Exception {
+        final int limit = 2;
+        seedCommand.resourceLimit = limit;
+
+        final String id1 = "1";
+        final String id2 = "2";
+        mockQueryResponse(limit * 2 + 1, new String[] {id1, id2}, new boolean[] {false, false});
+
+        runCommandAndVerifyResourceRequests(limit, resourceRequests -> {
+            assertThat(resourceRequests, hasSize(limit));
+            verifyResourceRequest(resourceRequests.get(0), id1);
+            verifyResourceRequest(resourceRequests.get(1), id2);
+        }, siteNames -> assertThat(siteNames, is(newArrayList(id1, id2))));
+
+        verify(framework, times(1)).query(any(QueryRequest.class));
+    }
+
+    @Test
+    public void testFewerResourcesThanLimit() throws Exception {
+        final int limit = 10;
+        seedCommand.resourceLimit = limit;
+
+        final String id1 = "1";
+        final String id2 = "2";
+        final String id3 = "3";
+        mockQueryResponse(limit + 1,
+                new String[] {id1, id2, id3},
+                new boolean[] {false, false, false});
+
+        final int expectedResourceRequests = 3;
+        runCommandAndVerifyResourceRequests(expectedResourceRequests, resourceRequests -> {
+            assertThat(resourceRequests, hasSize(expectedResourceRequests));
+            verifyResourceRequest(resourceRequests.get(0), id1);
+            verifyResourceRequest(resourceRequests.get(1), id2);
+            verifyResourceRequest(resourceRequests.get(2), id3);
+        }, siteNames -> assertThat(siteNames, is(newArrayList(id1, id2, id3))));
+
+        verify(framework, times(2)).query(any(QueryRequest.class));
+    }
+
+    @Test
+    public void testDoesNotDownloadCachedResource() throws Exception {
+        final int limit = 3;
+        seedCommand.resourceLimit = limit;
+
+        final String id1 = "1";
+        final String id2 = "2";
+        mockQueryResponse(limit * 2 + 1,
+                new String[] {id1, id2, "3"},
+                new boolean[] {false, false, true});
+        final int expectedResourceRequests = 3;
+
+        runCommandAndVerifyResourceRequests(expectedResourceRequests, resourceRequests -> {
+            assertThat(resourceRequests, hasSize(expectedResourceRequests));
+            verifyResourceRequest(resourceRequests.get(0), id1);
+            verifyResourceRequest(resourceRequests.get(1), id2);
+            verifyResourceRequest(resourceRequests.get(2), id1);
+        }, siteNames -> assertThat(siteNames, is(newArrayList(id1, id2, id1))));
+
+        verify(framework, times(2)).query(any(QueryRequest.class));
+    }
+
+    private void runCommandAndVerifyResourceRequests(int expectedResourceRequests,
+            Consumer<List<ResourceRequest>> requestAssertions,
+            Consumer<List<String>> siteNameAssertions) throws Exception {
+        seedCommand.executeWithSubject();
+
+        ArgumentCaptor<ResourceRequest> resourceRequestCaptor = ArgumentCaptor.forClass(
+                ResourceRequest.class);
+        ArgumentCaptor<String> siteNameCaptor = ArgumentCaptor.forClass(String.class);
+        verify(framework,
+                times(expectedResourceRequests)).getResource(resourceRequestCaptor.capture(),
+                siteNameCaptor.capture());
+
+        List<ResourceRequest> resourceRequests = resourceRequestCaptor.getAllValues();
+        requestAssertions.accept(resourceRequests);
+
+        List<String> siteNames = siteNameCaptor.getAllValues();
+        siteNameAssertions.accept(siteNames);
+    }
+
+    private void mockQueryResponse(int stopReturningResultsAtIndex, String[] ids, boolean[] cached)
+            throws Exception {
+        List<Result> results = new ArrayList<>();
+        for (int i = 0; i < ids.length; ++i) {
+            String id = ids[i];
+            Result mockResult = mock(Result.class);
+            MetacardImpl metacard = new MetacardImpl();
+            metacard.setId(id);
+            metacard.setSourceId(id);
+            metacard.setAttribute("internal.local-resource", cached[i]);
+            when(mockResult.getMetacard()).thenReturn(metacard);
+            results.add(mockResult);
+        }
+
+        QueryResponse response = mock(QueryResponse.class);
+        when(response.getResults()).thenReturn(results);
+        doReturn(response).when(framework)
+                .query(argThat(is(queryWithStartIndex(request -> request.getQuery()
+                        .getStartIndex() < stopReturningResultsAtIndex))));
+
+        QueryResponse noResults = mock(QueryResponse.class);
+        when(noResults.getResults()).thenReturn(Collections.emptyList());
+        doReturn(noResults).when(framework)
+                .query(argThat(is(queryWithStartIndex(request -> request.getQuery()
+                        .getStartIndex() >= stopReturningResultsAtIndex))));
+    }
+
+    private QueryWithStartIndex queryWithStartIndex(Predicate<QueryRequest> test) {
+        return new QueryWithStartIndex(test);
+    }
+
+    private class QueryWithStartIndex extends ArgumentMatcher<QueryRequest> {
+        private final Predicate<QueryRequest> test;
+
+        private QueryWithStartIndex(Predicate<QueryRequest> test) {
+            this.test = test;
+        }
+
+        @Override
+        public boolean matches(Object o) {
+            return test.test((QueryRequest) o);
+        }
+    }
+
+    private void verifyResourceRequest(ResourceRequest request, String expectedAttributeValue) {
+        assertThat(request.getAttributeName(), is(Metacard.ID));
+        assertThat(request.getAttributeValue(), is(expectedAttributeValue));
+    }
+
+    private ResourceResponse mockResourceResponse() throws IOException {
+        InputStream mockInputStream = mock(InputStream.class);
+        doReturn(-1).when(mockInputStream)
+                .read(any(byte[].class), anyInt(), anyInt());
+
+        Resource mockResource = mock(Resource.class);
+        when(mockResource.getInputStream()).thenReturn(mockInputStream);
+
+        ResourceResponse mockResponse = mock(ResourceResponse.class);
+        when(mockResponse.getResource()).thenReturn(mockResource);
+        return mockResponse;
+    }
+}
