dc16e227ef77c660fbe81b8e121594f615cce17c
Anisha Gupta <ani6gup@gmail.com>
2016-08-11 14:40:17 -0700

    DDF-2251 Refactor Registry Report Builder to be able to generate 'summaries' of parts of the overall report (#1053)

--- catalog/spatial/registry/registry-rest-endpoint/src/main/java/org/codice/ddf/registry/rest/endpoint/RegistryRestEndpoint.java
+++ catalog/spatial/registry/registry-rest-endpoint/src/main/java/org/codice/ddf/registry/rest/endpoint/RegistryRestEndpoint.java
@@ -15,9 +15,10 @@
 package org.codice.ddf.registry.rest.endpoint;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.ws.rs.DELETE;
@@ -35,14 +36,11 @@ import org.apache.commons.lang.StringUtils;
 import org.codice.ddf.registry.federationadmin.service.FederationAdminException;
 import org.codice.ddf.registry.federationadmin.service.FederationAdminService;
 import org.codice.ddf.registry.federationadmin.service.RegistryPublicationService;
-import org.codice.ddf.registry.rest.endpoint.report.RegistryReportMapBuilder;
+import org.codice.ddf.registry.rest.endpoint.report.RegistryReportBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.github.jknack.handlebars.Handlebars;
-import com.github.jknack.handlebars.Template;
-import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
-
+import ddf.catalog.data.Metacard;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.RegistryPackageType;
 
 @Path("/")
@@ -51,17 +49,11 @@ public class RegistryRestEndpoint {
 
     private FederationAdminService federationAdminService;
 
-    private ClassPathTemplateLoader templateLoader;
-
     private RegistryPublicationService registryPublicationService;
 
-    private RegistryReportMapBuilder reportMapBuilder;
+    private RegistryReportBuilder registryReportBuilder;
 
-    public RegistryRestEndpoint() {
-        templateLoader = new ClassPathTemplateLoader();
-        templateLoader.setPrefix("/templates");
-        templateLoader.setSuffix(".hbt");
-    }
+    List<String> validSections = Arrays.asList(registryReportBuilder.ORGANIZATIONS);
 
     @Path("/{registryId}/publication/{sourceId}")
     @POST
@@ -69,16 +61,19 @@ public class RegistryRestEndpoint {
             @PathParam("sourceId") String destinationId) {
 
         if (!isValidPublishUnpublishParams(registryId, destinationId)) {
+            String message = "Registry ID and destination ID cannot be blank";
             return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Registry ID and destination ID cannot be blank")
+                    .entity(getErrorHtmlString(message))
                     .build();
         }
 
         try {
             registryPublicationService.publish(registryId, destinationId);
         } catch (FederationAdminException e) {
+            String message = String.format("Error publishing node from destination %s",
+                    destinationId);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity("Error publishing node to destination " + destinationId)
+                    .entity(getErrorHtmlString(message))
                     .build();
         }
 
@@ -91,16 +86,19 @@ public class RegistryRestEndpoint {
     public Response unpublish(@PathParam("registryId") String registryId,
             @PathParam("sourceId") String destinationId) {
         if (!isValidPublishUnpublishParams(registryId, destinationId)) {
+            String message = "Registry ID and destination ID cannot be blank";
             return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Registry ID and destination ID cannot be blank")
+                    .entity(getErrorHtmlString(message))
                     .build();
         }
 
         try {
             registryPublicationService.unpublish(registryId, destinationId);
         } catch (FederationAdminException e) {
+            String message = String.format("Error unpublishing node from destination %s",
+                    destinationId);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity("Error unpublishing node from destination " + destinationId)
+                    .entity(getErrorHtmlString(message))
                     .build();
         }
 
@@ -115,8 +113,9 @@ public class RegistryRestEndpoint {
             @QueryParam("sourceId") final List<String> sourceIds) {
         String html = "";
         if (StringUtils.isBlank(registryId)) {
+            String message = "Registry ID cannot be blank";
             return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Registry Id cannot be blank")
+                    .entity(getErrorHtmlString(message))
                     .build();
         }
 
@@ -135,7 +134,7 @@ public class RegistryRestEndpoint {
                     registryId,
                     sourceIds);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity(message)
+                    .entity(getErrorHtmlString(message))
                     .build();
         }
 
@@ -146,22 +145,155 @@ public class RegistryRestEndpoint {
                     registryId,
                     sourceIds);
             return Response.status(Response.Status.NOT_FOUND)
-                    .entity(message)
+                    .entity(getErrorHtmlString(message))
                     .build();
         }
 
-        Map<String, Object> registryMap = reportMapBuilder.buildRegistryMap(registryPackage);
-
         try {
-            Handlebars handlebars = new Handlebars(templateLoader);
-            Template template = handlebars.compile("report");
-            html = template.apply(registryMap);
+            html = registryReportBuilder.getHtmlFromRegistryPackage(registryPackage,
+                    registryReportBuilder.REPORT);
         } catch (IOException e) {
-            LOGGER.error("Error compiling and applying report template.");
+            String message = "Error in compiling and applying report template.";
+            LOGGER.error("{} For registry id: '{}'", message, registryId);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(getErrorHtmlString(message))
+                    .build();
         }
 
         return Response.ok(html)
                 .build();
+    }
+
+    @Path("/{registryId}/report/summary")
+    @Produces(MediaType.TEXT_HTML)
+    @GET
+    public Response viewSummaryInfoHtml(@PathParam("registryId") final String registryId) {
+        String html = "";
+        Metacard metacard;
+
+        if (StringUtils.isBlank(registryId)) {
+            String message = "Registry ID cannot be blank";
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(getErrorHtmlString(message))
+                    .build();
+        }
+        try {
+            List<Metacard> metacardList =
+                    federationAdminService.getRegistryMetacardsByRegistryIds((Collections.singletonList(
+                            registryId)));
+            if (CollectionUtils.isNotEmpty(metacardList)) {
+                metacard = metacardList.get(0);
+            } else {
+                String message = "Could not retrieve Metacard";
+                LOGGER.error("{} For registry id: '{}'", message, registryId);
+                return Response.status(Response.Status.NOT_FOUND)
+                        .entity(getErrorHtmlString(message))
+                        .build();
+            }
+        } catch (FederationAdminException e) {
+            String message = "Error getting Metacard.";
+            LOGGER.error("{} For registry id: '{}'", message, registryId);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(getErrorHtmlString(message))
+                    .build();
+        }
+
+        try {
+            html = registryReportBuilder.getSummaryHtmlFromMetacard(metacard);
+        } catch (IOException e) {
+            String message = "Error in compiling and applying summary template.";
+            LOGGER.error("{} For registry id: '{}'", message, registryId);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(getErrorHtmlString(message))
+                    .build();
+        }
+
+        return Response.ok(html)
+                .build();
+    }
+
+    @Path("/{registryId}/report/{section}")
+    @Produces(MediaType.TEXT_HTML)
+    @GET
+    public Response viewSectionInfoHtml(@PathParam("registryId") final String registryId,
+            @QueryParam("sourceId") final List<String> sourceIds,
+            @PathParam("section") final String section) {
+        String html = "";
+        if (StringUtils.isBlank(registryId)) {
+            String message = "Registry ID cannot be blank";
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(getErrorHtmlString(message))
+                    .build();
+        }
+
+        if (!isValidSection(section)) {
+
+            String message =
+                    String.format("'%s' is an unknown section name. Valid Sections are: %s",
+                            section,
+                            validSections);
+            LOGGER.error("{} For registry id: '{}'", message, registryId);
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(getErrorHtmlString(message))
+                    .build();
+        }
+        RegistryPackageType registryPackage;
+        try {
+            Set<String> sourceIdsSet = new HashSet<>();
+            if (CollectionUtils.isNotEmpty(sourceIds)) {
+                sourceIdsSet.addAll(sourceIds);
+            }
+            registryPackage = federationAdminService.getRegistryObjectByRegistryId(registryId,
+                    sourceIdsSet);
+        } catch (FederationAdminException e) {
+            String message = "Error getting registry package.";
+            LOGGER.error("{} For registry id: '{}', optional sources: {}",
+                    message,
+                    registryId,
+                    sourceIds);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(getErrorHtmlString(message))
+                    .build();
+        }
+
+        if (registryPackage == null) {
+            String message = "No registry package was found.";
+            LOGGER.error("{} For registry id: '{}', optional source ids: {}.",
+                    message,
+                    registryId,
+                    sourceIds);
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity(getErrorHtmlString(message))
+                    .build();
+        }
+
+        try {
+            html = registryReportBuilder.getHtmlFromRegistryPackage(registryPackage, section);
+        } catch (IOException e) {
+            String message = String.format("Error when compiling and applying %s template.",
+                    section);
+            LOGGER.error("{} For registry id: '{}'", message, registryId);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(getErrorHtmlString(message))
+                    .build();
+        }
+
+        return Response.ok(html)
+                .build();
+    }
+
+    private String getErrorHtmlString(String message) {
+        try {
+            return registryReportBuilder.getErrorHtml(message);
+        } catch (IOException e) {
+            String errorMessage = "Error when compiling and applying error template.";
+            LOGGER.error(message);
+            return errorMessage;
+        }
+    }
+
+    private boolean isValidSection(String section) {
+        return validSections.contains(section);
     }
 
     private boolean isValidPublishUnpublishParams(String registryId, String destinationId) {
@@ -173,8 +305,8 @@ public class RegistryRestEndpoint {
         this.registryPublicationService = registryPublicationService;
     }
 
-    public void setRegistryReportMapBuilder(RegistryReportMapBuilder reportMapBuilder) {
-        this.reportMapBuilder = reportMapBuilder;
+    public void setRegistryReportBuilder(RegistryReportBuilder registryReportBuilder) {
+        this.registryReportBuilder = registryReportBuilder;
     }
 
     public void setFederationAdminService(FederationAdminService federationAdminService) {
