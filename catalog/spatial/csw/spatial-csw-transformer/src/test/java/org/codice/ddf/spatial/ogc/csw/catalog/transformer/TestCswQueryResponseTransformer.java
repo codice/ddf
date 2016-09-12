39c6fe9bdd7d0d100c8d46501107fbf17894b43a
bdeining <benjamin.deininger@connexta.com>
2016-09-12 16:48:55 -0600

    DDF-2466 Added unit test

--- catalog/spatial/csw/spatial-csw-transformer/src/test/java/org/codice/ddf/spatial/ogc/csw/catalog/transformer/TestCswQueryResponseTransformer.java
+++ catalog/spatial/csw/spatial-csw-transformer/src/test/java/org/codice/ddf/spatial/ogc/csw/catalog/transformer/TestCswQueryResponseTransformer.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (c) Codice Foundation
- * <p/>
+ * <p>
  * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or any later version.
- * <p/>
+ * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
@@ -13,10 +13,12 @@
  **/
 package org.codice.ddf.spatial.ogc.csw.catalog.transformer;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyMap;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.notNull;
 import static org.mockito.Mockito.mock;
@@ -35,6 +37,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 import javax.activation.MimeType;
 import javax.ws.rs.WebApplicationException;
@@ -57,6 +60,8 @@ import org.opengis.filter.Filter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+
 import ddf.catalog.data.BinaryContent;
 import ddf.catalog.data.Metacard;
 import ddf.catalog.data.Result;
@@ -73,6 +78,7 @@ import ddf.catalog.transform.CatalogTransformerException;
 import ddf.catalog.transform.MetacardTransformer;
 import ddf.catalog.transformer.api.PrintWriter;
 import ddf.catalog.transformer.api.PrintWriterProvider;
+
 import net.opengis.cat.csw.v_2_0_2.AcknowledgementType;
 import net.opengis.cat.csw.v_2_0_2.GetRecordsType;
 import net.opengis.cat.csw.v_2_0_2.ResultType;
@@ -217,9 +223,8 @@ public class TestCswQueryResponseTransformer {
         verify(mockPrintWriter, times(1)).startNode(strArgCaptor.capture());
 
         List<String> values = strArgCaptor.getAllValues();
-        assertThat("Missing root GetRecordByIdResponse node.",
-                values.get(0),
-                is(CswQueryResponseTransformer.RECORD_BY_ID_RESPONSE_QNAME));
+        assertThat("Missing root GetRecordByIdResponse node.", values.get(0), is(
+                CswQueryResponseTransformer.RECORD_BY_ID_RESPONSE_QNAME));
 
     }
 
@@ -359,6 +364,58 @@ public class TestCswQueryResponseTransformer {
     }
 
     @Test
+    public void testMarshalAcknowledgement2()
+            throws WebApplicationException, IOException, JAXBException,
+            CatalogTransformerException {
+
+        GetRecordsType query = new GetRecordsType();
+        query.setResultType(ResultType.RESULTS);
+        query.setMaxRecords(BigInteger.valueOf(6));
+        query.setStartPosition(BigInteger.valueOf(0));
+        SourceResponse sourceResponse = createSourceResponse(query, 6);
+
+        Map<String, Serializable> args = new HashMap<>();
+        args.put(CswConstants.RESULT_TYPE_PARAMETER, ResultType.RESULTS);
+        args.put(CswConstants.GET_RECORDS, query);
+
+        PrintWriter printWriter = getSimplePrintWriter();
+
+        MetacardTransformer mockMetacardTransformer = mock(MetacardTransformer.class);
+
+        AtomicLong atomicLong = new AtomicLong(0);
+        when(mockMetacardTransformer.transform(any(Metacard.class),
+                anyMap())).then(invocationOnMock -> {
+            if (atomicLong.incrementAndGet() == 2) {
+
+                throw new CatalogTransformerException("");
+            }
+
+            Metacard metacard = (Metacard) invocationOnMock.getArguments()[0];
+            BinaryContentImpl bci = new BinaryContentImpl(IOUtils.toInputStream(
+                    metacard.getId() + ","), new MimeType("application/xml"));
+            return bci;
+        });
+
+        when(mockPrintWriterProvider.build((Class<Metacard>) notNull())).thenReturn(printWriter);
+        when(mockTransformerManager.getTransformerBySchema(anyString())).thenReturn(
+                mockMetacardTransformer);
+
+        CswQueryResponseTransformer cswQueryResponseTransformer = new CswQueryResponseTransformer(
+                mockTransformerManager,
+                mockPrintWriterProvider);
+        cswQueryResponseTransformer.init();
+        BinaryContent content = cswQueryResponseTransformer.transform(sourceResponse, args);
+        cswQueryResponseTransformer.destroy();
+
+        String xml = new String(content.getByteArray());
+        assertThat(xml, containsString(
+                CswQueryResponseTransformer.NUMBER_OF_RECORDS_MATCHED_ATTRIBUTE + " 6"));
+        assertThat(xml, containsString(
+                CswQueryResponseTransformer.NUMBER_OF_RECORDS_RETURNED_ATTRIBUTE + " 5"));
+        assertThat(xml, containsString(CswQueryResponseTransformer.NEXT_RECORD_ATTRIBUTE + " 0"));
+    }
+
+    @Test
     public void verifyResultOrderIsMaintained() throws CatalogTransformerException, IOException {
         // when
         when(mockPrintWriterProvider.build((Class<Metacard>) notNull())).thenReturn(mockPrintWriter);
@@ -455,5 +512,63 @@ public class TestCswQueryResponseTransformer {
                 CswJAXBElementProvider.class.getClassLoader());
 
         return context;
+    }
+
+    private PrintWriter getSimplePrintWriter() {
+        return new PrintWriter() {
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            @Override
+            public void setRawValue(String s) {
+                stringBuilder.append(s);
+            }
+
+            @Override
+            public String makeString() {
+                return stringBuilder.toString();
+            }
+
+            @Override
+            public void startNode(String s, Class aClass) {
+                stringBuilder.append(s);
+            }
+
+            @Override
+            public void startNode(String s) {
+                stringBuilder.append(s);
+            }
+
+            @Override
+            public void addAttribute(String s, String s1) {
+                stringBuilder.append(s);
+                stringBuilder.append(" ");
+                stringBuilder.append(s1);
+            }
+
+            @Override
+            public void setValue(String s) {
+                stringBuilder.append(s);
+            }
+
+            @Override
+            public void endNode() {
+            }
+
+            @Override
+            public void flush() {
+
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public HierarchicalStreamWriter underlyingWriter() {
+                return null;
+            }
+        };
     }
 }
