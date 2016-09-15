b6f88fd199047f5d23faf83bd7d957bc5a2441aa
Garrett <garrett.freibott@connexta.com>
2016-09-15 09:34:49 -0700

    DDF-2453: LogViewer Pause/Resume Polling (#1228)
    After dicussing the use-case for this situation, we decided
    that the best solution would be to add a pause/play button
    to allow the admin to pause log polling in order to prevent
    both the window from scrolling, as well as ensure that the
    finite logviewer queue does not eventually push the logs
    under inspection out of the finite queue.
    
    Also cleaned up the redux code by using redux-thunk middleware
    and combineReducers() to make future additions easier.

--- platform/admin/core/admin-core-logviewer/src/main/webapp/js/reducers/index.js
+++ platform/admin/core/admin-core-logviewer/src/main/webapp/js/reducers/index.js
@@ -13,55 +13,81 @@
  *
  **/
 
+import { combineReducers } from 'redux'
+
 // total number of log entries to keep in memory that a
 // user can filter through
 const MAX_LOG_ENTRIES = 5000
 const INITIAL_DISPLAY_SIZE = 25
 const GROW_SIZE = 50
 
-export default (state, action) => {
-  // determines whether to toggle or switch the expandedHash
-  const toggleExpand = () => {
-    if (action.hash === state.expandedHash) {
-      return undefined
-    } else {
-      return action.hash
-    }
-  }
-
-  // starting state
-  if (state === undefined) {
-    return {
-      logs: [],
-      filter: { level: 'ALL' },
-      displaySize: INITIAL_DISPLAY_SIZE,
-      expandedHash: undefined
-    }
-  }
-
-  switch (action.type) {
+const filter = (state = { level: 'ALL' }, { type, filter } = {}) => {
+  switch (type) {
     case 'FILTER_LOGS':
-      return { ...state,
-        displaySize: INITIAL_DISPLAY_SIZE,
-        filter: { ...state.filter, ...action.filter }
-      }
-
-    case 'APPEND_LOGS':
-      return { ...state,
-        logs: action.entries.concat(state.logs).slice(0, MAX_LOG_ENTRIES)
-      }
-
-    case 'GROW_DISPLAY_SIZE':
-      return { ...state,
-        displaySize: state.displaySize + GROW_SIZE
-      }
-
-    case 'CHANGE_EXPANDED_ENTRY':
-      return { ...state,
-        expandedHash: toggleExpand()
-      }
-
+      return { ...state, ...filter }
     default:
       return state
   }
 }
+
+const logs = (state = [], { type, entries } = {}) => {
+  switch (type) {
+    case 'APPEND_LOGS':
+      return entries.concat(state).slice(0, MAX_LOG_ENTRIES)
+    default:
+      return state
+  }
+}
+
+const displaySize = (state = INITIAL_DISPLAY_SIZE, { type } = {}) => {
+  switch (type) {
+    case 'GROW_DISPLAY_SIZE':
+      return state + GROW_SIZE
+    default:
+      return state
+  }
+}
+
+const expandedHash = (state = null, { type, hash } = {}) => {
+  switch (type) {
+    case 'CHANGE_EXPANDED_ENTRY':
+      return (state === hash || hash === undefined) ? null : hash
+    default:
+      return state
+  }
+}
+
+const isPolling = (state = false, { type } = {}) => {
+  switch (type) {
+    case 'TOGGLE_POLLING':
+      return (!state)
+    case 'SHOW_ERROR':
+      return false
+    default:
+      return state
+  }
+}
+
+const isFetching = (state = false, { type } = {}) => {
+  switch (type) {
+    case 'BEGIN_FETCHING':
+      return true
+    case 'END_FETCHING':
+      return false
+    default:
+      return state
+  }
+}
+
+const errorState = (state = { isInError: false }, { type, message } = {}) => {
+  switch (type) {
+    case 'SHOW_ERROR':
+      return { isInError: true, message: message }
+    case 'DISMISS_ERROR':
+      return { isInError: false }
+    default:
+      return state
+  }
+}
+
+export default combineReducers({ logs, filter, displaySize, expandedHash, isPolling, isFetching, errorState })
