6543054a5dd4c7f1347660afa921a86896e7a885
Phillip Klinefelter <phillip.klinefelter@gmail.com>
2016-11-08 16:45:18 -0700

    DDF-2467 Added Solrj client agnostic Solr Catalog Provider

---
+++ catalog/solr/catalog-solr-provider/src/main/java/ddf/catalog/solr/provider/SolrCatalogProvider.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package ddf.catalog.solr.provider;
+
+import java.util.concurrent.Future;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.codice.solr.factory.SolrClientFactory;
+
+import ddf.catalog.filter.FilterAdapter;
+import ddf.catalog.source.solr.RemoteSolrCatalogProvider;
+import ddf.catalog.source.solr.SolrFilterDelegateFactory;
+
+/**
+ * Catalog Provider that interfaces with Solr
+ */
+public class SolrCatalogProvider extends RemoteSolrCatalogProvider {
+
+    private SolrClientFactory clientFactory;
+
+    public SolrCatalogProvider(FilterAdapter filterAdapter, SolrClientFactory clientFactory,
+            SolrFilterDelegateFactory solrFilterDelegateFactory) {
+        super(filterAdapter, null, solrFilterDelegateFactory, null);
+        this.clientFactory = clientFactory;
+        updateClient();
+    }
+
+    @Override
+    protected Future<SolrClient> createClient() {
+        return clientFactory.newClient(SOLR_CATALOG_CORE_NAME);
+    }
+
+}
