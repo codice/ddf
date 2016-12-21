1a2a40fe5ed7a9893bf9e6944868d612bdbebaf6
Ryan Zwiefelhofer <ryan.zwiefelhofer@gmail.com>
2016-12-21 16:58:30 -0700

    DDF-2644 Removed unecessary DeleteRequestImpl change

--- catalog/core/catalog-core-api-impl/src/main/java/ddf/catalog/operation/impl/DeleteRequestImpl.java
+++ catalog/core/catalog-core-api-impl/src/main/java/ddf/catalog/operation/impl/DeleteRequestImpl.java
@@ -149,7 +149,7 @@ public class DeleteRequestImpl extends OperationImpl implements DeleteRequest {
      * @see ddf.catalog.operation.DeleteRequest#getAttributeValues()
      */
     @Override
-    public List<? extends Serializable> getAttributeValues() {
+    public List<Serializable> getAttributeValues() {
         return values;
     }
 
