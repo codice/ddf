bc391d1f43a2a242d6d4fcd619e1392b35424523
Phillip Klinefelter <phillip.klinefelter@gmail.com>
2016-12-23 16:51:56 -0700

    DDF-2644 Update to latest SparkServlet

--- catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/SparkServlet.java
+++ catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/SparkServlet.java
@@ -10,71 +10,13 @@
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
  * is distributed along with this program and can be found at
  * <http://www.gnu.org/licenses/lgpl.html>.
- * <p>
- * Copyright 2011- Per Wendel
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * <p>
- * Copyright 2011- Per Wendel
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * <p>
- * Copyright 2011- Per Wendel
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * Copyright 2011- Per Wendel
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 package org.codice.ddf.catalog.ui;
 
 import java.io.IOException;
+import java.io.NotSerializableException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
@@ -82,6 +24,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -90,6 +35,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -98,25 +44,70 @@ import spark.http.matching.MatcherFilter;
 import spark.route.ServletRoutes;
 import spark.servlet.SparkApplication;
 import spark.staticfiles.StaticFilesConfiguration;
+import spark.utils.CollectionUtils;
 
-// Based on https://github.com/perwendel/spark/issues/193
+/**
+ * Servlet that can be configured through a web.xml to serve {@code SparkApplication}s.
+ * Needs to be initialized with the {@code applicationName} init parameter (or through a direct
+ * call to {@link #setSparkApplications(List)}) to the list of application classes defining routes.
+ * An optional {@code wrapperSupplierName} parameter can be provided (or directly set through the
+ * {@link #setRequestSupplier(BiFunction)} method) to provide added path flexibility.
+ */
 public class SparkServlet extends HttpServlet {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(SparkServlet.class);
 
     private static final String SLASH_WILDCARD = "/*";
 
     private static final String SLASH = "/";
 
+    private static final String APPLICATION_CLASS_PARAM = "applicationName";
+
+    private static final String WRAPPER_SUPPLIER_PARAM_NAME = "wrapperSupplierName";
+
     private static final String FILTER_MAPPING_PARAM = "filterMappingUrlPattern";
 
-    private List<SparkApplication> sparkApplication =
+    private static final BiFunction<HttpServletRequest, String, HttpServletRequestWrapper>
+            DEFAULT_REQ_FUNC =
+            new BiFunction<HttpServletRequest, String, HttpServletRequestWrapper>() {
+                @Override
+                public HttpServletRequestWrapper apply(HttpServletRequest req,
+                        String relativePath) {
+                    return new HttpServletRequestWrapper(req) {
+                        @Override
+                        public String getPathInfo() {
+                            return relativePath;
+                        }
+
+                        @Override
+                        public String getRequestURI() {
+                            return relativePath;
+                        }
+                    };
+                }
+            };
+
+    private BiFunction<HttpServletRequest, String, HttpServletRequestWrapper> requestSupplier;
+
+    private final List<SparkApplication> sparkApplications =
             Collections.synchronizedList(new ArrayList<>());
 
-    private static MatcherFilter matcherFilter;
+    private String filterMappingPattern = null;
 
-    public SparkServlet(List<SparkApplication> application) {
-        sparkApplication = application;
+    private String filterPath;
+
+    private MatcherFilter matcherFilter;
+
+    public void setRequestSupplier(
+            BiFunction<HttpServletRequest, String, HttpServletRequestWrapper> requestSupplier) {
+        this.requestSupplier = requestSupplier;
+    }
+
+    public void setSparkApplications(List<SparkApplication> sparkApplications) {
+        this.sparkApplications.addAll(sparkApplications);
+    }
+
+    public void setFilterMappingPattern(String filterMappingPattern) {
+        this.filterMappingPattern = filterMappingPattern;
     }
 
     @Override
@@ -124,84 +115,71 @@ public class SparkServlet extends HttpServlet {
         super.init(config);
         ServletFlag.runFromServlet();
 
-        sparkApplication.stream()
+        populateWrapperSupplier(config);
+        populateSparkApplications(config);
+
+        sparkApplications.stream()
                 .sequential()
                 .forEach(SparkApplication::init);
 
+        filterPath = getConfigPath(filterMappingPattern, config);
         matcherFilter = new MatcherFilter(ServletRoutes.get(),
                 StaticFilesConfiguration.servletInstance,
-                true,
+                false,
                 false);
     }
 
     @Override
     public void destroy() {
-        if (sparkApplication != null) {
-            sparkApplication.stream()
-                    .filter(Objects::nonNull)
-                    .forEach(SparkApplication::destroy);
-        }
-    }
-
-    private String getFilterPath() throws ServletException {
-        Optional<String> mapping = this.getServletContext()
-                .getServletRegistration(this.getServletName())
-                .getMappings()
-                .stream()
-                .findFirst();
-        if (mapping.isPresent()) {
-            String result = mapping.get();
-            if (result == null || result.equals(SLASH_WILDCARD)) {
-                return "";
-            } else if (!result.startsWith(SLASH) || !result.endsWith(SLASH_WILDCARD)) {
-                throw new RuntimeException("The " + FILTER_MAPPING_PARAM
-                        + " must start with \"/\" and end with \"/*\". It's: " + result);
-            }
-            return result.substring(1, result.length() - 1);
-        } else {
-            throw new ServletException("Unable to read servlet mapping");
-        }
+        sparkApplications.stream()
+                .filter(Objects::nonNull)
+                .forEach(SparkApplication::destroy);
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        final String relativePath = getRelativePath(req, filterPath);
 
-        final String requestPath = getFilterPath();
-        final String relativePath = getRelativePath(req, requestPath);
+        HttpServletRequestWrapper requestWrapper = requestSupplier.apply(req, relativePath);
 
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug(relativePath);
+        // handle static resources
+        boolean consumed = StaticFilesConfiguration.servletInstance.consume(req, resp);
+
+        if (consumed) {
+            return;
         }
-
-        HttpServletRequestWrapper requestWrapper = new HttpServletRequestWrapper(req) {
-            @Override
-            public String getPathInfo() {
-                return relativePath;
-            }
-
-            @Override
-            public String getRequestURI() {
-                return relativePath;
-            }
-        };
 
         matcherFilter.doFilter(requestWrapper, resp, null);
     }
 
-    static String getRelativePath(HttpServletRequest request, String filterPath) {
-        String path = request.getRequestURI();
-        String contextPath = request.getContextPath();
+    private static String getConfigPath(String filterMappingPattern, ServletConfig config) {
+        String result = Optional.ofNullable(filterMappingPattern)
+                .orElse(config.getInitParameter(FILTER_MAPPING_PARAM));
+        if (result == null || result.equals(SLASH_WILDCARD)) {
+            return "";
+        } else if (!result.startsWith(SLASH) || !result.endsWith(SLASH_WILDCARD)) {
+            throw new RuntimeException(String.format(
+                    "The %s must start with '/' and end with '/*'. Instead it is: %s",
+                    FILTER_MAPPING_PARAM,
+                    result));
+        }
+        return result.substring(1, result.length() - 1);
+    }
 
-        path = path.substring(contextPath.length());
+    private static String getRelativePath(HttpServletRequest request, String filterPath) {
+        String path = request.getRequestURI()
+                .substring(request.getContextPath()
+                        .length());
 
         if (path.length() > 0) {
             path = path.substring(1);
         }
 
-        if (!path.startsWith(filterPath) && filterPath.equals(path + SLASH)) {
+        if (filterPath.equals(path + SLASH)) {
             path += SLASH;
         }
+
         if (path.startsWith(filterPath)) {
             path = path.substring(filterPath.length());
         }
@@ -212,10 +190,80 @@ public class SparkServlet extends HttpServlet {
 
         try {
             path = URLDecoder.decode(path, "UTF-8");
-        } catch (UnsupportedEncodingException ex) {
+        } catch (UnsupportedEncodingException ignore) {
             // this can't really ever happen
         }
+
+        LOGGER.debug("Relative path = {}", path);
         return path;
     }
 
+    private void populateWrapperSupplier(ServletConfig config) {
+        // Do not override an injected supplier through initialization
+        if (requestSupplier != null) {
+            return;
+        }
+
+        String wrapperSupplierName = config.getInitParameter(WRAPPER_SUPPLIER_PARAM_NAME);
+
+        if (StringUtils.isNotBlank(wrapperSupplierName)) {
+            try {
+                Class<?> wrapperClass = Class.forName(wrapperSupplierName);
+                if (BiFunction.class.isAssignableFrom(wrapperClass)) {
+                    requestSupplier =
+                            (BiFunction<HttpServletRequest, String, HttpServletRequestWrapper>) wrapperClass.newInstance();
+                }
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+                LOGGER.debug(
+                        "Error converting {} to BiFunction<HttpServletRequest, String, HttpServletRequestWrapper>; "
+                                + "falling back to default",
+                        wrapperSupplierName,
+                        e);
+            }
+
+        }
+
+        if (requestSupplier == null) {
+            requestSupplier = DEFAULT_REQ_FUNC;
+        }
+    }
+
+    private void populateSparkApplications(ServletConfig config) {
+        // Do not override injected spark applications through initialization
+        if (!CollectionUtils.isEmpty(sparkApplications)) {
+            return;
+        }
+
+        String applications = config.getInitParameter(APPLICATION_CLASS_PARAM);
+
+        if (StringUtils.isNotBlank(applications)) {
+            sparkApplications.addAll(Pattern.compile(",")
+                    .splitAsStream(applications)
+                    .map(String::trim)
+                    .map(this::getApplication)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList()));
+        }
+    }
+
+    private SparkApplication getApplication(String applicationClassName) {
+        try {
+            Class<?> appClass = Class.forName(applicationClassName);
+            if (SparkApplication.class.isAssignableFrom(appClass)) {
+                return SparkApplication.class.cast(appClass.newInstance());
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            LOGGER.debug("Error converting {} to SparkApplication", applicationClassName, e);
+        }
+
+        return null;
+    }
+
+    private void writeObject(ObjectOutputStream stream) throws IOException {
+        throw new NotSerializableException(getClass().getName());
+    }
+
+    private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
+        throw new NotSerializableException(getClass().getName());
+    }
 }
