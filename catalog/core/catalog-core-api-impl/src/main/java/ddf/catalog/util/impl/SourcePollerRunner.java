8d868818d668ffbe82c24c89b4574981b0dcb557
stustison <scott.tustison@connexta.com>
2017-02-09 08:29:01 -0700

    DDF-2789 changed maps to no longer use the blueprint proxy object as the key

--- catalog/core/catalog-core-api-impl/src/main/java/ddf/catalog/util/impl/SourcePollerRunner.java
+++ catalog/core/catalog-core-api-impl/src/main/java/ddf/catalog/util/impl/SourcePollerRunner.java
@@ -33,11 +33,10 @@ import ddf.catalog.source.Source;
  * The poller to check the availability of all configured sources. This class is instantiated by the
  * CatalogFramework's blueprint and is scheduled by the {@link SourcePoller} to execute at a
  * configured rate, i.e., the polling interval.
- *
+ * <p>
  * This class maintains a list of all of the sources to be polled for their availability. Sources
  * are added to this list when they come online and when they are deleted. A cached map is
  * maintained of all the sources and their last availability states.
- *
  */
 public class SourcePollerRunner implements Runnable {
 
@@ -45,22 +44,21 @@ public class SourcePollerRunner implements Runnable {
 
     private List<Source> sources;
 
-    private Map<Source, CachedSource> cachedSources = new ConcurrentSkipListMap<>(
-            Comparator.comparing(Source::getId, Comparator
-                    .nullsLast(Comparator.naturalOrder()))
-                    .thenComparing(Source::getTitle, Comparator
-                            .nullsLast(Comparator.naturalOrder()))
-                    .thenComparing(Source::getVersion, Comparator
-                            .nullsLast(Comparator.naturalOrder()))
-                    .thenComparing(Source::getDescription, Comparator
-                            .nullsLast(Comparator.naturalOrder()))
-                    .thenComparing(Source::getOrganization, Comparator
-                            .nullsLast(Comparator.naturalOrder()))
-            );
+    private Map<SourceKey, CachedSource> cachedSources =
+            new ConcurrentSkipListMap<>(Comparator.comparing(SourceKey::getId,
+                    Comparator.nullsLast(Comparator.naturalOrder()))
+                    .thenComparing(SourceKey::getTitle,
+                            Comparator.nullsLast(Comparator.naturalOrder()))
+                    .thenComparing(SourceKey::getVersion,
+                            Comparator.nullsLast(Comparator.naturalOrder()))
+                    .thenComparing(SourceKey::getDescription,
+                            Comparator.nullsLast(Comparator.naturalOrder()))
+                    .thenComparing(SourceKey::getOrganization,
+                            Comparator.nullsLast(Comparator.naturalOrder())));
 
     private ExecutorService pool;
 
-    private Map<Source, Lock> sourceStatusThreadLocks = new ConcurrentHashMap<Source, Lock>();
+    private Map<CachedSource, Lock> sourceStatusThreadLocks = new ConcurrentHashMap<>();
 
     /**
      * Creates an empty list of {@link Source} sources to be polled for availability. This
@@ -69,7 +67,7 @@ public class SourcePollerRunner implements Runnable {
     public SourcePollerRunner() {
 
         LOGGER.debug("Creating source poller runner.");
-        sources = new CopyOnWriteArrayList<Source>();
+        sources = new CopyOnWriteArrayList<>();
     }
 
     /**
@@ -96,8 +94,7 @@ public class SourcePollerRunner implements Runnable {
      * Checks if the specified source is available, updating the internally maintained map of
      * sources and their status. Lock ensures only one status thread is running per source.
      *
-     * @param source
-     *            the source to check if it is available
+     * @param source the source to check if it is available
      */
     private void checkStatus(final Source source) {
 
@@ -105,32 +102,27 @@ public class SourcePollerRunner implements Runnable {
             pool = Executors.newCachedThreadPool();
 
         }
-        final Runnable statusRunner = new Runnable() {
+        final Runnable statusRunner = () -> {
+            final CachedSource cachedSource = cachedSources.get(getSourceKey(source));
+            if (cachedSource != null) {
+                Lock sourceStatusThreadLock = sourceStatusThreadLocks.get(cachedSource);
 
-            public void run() {
-
-                final CachedSource cachedSource = cachedSources.get(source);
-                if (cachedSource != null) {
-                    Lock sourceStatusThreadLock = sourceStatusThreadLocks.get(source);
-
-                    if (sourceStatusThreadLock.tryLock()) {
+                if (sourceStatusThreadLock.tryLock()) {
+                    try {
                         LOGGER.debug("Acquired lock for Source [{}] with id [{}]",
                                 source,
-                                source.getId());
-
-                        try {
-                            cachedSource.checkStatus();
-                        } finally {
-                            // release the lock acquired initially
-                            sourceStatusThreadLock.unlock();
-                            LOGGER.debug("Released lock for Source [{}] with id [{}]",
-                                    source,
-                                    source.getId());
-                        }
-                    } else {
-                        LOGGER.debug("Unable to get lock for Source [{}] with id [{}]."
-                                + "  A status thread is already running.", source, source.getId());
+                                getSourceKey(source).getId());
+                        cachedSource.checkStatus();
+                    } finally {
+                        // release the lock acquired initially
+                        sourceStatusThreadLock.unlock();
+                        LOGGER.debug("Released lock for Source [{}] with id [{}]",
+                                source,
+                                getSourceKey(source).getId());
                     }
+                } else {
+                    LOGGER.debug("Unable to get lock for Source [{}] with id [{}]."
+                            + "  A status thread is already running.", source, getSourceKey(source).getId());
                 }
             }
         };
@@ -141,17 +133,17 @@ public class SourcePollerRunner implements Runnable {
      * Adds the {@link Source} instance to the list and sets its current status to UNCHECKED,
      * indicating it will checked at the next polling interval.
      *
-     * @param source
-     *            the source to add to the list
+     * @param source the source to add to the list
      */
     public void bind(Source source) {
 
         LOGGER.debug("Binding source: {}", source);
-        if (source != null  && !sources.contains(source)) {
+        if (source != null && !sources.contains(source)) {
             LOGGER.debug("Marking new source {} as UNCHECKED.", source);
             sources.add(source);
-            sourceStatusThreadLocks.put(source, new ReentrantLock());
-            cachedSources.put(source, new CachedSource(source));
+            CachedSource cachedSource = new CachedSource(source);
+            cachedSources.put(getSourceKey(source), cachedSource);
+            sourceStatusThreadLocks.put(cachedSource, new ReentrantLock());
             checkStatus(source);
 
         }
@@ -161,34 +153,29 @@ public class SourcePollerRunner implements Runnable {
      * Removes the {@link Source} instance from the list of references so that its availability is
      * no longer polled.
      *
-     * @param source
-     *            the source to remove from the list
+     * @param source the source to remove from the list
      */
     public void unbind(Source source) {
         LOGGER.debug("Unbinding source [{}]", source);
         if (source != null) {
-            cachedSources.remove(source);
             sources.remove(source);
-            sourceStatusThreadLocks.remove(source);
+            sourceStatusThreadLocks.remove(cachedSources.remove(getSourceKey(source)));
         }
     }
 
     /**
-     * Retrieves a {@link CachedSource} which contains cached values from the 
-     * specified {@link Source}. Returns a {@link Source} with values from the 
+     * Retrieves a {@link CachedSource} which contains cached values from the
+     * specified {@link Source}. Returns a {@link Source} with values from the
      * last polling interval. If the {@link Source} is not known, null is returned.
      *
-     * @param source
-     *            the source to get the {@link CachedSource} for
-     *
+     * @param source the source to get the {@link CachedSource} for
      * @return a {@link CachedSource} which contains cached values
      */
     public CachedSource getCachedSource(Source source) {
-        return cachedSources.get(source);
+        return cachedSources.get(getSourceKey(source));
     }
 
     /**
-     *
      * Calls the @link ExecutorService to shutdown immediately
      */
     public void shutdown() {
@@ -197,5 +184,77 @@ public class SourcePollerRunner implements Runnable {
             pool.shutdownNow();
         }
         LOGGER.trace("Status threads shut down");
+    }
+
+    private SourceKey getSourceKey(Source source) {
+        return new SourceKey(source);
+    }
+
+    private static class SourceKey {
+        private final Source source;
+
+        private String id;
+
+        private String title;
+
+        private String version;
+
+        private String description;
+
+        private String organization;
+
+        public SourceKey(Source source) {
+            this.source = source;
+            this.id = source.getId();
+            this.title = source.getTitle();
+            this.version = source.getVersion();
+            this.description = source.getDescription();
+            this.organization = source.getOrganization();
+        }
+
+        public String getId() {
+            try {
+                id = source.getId();
+            } catch (Exception e) {
+                LOGGER.debug("Proxy source destroyed before cleanup", e);
+            }
+            return id;
+        }
+
+        public String getTitle() {
+            try {
+                title = source.getTitle();
+            } catch (Exception e) {
+                LOGGER.debug("Proxy source destroyed before cleanup", e);
+            }
+            return title;
+        }
+
+        public String getVersion() {
+            try {
+                version = source.getVersion();
+            } catch (Exception e) {
+                LOGGER.debug("Proxy source destroyed before cleanup", e);
+            }
+            return version;
+        }
+
+        public String getDescription() {
+            try {
+                description = source.getDescription();
+            } catch (Exception e) {
+                LOGGER.debug("Proxy source destroyed before cleanup", e);
+            }
+            return description;
+        }
+
+        public String getOrganization() {
+            try {
+                organization = source.getOrganization();
+            } catch (Exception e) {
+                LOGGER.debug("Proxy source destroyed before cleanup", e);
+            }
+            return organization;
+        }
     }
 }
