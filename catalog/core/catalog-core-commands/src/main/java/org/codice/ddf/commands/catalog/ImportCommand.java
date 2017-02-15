bc6b3b96e96b93cf0cc40b2eb87e499ff52eae18
Ryan Zwiefelhofer <ryan.zwiefelhofer@gmail.com>
2017-02-14 17:48:25 -0700

    DDF-2698 Fixed file separator bug

--- catalog/core/catalog-core-commands/src/main/java/org/codice/ddf/commands/catalog/ImportCommand.java
+++ catalog/core/catalog-core-commands/src/main/java/org/codice/ddf/commands/catalog/ImportCommand.java
@@ -110,7 +110,7 @@ public class ImportCommand extends CatalogCommands {
                     continue;
                 }
 
-                String[] pathParts = filename.split(File.pathSeparator);
+                String[] pathParts = filename.split("\\" + File.separator);
                 String id = pathParts[ID];
                 String type = pathParts[TYPE];
 
