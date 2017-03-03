221b55f2eaa4918f7173a98b4951ff77c345d2c8
Aaron Hoffer <github@hoffer.org>
2017-03-03 07:39:43 -0700

    DDF-2831 Change retry interval for unavailable endpoints (#1741)
    * DDF-2831 Change retry interval for unavailable endpoints
      The max retry interval can now be set in system properties and defaults to 300 seconds.
      The initial retry interval can be set in system properties and defaults to 10 seconds.
      The exponential behavior is not doubling the interval instead of growing by a factor of 10.
      The unit test includes a check to make sure no interval is longer than the max.
      Updated documentation, change log.
    
    * DDF-2831 Change retry interval for unavailable endpoints
      The max retry interval can now be set in system properties and defaults to 300 seconds.
      The initial retry interval can be set in system properties and defaults to 10 seconds.
      The exponential behavior is not doubling the interval instead of growing by a factor of 10.
      The unit test includes a check to make sure no interval is longer than the max.
      Updated documentation, change log.
      Added fixes from comments.
    
    * DDF-2831 Change retry interval for unavailable endpoints
      The max retry interval can now be set in system properties and defaults to 300 seconds.
      The initial retry interval can be set in system properties and defaults to 10 seconds.
      The exponential behavior is not doubling the interval instead of growing by a factor of 10.
      The unit test includes a check to make sure no interval is longer than the max.
      Updated documentation, change log.
      Added fixes from comments.

--- platform/util/platform-util-unavailableurls/src/test/java/org/codice/ddf/platform/util/http/UnavailableUrlsTest.java
+++ platform/util/platform-util-unavailableurls/src/test/java/org/codice/ddf/platform/util/http/UnavailableUrlsTest.java
@@ -13,7 +13,12 @@
  */
 package org.codice.ddf.platform.util.http;
 
+import static org.codice.ddf.platform.util.http.UnavailableUrls.INITIAL_TIMEOUT_SECONDS_PROPERTY;
+import static org.codice.ddf.platform.util.http.UnavailableUrls.MAX_TIMEOUT_SECONDS_PROPERTY;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.collection.IsArrayContaining.hasItemInArray;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
 
 import java.net.HttpURLConnection;
@@ -21,11 +26,27 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ProvideSystemProperty;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 public class UnavailableUrlsTest {
+
+    private static final String MAX_TIMEOUT_SECONDS = "300";
+
+    private static final String INITIAL_TIMEOUT_SECONDS = "10";
+
+    @Rule
+    public final ProvideSystemProperty initialTimeout = new ProvideSystemProperty(
+            INITIAL_TIMEOUT_SECONDS_PROPERTY,
+            INITIAL_TIMEOUT_SECONDS);
+
+    @Rule
+    public final ProvideSystemProperty maxTimout = new ProvideSystemProperty(
+            MAX_TIMEOUT_SECONDS_PROPERTY,
+            MAX_TIMEOUT_SECONDS);
 
     private ScheduledExecutorService scheduler;
 
@@ -59,20 +80,6 @@ public class UnavailableUrlsTest {
         UnavailableUrls set = initSet(200);
         set.add("hello");
         assertThat(set.contains("hello"), is(true));
-    }
-
-    private class SchedulerCapture {
-        public final Runnable runnable;
-
-        public final long timeout;
-
-        public final TimeUnit unit;
-
-        public SchedulerCapture(Runnable r, long to, TimeUnit tu) {
-            this.runnable = r;
-            this.timeout = to;
-            this.unit = tu;
-        }
     }
 
     // capture the call to the scheduler and return the parameters
@@ -121,13 +128,13 @@ public class UnavailableUrlsTest {
      * @param list - list of two or more positive integers
      * @return true if sequence is exponential, false otherwise
      */
-    private boolean isExponentialBackoff(long[] list) {
+    private boolean isExponentialBackoff(Long[] list) {
         if (list.length < 2) {
             return false;
         }
 
         double base = list[1] / (double) list[0];
-        long max = list[list.length - 1];
+        Long max = list[list.length - 1];
 
         if (!(base > 1)) {
             return false;
@@ -135,7 +142,7 @@ public class UnavailableUrlsTest {
 
         for (int i = 1; i < list.length - 1; i++) {
             double changeOfBase = Math.log(list[i]) / Math.log(base);
-            if (changeOfBase < i && list[i] != max) {
+            if (changeOfBase < i && !list[i].equals(max)) {
                 return false;
             }
         }
@@ -145,19 +152,19 @@ public class UnavailableUrlsTest {
 
     @Test
     public void testIsExponentialBackoff() {
-        assertThat(isExponentialBackoff(new long[] {1, 1, 1, 1, 1}), is(false)); // constant
-        assertThat(isExponentialBackoff(new long[] {1, 2, 3, 4, 5, 6}), is(false)); // linear
-        assertThat(isExponentialBackoff(new long[] {1, 4, 9, 16, 25}), is(false)); // quadratic
-        assertThat(isExponentialBackoff(new long[] {1, 8, 27, 64, 125}), is(false)); // cubic
-        assertThat(isExponentialBackoff(new long[] {1, 2, 4, 8, 16, 32}),
+        assertThat(isExponentialBackoff(new Long[] {1L, 1L, 1L, 1L, 1L}), is(false)); // constant
+        assertThat(isExponentialBackoff(new Long[] {1L, 2L, 3L, 4L, 5L, 6L}), is(false)); // linear
+        assertThat(isExponentialBackoff(new Long[] {1L, 4L, 9L, 16L, 25L}), is(false)); // quadratic
+        assertThat(isExponentialBackoff(new Long[] {1L, 8L, 27L, 64L, 125L}), is(false)); // cubic
+        assertThat(isExponentialBackoff(new Long[] {1L, 2L, 4L, 8L, 16L, 32L}),
                 is(true));  // exponential 2^n
-        assertThat(isExponentialBackoff(new long[] {4, 8, 16, 32, 64, 128, 256}),
+        assertThat(isExponentialBackoff(new Long[] {4L, 8L, 16L, 32L, 64L, 128L, 256L}),
                 is(true));  // exponential 2^n
-        assertThat(isExponentialBackoff(new long[] {1, 3, 9, 81, 243, 729}),
+        assertThat(isExponentialBackoff(new Long[] {1L, 3L, 9L, 81L, 243L, 729L}),
                 is(true));  // exponential 3^n
-        assertThat(isExponentialBackoff(new long[] {3, 9, 81, 243, 729, 2187}),
+        assertThat(isExponentialBackoff(new Long[] {3L, 9L, 81L, 243L, 729L, 2187L}),
                 is(true));  // exponential 3^n
-        assertThat(isExponentialBackoff(new long[] {1, 2, 6, 24, 120, 720, 5040}),
+        assertThat(isExponentialBackoff(new Long[] {1L, 2L, 6L, 24L, 120L, 720L, 5040L}),
                 is(true));  // factorial
     }
 
@@ -167,7 +174,7 @@ public class UnavailableUrlsTest {
         // Number of times to retry ping, should be big enough to reach max
         // timeout and plateau.
         int retries = 25;
-        long backoffs[] = new long[retries];
+        Long backoffs[] = new Long[retries];
 
         SchedulerCapture sc;
         UnavailableUrls set = initSet(404);
@@ -181,6 +188,42 @@ public class UnavailableUrlsTest {
         }
 
         assertThat(isExponentialBackoff(backoffs), is(true));
+        assertThat("List should not contain a value greater than the maximum cutoff value",
+                backoffs,
+                not(hasItemInArray(greaterThan(getMaxTimoutSeconds()))));
     }
 
+    @Test
+    public void testUnsetSysProperties() {
+        System.getProperties()
+                .remove(INITIAL_TIMEOUT_SECONDS_PROPERTY);
+        System.getProperties()
+                .remove(MAX_TIMEOUT_SECONDS_PROPERTY);
+        UnavailableUrls unavailableUrls = new UnavailableUrls();
+        assertThat("Failed to find a max retry interval",
+                unavailableUrls.getMaxRetryInterval(),
+                greaterThan(0));
+        assertThat("Failed to find a initial retry interval",
+                unavailableUrls.getInitialRetryInterval(),
+                greaterThan(0));
+
+    }
+
+    private Long getMaxTimoutSeconds() {
+        return Long.parseLong(MAX_TIMEOUT_SECONDS);
+    }
+
+    private class SchedulerCapture {
+        public final Runnable runnable;
+
+        public final long timeout;
+
+        public final TimeUnit unit;
+
+        SchedulerCapture(Runnable r, Long to, TimeUnit tu) {
+            this.runnable = r;
+            this.timeout = to;
+            this.unit = tu;
+        }
+    }
 }
