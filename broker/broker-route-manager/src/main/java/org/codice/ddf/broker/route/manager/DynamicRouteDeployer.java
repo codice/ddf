b0851e7ed66a0e7721bf7022f806669968c51709
Harrison Tarr <harrison.tarr@connexta.com>
2017-03-22 12:20:36 -0700

    DDF-2861 Rename routemanager

--- broker/broker-route-manager/src/main/java/org/codice/ddf/broker/route/manager/DynamicRouteDeployer.java
+++ broker/broker-route-manager/src/main/java/org/codice/ddf/broker/route/manager/DynamicRouteDeployer.java
@@ -11,7 +11,7 @@
  * is distributed along with this program and can be found at
  * <http://www.gnu.org/licenses/lgpl.html>.
  */
-package org.codice.ddf.broker.routemanager;
+package org.codice.ddf.broker.route.manager;
 
 import java.io.File;
 import java.io.FileInputStream;
