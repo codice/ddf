ec949d306afe8069adec7441acc986ee1fb2259b
Aaron Hoffer <github@hoffer.org>
2017-04-18 15:32:24 -0700

    DDF-2875 Increase unit test coverage (#1893)
    * DDF-2875 Added RoleClaimsHandler unit tests
    
    * DDF-2875 Added RoleClaimsHandler unit tests
    
    * DDF-2875 Added RoleClaimsHandler unit tests

--- platform/security/sts/security-sts-ldapclaimshandler/src/test/java/ddf/security/sts/claimsHandler/RoleClaimsHandlerTest.java
+++ platform/security/sts/security-sts-ldapclaimshandler/src/test/java/ddf/security/sts/claimsHandler/RoleClaimsHandlerTest.java
@@ -14,23 +14,119 @@
 package ddf.security.sts.claimsHandler;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.anyVararg;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.matches;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.net.URI;
+import java.util.List;
 
 import org.apache.cxf.rt.security.claims.ClaimCollection;
 import org.apache.cxf.sts.claims.ClaimsParameters;
+import org.apache.cxf.sts.claims.ProcessedClaim;
 import org.apache.cxf.sts.claims.ProcessedClaimCollection;
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
+import org.apache.karaf.jaas.boot.principal.UserPrincipal;
+import org.forgerock.opendj.ldap.Connection;
+import org.forgerock.opendj.ldap.LDAPConnectionFactory;
+import org.forgerock.opendj.ldap.LdapException;
+import org.forgerock.opendj.ldap.LinkedAttribute;
+import org.forgerock.opendj.ldap.SearchResultReferenceIOException;
+import org.forgerock.opendj.ldap.responses.BindResult;
+import org.forgerock.opendj.ldap.responses.SearchResultEntry;
+import org.forgerock.opendj.ldif.ConnectionEntryReader;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({LDAPConnectionFactory.class})
 public class RoleClaimsHandlerTest {
+    public static final String USER_CN = "tstark";
+
     @Test
     public void testRetrieveClaimsValuesNullPrincipal() {
         RoleClaimsHandler claimsHandler = new RoleClaimsHandler();
         ClaimsParameters claimsParameters = new ClaimsParameters();
         ClaimCollection claimCollection = new ClaimCollection();
-        ProcessedClaimCollection processedClaims = claimsHandler.retrieveClaimValues(
-                claimCollection, claimsParameters);
+        ProcessedClaimCollection processedClaims =
+                claimsHandler.retrieveClaimValues(claimCollection, claimsParameters);
+        assertThat(processedClaims, is(empty()));
+    }
 
-        Assert.assertThat(processedClaims.size(), CoreMatchers.is(equalTo(0)));
+    @Test
+    public void testRetrieveClaimsValuesNotNullPrincipal()
+            throws LdapException, SearchResultReferenceIOException {
+        BindResult bindResult = mock(BindResult.class);
+        ClaimsParameters claimsParameters;
+        Connection connection = mock(Connection.class);
+        ConnectionEntryReader membershipReader = mock(ConnectionEntryReader.class);
+        ConnectionEntryReader groupNameReader = mock(ConnectionEntryReader.class);
+        LDAPConnectionFactory connectionFactory = PowerMockito.mock(LDAPConnectionFactory.class);
+        LinkedAttribute membershipAttribute = new LinkedAttribute("uid");
+        LinkedAttribute groupNameAttribute = new LinkedAttribute("cn");
+        ProcessedClaimCollection processedClaims;
+        RoleClaimsHandler claimsHandler;
+        SearchResultEntry membershipSearchResult = mock(SearchResultEntry.class);
+        SearchResultEntry groupNameSearchResult = mock(SearchResultEntry.class);
+        String groupName = "avengers";
+
+        when(bindResult.isSuccess()).thenReturn(true);
+
+        membershipAttribute.add("tstark");
+        when(membershipSearchResult.getAttribute(anyString())).thenReturn(membershipAttribute);
+
+        // hasNext() returns 'true' the first time, then 'false' every time after.
+        when(membershipReader.hasNext()).thenReturn(true, false);
+        when(membershipReader.readEntry()).thenReturn(membershipSearchResult);
+
+        groupNameAttribute.add(groupName);
+        when(groupNameSearchResult.getAttribute(anyString())).thenReturn(groupNameAttribute);
+
+        when(groupNameReader.hasNext()).thenReturn(true, false);
+        when(groupNameReader.readEntry()).thenReturn(groupNameSearchResult);
+
+        when(connection.bind(anyObject())).thenReturn(bindResult);
+        when(connection.search(anyObject(),
+                anyObject(),
+                eq("(&(objectClass=groupOfNames)(member=uid=tstark,))"),
+                anyVararg())).thenReturn(groupNameReader);
+        when(connection.search(anyString(), anyObject(), anyString(), matches("uid"))).thenReturn(
+                membershipReader);
+
+        when(connectionFactory.getConnection()).thenReturn(connection);
+
+        claimsHandler = new RoleClaimsHandler();
+        claimsHandler.setLdapConnectionFactory(connectionFactory);
+        claimsHandler.setBindMethod("Simple");
+        claimsHandler.setBindUserCredentials("foo");
+        claimsHandler.setBindUserDN("bar");
+
+        claimsParameters = new ClaimsParameters();
+        claimsParameters.setPrincipal(new UserPrincipal(USER_CN));
+        ClaimCollection claimCollection = new ClaimCollection();
+        processedClaims = claimsHandler.retrieveClaimValues(claimCollection, claimsParameters);
+        assertThat(processedClaims, hasSize(1));
+        ProcessedClaim claim = processedClaims.get(0);
+        assertThat(claim.getPrincipal(), equalTo(new UserPrincipal(USER_CN)));
+        assertThat(claim.getValues(), hasSize(1));
+        assertThat(claim.getValues()
+                .get(0), equalTo(groupName));
+    }
+
+    @Test
+    public void testSupportClaimTypes() {
+        RoleClaimsHandler claimsHandler = new RoleClaimsHandler();
+        List<URI> uris = claimsHandler.getSupportedClaimTypes();
+        assertThat(uris, hasSize(1));
     }
 }
