1f734309739523261925b1eb577ccee8cf53cb03
Ethan Manns <ethan.manns@connexta.com>
2017-05-15 16:59:00 -0700

    DDF-3012 Added mimetype detection for files without extensions

--- platform/mime/core/platform-mime-core-impl/src/test/java/ddf/mime/mapper/MimeTypeMapperTest.java
+++ platform/mime/core/platform-mime-core-impl/src/test/java/ddf/mime/mapper/MimeTypeMapperTest.java
@@ -18,13 +18,15 @@ import static org.junit.Assert.assertNull;
 
 import java.io.File;
 import java.io.InputStream;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableList;
 
 import ddf.mime.MimeTypeMapper;
 import ddf.mime.MimeTypeResolver;
@@ -35,18 +37,46 @@ public class MimeTypeMapperTest {
 
     private static final String CSW_RECORD_FILE = "src/test/resources/csw_record.xml";
 
+    private static final String CSW_RECORD_FILE_NO_EXTENSION =
+            "src/test/resources/csw_record_no_extension";
+
     private static final String XML_METACARD_FILE = "src/test/resources/metacard_sample.xml";
+
+    private static final String XML_METACARD_FILE_NO_EXTENSION =
+            "src/test/resources/metacard_sample_no_extension";
 
     private static final String NO_NAMESPACE_MATCHES_XML_FILE =
             "src/test/resources/no_namespace_matches.xml";
 
     private static final String NO_NAMESPACES_XML_FILE = "src/test/resources/no_namespaces.xml";
 
+    private static final String NITF_FILE = "src/test/resources/sampleNitf.nitf";
+
+    private static final TikaMimeTypeResolver TIKA_MIME_TYPE_RESOLVER;
+
+    static {
+        TIKA_MIME_TYPE_RESOLVER = new TikaMimeTypeResolver();
+        TIKA_MIME_TYPE_RESOLVER.setPriority(-1);
+    }
+
+    private static final List<MimeTypeResolver> MOCK_MIME_TYPE_RESOLVERS =
+            ImmutableList.of(new MockMimeTypeResolver("NitfResolver",
+                            10,
+                            new String[] {"nitf=image/nitf", "ntf=image/nitf"},
+                            null),
+                    new MockMimeTypeResolver("XmlMetacardResolver",
+                            10,
+                            new String[] {"xml=text/xml"},
+                            "urn:catalog:metacard"),
+                    new MockMimeTypeResolver("CswResolver",
+                            5,
+                            new String[] {"xml=text/xml;id=csw"},
+                            "http://www.opengis.net/cat/csw/2.0.2"),
+                    TIKA_MIME_TYPE_RESOLVER);
+
     @Test
     public void testNoResolvers() throws Exception {
-        List<MimeTypeResolver> resolvers = new ArrayList<MimeTypeResolver>();
-
-        MimeTypeMapper mapper = new MimeTypeMapperImpl(resolvers);
+        MimeTypeMapper mapper = new MimeTypeMapperImpl(Collections.emptyList());
         String fileExtension = mapper.getFileExtensionForMimeType("image/nitf");
         LOGGER.debug("fileExtension = {}", fileExtension);
         assertNull(fileExtension);
@@ -54,10 +84,7 @@ public class MimeTypeMapperTest {
 
     @Test
     public void testSingleResolver() throws Exception {
-        List<MimeTypeResolver> resolvers = new ArrayList<MimeTypeResolver>();
-        resolvers.add(new MockMimeTypeResolver("Resolver_1", 10));
-
-        MimeTypeMapper mapper = new MimeTypeMapperImpl(resolvers);
+        MimeTypeMapper mapper = new MimeTypeMapperImpl(MOCK_MIME_TYPE_RESOLVERS);
         String fileExtension = mapper.getFileExtensionForMimeType("image/nitf");
         LOGGER.debug("fileExtension = {}", fileExtension);
         assertEquals(".nitf", fileExtension);
@@ -65,12 +92,7 @@ public class MimeTypeMapperTest {
 
     @Test
     public void testMultipleResolvers() throws Exception {
-        List<MimeTypeResolver> resolvers = new ArrayList<MimeTypeResolver>();
-        resolvers.add(new MockMimeTypeResolver("Resolver_1", 10));
-        resolvers.add(new MockMimeTypeResolver("Resolver_2", -1));
-        resolvers.add(new MockMimeTypeResolver("Resolver_3", 100));
-
-        MimeTypeMapper mapper = new MimeTypeMapperImpl(resolvers);
+        MimeTypeMapper mapper = new MimeTypeMapperImpl(MOCK_MIME_TYPE_RESOLVERS);
         String fileExtension = mapper.getFileExtensionForMimeType("image/nitf");
         LOGGER.debug("fileExtension = {}", fileExtension);
         assertEquals(".nitf", fileExtension);
@@ -78,25 +100,7 @@ public class MimeTypeMapperTest {
 
     @Test
     public void testGuessMimeTypeForXmlIngest() throws Exception {
-
-        List<MimeTypeResolver> resolvers = new ArrayList<MimeTypeResolver>();
-        resolvers.add(new MockMimeTypeResolver("NitfResolver",
-                10,
-                new String[] {"nitf=image/nitf", "ntf=image/nitf"},
-                null));
-        resolvers.add(new MockMimeTypeResolver("XmlMetacardResolver",
-                10,
-                new String[] {"xml=text/xml"},
-                "urn:catalog:metacard"));
-        resolvers.add(new MockMimeTypeResolver("CswResolver",
-                10,
-                new String[] {"xml=text/xml;id=csw"},
-                "http://www.opengis.net/cat/csw/2.0.2"));
-        TikaMimeTypeResolver tikaMimeTypeResolver = new TikaMimeTypeResolver();
-        tikaMimeTypeResolver.setPriority(-1);
-        resolvers.add(tikaMimeTypeResolver);
-
-        MimeTypeMapper mapper = new MimeTypeMapperImpl(resolvers);
+        MimeTypeMapper mapper = new MimeTypeMapperImpl(MOCK_MIME_TYPE_RESOLVERS);
 
         InputStream is = FileUtils.openInputStream(new File(CSW_RECORD_FILE));
         String mimeType = mapper.guessMimeType(is, "xml");
@@ -107,6 +111,11 @@ public class MimeTypeMapperTest {
         mimeType = mapper.guessMimeType(is, "xml");
         LOGGER.debug("mimeType = {}", mimeType);
         assertEquals("text/xml", mimeType);
+
+        is = FileUtils.openInputStream(new File(NITF_FILE));
+        mimeType = mapper.guessMimeType(is, "nitf");
+        LOGGER.debug("mimeType = {}", mimeType);
+        assertEquals("image/nitf", mimeType);
 
         // Verify an XML file with a root element namespace, e.g., a pom.xml file, that does not match any
         // MimeTypeResolver returns a null mime type
@@ -121,6 +130,34 @@ public class MimeTypeMapperTest {
         mimeType = mapper.guessMimeType(is, "xml");
         LOGGER.debug("mimeType = {}", mimeType);
         assertNull(mimeType);
+    }
+
+    @Test
+    public void testGuessMimeTypeForXmlIngestNoExtension() throws Exception {
+        MimeTypeMapper mapper = new MimeTypeMapperImpl(MOCK_MIME_TYPE_RESOLVERS);
+
+        InputStream is = FileUtils.openInputStream(new File(CSW_RECORD_FILE_NO_EXTENSION));
+        String mimeType = mapper.guessMimeType(is, "");
+        LOGGER.debug("mimeType = {}", mimeType);
+        assertEquals("text/xml;id=csw", mimeType);
+
+        is = FileUtils.openInputStream(new File(XML_METACARD_FILE_NO_EXTENSION));
+        mimeType = mapper.guessMimeType(is, "");
+        LOGGER.debug("mimeType = {}", mimeType);
+        assertEquals("text/xml", mimeType);
+    }
+
+    @Test
+    public void testGuessMimeTypeForFileExtension() throws Exception {
+        MimeTypeMapper mapper = new MimeTypeMapperImpl(MOCK_MIME_TYPE_RESOLVERS);
+
+        String mimeType = mapper.getMimeTypeForFileExtension("xml");
+        LOGGER.debug("mimeType = {}", mimeType);
+        assertEquals("text/xml", mimeType);
+
+        mimeType = mapper.getMimeTypeForFileExtension(".nitf");
+        LOGGER.debug("mimeType = {}", mimeType);
+        assertEquals("image/nitf", mimeType);
     }
 
 }
