11f5a7b902e9fb5fab75ef9e489420096e81f27f
Adam Dimka <adam.dimka@connexta.com>
2017-06-15 12:21:46 -0700

    DDF-3091 Extract ExpertimentalEnumerationExtractor

--- catalog/ui/catalog-ui-enumeration/src/main/java/org/codice/ddf/catalog/ui/enumeration/ExperimentalEnumerationExtractor.java
+++ catalog/ui/catalog-ui-enumeration/src/main/java/org/codice/ddf/catalog/ui/enumeration/ExperimentalEnumerationExtractor.java
@@ -11,7 +11,7 @@
  * is distributed along with this program and can be found at
  * <http://www.gnu.org/licenses/lgpl.html>.
  */
-package org.codice.ddf.catalog.ui.metacard.enumerations;
+package org.codice.ddf.catalog.ui.enumeration;
 
 import static org.apache.commons.lang.StringUtils.isBlank;
 
