0b185ddc7793a4351f91e07e3f90d020e6b31367
Matthew Weser <matthew.weser@connexta.com>
2017-07-17 16:21:18 -0700

    DDF-3161 Unit test for EndpointUtil
    DDF-3161 First test passing with results properly populated

--- catalog/ui/catalog-ui-search/src/test/java/org/codice/ddf/catalog/ui/util/EndpointUtilTest.java
+++ catalog/ui/catalog-ui-search/src/test/java/org/codice/ddf/catalog/ui/util/EndpointUtilTest.java
@@ -1,9 +1,24 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
 package org.codice.ddf.catalog.ui.util;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -17,6 +32,7 @@ import org.opengis.filter.Filter;
 import ddf.catalog.CatalogFramework;
 import ddf.catalog.data.AttributeRegistry;
 import ddf.catalog.data.InjectableAttribute;
+import ddf.catalog.data.Metacard;
 import ddf.catalog.data.MetacardType;
 import ddf.catalog.data.Result;
 import ddf.catalog.federation.FederationException;
@@ -28,14 +44,9 @@ import ddf.catalog.operation.impl.QueryRequestImpl;
 import ddf.catalog.source.SourceUnavailableException;
 import ddf.catalog.source.UnsupportedQueryException;
 
-/**
- * Created by mweser on 7/17/17.
- */
 public class EndpointUtilTest {
 
     EndpointUtil endpointUtil;
-
-    Map<String, Result> expectedMap;
 
     List<MetacardType> metacardTypeList;
 
@@ -59,6 +70,10 @@ public class EndpointUtilTest {
 
     QueryResponse responseMock;
 
+    Metacard metacardMock;
+
+    Result resultMock;
+
     @Before
     public void setUp() throws Exception {
 
@@ -76,14 +91,13 @@ public class EndpointUtilTest {
         injectableAttributeList.add(injectableAttributeMock);
 
         attributeRegistryMock = mock(AttributeRegistry.class);
-
         filterMock = mock(Filter.class);
-
         attributeBuilderMock = mock(AttributeBuilder.class);
-
         contextualExpressionBuilderMock = mock(ContextualExpressionBuilder.class);
 
         responseMock = mock(QueryResponse.class);
+        metacardMock = mock(Metacard.class);
+        resultMock = mock(Result.class);
 
         // when
         when(filterBuilderMock.attribute(any())).thenReturn(attributeBuilderMock);
@@ -92,6 +106,9 @@ public class EndpointUtilTest {
         when(contextualExpressionBuilderMock.text(anyString())).thenReturn(filterMock);
         when(catalogFrameworkMock.query(any(QueryRequestImpl.class))).thenReturn(responseMock);
 
+        when(resultMock.getMetacard()).thenReturn(metacardMock);
+        when(metacardMock.getId()).thenReturn("MOCK METACARD");
+
         // constructor
         endpointUtil = new EndpointUtil(metacardTypeList,
                 catalogFrameworkMock,
@@ -99,22 +116,44 @@ public class EndpointUtilTest {
                 injectableAttributeList,
                 attributeRegistryMock);
 
-        // TODO: 7/17/17 Instantiate expectedMap here
     }
 
     @Test
     public void testGetMetacardsByFilterExpectAll()
             throws UnsupportedQueryException, SourceUnavailableException, FederationException {
 
-        String tagFilter = "*";
-        Map<String, Result> expected;
+        int numResults = 100;
 
-        expected = endpointUtil.getMetacardsByFilter(tagFilter);
+        // TODO: 7/17/17 Look into why tagFilter is irrelevant (might need for other test)
+        String tagFilter = "";
 
-        assertThat("Something expected", true);
+        List<Result> resultList = populateResultMockList(numResults);
+        List<Result> emptyList = populateResultMockList(0);
 
-        //verify
+        when(responseMock.getResults()).thenReturn(resultList, resultList, emptyList);
 
+        Map<String, Result> expected = endpointUtil.getMetacardsByFilter(tagFilter);
+
+        assertThat("Should return " + numResults + " results, but returned " + expected.size(),
+                expected.size() == numResults);
+
+        verify(responseMock, times(4)).getResults();
     }
 
+    private List<Result> populateResultMockList(int size) {
+
+        List<Result> resultMockList = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            resultMock = mock(Result.class);
+            metacardMock = mock(Metacard.class);
+
+            when(resultMock.getMetacard()).thenReturn(metacardMock);
+            when(metacardMock.getId()).thenReturn("MOCK METACARD " + (i + 1));
+
+            resultMockList.add(resultMock);
+        }
+
+        return resultMockList;
+    }
 }
