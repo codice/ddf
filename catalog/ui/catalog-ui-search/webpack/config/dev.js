6a6f57eef87d0a5db4e3b5d597fc21d47ae37acb
brendan-hofmann <brendan.hofmann@connexta.com>
2017-07-21 12:57:08 -0700

    DDF-3159 Add proxy http login (#2146)

--- catalog/ui/catalog-ui-search/webpack/config/dev.js
+++ catalog/ui/catalog-ui-search/webpack/config/dev.js
@@ -8,6 +8,25 @@ var resolve = function (place) {
     return path.resolve(__dirname, '../../', place)
 };
 
+var handleProxyRes = function (proxyRes, req, res) {
+    // remove so we can still login in through http
+    delete proxyRes.headers['x-xss-protection'];
+    var cookie = proxyRes.headers['set-cookie'];
+    if (cookie !== undefined) {
+        // force the cookie to be insecure since the proxy is over http
+        proxyRes.headers['set-cookie'] = cookie[0].replace(new RegExp(/;\w?Secure/), '')
+    }
+};
+
+var proxyConfig = function (target) {
+    return {
+        target: target,
+        secure: false,
+        changeOrigin: true,
+        onProxyRes: handleProxyRes
+    }
+};
+
 module.exports = merge.smart(base, {
     devServer: {
         progress: true,
@@ -16,21 +35,9 @@ module.exports = merge.smart(base, {
         hot: true,
         contentBase: [resolve('./node_modules/cesium/Build/'), resolve('/src/main/webapp/')],
         proxy: {
-            '/search/catalog/**': {
-                target: 'https://localhost:8993',
-                secure: false,
-                changeOrigin: true
-            },
-            '/services/**': {
-                target: 'https://localhost:8993',
-                secure: false,
-                changeOrigin: true
-            },
-            '/styles/**': {
-                target: 'https://localhost:8993/search/catalog',
-                secure: false,
-                changeOrigin: true
-            }
+            '/search/catalog/**': proxyConfig('https://localhost:8993'),
+            '/services/**': proxyConfig('https://localhost:8993'),
+            '/styles/**': proxyConfig('https://localhost:8993/search/catalog')
         }
     },
     entry: [
