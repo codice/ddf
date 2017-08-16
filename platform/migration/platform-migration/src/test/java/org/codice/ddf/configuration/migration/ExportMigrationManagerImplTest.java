15383f9e7f7977ddd32a59b3287b3c3d6aac9a7c
Patrick Ouellet <patrick.ouellet@connexta.com>
2017-08-15 22:43:02 -0700

    DDF-3142 - added more tests

--- platform/migration/platform-migration/src/test/java/org/codice/ddf/configuration/migration/ExportMigrationManagerImplTest.java
+++ platform/migration/platform-migration/src/test/java/org/codice/ddf/configuration/migration/ExportMigrationManagerImplTest.java
@@ -1,14 +1,17 @@
 package org.codice.ddf.configuration.migration;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.FileUtils;
 import org.codice.ddf.migration.ExportMigrationException;
 import org.codice.ddf.migration.Migratable;
+import org.codice.ddf.migration.MigrationException;
 import org.codice.ddf.migration.MigrationOperation;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
@@ -122,8 +125,94 @@ public class ExportMigrationManagerImplTest extends AbstractMigrationReportTest 
     @Test
     public void testDoExport() throws Exception {
         MGR.doExport(VERSION);
-        final Map<String, Object> metadata = MGR.getMetadata();
 
+        assertMetaData(MGR.getMetadata());
+    }
+
+    @Test
+    public void testDoExportWhenOneMigratableAborts() throws Exception {
+        final MigrationException ME = new MigrationException("testing");
+
+        Mockito.doThrow(ME)
+                .when(MIGRATABLE2)
+                .doExport(Mockito.any());
+
+        thrown.expect(Matchers.sameInstance(ME));
+
+        MGR.doExport(VERSION);
+    }
+
+    @Test
+    public void testDoExportWithNullProductVersion() throws Exception {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage(Matchers.containsString("null product version"));
+
+        MGR.doExport(null);
+    }
+
+    @Test
+    public void testClose() throws Exception {
+        MGR.doExport(VERSION);
+
+        MGR.close();
+
+        final Map<String, ZipEntry> entries = AbstractMigrationTest.getEntriesFrom(EXPORT_FILE);
+
+        Assert.assertThat(entries, Matchers.aMapWithSize(1));
+        Assert.assertThat(entries,
+                Matchers.hasKey(MigrationContextImpl.METADATA_FILENAME.toString()));
+        final Object ometadata =
+                JsonUtils.MAPPER.fromJson(entries.get(MigrationContextImpl.METADATA_FILENAME.toString())
+                        .getContent());
+
+        Assert.assertThat(ometadata, Matchers.instanceOf(Map.class));
+        assertMetaData((Map<String, Object>) ometadata);
+    }
+
+    @Test
+    public void testCloseWhenAlreadyClosed() throws Exception {
+        final ZipOutputStream ZOS = Mockito.mock(ZipOutputStream.class);
+        final ExportMigrationManagerImpl MGR = new ExportMigrationManagerImpl(REPORT, EXPORT_FILE, Stream.of(MIGRATABLES), ZOS);
+
+        Mockito.doNothing().when(ZOS).closeEntry();
+
+        MGR.close();
+
+        MGR.close();
+
+        Mockito.verify(ZOS).closeEntry();
+    }
+
+    @Test
+    public void testCloseWhileFailingToCreateMetadataEntry() throws Exception {
+        final ZipOutputStream ZOS = Mockito.mock(ZipOutputStream.class);
+        final ExportMigrationManagerImpl MGR = new ExportMigrationManagerImpl(REPORT, EXPORT_FILE, Stream.of(MIGRATABLES), ZOS);
+        final IOException IOE = new IOException("testing");
+
+        Mockito.doNothing().when(ZOS).closeEntry();
+        Mockito.doThrow(IOE).when(ZOS).putNextEntry(Mockito.any());
+
+        thrown.expect(ExportMigrationException.class);
+        thrown.expectMessage(Matchers.containsString("unable to create metadata"));
+        thrown.expectCause(Matchers.sameInstance(IOE));
+
+        MGR.close();
+    }
+
+    @Test
+    public void testCloseWhileFailingToCloseLastEntry() throws Exception {
+        final ZipOutputStream ZOS = Mockito.mock(ZipOutputStream.class);
+        final ExportMigrationManagerImpl MGR = new ExportMigrationManagerImpl(REPORT, EXPORT_FILE, Stream.of(MIGRATABLES), ZOS);
+        final IOException IOE = new IOException("testing");
+
+        Mockito.doThrow(IOE).when(ZOS).closeEntry();
+
+        thrown.expect(Matchers.sameInstance(IOE));
+
+        MGR.close();
+    }
+
+    private void assertMetaData(Map<String, Object> metadata) {
         Assert.assertThat(metadata, Matchers.aMapWithSize(4));
         Assert.assertThat(metadata,
                 Matchers.hasEntry(MigrationContextImpl.METADATA_VERSION,
@@ -147,12 +236,16 @@ public class ExportMigrationManagerImplTest extends AbstractMigrationReportTest 
                             (Map<String, Object>) mmetadatas.get(m.getId());
 
                     Assert.assertThat(mmetadata, Matchers.aMapWithSize(4));
-                    Assert.assertThat(mmetadata, Matchers.hasEntry(MigrationContextImpl.METADATA_VERSION, VERSION));
-                    Assert.assertThat(mmetadata, Matchers.hasEntry(MigrationContextImpl.METADATA_TITLE, TITLE));
-                    Assert.assertThat(mmetadata, Matchers.hasEntry(MigrationContextImpl.METADATA_DESCRIPTION, DESCRIPTION));
-                    Assert.assertThat(mmetadata, Matchers.hasEntry(MigrationContextImpl.METADATA_ORGANIZATION, ORGANIZATION));
+                    Assert.assertThat(mmetadata,
+                            Matchers.hasEntry(MigrationContextImpl.METADATA_VERSION, VERSION));
+                    Assert.assertThat(mmetadata,
+                            Matchers.hasEntry(MigrationContextImpl.METADATA_TITLE, TITLE));
+                    Assert.assertThat(mmetadata,
+                            Matchers.hasEntry(MigrationContextImpl.METADATA_DESCRIPTION,
+                                    DESCRIPTION));
+                    Assert.assertThat(mmetadata,
+                            Matchers.hasEntry(MigrationContextImpl.METADATA_ORGANIZATION,
+                                    ORGANIZATION));
                 });
     }
 }
-
-
