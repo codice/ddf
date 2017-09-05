33f90d8029ea6f97f2f8b2e9de6993a86bfc9585
Andrew Cross <andrew.cross@connexta.com>
2017-09-05 15:20:50 -0700

    DDF-3251 Reject invalid attributes values and log them

--- catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/config/ConfigurationApplication.java
+++ catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/config/ConfigurationApplication.java
@@ -173,7 +173,16 @@ public class ConfigurationApplication implements SparkApplication {
     public void setAttributeAliases(List<String> attributeAliases) {
         this.attributeAliases = attributeAliases.stream()
                 .map(str -> str.split("=", 2))
-                .collect(Collectors.toMap(list -> list[0].trim(), list -> list.length > 1 ? list[1].trim() : ""));
+                .filter((list) -> {
+                    if (list.length <= 1) {
+                        if (LOGGER.isDebugEnabled()) {
+                            LOGGER.debug("Filtered out invalid attribute/alias: {}", list[0]);
+                        }
+                        return false;
+                    }
+                    return true;
+                })
+                .collect(Collectors.toMap(list -> list[0].trim(), list -> list[1].trim()));
     }
 
     public void setHiddenAttributes(List<String> hiddenAttributes) {
@@ -183,7 +192,16 @@ public class ConfigurationApplication implements SparkApplication {
     public void setAttributeDescriptions(List<String> attributeDescriptions) {
         this.attributeDescriptions = attributeDescriptions.stream()
                 .map(str -> str.split("=", 2))
-                .collect(Collectors.toMap(list -> list[0].trim(), list -> list.length > 1 ? list[1].trim() : ""));
+                .filter((list) -> {
+                    if (list.length <= 1) {
+                        if (LOGGER.isDebugEnabled()) {
+                            LOGGER.debug("Filtered out invalid attribute/description: {}", list[0]);
+                        }
+                        return false;
+                    }
+                    return true;
+                })
+                .collect(Collectors.toMap(list -> list[0].trim(), list -> list[1].trim()));
     }
 
     private List<String> readOnly = ImmutableList.of("checksum",
