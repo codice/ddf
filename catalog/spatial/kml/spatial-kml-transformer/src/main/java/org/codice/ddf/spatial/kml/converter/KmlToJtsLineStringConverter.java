20620a17f48d2729b4ecc59e94f99db178932d30
gordocanchola <gordo.canchola@connexta.com>
2017-09-14 19:24:15 -0700

    DDF-3243 KML InputTransformer (#2282)
    Adding the Kml Input Transformer
    Adding the Kml Converters to convert from Kml to Jts Geometry
    Adding the Kml to Metacard converter used to populate the metacard from the KML

---
+++ catalog/spatial/kml/spatial-kml-transformer/src/main/java/org/codice/ddf/spatial/kml/converter/KmlToJtsLineStringConverter.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Codice Foundation
+ * <p/>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ **/
+package org.codice.ddf.spatial.kml.converter;
+
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.springframework.util.CollectionUtils;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+
+import de.micromata.opengis.kml.v_2_2_0.LineString;
+
+public class KmlToJtsLineStringConverter {
+    private static final GeometryFactory GEOMETRY_FACTORY = JTSFactoryFinder.getGeometryFactory();
+
+    private KmlToJtsLineStringConverter() {
+    }
+
+    public static com.vividsolutions.jts.geom.LineString from(LineString kmlLineString) {
+        if (!isValidKmlLineString(kmlLineString)) {
+            return null;
+        }
+        Coordinate[] jtsCoordinates =
+                KmlToJtsCoordinateConverter.from(kmlLineString.getCoordinates());
+
+        return GEOMETRY_FACTORY.createLineString(jtsCoordinates);
+    }
+
+    public static boolean isValidKmlLineString(LineString kmlLineString) {
+        return kmlLineString != null && !CollectionUtils.isEmpty(kmlLineString.getCoordinates());
+    }
+}
---
+++ catalog/spatial/kml/spatial-kml-transformer/src/main/java/org/codice/ddf/spatial/kml/converter/KmlToJtsLinearRingConverter.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Codice Foundation
+ * <p/>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ **/
+package org.codice.ddf.spatial.kml.converter;
+
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.springframework.util.CollectionUtils;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+
+import de.micromata.opengis.kml.v_2_2_0.LinearRing;
+
+public class KmlToJtsLinearRingConverter {
+    private KmlToJtsLinearRingConverter() {
+    }
+
+    public static com.vividsolutions.jts.geom.LinearRing from(LinearRing kmlLinearRing) {
+        if (!isValidKmlLinearRing(kmlLinearRing)) {
+            return null;
+        }
+
+        GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
+
+        Coordinate[] jtsCoordinates =
+                KmlToJtsCoordinateConverter.from(kmlLinearRing.getCoordinates());
+
+        return geometryFactory.createLinearRing(jtsCoordinates);
+    }
+
+    public static boolean isValidKmlLinearRing(LinearRing kmlLinearRing) {
+        return kmlLinearRing != null && !CollectionUtils.isEmpty(kmlLinearRing.getCoordinates());
+    }
+}
