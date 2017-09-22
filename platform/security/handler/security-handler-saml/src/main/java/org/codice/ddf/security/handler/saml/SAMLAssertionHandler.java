266fdab0a2b1723715cc59ce139fcbe5377d661b
Matt Ramey <i.am.mrmateo@gmail.com>
2017-09-22 09:53:12 -0700

    DDF-3311: Removed unused private static declaration.

--- platform/security/handler/security-handler-saml/src/main/java/org/codice/ddf/security/handler/saml/SAMLAssertionHandler.java
+++ platform/security/handler/security-handler-saml/src/main/java/org/codice/ddf/security/handler/saml/SAMLAssertionHandler.java
@@ -59,8 +59,6 @@ public class SAMLAssertionHandler implements AuthenticationHandler {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SAMLAssertionHandler.class);
 
-  private static final String SAML_NAMESPACE = "urn:oasis:names:tc:SAML:2.0:assertion";
-
   private static final String EVIDENCE =
       "<%1$s:Evidence xmlns:%1$s=\"urn:oasis:names:tc:SAML:2.0:assertion\">%2$s</%1$s:Evidence>";
 
