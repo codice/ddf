f8a7e005f6d8965d0954e37751853c32dbfb1b45
Andrew Fiedler <andrewkfiedler@gmail.com>
2017-10-09 11:39:10 -0700

    DDF-3357 Adds distinguishment between dev and prod builds of Intrigue
     - Header and footer are now displayed on dev builds of Intrigue.
     - Hot reloading is now indicated, as well as the commit hash.  Also includes whether or not there were changes on top of the commit.
     - Still allows header and footer config, in case developers are testing them out.  Also allows closing the header and footer (if only the warning message).

--- catalog/ui/catalog-ui-search/webpack/config/dev.js
+++ catalog/ui/catalog-ui-search/webpack/config/dev.js
@@ -1,58 +1,10 @@
-var path = require('path');
 var webpack = require('webpack');
 var merge = require('webpack-merge');
-
 var base = require('./base');
 
-var resolve = function (place) {
-    return path.resolve(__dirname, '../../', place)
-};
-
-var handleProxyRes = function (proxyRes, req, res) {
-    // remove so we can still login in through http
-    delete proxyRes.headers['x-xss-protection'];
-    var cookie = proxyRes.headers['set-cookie'];
-    if (cookie !== undefined) {
-        // force the cookie to be insecure since the proxy is over http
-        proxyRes.headers['set-cookie'] = cookie[0].replace(new RegExp(/;\w?Secure/), '')
-    }
-};
-
-var proxyConfig = function (target) {
-    return {
-        target: target,
-        secure: false,
-        changeOrigin: true,
-        onProxyRes: handleProxyRes
-    }
-};
-
 module.exports = merge.smart(base, {
-    devServer: {
-        progress: true,
-        historyApiFallback: true,
-        inline: true,
-        hot: true,
-        contentBase: [resolve('./node_modules/cesium/Build/'), resolve('/src/main/webapp/')],
-        proxy: {
-            '/search/catalog/**': proxyConfig('https://localhost:8993'),
-            '/services/**': proxyConfig('https://localhost:8993'),
-            '/styles/**': proxyConfig('https://localhost:8993/search/catalog')
-        }
-    },
     entry: [
         'stack-source-map/register',
         'console-polyfill'
-    ],
-    module: {
-        loaders: [{
-            test: /\.jsx$/,
-            loaders: ['react-hot']
-        }]
-    },
-    plugins: [
-        new webpack.NoErrorsPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
     ]
 });
