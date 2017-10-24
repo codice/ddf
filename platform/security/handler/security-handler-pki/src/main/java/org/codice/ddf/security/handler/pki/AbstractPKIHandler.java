e81691e0834e2f6d84be8dd94863cc8b5f33f7c8
JosephThweatt <josephthweatt@gmail.com>
2017-10-24 15:49:40 -0700

    DDF-3394 Revoked Certificate now returns 403 instead of 500
    DDF-3394 Added /error servlet to whitelist

--- platform/security/handler/security-handler-pki/src/main/java/org/codice/ddf/security/handler/pki/AbstractPKIHandler.java
+++ platform/security/handler/security-handler-pki/src/main/java/org/codice/ddf/security/handler/pki/AbstractPKIHandler.java
@@ -103,7 +103,14 @@ public abstract class AbstractPKIHandler implements AuthenticationHandler {
     } else {
       String errorMsg = "The certificate used to complete the request has been revoked.";
       LOGGER.info(errorMsg);
-      throw new ServletException(errorMsg);
+      try {
+        httpResponse.sendError(
+            HttpServletResponse.SC_FORBIDDEN, "Your certificate has been revoked.");
+        httpResponse.flushBuffer();
+      } catch (Exception e) {
+        LOGGER.error("Error returning 403 to revoked certificate request.");
+      }
+      handlerResult.setStatus(HandlerResult.Status.REDIRECTED);
     }
 
     return handlerResult;
