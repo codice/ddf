bc0ee2bf6f4971d47bbc6ec02e01d9d998accfd9
Patrick Ouellet <patrick.ouellet@connexta.com>
2017-10-30 16:50:27 -0700

    DDF-3416 updated Windows scripts to support restarting when out of memory and fatal errors occur
    DDF-3416 introduced our own version of the start scripts to also support this feature when running as a background process

--- distribution/ddf-common/src/main/resources/bin/ddf.bat
+++ distribution/ddf-common/src/main/resources/bin/ddf.bat
@@ -1,9 +1,27 @@
-@echo off
-setlocal
+@ECHO off
+SETLOCAL
 
-set ARGS=%*
-set DIRNAME=%~dp0%
+SET ARGS=%*
+SET DIRNAME=%~dp0%
 
-rem Actually invoke ddf to gain restart support
-call "%DIRNAME%/karaf.bat" %ARGS%
+:RESTART
+REM Remove the restart file indicator so we can detect later if restart was requested
+IF EXIST "%DIRNAME%\restart.jvm" (
+  DEL "%DIRNAME%\restart.jvm"
+)
 
+REM Actually invoke ddf to gain restart support
+CALL "%DIRNAME%\karaf.bat" %ARGS%
+SET RC=%ERRORLEVEL%
+
+IF "%1"== "daemon" (
+  EXIT /B %RC%
+) ELSE (
+  REM Check if restart was requested by ddf_on_error.bat
+  IF EXIST "%DIRNAME%\restart.jvm" (
+    ECHO Restarting JVM...
+    GOTO :RESTART
+  ) ELSE (
+    EXIT /B %RC%
+  )
+)
