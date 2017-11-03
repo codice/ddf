7411d6707dbebd75a55d3063e4f187062208a253
Gordo Canchola <gordo.canchola@connexta.com>
2017-11-03 09:31:14 -0700

    DDF-3270 KMZ Input Transformer PR Comments
    Adding tests

--- catalog/spatial/kml/spatial-kml-transformer/src/test/java/org/codice/ddf/spatial/kml/transformer/TestKmzInputTransformer.java
+++ catalog/spatial/kml/spatial-kml-transformer/src/test/java/org/codice/ddf/spatial/kml/transformer/TestKmzInputTransformer.java
@@ -13,20 +13,81 @@
  **/
 package org.codice.ddf.spatial.kml.transformer;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Mockito.mock;
 
+import ddf.catalog.data.Metacard;
+import ddf.catalog.data.impl.MetacardImpl;
+import ddf.catalog.data.impl.MetacardTypeImpl;
 import ddf.catalog.transform.CatalogTransformerException;
 import java.io.InputStream;
+import java.util.HashSet;
 import org.junit.Test;
 
 public class TestKmzInputTransformer {
 
+  private static final String EXPECTED_LONG = "-122.0822035425683";
+  private static final String EXPECTED_LAT = "37.42228990140251";
+  private static final String EXPECTED_TYPE = "POINT";
+
   @Test
   public void testKmzWithKmlInside() throws Exception {
     KmzInputTransformer kmzInputTransformer =
-        new KmzInputTransformer(mock(KmlInputTransformer.class));
+        new KmzInputTransformer(
+            new KmlInputTransformer(new MetacardTypeImpl("kmlMetacardType", new HashSet<>())));
     InputStream stream = TestKmzInputTransformer.class.getResourceAsStream("/kml_inside.kmz");
-    kmzInputTransformer.transform(stream);
+    Metacard metacard = kmzInputTransformer.transform(stream);
+    assertThat(metacard, notNullValue());
+
+    String location = metacard.getLocation();
+    assertThat(location, not(isEmptyOrNullString()));
+    assertThat(location, containsString(EXPECTED_LAT));
+    assertThat(location, containsString(EXPECTED_LONG));
+    assertThat(location, containsString(EXPECTED_TYPE));
+  }
+
+  @Test
+  public void testKmzWithMultipleKmlAndOtherStuffInside() throws Exception {
+    KmzInputTransformer kmzInputTransformer =
+        new KmzInputTransformer(
+            new KmlInputTransformer(new MetacardTypeImpl("kmlMetacardType", new HashSet<>())));
+    InputStream stream =
+        TestKmzInputTransformer.class.getResourceAsStream("/kmzWithMultipleKml.kmz");
+    Metacard metacard = kmzInputTransformer.transform(stream);
+    assertThat(metacard, notNullValue());
+
+    String location = metacard.getLocation();
+    assertThat(location, not(isEmptyOrNullString()));
+    assertThat(location, containsString(EXPECTED_LAT));
+    assertThat(location, containsString(EXPECTED_LONG));
+    assertThat(location, containsString(EXPECTED_TYPE));
+  }
+
+  @Test
+  public void testSameKmlInSingleAndMultipleKmzGeneratesSameValues() throws Exception {
+    KmzInputTransformer kmzInputTransformer =
+        new KmzInputTransformer(
+            new KmlInputTransformer(new MetacardTypeImpl("kmlMetacardType", new HashSet<>())));
+    InputStream multipleKmlStream =
+        TestKmzInputTransformer.class.getResourceAsStream("/kmzWithMultipleKml.kmz");
+    InputStream singleKmlStream =
+        TestKmzInputTransformer.class.getResourceAsStream("/kml_inside.kmz");
+    Metacard singleKmlMetacard = kmzInputTransformer.transform(singleKmlStream);
+    assertThat(singleKmlMetacard, notNullValue());
+
+    Metacard multipleKmlMetacard = kmzInputTransformer.transform(multipleKmlStream);
+    assertThat(multipleKmlMetacard, notNullValue());
+
+    assertThat(
+        ((MetacardImpl) multipleKmlMetacard).getDescription(),
+        equalTo(((MetacardImpl) singleKmlMetacard).getDescription()));
+    assertThat(multipleKmlMetacard.getMetadata(), equalTo(singleKmlMetacard.getMetadata()));
+    assertThat(multipleKmlMetacard.getLocation(), equalTo(singleKmlMetacard.getLocation()));
   }
 
   @Test(expected = CatalogTransformerException.class)
