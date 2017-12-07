2ad5de975b1ae82adebe3b5dcb9f3cc27b46a10a
Blen Desta <blen.desta@connexta.com>
2017-12-07 08:04:25 -0700

    DDF-3469 Sanitize passwords from the security log (#2701)
    - Auditing the fact that a configuration has been update rather than the updated configuration itself
     	- Addressed static analysis findings in SeccurityLogger

--- platform/security/core/security-core-impl/src/main/java/ddf/security/config/impl/ConfigurationSecurityLogger.java
+++ platform/security/core/security-core-impl/src/main/java/ddf/security/config/impl/ConfigurationSecurityLogger.java
@@ -14,78 +14,34 @@
 package ddf.security.config.impl;
 
 import ddf.security.common.audit.SecurityLogger;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-import java.util.Dictionary;
 import javax.security.auth.Subject;
-import org.apache.felix.cm.file.ConfigurationHandler;
 import org.apache.shiro.util.ThreadContext;
-import org.osgi.service.cm.Configuration;
-import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.cm.ConfigurationEvent;
 import org.osgi.service.cm.SynchronousConfigurationListener;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ConfigurationSecurityLogger implements SynchronousConfigurationListener {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(ConfigurationSecurityLogger.class);
-
-  private ConfigurationAdmin configurationAdmin;
-
   @Override
-  @SuppressWarnings("squid:S1181" /* Intentionally catching all Throwable */)
   public void configurationEvent(ConfigurationEvent event) {
     AccessController.doPrivileged(
         (PrivilegedAction<Void>)
             () -> {
-              try {
-                String type = getType(event);
-                Configuration[] configuration =
-                    configurationAdmin.listConfigurations("(service.pid=" + event.getPid() + ')');
+              String type = getType(event);
 
-                String updatedConfiguration = "{}";
-                // the service.pid is unique so we just need to check if we got anything.
-                if (configuration != null && configuration.length > 0) {
-                  updatedConfiguration = dictionaryToString(configuration[0].getProperties());
-                }
-
-                // check if there is a subject associated with the configuration change
-                if (ThreadContext.getSubject() != null
-                    || Subject.getSubject(AccessController.getContext()) != null) {
-                  SecurityLogger.audit("Configuration {}: {}", type, updatedConfiguration);
-                } else {
-                  // there was no subject change was caused by an update to the config file on the
-                  // filesystem
-                  SecurityLogger.auditWarn(
-                      "Configuration {} via filesystem: {}", type, updatedConfiguration);
-                }
-              } catch (Throwable e) {
-                LOGGER.error("Error auditing config update for {}", event.getPid(), e);
-                SecurityLogger.audit("Error auditing config update for {}", event.getPid());
+              // check if there is a subject associated with the configuration change
+              if (ThreadContext.getSubject() != null
+                  || Subject.getSubject(AccessController.getContext()) != null) {
+                SecurityLogger.audit("Configuration {} for {}.", type, event.getPid());
+              } else {
+                // there was no subject change was caused by an update to the config file on the
+                // filesystem
+                SecurityLogger.auditWarn(
+                    "Configuration {} via filesystem for {}.", type, event.getPid());
               }
-
               return null;
             });
-  }
-
-  @SuppressWarnings("squid:S00112" /* Generic RuntimeException is acceptable here */)
-  private String dictionaryToString(Dictionary<String, Object> properties) {
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    try {
-
-      ConfigurationHandler.write(baos, properties);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-    return '{' + new String(baos.toByteArray(), StandardCharsets.UTF_8).replace("\r\n", " ") + '}';
-  }
-
-  public void setConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
-    this.configurationAdmin = configurationAdmin;
   }
 
   private String getType(ConfigurationEvent event) {
