46077ea79265d5dc651a262a07cef4a560bca2d9
Brandan Jeter <brandanjeter@gmail.com>
2017-12-20 12:20:08 -0700

    DDF-3499 Doing null check before returning the center point

--- catalog/core/catalog-core-solr/src/main/java/ddf/catalog/source/solr/DynamicSchemaResolver.java
+++ catalog/core/catalog-core-solr/src/main/java/ddf/catalog/source/solr/DynamicSchemaResolver.java
@@ -414,6 +414,10 @@ public class DynamicSchemaResolver {
       centerPoint = geometries.get(0).getCentroid();
     }
 
+    if (centerPoint == null || centerPoint.isEmpty()) {
+        return null;
+    }
+
     return centerPoint.getY() + "," + centerPoint.getX();
   }
 
