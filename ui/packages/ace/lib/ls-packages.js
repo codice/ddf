8d718f01f77f9fda5eee533bb3ef9570513599d0
Chris Badahdah <a.s.badahdah@gmail.com>
2018-01-29 14:30:04 -0700

    DDF-3584 Adds node maven/osgi bundler tooling

---
+++ ui/packages/ace/lib/ls-packages.js
@@ -0,0 +1,17 @@
+const glob = require('glob')
+const path = require('path')
+
+const flatten = (l, v) => l.concat(v)
+
+module.exports = (dirs) => {
+  return dirs
+    .map((d) => path.resolve(d))
+    .map((d) => glob.sync(d))
+    .reduce(flatten, [])
+    .map((d) => path.join(d, 'package.json'))
+    .map((d) => {
+      const json = require(d)
+      json.__path = d
+      return json
+    })
+}
