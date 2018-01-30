codice/ddf-libs

DDF-3105 Create Component test for REST endpoint (#2090)
Created initial REST endpoint component tests and common test library.
Moved Pax Exam specific classes from itests-common to common test library.
Moved PaxExamRule to the new common test library, improved TestFailureLogger and added JavaDoc comments.
Updated itests to use new common test library.

test-common/src/main/java/org/codice/ddf/test/common/AbstractComponentTest.java
@@ -1,0 +1,103 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.codice.ddf.test.common;
+
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+import org.codice.ddf.test.common.configurators.ApplicationOptions;
+import org.codice.ddf.test.common.configurators.BundleOptionBuilder;
+import org.codice.ddf.test.common.configurators.BundleOptionBuilder.BundleOption;
+import org.codice.ddf.test.common.configurators.ContainerOptions;
+import org.codice.ddf.test.common.configurators.KarafOptions;
+import org.codice.ddf.test.common.configurators.PortFinder;
+import org.codice.ddf.test.common.rules.TestFailureLogger;
+import org.junit.Rule;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Abstract base class for component tests. Extend to provide and implementation for the {@link
+ * #getApplicationOptions()} methods and the test methods.
+ *
+ * <p>It is important to remember that the {@link #config()} method and all the methods it calls,
+ * i.e., {@link #getContainerOptions()} and {@link #getApplicationOptions()}, are called inside the
+ * test runner process and are only used to configure the test container. All the other methods in
+ * this class and its sub-classes will be run inside the test container, which is a separate
+ * process.
+ *
+ * @see ApplicationOptions
+ * @see ContainerOptions
+ */
+public abstract class AbstractComponentTest {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(AbstractComponentTest.class);
+
+  private static PortFinder portFinder;
+
+  @Rule public TestFailureLogger testFailureLogger = new TestFailureLogger();
+
+  /**
+   * Configuration method called during Pax Exam configuration. The {@link Option}s returned will be
+   * used by Pax Exam to configure the container that will be spawn and used to execute the tests.
+   *
+   * @return Pax Exam {@link Option} that will be used to configure the Pax Exam test container
+   */
+  @Configuration
+  @SuppressWarnings("squid:S2696") // See comment below.
+  public Option[] config() {
+    LOGGER.trace("config");
+
+    // Can't call close on PortFinder because the port range needs to remain locked until
+    // all the tests have executed. The port range will be automatically released when the
+    // test runner process exists. Variable also needs to be static to make sure the PortFinder
+    // object doesn't get garbage collected between the time config is called and the tests are
+    // run.
+    portFinder = new PortFinder();
+
+    return options(
+        getContainerOptions().get(), getApplicationOptions().get(), getTestBundleOptions().build());
+  }
+
+  public static PortFinder getPortFinder() {
+    return portFinder;
+  }
+
+  /**
+   * Gets the object to use to configure the container.
+   *
+   * @return object that returns the container's configuration {@link Option}s. Default
+   *     implementation returns {@link KarafOptions}.
+   */
+  protected ContainerOptions getContainerOptions() {
+    return new KarafOptions(portFinder);
+  }
+
+  /**
+   * Gets the object to use to configure the component or application inside the container.
+   *
+   * @return object that returns the application's configuration {@link Option}s
+   */
+  protected abstract ApplicationOptions getApplicationOptions();
+
+  private BundleOption getTestBundleOptions() {
+    return BundleOptionBuilder.add("org.mockito", "mockito-core")
+        .add("org.objenesis", "objenesis")
+        .add("org.awaitility", "awaitility")
+        .add("org.apache.commons", "commons-lang3")
+        .add("ddf.lib", "test-common")
+        .add("ddf.lib", "common-system");
+  }
+}

codice/ddf

libs/test-common/src/main/java/org/codice/ddf/test/common/AbstractComponentTest.java
@@ -1,0 +1,102 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.codice.ddf.test.common;
+
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+import org.codice.ddf.test.common.configurators.ApplicationOptions;
+import org.codice.ddf.test.common.configurators.BundleOptionBuilder;
+import org.codice.ddf.test.common.configurators.BundleOptionBuilder.BundleOption;
+import org.codice.ddf.test.common.configurators.ContainerOptions;
+import org.codice.ddf.test.common.configurators.KarafOptions;
+import org.codice.ddf.test.common.configurators.PortFinder;
+import org.codice.ddf.test.common.rules.TestFailureLogger;
+import org.junit.Rule;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Abstract base class for component tests. Extend to provide and implementation for the {@link
+ * #getApplicationOptions()} methods and the test methods.
+ *
+ * <p>It is important to remember that the {@link #config()} method and all the methods it calls,
+ * i.e., {@link #getContainerOptions()} and {@link #getApplicationOptions()}, are called inside the
+ * test runner process and are only used to configure the test container. All the other methods in
+ * this class and its sub-classes will be run inside the test container, which is a separate
+ * process.
+ *
+ * @see ApplicationOptions
+ * @see ContainerOptions
+ */
+public abstract class AbstractComponentTest {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(AbstractComponentTest.class);
+
+  private static PortFinder portFinder;
+
+  @Rule public TestFailureLogger testFailureLogger = new TestFailureLogger();
+
+  /**
+   * Configuration method called during Pax Exam configuration. The {@link Option}s returned will be
+   * used by Pax Exam to configure the container that will be spawn and used to execute the tests.
+   *
+   * @return Pax Exam {@link Option} that will be used to configure the Pax Exam test container
+   */
+  @Configuration
+  @SuppressWarnings("squid:S2696") // See comment below.
+  public Option[] config() {
+    LOGGER.trace("config");
+
+    // Can't call close on PortFinder because the port range needs to remain locked until
+    // all the tests have executed. The port range will be automatically released when the
+    // test runner process exists. Variable also needs to be static to make sure the PortFinder
+    // object doesn't get garbage collected between the time config is called and the tests are
+    // run.
+    portFinder = new PortFinder();
+
+    return options(
+        getContainerOptions().get(), getApplicationOptions().get(), getTestBundleOptions().build());
+  }
+
+  public static PortFinder getPortFinder() {
+    return portFinder;
+  }
+
+  /**
+   * Gets the object to use to configure the container.
+   *
+   * @return object that returns the container's configuration {@link Option}s. Default
+   *     implementation returns {@link KarafOptions}.
+   */
+  protected ContainerOptions getContainerOptions() {
+    return new KarafOptions(portFinder);
+  }
+
+  /**
+   * Gets the object to use to configure the component or application inside the container.
+   *
+   * @return object that returns the application's configuration {@link Option}s
+   */
+  protected abstract ApplicationOptions getApplicationOptions();
+
+  private BundleOption getTestBundleOptions() {
+    return BundleOptionBuilder.add("org.mockito", "mockito-core")
+        .add("org.objenesis", "objenesis")
+        .add("org.awaitility", "awaitility")
+        .add("org.apache.commons", "commons-lang3")
+        .add("ddf.lib", "test-common");
+  }
+}