dc8aa8d810176e2b21b2ca60cfbb5d654fa14ab1
Patrick Ouellet <patrick.ouellet@connexta.com>
2018-02-01 17:41:25 -0700

    DDF-3599 Moved generic matchers to test common library

--- libs/test-common/src/main/java/org/codice/ddf/test/common/matchers/ThrowableMatchers.java
+++ libs/test-common/src/main/java/org/codice/ddf/test/common/matchers/ThrowableMatchers.java
@@ -11,7 +11,7 @@
  * License is distributed along with this program and can be found at
  * <http://www.gnu.org/licenses/lgpl.html>.
  */
-package org.codice.ddf.test.matchers;
+package org.codice.ddf.test.common.matchers;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
