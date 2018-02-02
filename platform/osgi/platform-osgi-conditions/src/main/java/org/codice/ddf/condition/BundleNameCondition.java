31531e21f12f47fce8ab1d3bf92d92c20f9b61c3
stustison <scott.tustison@connexta.com>
2018-02-02 09:51:11 -0700

    DDF-3297 add security manager to DDF

--- platform/osgi/platform-osgi-conditions/src/main/java/org/codice/ddf/condition/BundleNameCondition.java
+++ platform/osgi/platform-osgi-conditions/src/main/java/org/codice/ddf/condition/BundleNameCondition.java
@@ -20,6 +20,13 @@ import org.osgi.framework.Bundle;
 import org.osgi.service.condpermadmin.Condition;
 import org.osgi.service.condpermadmin.ConditionInfo;
 
+/**
+ * The interface implemented by a Condition. Conditions are bound to Permissions using Conditional
+ * Permission Info. The Permissions of a ConditionalPermission Info can only be used if the
+ * associated Conditions are satisfied.
+ *
+ * <p>Selects bundles based on the name. @ThreadSafe
+ */
 public class BundleNameCondition implements Condition {
 
   private static final ConcurrentHashMap<String, Boolean> DECISION_MAP =
@@ -35,11 +42,32 @@ public class BundleNameCondition implements Condition {
     args = conditionInfo.getArgs();
   }
 
+  /**
+   * Returns whether the evaluation must be postponed until the end of the permission check. If this
+   * method returns {@code false} (or this Condition is immutable), then this Condition must be able
+   * to directly answer the {@link #isSatisfied()} method. In other words, isSatisfied() will return
+   * very quickly since no external sources, such as for example users or networks, need to be
+   * consulted. <br>
+   * This method must always return the same value whenever it is called so that the Conditional
+   * Permission Admin can cache its result.
+   *
+   * @return {@code true} to indicate the evaluation must be postponed. Otherwise, {@code false} if
+   *     the evaluation can be performed immediately.
+   */
   @Override
   public boolean isPostponed() {
     return false;
   }
 
+  /**
+   * Returns whether the Condition is satisfied. This method is only called for immediate Condition
+   * objects or immutable postponed conditions, and must always be called inside a permission check.
+   * Mutable postponed Condition objects will be called with the grouped version {@link
+   * #isSatisfied(Condition[],Dictionary)} at the end of the permission check.
+   *
+   * @return {@code true} to indicate the Conditions is satisfied. Otherwise, {@code false} if the
+   *     Condition is not satisfied.
+   */
   @Override
   public boolean isSatisfied() {
     if (args.length == 0) {
@@ -62,11 +90,35 @@ public class BundleNameCondition implements Condition {
     return false;
   }
 
+  /**
+   * Returns whether the Condition is mutable. A Condition can go from mutable ({@code true}) to
+   * immutable ({@code false}) over time but never from immutable ({@code false}) to mutable ({@code
+   * true}).
+   *
+   * @return {@code true} {@link #isSatisfied()} can change. Otherwise, {@code false} if the value
+   *     returned by {@link #isSatisfied()} will not change for this condition.
+   */
   @Override
   public boolean isMutable() {
     return false;
   }
 
+  /**
+   * Returns whether the specified set of Condition objects are satisfied. Although this method is
+   * not static, it must be implemented as if it were static. All of the passed Condition objects
+   * will be of the same type and will correspond to the class type of the object on which this
+   * method is invoked. This method must be called inside a permission check only.
+   *
+   * @param conditions The array of Condition objects, which must all be of the same class and
+   *     mutable. The receiver must be one of those Condition objects.
+   * @param context A Dictionary object that implementors can use to track state. If this method is
+   *     invoked multiple times in the same permission check, the same Dictionary will be passed
+   *     multiple times. The SecurityManager treats this Dictionary as an opaque object and simply
+   *     creates an empty dictionary and passes it to subsequent invocations if multiple invocations
+   *     are needed.
+   * @return {@code true} if all the Condition objects are satisfied. Otherwise, {@code false} if
+   *     one of the Condition objects is not satisfied.
+   */
   @Override
   public boolean isSatisfied(Condition[] conditions, Dictionary<Object, Object> context) {
     for (Condition condition : conditions) {
--- platform/osgi/platform-osgi-conditions/src/main/java/org/codice/ddf/condition/SignerCondition.java
+++ platform/osgi/platform-osgi-conditions/src/main/java/org/codice/ddf/condition/SignerCondition.java
@@ -38,6 +38,13 @@ import org.osgi.framework.Bundle;
 import org.osgi.service.condpermadmin.Condition;
 import org.osgi.service.condpermadmin.ConditionInfo;
 
+/**
+ * The interface implemented by a Condition. Conditions are bound to Permissions using Conditional
+ * Permission Info. The Permissions of a ConditionalPermission Info can only be used if the
+ * associated Conditions are satisfied.
+ *
+ * <p>Selects bundles based on the identity of the signer. @ThreadSafe
+ */
 public class SignerCondition implements Condition {
 
   private Bundle bundle;
@@ -48,11 +55,32 @@ public class SignerCondition implements Condition {
     args = conditionInfo.getArgs();
   }
 
+  /**
+   * Returns whether the evaluation must be postponed until the end of the permission check. If this
+   * method returns {@code false} (or this Condition is immutable), then this Condition must be able
+   * to directly answer the {@link #isSatisfied()} method. In other words, isSatisfied() will return
+   * very quickly since no external sources, such as for example users or networks, need to be
+   * consulted. <br>
+   * This method must always return the same value whenever it is called so that the Conditional
+   * Permission Admin can cache its result.
+   *
+   * @return {@code true} to indicate the evaluation must be postponed. Otherwise, {@code false} if
+   *     the evaluation can be performed immediately.
+   */
   @Override
   public boolean isPostponed() {
     return false;
   }
 
+  /**
+   * Returns whether the Condition is satisfied. This method is only called for immediate Condition
+   * objects or immutable postponed conditions, and must always be called inside a permission check.
+   * Mutable postponed Condition objects will be called with the grouped version {@link
+   * #isSatisfied(Condition[],Dictionary)} at the end of the permission check.
+   *
+   * @return {@code true} to indicate the Conditions is satisfied. Otherwise, {@code false} if the
+   *     Condition is not satisfied.
+   */
   @Override
   public boolean isSatisfied() {
     Map<X509Certificate, List<X509Certificate>> signerCertificates =
@@ -74,11 +102,35 @@ public class SignerCondition implements Condition {
     return true;
   }
 
+  /**
+   * Returns whether the Condition is mutable. A Condition can go from mutable ({@code true}) to
+   * immutable ({@code false}) over time but never from immutable ({@code false}) to mutable ({@code
+   * true}).
+   *
+   * @return {@code true} {@link #isSatisfied()} can change. Otherwise, {@code false} if the value
+   *     returned by {@link #isSatisfied()} will not change for this condition.
+   */
   @Override
   public boolean isMutable() {
     return false;
   }
 
+  /**
+   * Returns whether the specified set of Condition objects are satisfied. Although this method is
+   * not static, it must be implemented as if it were static. All of the passed Condition objects
+   * will be of the same type and will correspond to the class type of the object on which this
+   * method is invoked. This method must be called inside a permission check only.
+   *
+   * @param conditions The array of Condition objects, which must all be of the same class and
+   *     mutable. The receiver must be one of those Condition objects.
+   * @param context A Dictionary object that implementors can use to track state. If this method is
+   *     invoked multiple times in the same permission check, the same Dictionary will be passed
+   *     multiple times. The SecurityManager treats this Dictionary as an opaque object and simply
+   *     creates an empty dictionary and passes it to subsequent invocations if multiple invocations
+   *     are needed.
+   * @return {@code true} if all the Condition objects are satisfied. Otherwise, {@code false} if
+   *     one of the Condition objects is not satisfied.
+   */
   @Override
   public boolean isSatisfied(Condition[] conditions, Dictionary<Object, Object> context) {
     for (Condition condition : conditions) {
