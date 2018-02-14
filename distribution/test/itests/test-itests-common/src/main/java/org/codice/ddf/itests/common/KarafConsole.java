b2f4ccdb35f72d4a1ddb29c5c22700d675fde476
stustison <scott.tustison@connexta.com>
2018-02-14 14:43:19 -0700

    DDF-3297 add security manager to DDF

--- distribution/test/itests/test-itests-common/src/main/java/org/codice/ddf/itests/common/KarafConsole.java
+++ distribution/test/itests/test-itests-common/src/main/java/org/codice/ddf/itests/common/KarafConsole.java
@@ -85,7 +85,7 @@ public class KarafConsole extends KarafTestSupport {
    */
   @SuppressWarnings("squid:S00112")
   public String runCommand(String command) {
-    String response = executeCommand(command, 60000L, false, DEFAULT_ROLES);
+    String response = executeCommand(command, 120000L, false, DEFAULT_ROLES);
     if (response.contains("SHELL COMMAND TIMED OUT:")) {
       throw new RuntimeException("Shell command timed out: " + command);
     }
