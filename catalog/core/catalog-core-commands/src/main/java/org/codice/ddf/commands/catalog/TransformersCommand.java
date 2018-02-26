f3b6ac39389b63584ad79a38c0547ed3768cf2b5
kcover <kcover@users.noreply.github.com>
2018-02-26 09:34:25 -0700

    DDF-3202 Extended catalog:transformer command (#2970)
    It now lists metacard transformers as well. You can also indicate if you
    want just input transformers or just metacard transformers with the
    -i and -m options. Lastly, if you want all the properties of the
    transformers (instead of just id, schema, and mime-types) use the -a
    option.

--- catalog/core/catalog-core-commands/src/main/java/org/codice/ddf/commands/catalog/TransformersCommand.java
+++ catalog/core/catalog-core-commands/src/main/java/org/codice/ddf/commands/catalog/TransformersCommand.java
@@ -13,18 +13,23 @@
  */
 package org.codice.ddf.commands.catalog;
 
+import com.google.common.annotations.VisibleForTesting;
 import ddf.catalog.Constants;
 import ddf.catalog.transform.InputTransformer;
+import ddf.catalog.transform.MetacardTransformer;
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 
 /** Provides information on available transformers */
@@ -32,15 +37,18 @@ import org.osgi.framework.ServiceReference;
 @Command(
   scope = CatalogCommands.NAMESPACE,
   name = "transformers",
-  description = "Provides information on available transformers."
+  description =
+      "Provides information on available transformers. By default, prints out all Metacard and Input transformers."
 )
 public class TransformersCommand extends CatalogCommands {
 
   // output strings
 
-  static final String ACTIVE_TRANSFORMERS_HEADER = "Active Transformers: ";
+  private static final String METACARD = "Metacard";
 
-  static final String NO_ACTIVE_TRANSFORMERS = "There are no active transformers";
+  private static final String INPUT = "Input";
+
+  private static final String FILTER = "(id=*)";
 
   private static final String LINE = "------------------------";
 
@@ -52,58 +60,100 @@ public class TransformersCommand extends CatalogCommands {
 
   private static final String NOT_AVAILABLE = "N/A";
 
+  @Option(
+    name = "--input",
+    required = false,
+    aliases = {"-i", "-input"},
+    multiValued = false,
+    description = "Displays only input transformers"
+  )
+  private boolean inputOption = false;
+
+  @Option(
+    name = "--metacard",
+    required = false,
+    aliases = {"-m", "-metacard"},
+    multiValued = false,
+    description = "Displays only metacard transformers"
+  )
+  private boolean metacardOption = false;
+
+  @Option(
+    name = "--all",
+    required = false,
+    aliases = {"-a", "-all"},
+    multiValued = false,
+    description = "Displays all the properties of the desired transformers"
+  )
+  private boolean allOption = false;
+
   @Override
   protected Object executeWithSubject() throws Exception {
+    if (!inputOption && !metacardOption) {
+      inputOption = true;
+      metacardOption = true;
+    }
 
-    // ServiceReferences are needed to get property information from InputTransformers
-    List<ServiceReference<InputTransformer>> serviceReferences =
-        (List<ServiceReference<InputTransformer>>)
-            bundleContext.getServiceReferences(InputTransformer.class, "(id=*)");
+    if (inputOption) {
+      printTransformers(InputTransformer.class, INPUT);
+    }
+    if (metacardOption) {
+      printTransformers(MetacardTransformer.class, METACARD);
+    }
+
+    return null;
+  }
+
+  private void printTransformers(Class transformerClass, String type)
+      throws InvalidSyntaxException {
+
+    Collection<ServiceReference> sref =
+        bundleContext.getServiceReferences(transformerClass, FILTER);
 
     List<TransformerProperties> transformersProperties =
-        serviceReferences.stream().map(TransformerProperties::new).collect(Collectors.toList());
+        sref.stream().map(TransformerProperties::new).collect(Collectors.toList());
 
     int activeTransformers = transformersProperties.size();
 
     if (activeTransformers == 0) {
-      console.printf("%s%n%n", NO_ACTIVE_TRANSFORMERS);
-      return null;
+      console.printf("There are no active %s transformers%n%n", type);
+      return;
     }
 
-    console.printf("%n%s%d%n%s%n%n", ACTIVE_TRANSFORMERS_HEADER, activeTransformers, LINE);
+    console.printf("%n%n%n%nActive %s Transformers: %d%n%s%n%n", type, activeTransformers, LINE);
 
     Iterator<TransformerProperties> tpIterator = transformersProperties.iterator();
     TransformerProperties tp;
 
     while (tpIterator.hasNext()) {
       tp = tpIterator.next();
-      console.printf("%s", tp.printProperties());
+
+      if (allOption) {
+        console.printf("%s", tp.printAllProperties());
+      } else {
+        console.printf("%s", tp.printDefaultProperties());
+      }
 
       if (tpIterator.hasNext()) {
         console.printf("%n%s%n", StringUtils.repeat(LINE, 3));
       }
       console.printf("%n");
     }
-
-    return null;
   }
 
   private static class TransformerProperties {
 
-    private String id;
+    private ServiceReference ref;
 
-    private String schema;
-
-    private List<String> mimeTypes;
-
-    public TransformerProperties(ServiceReference ref) {
-
-      this.id = getTransformerPropertyString(ref, Constants.SERVICE_ID);
-      this.schema = getTransformerPropertyString(ref, SCHEMA);
-      this.mimeTypes = getTransformerMimeTypes(ref);
+    private TransformerProperties(ServiceReference ref) {
+      this.ref = ref;
     }
 
-    public String printProperties() {
+    private String printDefaultProperties() {
+
+      String id = getTransformerPropertyString(Constants.SERVICE_ID);
+      String schema = getTransformerPropertyString(SCHEMA);
+      List<String> mimeTypes = getTransformerMimeTypes();
 
       StringBuilder s =
           new StringBuilder(
@@ -111,21 +161,43 @@ public class TransformersCommand extends CatalogCommands {
                   "{0}: {1}\n\n\t{2}: {3}\n\t{4}s: {5}\n",
                   Constants.SERVICE_ID, id, SCHEMA, schema, MIME_TYPE, mimeTypes.remove(0)));
 
-      for (String mimeType : mimeTypes) {
-        s.append("\t\t    ").append(mimeType).append("\n");
-      }
+      s.append(printMimeTypes(mimeTypes));
+
       return s.toString();
     }
 
-    private String getTransformerPropertyString(ServiceReference ref, String property) {
+    private String printAllProperties() {
+
+      StringBuilder s = new StringBuilder();
+      String[] propertyKeys = ref.getPropertyKeys();
+      List<String> mimeTypes = getTransformerMimeTypes();
+      String id = getTransformerPropertyString(Constants.SERVICE_ID);
+      String value;
+
+      s.append(MessageFormat.format("{0}: {1}\n\n\t", Constants.SERVICE_ID, id));
+
+      for (String key : propertyKeys) {
+        if (key.equals(MIME_TYPE) || key.equals(Constants.SERVICE_ID)) {
+          continue;
+        }
+
+        value = getTransformerPropertyString(key);
+        s.append(MessageFormat.format("\n\t{0}: {1}", key, value));
+      }
+
+      s.append(MessageFormat.format("\n\t{0}s: {1}\n", MIME_TYPE, mimeTypes.remove(0)));
+      s.append(printMimeTypes(mimeTypes));
+      return s.toString();
+    }
+
+    private String getTransformerPropertyString(String property) {
 
       return Optional.ofNullable(ref.getProperty(property))
-          .map(String.class::cast)
-          .filter(String.class::isInstance)
+          .map(Object::toString)
           .orElse(NOT_AVAILABLE);
     }
 
-    private List<String> getTransformerMimeTypes(ServiceReference ref) {
+    private List<String> getTransformerMimeTypes() {
 
       List<String> mimeProperties;
 
@@ -133,14 +205,39 @@ public class TransformersCommand extends CatalogCommands {
         mimeProperties = (ArrayList<String>) ref.getProperty(MIME_TYPE);
       } else {
         mimeProperties = new ArrayList<>();
-        mimeProperties.add(getTransformerPropertyString(ref, MIME_TYPE));
+        mimeProperties.add(getTransformerPropertyString(MIME_TYPE));
       }
       return mimeProperties;
     }
+
+    private String printMimeTypes(List<String> mimeTypes) {
+      StringBuilder s = new StringBuilder();
+
+      for (String mimeType : mimeTypes) {
+        s.append("\t\t    ").append(mimeType).append("\n");
+      }
+
+      return s.toString();
+    }
   }
 
-  // Used for testing TransformersCommand
+  @VisibleForTesting
   protected void setBundleContext(BundleContext bundleContext) {
     this.bundleContext = bundleContext;
+  }
+
+  @VisibleForTesting
+  protected void setInputOption(Boolean b) {
+    inputOption = b;
+  }
+
+  @VisibleForTesting
+  protected void setMetacardOption(Boolean b) {
+    metacardOption = b;
+  }
+
+  @VisibleForTesting
+  protected void setAllOption(Boolean b) {
+    allOption = b;
   }
 }
