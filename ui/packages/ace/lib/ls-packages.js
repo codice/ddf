57824b8a494b74a24b398247b14ff26b1308b095
Chris Badahdah <a.s.badahdah@gmail.com>
2018-04-12 09:50:48 -0700

    DDF-3719 Updates ace to be more resilient
    Sometimes when switching branches, the `ui/packages` directory can
    contain empty directories which cause ace to fail. By not assuming that
    all directories in `ui/packages` are valid packages (have a
    `package.json`), we can prevent build failures.

--- ui/packages/ace/lib/ls-packages.js
+++ ui/packages/ace/lib/ls-packages.js
@@ -7,9 +7,8 @@ module.exports = (dirs) => {
   return dirs
     .filter((dir) => !dir.match('target'))
     .map((d) => path.resolve(d))
-    .map((d) => glob.sync(d))
+    .map((d) => glob.sync(d + '/package.json'))
     .reduce(flatten, [])
-    .map((d) => path.join(d, 'package.json'))
     .map((d) => {
       const json = require(d)
       json.__path = d
