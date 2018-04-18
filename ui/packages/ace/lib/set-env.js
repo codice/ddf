1ac90bf7ce36bd5b24617a08b4d439fe3442347f
Joshua Mack <joshua.mack@connexta.com>
2018-04-18 16:03:22 -0700

    DDF-3719 Reduced CI logs & improved set-env.js (#3167)

--- ui/packages/ace/lib/set-env.js
+++ ui/packages/ace/lib/set-env.js
@@ -4,6 +4,6 @@ module.exports = ({ args: [cmd, ...args], pkg }) => {
   spawn(cmd, args, {
     stdio: 'inherit',
     env: Object.assign({}, process.env, { ACE_BUILD: Date.now() })
-  })
+  }).on('exit', process.exit)
 }
 
