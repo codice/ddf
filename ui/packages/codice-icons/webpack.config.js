eb7fee91d3cf30bf64fbfe2f36e9b105c5204fa3
Alexa Bird <alexa.bird@connexta.com>
2018-04-25 14:30:25 -0700

    DDF-3785 create codice webfont for custom icons

---
+++ ui/packages/codice-icons/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    entry: [
+        './codice.font.config.js',
+        './codice.font.css'
+    ],
+    output: {
+        path: path.resolve(__dirname, 'target'),
+        publicPath: '/',
+        filename: 'codice.font.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.font\.config\.js/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'webfonts-loader'
+                ]
+            },
+            {
+                test: /\.css/,
+                    use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+};
