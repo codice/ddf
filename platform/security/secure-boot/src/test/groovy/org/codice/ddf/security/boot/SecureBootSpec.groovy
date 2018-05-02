3f65a973c10031db9140bcaa9e17e947ccef3cf0
figliold <daniel.figliola@connexta.com>
2018-05-02 16:53:09 -0700

    DDF-3798: Users should be prevented from running DDF under specific when security manager is enabled (#3191)
    DDF-3798: Users should be prevented from running DDF under specific
                      directories when security manager is enabled

---
+++ platform/security/secure-boot/src/test/groovy/org/codice/ddf/security/boot/SecureBootSpec.groovy
@@ -0,0 +1,312 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.codice.ddf.security.boot
+
+import java.nio.file.Paths
+import org.apache.karaf.system.SystemService
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+import spock.util.environment.RestoreSystemProperties
+
+import java.nio.file.Files
+
+@RestoreSystemProperties
+class SecureBootSpec  extends Specification {
+
+    @Rule
+    TemporaryFolder temporaryFolder
+
+    @Rule
+    TemporaryFolder ddfHome
+
+    @Rule
+    TemporaryFolder userHome
+
+    def 'System exits normally when security manager enabled and ddf installed inside of user home dir'() {
+        setup:
+        def exitedDueToSystemServiceFailureToExit = false
+        def userHomeSysProp = userHome.getRoot().toString()
+        def ddfHomeSysProp = userHome.newFolder("ddf").toString()
+        def systemService = Mock(SystemService)
+        def securityManager = Mock(SecurityManager)
+        System.properties.'ddf.home' = ddfHomeSysProp
+        System.properties.'user.home' = userHomeSysProp
+
+        when:
+        def secureBoot = new SecureBoot(systemService) {
+            boolean securityManagerEnabled() {
+                return securityManager;
+            }
+
+            void systemExit(Exception e) {
+                exitedDueToSystemServiceFailureToExit = true
+            }
+        }
+        secureBoot.init()
+
+        then:
+        1 * systemService.halt("0")
+        exitedDueToSystemServiceFailureToExit == false
+    }
+
+    def 'System boots when security manager enabled and ddf installed outside of user home dir'() {
+        setup:
+        def exitedDueToSystemServiceFailureToExit = false
+        def userHomeSysProp = userHome.newFolder("tomPenny").toString()
+        def ddfHomeSysProp = ddfHome.newFolder("ddf").toString()
+        def systemService = Mock(SystemService)
+        def securityManager = Mock(SecurityManager)
+        System.properties.'ddf.home' = ddfHomeSysProp
+        System.properties.'user.home' = userHomeSysProp
+
+        when:
+        def secureBoot = new SecureBoot(systemService) {
+            boolean securityManagerEnabled() {
+                return securityManager;
+            }
+
+            void systemExit(Exception e) {
+                exitedDueToSystemServiceFailureToExit = true
+            }
+        }
+        secureBoot.init()
+
+        then:
+        0 * systemService.halt("0")
+        exitedDueToSystemServiceFailureToExit == false
+    }
+
+    def 'System boots when security manager disabled and ddf installed inside of user home dir'() {
+        setup:
+        def exitedDueToSystemServiceFailureToExit = false
+        def userHomeSysProp = userHome.getRoot().toString()
+        def ddfHomeSysProp = userHome.newFolder("ddf").toString()
+        def systemService = Mock(SystemService)
+        System.properties.'ddf.home' = ddfHomeSysProp
+        System.properties.'user.home' = userHomeSysProp
+
+        when:
+        def secureBoot = new SecureBoot(systemService) {
+            boolean securityManagerEnabled() {
+                return null;
+            }
+            void systemExit(Exception e) {
+                exitedDueToSystemServiceFailureToExit = true
+            }
+        }
+        secureBoot.init()
+
+        then:
+        0 * systemService.halt("0")
+        exitedDueToSystemServiceFailureToExit == false
+    }
+
+    def 'System boots when security manager disabled and ddf installed outside of user home dir'() {
+        setup:
+        def exitedDueToSystemServiceFailureToExit = false
+        def userHomeSysProp = userHome.newFolder("tomPenny").toString()
+        def ddfHomeSysProp = ddfHome.newFolder("ddf").toString()
+        def systemService = Mock(SystemService)
+        System.properties.'ddf.home' = ddfHomeSysProp
+        System.properties.'user.home' = userHomeSysProp
+
+        when:
+        def secureBoot = new SecureBoot(systemService) {
+            boolean securityManagerEnabled() {
+                return null;
+            }
+            void systemExit(Exception e) {
+                exitedDueToSystemServiceFailureToExit = true
+            }
+        }
+        secureBoot.init()
+
+        then:
+        0 * systemService.halt("0")
+        exitedDueToSystemServiceFailureToExit == false
+    }
+
+    def 'SystemService throws exception when asked to halt'() {
+        setup:
+        def exitedDueToSystemServiceFailureToExit = false
+        def userHomeSysProp = userHome.getRoot().toString()
+        def ddfHomeSysProp = userHome.newFolder("ddf").toString()
+        def systemService = Mock(SystemService)
+        def securityManager = Mock(SecurityManager)
+        System.properties.'ddf.home' = ddfHomeSysProp
+        System.properties.'user.home' = userHomeSysProp
+
+        when:
+        def secureBoot = new SecureBoot(systemService) {
+            boolean securityManagerEnabled() {
+                return securityManager;
+            }
+
+            void systemExit(Exception e) {
+                exitedDueToSystemServiceFailureToExit = true
+            }
+        }
+        secureBoot.init()
+
+        then:
+        1 * systemService.halt("0") >> { throw new Exception("unable to halt")}
+        exitedDueToSystemServiceFailureToExit == true
+    }
+
+    def 'System exits normally when attempting to read invalid ddf.home'() {
+        setup:
+        def exitedDueToSystemServiceFailureToExit = false
+        def userHomeSysProp = userHome.getRoot().toString()
+        def ddfHomeSysProp = "/" + System.currentTimeMillis() + "/" + UUID.randomUUID() + "/ddf"
+        def systemService = Mock(SystemService)
+        def securityManager = Mock(SecurityManager)
+        System.properties.'ddf.home' = ddfHomeSysProp
+        System.properties.'user.home' = userHomeSysProp
+
+        when:
+        def secureBoot = new SecureBoot(systemService) {
+            boolean securityManagerEnabled() {
+                return securityManager;
+            }
+
+            void systemExit(Exception e) {
+                exitedDueToSystemServiceFailureToExit = true
+            }
+        }
+        secureBoot.init()
+
+        then:
+        1 * systemService.halt("0")
+        exitedDueToSystemServiceFailureToExit == false
+    }
+
+    def 'System exits normally when attempting to read invalid user.home'() {
+        setup:
+        def exitedDueToSystemServiceFailureToExit = false
+        def userHomeSysProp = "/" + System.currentTimeMillis() + "/" + UUID.randomUUID() + "/tomPenny"
+        def ddfHomeSysProp = ddfHome.newFolder("ddf").toString()
+        def systemService = Mock(SystemService)
+        def securityManager = Mock(SecurityManager)
+        System.properties.'ddf.home' = ddfHomeSysProp
+        System.properties.'user.home' = userHomeSysProp
+
+        when:
+        def secureBoot = new SecureBoot(systemService) {
+            boolean securityManagerEnabled() {
+                return securityManager;
+            }
+
+            void systemExit(Exception e) {
+                exitedDueToSystemServiceFailureToExit = true
+            }
+        }
+        secureBoot.init()
+
+        then:
+        1 * systemService.halt("0")
+        exitedDueToSystemServiceFailureToExit == false
+    }
+
+    def 'System exits normally when attempting to read invalid ddf.home and invalid user.home'() {
+        setup:
+        def exitedDueToSystemServiceFailureToExit = false
+        def userHomeSysProp = "/" + System.currentTimeMillis() + "/" + UUID.randomUUID() + "/tomPenny"
+        def ddfHomeSysProp = "/" + System.currentTimeMillis() + "/" + UUID.randomUUID() + "/ddf"
+        def systemService = Mock(SystemService)
+        def securityManager = Mock(SecurityManager)
+        System.properties.'ddf.home' = ddfHomeSysProp
+        System.properties.'user.home' = userHomeSysProp
+
+        when:
+        def secureBoot = new SecureBoot(systemService) {
+            boolean securityManagerEnabled() {
+                return securityManager;
+            }
+
+            void systemExit(Exception e) {
+                exitedDueToSystemServiceFailureToExit = true
+            }
+        }
+        secureBoot.init()
+
+        then:
+        1 * systemService.halt("0")
+        exitedDueToSystemServiceFailureToExit == false
+    }
+
+    /**
+     * ie. /opt/ddfHomeSymLink -> /users/tomPenny/ddf
+     */
+    def 'System exits normally when security manager enabled and symbolic link outside of user home dir pointing to ddf installation inside of user home dir'() {
+        setup:
+        def exitedDueToSystemServiceFailureToExit = false
+        def optDir = temporaryFolder.newFolder("opt").toPath()
+        def tomPennyHomeDir = temporaryFolder.newFolder("tomPenny")
+        def ddfHomeDir = Files.createDirectory(Paths.get(tomPennyHomeDir.toString(), "ddf"))
+        def ddfHomeSymLink = Files.createSymbolicLink(Paths.get(optDir.toString(), "ddfHomeSymLink"), ddfHomeDir)
+        def systemService = Mock(SystemService)
+        def securityManager = Mock(SecurityManager)
+        System.properties.'ddf.home' = ddfHomeSymLink.toString()
+        System.properties.'user.home' = tomPennyHomeDir.toString()
+
+        when:
+        def secureBoot = new SecureBoot(systemService) {
+            boolean securityManagerEnabled() {
+                return securityManager;
+            }
+
+            void systemExit(Exception e) {
+                exitedDueToSystemServiceFailureToExit = true
+            }
+        }
+        secureBoot.init()
+
+        then:
+        1 * systemService.halt("0")
+        exitedDueToSystemServiceFailureToExit == false
+    }
+
+    /**
+     * ie. /users/tomPenny/ddfHomeSymLink -> /opt/ddf
+     */
+    def 'System boots when security manager enabled and symbolic link inside of user home dir pointing to ddf installation outside of user home dir'() {
+        setup:
+        def exitedDueToSystemServiceFailureToExit = false
+        def optDir = temporaryFolder.newFolder("opt").toPath()
+        def ddfHomeDir = Files.createDirectory(Paths.get(optDir.toString(), "ddf"))
+        def tomPennyHomeDir = temporaryFolder.newFolder("tomPenny")
+        def ddfHomeSymLink = Files.createSymbolicLink(Paths.get(tomPennyHomeDir.toString(), "ddfHomeSymLink"), ddfHomeDir)
+        def systemService = Mock(SystemService)
+        def securityManager = Mock(SecurityManager)
+        System.properties.'ddf.home' = ddfHomeSymLink.toString()
+        System.properties.'user.home' = tomPennyHomeDir.toString()
+
+        when:
+        def secureBoot = new SecureBoot(systemService) {
+            boolean securityManagerEnabled() {
+                return securityManager;
+            }
+
+            void systemExit(Exception e) {
+                exitedDueToSystemServiceFailureToExit = true
+            }
+        }
+        secureBoot.init()
+
+        then:
+        0 * systemService.halt("0")
+        exitedDueToSystemServiceFailureToExit == false
+    }
+}
