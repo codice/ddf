1c94fa89ba795db0362cd29df77e688dafce2651
Lombardi <steve.lombardi@connexta.com>
2018-05-09 14:38:50 -0700

    DDF-3672 Fix issue where templated JSON nodes are not being visited correctly

--- catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/forms/filter/VisitableJsonElementImpl.java
+++ catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/forms/filter/VisitableJsonElementImpl.java
@@ -47,11 +47,17 @@ import org.codice.ddf.catalog.ui.forms.model.FilterNode;
  * @see PropertyIsBetweenType
  */
 public class VisitableJsonElementImpl implements VisitableElement<Object> {
+  private static final String LIKE = "LIKE";
+
+  private static final String FAKE_PROPERTY_OPERATOR = "PROPERTY";
+
+  private static final String FAKE_VALUE_OPERATOR = "VALUE";
+
   private static final Map<String, BiConsumer<FilterVisitor2, VisitableElement>> VISIT_METHODS =
       ImmutableMap.<String, BiConsumer<FilterVisitor2, VisitableElement>>builder()
           // Fake operators to give a flat structure an XML-like "embedded" structure
-          .put("PROPERTY", FilterVisitor2::visitString)
-          .put("VALUE", FilterVisitor2::visitLiteralType)
+          .put(FAKE_PROPERTY_OPERATOR, FilterVisitor2::visitString)
+          .put(FAKE_VALUE_OPERATOR, FilterVisitor2::visitLiteralType)
           // Logical operator mapping
           .put("AND", FilterVisitor2::visitBinaryLogicType)
           .put("OR", FilterVisitor2::visitBinaryLogicType)
@@ -72,8 +78,6 @@ public class VisitableJsonElementImpl implements VisitableElement<Object> {
           .put("ILIKE", FilterVisitor2::visitPropertyIsLikeType)
           .build();
 
-  private static final String LIKE = "LIKE";
-
   private final BiConsumer<FilterVisitor2, VisitableElement> visitMethod;
   private final String name;
   private final Object value;
@@ -86,21 +90,23 @@ public class VisitableJsonElementImpl implements VisitableElement<Object> {
     if (LIKE.equals(node.getOperator())) {
       throw new UnsupportedOperationException("LIKE (case sensitive) currently is not supported");
     }
-    this.name = node.getOperator();
-    if (node.isTemplated()) {
-      this.value = node.getTemplateProperties();
-      this.visitMethod = FilterVisitor2::visitFunctionType;
-      return;
-    }
-    if (node.isLeaf()) {
-      this.value = wrap(node.getProperty(), node.getValue());
-    } else {
-      this.value = wrap(node.getChildren());
-    }
+
     this.visitMethod = VISIT_METHODS.get(node.getOperator());
     if (this.visitMethod == null) {
       throw new FilterProcessingException(
           "Encountered an unexpected or unsupported type: " + node.getOperator());
+    }
+
+    this.name = node.getOperator();
+    if (node.isTemplated()) {
+      this.value = wrap(node.getProperty(), node.getTemplateProperties());
+      return;
+    }
+
+    if (node.isLeaf()) {
+      this.value = wrap(node.getProperty(), node.getValue());
+    } else {
+      this.value = wrap(node.getChildren());
     }
   }
 
@@ -114,10 +120,27 @@ public class VisitableJsonElementImpl implements VisitableElement<Object> {
     }
   }
 
+  private VisitableJsonElementImpl(
+      final String operator,
+      final Object value,
+      final BiConsumer<FilterVisitor2, VisitableElement> visitMethod) {
+    this.name = operator;
+    this.value = value;
+    this.visitMethod = visitMethod;
+  }
+
   private static List<VisitableElement<?>> wrap(String property, String value) {
     return Arrays.asList(
-        new VisitableJsonElementImpl("PROPERTY", property),
-        new VisitableJsonElementImpl("VALUE", Collections.singletonList(value)));
+        new VisitableJsonElementImpl(FAKE_PROPERTY_OPERATOR, property),
+        new VisitableJsonElementImpl(FAKE_VALUE_OPERATOR, Collections.singletonList(value)));
+  }
+
+  private static List<VisitableElement<?>> wrap(
+      String property, Map<String, Object> templateProperties) {
+    return Arrays.asList(
+        new VisitableJsonElementImpl(FAKE_PROPERTY_OPERATOR, property),
+        new VisitableJsonElementImpl(
+            FAKE_VALUE_OPERATOR, templateProperties, FilterVisitor2::visitFunctionType));
   }
 
   private static List<VisitableElement<?>> wrap(List<FilterNode> children) {
