5efb82ee7d6acd78ba489b8a4c7442403fa57751
gordocanchola <gordo.canchola@connexta.com>
2018-05-15 09:13:56 -0700

    DDF-3808 Appending queryId to the action provided urls (#3233)
    * DDF-3808 Appending queryId to the action provided urls
    Removing the call to decode the url
    Changing it to do a string replace 'decoding' of the {&queryId} template
    
    * DDF-3808 Add queryId to metacard action urls
      Using URITemplate as a constructor to make codacy happy
      Removing hard coded encoded queryId template in favor of actually encoding the queryId template

--- ui/packages/catalog-ui-search/src/main/webapp/js/model/MetacardAction.js
+++ ui/packages/catalog-ui-search/src/main/webapp/js/model/MetacardAction.js
@@ -12,6 +12,8 @@
 var Backbone = require('backbone');
 require('backbone-associations');
 const URITemplate = require('urijs/src/URITemplate');
+const DECODED_QUERY_ID_TEMPLATE = '{&queryId}';
+const ENCODED_QUERY_ID_TEMPLATE = encodeURIComponent(DECODED_QUERY_ID_TEMPLATE);
 
 module.exports = Backbone.AssociatedModel.extend({
     defaults: function() {
@@ -32,10 +34,21 @@ module.exports = Backbone.AssociatedModel.extend({
     },
     handleQueryId: function() {
         if (this.get('queryId') !== undefined) {
-            const decodedUrl = decodeURIComponent(this.get('url'));
-            const expandedUrl = URITemplate(decodedUrl).expand({
+            // This is the story:
+            // An action provider can include {&queryId} as a template in the url if it needs the queryId
+            // The backend is encoding {&queryId} because it has to.
+            // The entire url was being decoded and that caused issues because it decoded things that were supposed to be remain encoded
+            // The entire url couldn't be encoded because it was returning a useless url
+            // An attempt was made at decoding and encoding the individual parts of both the path and query params
+            // This caused an issue because it was encoding the transform ids, some of which include a ':'
+            // So that's why the string replace "decoding" is currently being done
+            const url = this.get('url');
+            const replacedUrl = url.replace(ENCODED_QUERY_ID_TEMPLATE, DECODED_QUERY_ID_TEMPLATE);
+            const replacedUrlTemplate = new URITemplate(replacedUrl);
+            const expandedUrl = replacedUrlTemplate.expand({
                 queryId: this.get('queryId')
             });
+
             this.set('url', expandedUrl);
         }
     }
