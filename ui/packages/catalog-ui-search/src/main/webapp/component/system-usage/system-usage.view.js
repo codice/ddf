89043597f43523b6c8b235c52a45cc772483d11c
Jeffrey Cheng <jcheng@actionet.com>
2018-06-11 17:49:30 -0400

    DDF-3885 System Usage Message does not populate on Intrigue

--- ui/packages/catalog-ui-search/src/main/webapp/component/system-usage/system-usage.view.js
+++ ui/packages/catalog-ui-search/src/main/webapp/component/system-usage/system-usage.view.js
@@ -23,7 +23,7 @@ var $ = require('jquery');
 
 function getSrc() {
     return '<html class="is-iframe" style="font-size: '+preferences.get('fontSize')+'px">' +
-    '<link href="css/styles.' + document.querySelector('link[href*="css/styles."]').href.split('css/styles.')[1] + '" rel="stylesheet">' +
+    '<link href="styles.' + document.querySelector('link[href*="styles."]').href.split('styles.')[1] + '" rel="stylesheet">' +
     properties.ui.systemUsageMessage +
     '</html>';
 }
