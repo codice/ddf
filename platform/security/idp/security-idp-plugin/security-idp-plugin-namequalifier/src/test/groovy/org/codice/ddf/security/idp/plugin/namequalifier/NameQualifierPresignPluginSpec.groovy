1cf074dd8b8ec9b13bdce17bec49df6e0062c4b9
Philipp Bantillo <philipp.bantillo@wyle.com>
2018-06-15 15:33:08 -0400

    DDF-3903 The IdP should add its entity ID in the NameQualifier field of all Persistent Identifiers

---
+++ platform/security/idp/security-idp-plugin/security-idp-plugin-namequalifier/src/test/groovy/org/codice/ddf/security/idp/plugin/namequalifier/NameQualifierPresignPluginSpec.groovy
@@ -0,0 +1,182 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.codice.ddf.security.idp.plugin.namequalifier
+
+import ddf.security.samlp.SamlProtocol
+import org.apache.wss4j.common.saml.OpenSAMLUtil
+import org.opensaml.saml.saml2.core.Assertion
+import org.opensaml.saml.saml2.core.AuthnRequest
+import org.opensaml.saml.saml2.core.Issuer
+import org.opensaml.saml.saml2.core.Response
+import org.opensaml.saml.saml2.core.Subject
+import org.opensaml.saml.saml2.core.NameID
+import spock.lang.Specification
+import org.apache.wss4j.common.saml.builder.SAML2Constants
+import org.opensaml.saml.saml2.core.NameIDPolicy
+
+class NameQualifierPresignPluginSpec extends Specification {
+
+    NameQualifierPresignPlugin plugin = new NameQualifierPresignPlugin()
+
+    // general mocks
+    AuthnRequest authNReq = Mock(AuthnRequest)
+    Issuer issuer = Mock(Issuer)
+
+    List<String> spMetadata = new ArrayList<>()
+    Set<SamlProtocol.Binding> bindings = new HashSet<>()
+
+    // set of mocks and concrete objects
+    Response response = Mock(Response)
+
+    List<Assertion> assertions = new ArrayList<>()
+    Assertion assertion = Mock(Assertion)
+
+    Subject subject = Mock(Subject)
+    NameID nameID = Mock(NameID)
+
+    NameIDPolicy nameIdPolicy = Mock(NameIDPolicy)
+
+    String format = new String(SAML2Constants.NAMEID_FORMAT_PERSISTENT)
+
+    def NAME_QUALIFIER = 'https://localhost:8993/services/idp/login'
+
+    def PERSISTENT = 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent'
+
+    def NEGATIVE_TEST = 'negative test'
+
+    def setupSpec() {
+        OpenSAMLUtil.initSamlEngine()
+    }
+
+    def setup() {
+
+        // set up authNReq
+        authNReq.getIssuer() >> issuer
+
+        // set up issuer
+        response.getIssuer() >> issuer
+
+        // set up subject
+        assertion.getIssuer() >> issuer
+
+        // set up basic response
+        response.assertions >> assertions
+        //assertions.add(assertion)
+
+        // set up the name id policy
+        authNReq.getNameIDPolicy() >> nameIdPolicy;
+
+    }
+
+     def 'test name qualifier when issuer format is not null and is persistent'() {
+
+        setup:
+        response.getIssuer().getFormat() >> format
+        assertions.add(assertion)
+        assertion.getSubject() >> subject
+        assertion.getSubject().getNameID() >> nameID
+        nameID.getFormat() >> PERSISTENT
+
+        when:
+        plugin.processPresign(response, authNReq, spMetadata, bindings)
+
+        then:
+        1 * nameID.getFormat()
+
+    }
+
+    def 'test name qualifier when no assertions'() {
+
+        setup:
+        response.getIssuer().getFormat() >> format
+        assertion.getSubject() >> subject
+        assertion.getSubject().getNameID() >> nameID
+        nameID.getFormat() >> PERSISTENT
+
+        when:
+        plugin.processPresign(response, authNReq, spMetadata, bindings)
+
+        then:
+        0 * nameID.setNameQualifier(NAME_QUALIFIER)
+
+    }
+
+
+    def 'test name qualifier when issuer format is null'() {
+
+        setup:
+        response.getIssuer().getFormat() >> null
+        assertions.add(assertion)
+
+        when:
+        plugin.processPresign(response, authNReq, spMetadata, bindings)
+
+        then:
+        0 * nameID.getFormat()
+
+    }
+
+    def 'test name qualifier when issuer format is not persistent'() {
+
+        setup:
+        response.getIssuer().getFormat() >>  NEGATIVE_TEST
+        assertions.add(assertion)
+
+        when:
+        plugin.processPresign(response, authNReq, spMetadata, bindings)
+
+        then:
+        0 * nameID.getFormat()
+
+    }
+
+    def 'test name qualifier when assertion subject is null'() {
+
+        setup:
+        response.getIssuer().getFormat() >> format
+        assertions.add(assertion)
+        assertion.getSubject() >> null
+
+        when:
+        plugin.processPresign(response, authNReq, spMetadata, bindings)
+
+        then:
+        0 * nameID.getFormat()
+
+    }
+
+    def 'test name qualifier when assertion subject is not null and name id is persistent'() {
+
+        setup:
+        response.getIssuer().getFormat() >> format
+        assertions.add(assertion)
+        assertion.getSubject() >> subject
+        assertion.getSubject().getNameID() >> nameID
+
+        when:
+        plugin.processPresign(response, authNReq, spMetadata, bindings)
+
+        then:
+        1 * nameID.getFormat()
+
+
+    }
+
+
+
+
+
+
+
+}
