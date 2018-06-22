18295ce41336f97c59319a66d7ae5f056e5d3483
Andrew Fiedler <andrewkfiedler@gmail.com>
2018-06-22 11:30:00 -0700

    DDF-3838 Updates router loader to add dynamic path resolution
     - This allows downstream distributions to utilize a mix of upstream
     components and downstream components.

--- ui/packages/catalog-ui-search/src/main/webapp/js/router/routes-loader.js
+++ ui/packages/catalog-ui-search/src/main/webapp/js/router/routes-loader.js
@@ -1,4 +1,21 @@
+const loaderUtils = require('loader-utils')
+const fs = require('fs')
+const path = require('path')
+
 module.exports = function (source, map) {
+    const rootContext = path.join(this.rootContext, '/src/main/webapp/');
+    const options = {
+        prefix: 'ddf/',
+        resolve(filePath) {
+            const absolutePathToFile = path.join(rootContext, `${filePath}.js`);
+            if (fs.existsSync(absolutePathToFile)) {
+                return filePath;
+            } else {
+                return `${this.prefix}${filePath}`;
+            }
+        },
+        ...loaderUtils.getOptions(this)
+    };
     this.cacheable()
     const routeDefinitions = eval(`${source}`);
     const routesObjectString = Object.keys(routeDefinitions).map((routeName) => {
@@ -13,7 +30,7 @@ module.exports = function (source, map) {
             getComponent: function() {
                 var deferred = new $.Deferred();
                 require([
-                    ${JSON.stringify(component)}
+                    ${JSON.stringify(options.resolve(component))}
                 ], onComponentResolution.bind(this, deferred, {}));
                 return deferred;
             },
@@ -21,7 +38,7 @@ module.exports = function (source, map) {
                 getComponent: function() {
                     var deferred = new $.Deferred();
                     require([
-                        ${JSON.stringify(menu.component ? menu.component : 'component/navigation-middle/navigation-middle.view')}
+                        ${JSON.stringify(options.resolve(menu.component ? menu.component : 'component/navigation-middle/navigation-middle.view'))}
                     ], onComponentResolution.bind(this, deferred, ${JSON.stringify(menu.component ? {} : menu)}));
                     return deferred;
                 }
