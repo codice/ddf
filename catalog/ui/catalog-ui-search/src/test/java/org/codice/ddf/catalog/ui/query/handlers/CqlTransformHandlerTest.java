66698d04653c7558a33da5362e1feee02ec00aa5
Corey-Collins <coreycwcollins@gmail.com>
2018-08-13 16:32:10 -0600

    DDF-3997 Annotated mock objects

--- catalog/ui/catalog-ui-search/src/test/java/org/codice/ddf/catalog/ui/query/handlers/CqlTransformHandlerTest.java
+++ catalog/ui/catalog-ui-search/src/test/java/org/codice/ddf/catalog/ui/query/handlers/CqlTransformHandlerTest.java
@@ -16,7 +16,6 @@ package org.codice.ddf.catalog.ui.query.handlers;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -38,6 +37,7 @@ import org.codice.ddf.catalog.ui.query.cql.CqlRequest;
 import org.codice.ddf.catalog.ui.util.EndpointUtil;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import spark.Request;
@@ -45,19 +45,20 @@ import spark.Response;
 
 public class CqlTransformHandlerTest {
 
-  private BundleContext mockBundleContext;
   private List<ServiceReference> queryResponseTransformers;
   private CqlTransformHandler cqlTransformHandler;
-  private ServiceReference<QueryResponseTransformer> mockQueryResponseTransformer;
   private BinaryContent binaryContent;
-  private EndpointUtil mockEndpointUtil;
-  private Request mockRequest;
-  private Response mockResponse;
-  private CqlQueryResponse mockCqlQueryResponse;
-  private QueryResponse mockQueryResponse;
-  private QueryResponseTransformer mockServiceReference;
-  private ServletOutputStream mockServletOutputStream;
-  private HttpServletResponse mockHttpServiceResponse;
+
+  @Mock private ServiceReference<QueryResponseTransformer> mockQueryResponseTransformer;
+  @Mock private BundleContext mockBundleContext;
+  @Mock private EndpointUtil mockEndpointUtil;
+  @Mock private Request mockRequest;
+  @Mock private Response mockResponse;
+  @Mock private CqlQueryResponse mockCqlQueryResponse;
+  @Mock private QueryResponse mockQueryResponse;
+  @Mock private QueryResponseTransformer mockServiceReference;
+  @Mock private ServletOutputStream mockServletOutputStream;
+  @Mock private HttpServletResponse mockHttpServiceResponse;
 
   private static final String GZIP = "gzip";
   private static final String NO_GZIP = "";
@@ -74,17 +75,6 @@ public class CqlTransformHandlerTest {
   @Before
   public void setUp() throws Exception {
     initMocks(this);
-
-    mockBundleContext = mock(BundleContext.class);
-    mockEndpointUtil = mock(EndpointUtil.class);
-    mockRequest = mock(Request.class);
-    mockResponse = mock(Response.class);
-    mockCqlQueryResponse = mock(CqlQueryResponse.class);
-    mockQueryResponse = mock(QueryResponse.class);
-    mockServiceReference = mock(QueryResponseTransformer.class);
-    mockQueryResponseTransformer = mock(ServiceReference.class);
-    mockServletOutputStream = mock(ServletOutputStream.class);
-    mockHttpServiceResponse = mock(HttpServletResponse.class);
 
     queryResponseTransformers = new ArrayList<>();
 
