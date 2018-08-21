ab008c65db960dbedcfcf6bf88afc6737714fdb4
Aaron Hoffer <aaron.hoffer@connexta.com>
2018-08-21 13:29:17 -0700

    DDF-2824 Solr documentation and service update     fixed typo in ddf.bat     shuffled adoc files to better places

--- distribution/ddf-common/src/main/resources/bin/ddf.bat
+++ distribution/ddf-common/src/main/resources/bin/ddf.bat
@@ -7,42 +7,37 @@ PUSHD %DIRNAME%..
 SET DDF_HOME=%CD%
 POPD
 
-IF "%COMMAND%"=="" (
-  ECHO Missing command. Use start, restart, stop
+SET GET_PROPERTY=%DIRNAME%get_property.bat
+SET SOLR_EXEC=%DDF_HOME%\bin\ddfsolr.bat
 
-) ELSE (
+:RESTART
+REM Remove the restart file indicator so we can detect later if restart was requested
+IF EXIST "%DIRNAME%restart.jvm" DEL "%DIRNAME%restart.jvm"
 
-    SET GET_PROPERTY=%DIRNAME%get_property.bat
-    SET SOLR_EXEC=%DDF_HOME%\bin\ddfsolr.bat
+REM Get Solr start property
+CALL %GET_PROPERTY% start.solr
 
-    :RESTART
-    REM Remove the restart file indicator so we can detect later if restart was requested
-    IF EXIST "%DIRNAME%restart.jvm" DEL "%DIRNAME%restart.jvm"
+REM Get Karaf start property
+CALL %GET_PROPERTY% start.ddf
 
-    REM Get Solr start property
-    CALL %GET_PROPERTY% start.solr
-
-    REM Get Karaf start property
-    CALL %GET_PROPERTY% start.ddf
-
-    REM Start Solr if needed
-    IF "%start.solr%" == "true" (
-        CALL %SOLR_EXEC% restart
-    )
-
-    REM Actually invoke ddf to gain restart support
-    IF "%start.ddf%" == "true" CALL "%DIRNAME%karaf.bat" %ARGS%
-
-    REM Check if restart was requested by ddf_on_error.bat
-    IF EXIST "%DIRNAME%restart.jvm" (
-        ECHO Restarting JVM...
-        CALL :STOP_SOLR
-        GOTO :RESTART
-    ) ELSE (
-        CALL :STOP_SOLR
-        EXIT /B %RC%
-    )
+REM Start Solr if needed
+IF "%start.solr%" == "true" (
+    CALL %SOLR_EXEC% restart
 )
+
+REM Actually invoke ddf to gain restart support
+IF "%start.ddf%" == "true" CALL "%DIRNAME%karaf.bat" %ARGS%
+
+REM Check if restart was requested by ddf_on_error.bat
+IF EXIST "%DIRNAME%restart.jvm" (
+    ECHO Restarting JVM...
+    CALL :STOP_SOLR
+    GOTO :RESTART
+) ELSE (
+    CALL :STOP_SOLR
+    EXIT /B %RC%
+)
+
 EXIT /B
 
 :STOP_SOLR
