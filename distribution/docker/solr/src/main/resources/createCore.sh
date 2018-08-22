0a69400d8222fe25ff155787bb001a8fce5df85c
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2018-08-22 11:59:30 -0400

    DDF-4331 Add ddf and solr docker images

---
+++ distribution/docker/solr/src/main/resources/createCore.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SRC=${CONFIG_SRC:=configs}
+DEST=${CORE_DEST:=cores/}
+
+if [ $# -eq 0 ]; then
+  printf "\nNo core arguent given\n"
+  exit 1
+fi
+
+for core in "$@"
+do
+  printf "\nCreating core ${core}...\n\tUsing configs from ${SRC}\n\tStoring in ${DEST}\n"
+
+  mkdir -p ${DEST}/${core}/conf
+  cp -r ${SRC}/* ${DEST}/${core}/conf/
+cat > ${DEST}/${core}/core.properties << EOF
+name=${core}
+config=solrconfig.xml
+schema=schema.xml
+dataDir=data
+ulogDir=data
+EOF
+
+done
