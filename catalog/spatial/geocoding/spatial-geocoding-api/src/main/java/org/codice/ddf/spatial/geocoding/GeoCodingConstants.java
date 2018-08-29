bfee401f737c35e05d15564448c5ee538a5c175f
Gabriel Vera <36056781+gjvera@users.noreply.github.com>
2018-08-29 07:23:53 -0700

    DDF-4038 Fix Sorting of Gazetteer Suggestions  (#3604)
    * DDF-4038 Improving Gazetteer Suggestions
    
    * DDF-4038 fix typo
    
    * DDF-4038 fixing formatting
    
    * DDF-4038 Build suggester on startup
    
    * DDF-4038 Using feature codes for sorting
    
    * DDF-4038 fixing coverage checks
    
    * DDF-4038 fixing NPE

--- catalog/spatial/geocoding/spatial-geocoding-api/src/main/java/org/codice/ddf/spatial/geocoding/GeoCodingConstants.java
+++ catalog/spatial/geocoding/spatial-geocoding-api/src/main/java/org/codice/ddf/spatial/geocoding/GeoCodingConstants.java
@@ -14,7 +14,9 @@
 package org.codice.ddf.spatial.geocoding;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import java.util.List;
+import java.util.Map;
 
 public final class GeoCodingConstants {
   public static final String ADMINISTRATIVE_DIVISION = "ADM";
@@ -59,12 +61,57 @@ public final class GeoCodingConstants {
 
   public static final String SUGGEST_PLACE_KEY = "suggestPlace";
 
+  public static final int COUNTRY_GAZETTEER_SORT_VALUE = Integer.MAX_VALUE;
+
+  public static final int MAXIMUM_GAZETTEER_SORT_VALUE = Integer.MAX_VALUE - 1;
+
+  public static final int SPECIAL_GAZETTEER_SORT_VALUE = 8;
+
+  public static final String ADMIN_FEATURE_CLASS = "A";
+
+  public static final String HYDROGRAPHIC_FEATURE_CLASS = "H";
+
+  public static final String AREA_FEATURE_CLASS = "L";
+
+  public static final String POPULATED_FEATURE_CLASS = "P";
+
+  public static final String ROAD_FEATURE_CLASS = "R";
+
+  public static final String SPOT_FEATURE_CLASS = "S";
+
+  public static final String MOUNTAIN_FEATURE_CLASS = "T";
+
+  public static final String UNDERSEA_FEATURE_CLASS = "U";
+
+  public static final String VEGETATION_FEATURE_CLASS = "V";
+
+  public static final String OCEAN_FEATURE_CODE = "OCN";
+
+  public static final String SEA_FEATURE_CODE = "SEA";
+
+  public static final String MOUNTAIN_FEATURE_CODE = "MT";
+
+  public static final String MOUNTAIN_RANGE_FEATURE_CODE = "MTS";
+
   // The GeoNames feature codes for cities, excluding cities that no longer exist or that have
   // been destroyed.
   public static final List<String> CITY_FEATURE_CODES =
       ImmutableList.of(
           "PPL", "PPLA", "PPLA2", "PPLA3", "PPLA4", "PPLC", "PPLCH", "PPLF", "PPLG", "PPLL", "PPLR",
           "PPLS", "PPLX");
+
+  public static final Map<String, Integer> FEATURE_CLASS_VALUES =
+      new ImmutableMap.Builder<String, Integer>()
+          .put(ADMIN_FEATURE_CLASS, MAXIMUM_GAZETTEER_SORT_VALUE)
+          .put(POPULATED_FEATURE_CLASS, 8)
+          .put(HYDROGRAPHIC_FEATURE_CLASS, 7)
+          .put(AREA_FEATURE_CLASS, 6)
+          .put(ROAD_FEATURE_CLASS, 5)
+          .put(SPOT_FEATURE_CLASS, 4)
+          .put(MOUNTAIN_FEATURE_CLASS, 3)
+          .put(UNDERSEA_FEATURE_CLASS, 2)
+          .put(VEGETATION_FEATURE_CLASS, 1)
+          .build();
 
   public static final List<String> COUNTRY_FEATURE_CODES =
       ImmutableList.of(
