9a7697066006e8cad2afa93ce57e73ff85061357
William <william.miller@connexta.com>
2018-09-05 13:49:43 -0400

    DDF-4250: Cleaned up date parsing in catalog UI utils, consistently returning an Instant when understood, and a null otherwise.  Extended the number of formats understood (iso8601 with offset or z, instant, milliseconds since epoch, java date and instant objects, and preexisting date patterns)

--- catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/metacard/workspace/transformer/impl/WorkspaceTransformerImpl.java
+++ catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/metacard/workspace/transformer/impl/WorkspaceTransformerImpl.java
@@ -128,12 +128,18 @@ public class WorkspaceTransformerImpl implements WorkspaceTransformer {
         entry.getKey(),
         value);
 
-    if (value instanceof Serializable) {
-      metacard.setAttribute(new AttributeImpl(entry.getKey(), (Serializable) value));
-    } else if (value instanceof List) {
-      metacard.setAttribute(new AttributeImpl(entry.getKey(), (List<Serializable>) value));
-    } else {
-      LOGGER.debug("The value was expected to be Serializable or a list of Serializable items.");
+    try {
+      if (value instanceof Serializable) {
+        metacard.setAttribute(new AttributeImpl(entry.getKey(), (Serializable) value));
+      } else if (value instanceof List) {
+        metacard.setAttribute(new AttributeImpl(entry.getKey(), (List<Serializable>) value));
+      } else {
+        LOGGER.debug(
+            "The {} was expected to be Serializable or a list of Serializable items.", value);
+      }
+    } catch (ClassCastException ex) {
+      LOGGER.debug(
+          "The value was expected to be Serializable or a list of Serializable items.", ex);
     }
   }
 
