2b69a62b8e80e0d0f88bc7042711fbea2f4e0379
Aaron Hoffer <aaron.hoffer@connexta.com>
2018-09-13 18:14:40 -0700

    Revert: DDF-4133 Improve communication with Solr

--- platform/solr/solr-factory/src/main/java/org/codice/solr/factory/impl/SolrClientProxy.java
+++ platform/solr/solr-factory/src/main/java/org/codice/solr/factory/impl/SolrClientProxy.java
@@ -465,7 +465,6 @@ public abstract class SolrClientProxy extends SolrClient {
   @Override
   public NamedList<Object> request(SolrRequest request, String collection)
       throws SolrServerException, IOException {
-    request.setBasicAuthCredentials("ddf", "changeit");
     return handle(c -> c.request(request, collection));
   }
 }
