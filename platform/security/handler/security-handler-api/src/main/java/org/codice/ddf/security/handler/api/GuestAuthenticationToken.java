21bb7637dba6c87d8b994ac25525350e074b7754
R.A. Porter <coyotesqrl@gmail.com>
2018-09-19 12:02:28 -0700

    DDF-4144 Changes name format of Guest Principal so it no longer includes IP Address (#3744) (#3757)
    - Adds a security audit indicating the mapping from the incoming IP Address to the generated internal ID
    - Caches the internal IDs for 30 minutes so they can be reused for Guest access from the same IP

--- platform/security/handler/security-handler-api/src/main/java/org/codice/ddf/security/handler/api/GuestAuthenticationToken.java
+++ platform/security/handler/security-handler-api/src/main/java/org/codice/ddf/security/handler/api/GuestAuthenticationToken.java
@@ -13,12 +13,21 @@
  */
 package org.codice.ddf.security.handler.api;
 
+import static ddf.security.principal.GuestPrincipal.NAME_DELIMITER;
+
 import ddf.security.common.audit.SecurityLogger;
 import ddf.security.principal.GuestPrincipal;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import javax.annotation.Nullable;
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Authentication token representing a guest user's credentials */
 public class GuestAuthenticationToken extends BSTAuthenticationToken {
+  private static final Logger LOGGER = LoggerFactory.getLogger(GuestAuthenticationToken.class);
 
   public static final String GUEST_CREDENTIALS = "Guest";
 
@@ -27,33 +36,52 @@ public class GuestAuthenticationToken extends BSTAuthenticationToken {
   public static final String GUEST_TOKEN_VALUE_TYPE =
       BSTAuthenticationToken.BST_NS + BSTAuthenticationToken.TOKEN_VALUE_SEPARATOR + BST_GUEST_LN;
 
-  public GuestAuthenticationToken(String realm, String ip) {
-    super(new GuestPrincipal(ip), GUEST_CREDENTIALS, realm);
+  public GuestAuthenticationToken(String realm, String name) {
+    super(new GuestPrincipal(name), GUEST_CREDENTIALS, realm);
     setTokenValueType(BSTAuthenticationToken.BST_NS, BST_GUEST_LN);
     setTokenId(BST_GUEST_LN);
 
-    if (!StringUtils.isEmpty(ip)) {
-      SecurityLogger.audit("Guest token generated for IP address: " + ip);
+    if (!StringUtils.isEmpty(name)) {
+      SecurityLogger.audit("Guest token generated for IP address: " + name);
     }
+  }
+
+  public static String parseAddressFromName(@Nullable String fullName) {
+    if (!StringUtils.isEmpty(fullName)) {
+      String[] parts = fullName.split(NAME_DELIMITER);
+      if (parts.length == 2) {
+        return formatIpAddress(parts[1]);
+      }
+    }
+    return null;
+  }
+
+  // IPv6 addresses should be contained within brackets to conform
+  // to the spec IETF RFC 2732
+  private static String formatIpAddress(String ipAddress) {
+    try {
+      if (InetAddress.getByName(ipAddress) instanceof Inet6Address && !ipAddress.contains("[")) {
+        ipAddress = "[" + ipAddress + "]";
+      }
+    } catch (UnknownHostException e) {
+      LOGGER.debug("Error formatting the ip address, using the unformatted ipaddress", e);
+    }
+
+    return ipAddress;
   }
 
   public String getIpAddress() {
     String ip = null;
     if (principal instanceof GuestPrincipal) {
-      ip = ((GuestPrincipal) principal).getAddress();
+      ip = parseAddressFromName(((GuestPrincipal) principal).getName());
     } else if (principal instanceof String) {
-      ip = GuestPrincipal.parseAddressFromName((String) principal);
+      ip = parseAddressFromName((String) principal);
     }
     return ip;
   }
 
   @Override
   public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("Guest IP: ");
-    sb.append(getIpAddress());
-    sb.append("; realm: ");
-    sb.append(realm);
-    return sb.toString();
+    return String.format("Guest IP: %s; realm: %s", getIpAddress(), realm);
   }
 }
