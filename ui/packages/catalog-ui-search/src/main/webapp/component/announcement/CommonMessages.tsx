dc6cac1ac06bd89b7faeb1d8259fb0d873fec8db
Andrew Zimmerman <andrew.zimmerman@connexta.com>
2018-10-02 11:15:24 -0600

    DDF-4192 addressing comments

--- ui/packages/catalog-ui-search/src/main/webapp/component/announcement/CommonMessages.tsx
+++ ui/packages/catalog-ui-search/src/main/webapp/component/announcement/CommonMessages.tsx
@@ -1,5 +1,5 @@
 export const InvalidSearchFormMessage = {
   title: 'Validation Issues: Search Form cannot be run.',
-  message: 'Please verify your configurations and attempt to search again.',
+  message: 'Please verify your search criteria and settings before attempting to search again.',
   type: 'error',
 }
