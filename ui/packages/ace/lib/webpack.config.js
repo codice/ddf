7bb5869f7b2d7cbfe05f74ac27b825111c9f413e
Chris Badahdah <djblue@users.noreply.github.com>
2018-10-17 08:42:13 -0700

    DDF-4232 Improves ui dev server CPU utilization (#3892)

--- ui/packages/ace/lib/webpack.config.js
+++ ui/packages/ace/lib/webpack.config.js
@@ -257,6 +257,10 @@ const prod = (base, { main }) =>
   })
 
 const devServer = ({ auth, target, publicPath }) => ({
+  watchOptions: {
+    ignored: /node_modules/,
+    poll: 1000,
+  },
   publicPath,
   hotOnly: true,
   inline: true,
