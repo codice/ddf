9923b484e37745ed7d99fd7305002d0a39697033
Jason Lam <jason.lam@connexta.com>
2018-10-30 10:11:12 -0400

    DDF-4220 use emptyList

--- catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/metacard/MetacardApplication.java
+++ catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/metacard/MetacardApplication.java
@@ -235,7 +235,7 @@ public class MetacardApplication implements SparkApplication {
         StringUtils.isNotEmpty(accessControlSecurityConfiguration.getSystemUserAttributeValue())
             ? Collections.singletonList(
                 accessControlSecurityConfiguration.getSystemUserAttributeValue())
-            : Collections.EMPTY_LIST;
+            : Collections.emptyList();
     return !Collections.disjoint(subjectRoles, systemUserList);
   }
 
