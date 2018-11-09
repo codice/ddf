ed035224ad6bf22990ed244b7df663c17d1860ea
Will Will <wbw4sv@virginia.edu>
2018-11-09 15:51:55 -0700

    DDF-4324 Run Formatter

--- ui/packages/catalog-ui-search/src/main/webapp/react-component/location/gazetteer.js
+++ ui/packages/catalog-ui-search/src/main/webapp/react-component/location/gazetteer.js
@@ -197,7 +197,10 @@ class Gazetteer extends React.Component {
     })
   }
   isMultiPolygon(coordinates) {
-    return coordinates[0][0][0] !== undefined && coordinates[0][0][0][0] !== undefined
+    return (
+      coordinates[0][0][0] !== undefined &&
+      coordinates[0][0][0][0] !== undefined
+    )
   }
   async geofeature(suggestion) {
     const [type, id] = suggestion.id.split(':')
