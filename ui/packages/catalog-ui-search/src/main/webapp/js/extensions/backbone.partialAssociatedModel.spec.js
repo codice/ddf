d55f07f073e41abcfdfd7ca09a56125e41185730
Andrew Fiedler <andrewkfiedler@gmail.com>
2018-11-15 10:35:55 -0700

    update to fix content.view, add more tests to the spec

--- ui/packages/catalog-ui-search/src/main/webapp/js/extensions/backbone.partialAssociatedModel.spec.js
+++ ui/packages/catalog-ui-search/src/main/webapp/js/extensions/backbone.partialAssociatedModel.spec.js
@@ -31,171 +31,508 @@ const generateMockFetch = (data, options = { delay: 0, multi: false }) => {
 }
 
 describe('Backbone Partial Associated Model', () => {
-  it('identifies partial models', () => {
-    const ParentModel = PartialAssociatedModel.extend({})
-    const instanceParent = new ParentModel({ id: 1 })
-    expect(instanceParent.isPartial()).to.equal(true)
+  describe('options testing', () => {
+    it('options are kept around for later', () => {
+      const ParentModel = PartialAssociatedModel.extend({})
+      const instanceParent = new ParentModel({ id: 1 }, { fetch: true })
+      expect(instanceParent.options.fetch).to.equal(true)
+    })
+
+    it('default fetch used if none provided in options', () => {
+      const ParentModel = PartialAssociatedModel.extend({})
+      const instanceParent = new ParentModel({ id: 1 })
+      expect(instanceParent.options.fetch).to.equal(fetch)
+    })
   })
 
-  it('identifies non-partial models', () => {
-    const ParentModel = PartialAssociatedModel.extend({})
-    const instanceParent = new ParentModel({ id: 1, name: 'rick' })
-    expect(instanceParent.isPartial()).to.equal(false)
-  })
+  describe('models without relations', () => {
+    it('identifies partial models', () => {
+      const ParentModel = PartialAssociatedModel.extend({})
+      const instanceParent = new ParentModel({ id: 1 })
+      expect(instanceParent.isPartial()).to.equal(true)
+    })
 
-  it('identifies partial models (relations)', () => {
-    const ParentModel = PartialAssociatedModel.extend({
-      relations: [
-        {
-          type: Backbone.Many,
-          key: 'subthing',
-          relatedModel: PartialAssociatedModel,
-        },
-      ],
-    })
-    const instanceParent = new ParentModel({
-      id: 1,
-      name: 'rick',
-      subthing: [{ id: 1 }],
-    })
-    expect(instanceParent.isPartial()).to.equal(true)
-  })
-
-  it('identifies non-partial models with partial relations', () => {
-    const Model = PartialAssociatedModel.extend({
-      relations: [
-        {
-          type: Backbone.Many,
-          key: 'subthing',
-          relatedModel: PartialAssociatedModel,
-        },
-      ],
-    })
-    const instanceParent = new Model({
-      id: 1,
-      name: 'rick',
-      subthing: [{ id: 1, name: 'rick' }],
-    })
-    expect(instanceParent.isPartial()).to.equal(false)
-  })
-
-  it('fetches partial models', done => {
-    const ParentModel = PartialAssociatedModel.extend({
-      urlRoot: './parents',
-    })
-    const instanceParent = new ParentModel(
-      { id: 1 },
-      { fetch: generateMockFetch({ name: 'rick' }) }
-    )
-    instanceParent.once('partialSync', () => {
+    it('identifies non-partial models', () => {
+      const ParentModel = PartialAssociatedModel.extend({})
+      const instanceParent = new ParentModel({ id: 1, name: 'rick' })
       expect(instanceParent.isPartial()).to.equal(false)
-      done()
     })
-    if (instanceParent.isPartial()) {
-      instanceParent.fetchPartial()
-    }
-  })
 
-  it('fetches partial models more than once', done => {
-    const ParentModel = PartialAssociatedModel.extend({
-      urlRoot: './parents',
-    })
-    const instanceParent = new ParentModel(
-      { id: 1 },
-      { fetch: generateMockFetch({ name: 'rick' }) }
-    )
-    instanceParent.once('partialSync', () => {
-      expect(instanceParent.isPartial()).to.equal(false)
+    it('fetches partial models', done => {
+      const ParentModel = PartialAssociatedModel.extend({
+        urlRoot: './parents',
+      })
+      const instanceParent = new ParentModel(
+        { id: 1 },
+        {
+          fetch: generateMockFetch({ name: 'rick' }),
+        }
+      )
       instanceParent.once('partialSync', () => {
         expect(instanceParent.isPartial()).to.equal(false)
         done()
       })
-      setTimeout(() => {
+      if (instanceParent.isPartial()) {
         instanceParent.fetchPartial()
-      }, 100)
+      }
     })
-    if (instanceParent.isPartial()) {
-      instanceParent.fetchPartial()
-    }
+
+    it('fetches partial models more than once', done => {
+      const ParentModel = PartialAssociatedModel.extend({
+        urlRoot: './parents',
+      })
+      const instanceParent = new ParentModel(
+        { id: 1 },
+        {
+          fetch: generateMockFetch({ name: 'rick' }),
+        }
+      )
+      instanceParent.once('partialSync', () => {
+        instanceParent.once('partialSync', () => {
+          expect(instanceParent.isPartial()).to.equal(false)
+          done()
+        })
+        setTimeout(() => {
+          instanceParent.fetchPartial()
+        }, 0)
+      })
+      if (instanceParent.isPartial()) {
+        instanceParent.fetchPartial()
+      }
+    })
+
+    it('fetches partial models efficiently (one request at a time)', done => {
+      const ParentModel = PartialAssociatedModel.extend({
+        urlRoot: './parents',
+      })
+      const instanceParent = new ParentModel(
+        { id: 1 },
+        {
+          fetch: generateMockFetch({ name: 'rick' }),
+        }
+      )
+      let timesFetched = 0
+      instanceParent.on('partialSync', () => {
+        timesFetched++
+      })
+      if (instanceParent.isPartial()) {
+        instanceParent.fetchPartial()
+        instanceParent.fetchPartial()
+      }
+      setTimeout(() => {
+        expect(timesFetched).to.equal(1)
+        done()
+      }, 500)
+    })
   })
 
-  it('fetches partial relations with Many relationship', done => {
-    const ParentModel = PartialAssociatedModel.extend({
-      urlRoot: './parents',
-      relations: [
-        {
-          type: Backbone.Many,
-          key: 'subthing',
-          relatedModel: PartialAssociatedModel,
-        },
-      ],
-    })
-    const instanceParent = new ParentModel(
-      { id: 1, name: 'rick', subthing: [{ id: 2 }, { id: 3 }] },
-      {
-        fetch: generateMockFetch(
+  describe('models with 1 relation', () => {
+    it('identifies partial models', () => {
+      const ChildModel = PartialAssociatedModel.extend({})
+      const ParentModel = PartialAssociatedModel.extend({
+        urlRoot: './parents',
+        relations: [
           {
-            './parents/1': {
-              name: 'rick',
+            type: Backbone.Many,
+            key: 'things',
+            relatedModel: ChildModel,
+          },
+        ],
+      })
+      const instanceParent = new ParentModel({ id: 1, things: [{ id: 1 }] })
+      expect(instanceParent.isPartial()).to.equal(true)
+    })
+
+    it('identifies non-partial models', () => {
+      const ChildModel = PartialAssociatedModel.extend({})
+      const ParentModel = PartialAssociatedModel.extend({
+        relations: [
+          {
+            type: Backbone.Many,
+            key: 'things',
+            relatedModel: ChildModel,
+          },
+        ],
+      })
+      const instanceParent = new ParentModel({
+        id: 1,
+        things: [{ id: 1, name: 'car' }],
+      })
+      expect(instanceParent.isPartial()).to.equal(false)
+    })
+
+    it('fetches partial models', done => {
+      const ChildModel = PartialAssociatedModel.extend({})
+      const ParentModel = PartialAssociatedModel.extend({
+        urlRoot: './parents',
+        relations: [
+          {
+            type: Backbone.Many,
+            key: 'things',
+            relatedModel: ChildModel,
+          },
+        ],
+      })
+      const instanceParent = new ParentModel(
+        {
+          id: 1,
+          things: [{ id: 1 }],
+        },
+        {
+          fetch: generateMockFetch(
+            {
+              './parents/1': { id: 1, things: [{ id: 1 }] },
+              './parents/1/things': [{ id: 1, name: 'car' }],
             },
-            './parents/1/subthing': [
-              {
+            { multi: true }
+          ),
+        }
+      )
+      instanceParent.once('partialSync', () => {
+        expect(instanceParent.isPartial()).to.equal(false)
+        done()
+      })
+      if (instanceParent.isPartial()) {
+        instanceParent.fetchPartial()
+      }
+    })
+
+    it('fetches partial relations with One relationship', done => {
+      const ParentModel = PartialAssociatedModel.extend({
+        urlRoot: './parents',
+        relations: [
+          {
+            type: Backbone.One,
+            key: 'subthing',
+            relatedModel: PartialAssociatedModel,
+          },
+        ],
+      })
+      const instanceParent = new ParentModel(
+        { id: 1, name: 'rick', subthing: { id: 2 } },
+        {
+          fetch: generateMockFetch(
+            {
+              './parents/1': {
+                name: 'rick',
+              },
+              './parents/1/subthing': {
                 id: 2,
                 name: 'rick2',
               },
-              {
-                id: 3,
-                name: 'rick3',
-              },
-            ],
-          },
-          { multi: true }
-        ),
+            },
+            { multi: true }
+          ),
+        }
+      )
+      instanceParent.once('partialSync', () => {
+        expect(instanceParent.isPartial()).to.equal(false)
+        done()
+      })
+      if (instanceParent.isPartial()) {
+        instanceParent.fetchPartial()
       }
-    )
-    instanceParent.once('partialSync', () => {
-      expect(instanceParent.isPartial()).to.equal(false)
-      done()
     })
-    if (instanceParent.isPartial()) {
-      instanceParent.fetchPartial()
-    }
+
+    it('fetches partial models more than once', done => {
+      const ChildModel = PartialAssociatedModel.extend({})
+      const ParentModel = PartialAssociatedModel.extend({
+        urlRoot: './parents',
+        relations: [
+          {
+            type: Backbone.Many,
+            key: 'things',
+            relatedModel: ChildModel,
+          },
+        ],
+      })
+      const instanceParent = new ParentModel(
+        {
+          id: 1,
+          things: [{ id: 1 }],
+        },
+        {
+          fetch: generateMockFetch(
+            {
+              './parents/1': { id: 1, things: [{ id: 1 }] },
+              './parents/1/things': [{ id: 1, name: 'car' }],
+            },
+            { multi: true }
+          ),
+        }
+      )
+      instanceParent.once('partialSync', () => {
+        expect(instanceParent.isPartial()).to.equal(false)
+        instanceParent.once('partialSync', () => {
+          expect(instanceParent.isPartial()).to.equal(false)
+          done()
+        })
+        setTimeout(() => {
+          instanceParent.fetchPartial()
+        }, 0)
+      })
+      if (instanceParent.isPartial()) {
+        instanceParent.fetchPartial()
+      }
+    })
+
+    it('fetches partial models efficiently (one request at a time)', done => {
+      const ChildModel = PartialAssociatedModel.extend({})
+      const ParentModel = PartialAssociatedModel.extend({
+        urlRoot: './parents',
+        relations: [
+          {
+            type: Backbone.Many,
+            key: 'things',
+            relatedModel: ChildModel,
+          },
+        ],
+      })
+      const instanceParent = new ParentModel(
+        {
+          id: 1,
+          things: [{ id: 1 }],
+        },
+        {
+          fetch: generateMockFetch(
+            {
+              './parents/1': { id: 1, things: [{ id: 1 }] },
+              './parents/1/things': [{ id: 1, name: 'car' }],
+            },
+            { multi: true }
+          ),
+        }
+      )
+      let timesFetched = 0
+      instanceParent.on('partialSync', () => {
+        timesFetched++
+      })
+      if (instanceParent.isPartial()) {
+        instanceParent.fetchPartial()
+        instanceParent.fetchPartial()
+      }
+      setTimeout(() => {
+        expect(timesFetched).to.equal(1)
+        done()
+      }, 500)
+    })
   })
 
-  it('fetches partial relations with One relationship', done => {
-    const ParentModel = PartialAssociatedModel.extend({
-      urlRoot: './parents',
-      relations: [
-        {
-          type: Backbone.One,
-          key: 'subthing',
-          relatedModel: PartialAssociatedModel,
-        },
-      ],
+  describe('models with > 1 relation', () => {
+    describe('identifies partial models', () => {
+      it('one relation is partial', () => {
+        const ChildModel = PartialAssociatedModel.extend({})
+        const ParentModel = PartialAssociatedModel.extend({
+          urlRoot: './parents',
+          relations: [
+            {
+              type: Backbone.Many,
+              key: 'things',
+              relatedModel: ChildModel,
+            },
+            {
+              type: Backbone.Many,
+              key: 'other_things',
+              relatedModel: ChildModel,
+            },
+          ],
+        })
+        const instanceParent = new ParentModel({
+          id: 1,
+          things: [{ id: 1 }],
+          other_things: [{ id: 1, name: 'car' }],
+        })
+        expect(instanceParent.isPartial()).to.equal(true)
+      })
+
+      it('both relationl are partial', () => {
+        const ChildModel = PartialAssociatedModel.extend({})
+        const ParentModel = PartialAssociatedModel.extend({
+          urlRoot: './parents',
+          relations: [
+            {
+              type: Backbone.Many,
+              key: 'things',
+              relatedModel: ChildModel,
+            },
+            {
+              type: Backbone.Many,
+              key: 'other_things',
+              relatedModel: ChildModel,
+            },
+          ],
+        })
+        const instanceParent = new ParentModel({
+          id: 1,
+          things: [{ id: 1 }],
+          other_things: [{ id: 1 }],
+        })
+        expect(instanceParent.isPartial()).to.equal(true)
+      })
     })
-    const instanceParent = new ParentModel(
-      { id: 1, name: 'rick', subthing: { id: 2 } },
-      {
-        fetch: generateMockFetch(
+
+    it('identifies non-partial models', () => {
+      const ChildModel = PartialAssociatedModel.extend({})
+      const ParentModel = PartialAssociatedModel.extend({
+        relations: [
           {
-            './parents/1': {
-              name: 'rick',
-            },
-            './parents/1/subthing': {
-              id: 2,
-              name: 'rick2',
-            },
+            type: Backbone.Many,
+            key: 'things',
+            relatedModel: ChildModel,
           },
-          { multi: true }
-        ),
-      }
-    )
-    instanceParent.once('partialSync', () => {
+          {
+            type: Backbone.Many,
+            key: 'other_things',
+            relatedModel: ChildModel,
+          },
+        ],
+      })
+      const instanceParent = new ParentModel({
+        id: 1,
+        things: [{ id: 1, name: 'car' }],
+        other_things: [{ id: 1, name: 'car' }],
+      })
       expect(instanceParent.isPartial()).to.equal(false)
-      done()
     })
-    if (instanceParent.isPartial()) {
-      instanceParent.fetchPartial()
-    }
+
+    it('fetches partial models', done => {
+      const ChildModel = PartialAssociatedModel.extend({})
+      const ParentModel = PartialAssociatedModel.extend({
+        urlRoot: './parents',
+        relations: [
+          {
+            type: Backbone.Many,
+            key: 'things',
+            relatedModel: ChildModel,
+          },
+          {
+            type: Backbone.Many,
+            key: 'other_things',
+            relatedModel: ChildModel,
+          },
+        ],
+      })
+      const instanceParent = new ParentModel(
+        {
+          id: 1,
+          things: [{ id: 1 }],
+          other_things: [{ id: 1 }],
+        },
+        {
+          fetch: generateMockFetch(
+            {
+              './parents/1': { id: 1, things: [{ id: 1 }] },
+              './parents/1/things': [{ id: 1, name: 'car' }],
+              './parents/1/other_things': [{ id: 1, name: 'car' }],
+            },
+            { multi: true }
+          ),
+        }
+      )
+      instanceParent.once('partialSync', () => {
+        expect(instanceParent.isPartial()).to.equal(false)
+        done()
+      })
+      if (instanceParent.isPartial()) {
+        instanceParent.fetchPartial()
+      }
+    })
+
+    it('fetches partial models more than once', done => {
+      const ChildModel = PartialAssociatedModel.extend({})
+      const ParentModel = PartialAssociatedModel.extend({
+        urlRoot: './parents',
+        relations: [
+          {
+            type: Backbone.Many,
+            key: 'things',
+            relatedModel: ChildModel,
+          },
+          {
+            type: Backbone.Many,
+            key: 'other_things',
+            relatedModel: ChildModel,
+          },
+        ],
+      })
+      const instanceParent = new ParentModel(
+        {
+          id: 1,
+          things: [{ id: 1 }],
+          other_things: [{ id: 1 }],
+        },
+        {
+          fetch: generateMockFetch(
+            {
+              './parents/1': { id: 1, things: [{ id: 1 }] },
+              './parents/1/things': [{ id: 1, name: 'car' }],
+              './parents/1/other_things': [{ id: 1, name: 'car' }],
+            },
+            { multi: true }
+          ),
+        }
+      )
+      instanceParent.once('partialSync', () => {
+        expect(instanceParent.isPartial()).to.equal(false)
+        instanceParent.once('partialSync', () => {
+          expect(instanceParent.isPartial()).to.equal(false)
+          done()
+        })
+        setTimeout(() => {
+          instanceParent.fetchPartial()
+        }, 0)
+      })
+      if (instanceParent.isPartial()) {
+        instanceParent.fetchPartial()
+      }
+    })
+
+    it('fetches partial models efficiently (one request at a time)', done => {
+      const ChildModel = PartialAssociatedModel.extend({})
+      const ParentModel = PartialAssociatedModel.extend({
+        urlRoot: './parents',
+        relations: [
+          {
+            type: Backbone.Many,
+            key: 'things',
+            relatedModel: ChildModel,
+          },
+          {
+            type: Backbone.Many,
+            key: 'other_things',
+            relatedModel: ChildModel,
+          },
+        ],
+      })
+      const instanceParent = new ParentModel(
+        {
+          id: 1,
+          things: [{ id: 1 }],
+          other_things: [{ id: 1 }],
+        },
+        {
+          fetch: generateMockFetch(
+            {
+              './parents/1': { id: 1, things: [{ id: 1 }] },
+              './parents/1/things': [{ id: 1, name: 'car' }],
+              './parents/1/other_things': [{ id: 1, name: 'car' }],
+            },
+            { multi: true }
+          ),
+        }
+      )
+      let timesFetched = 0
+      instanceParent.on('partialSync', () => {
+        timesFetched++
+      })
+      if (instanceParent.isPartial()) {
+        instanceParent.fetchPartial()
+        instanceParent.fetchPartial()
+      }
+      setTimeout(() => {
+        expect(timesFetched).to.equal(1)
+        done()
+      }, 500)
+    })
   })
 })
