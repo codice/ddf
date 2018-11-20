69721fc44b683538a390631dc4fe1aa0b8d3ecab
Tyler Clemens <tyler.clemens@connexta.com>
2018-11-20 10:05:58 -0700

    DDF-4157 Fixed UNIT tests

--- catalog/spatial/registry/registry-publication-action-provider/src/main/java/org/codice/ddf/registry/publication/manager/RegistryPublicationManager.java
+++ catalog/spatial/registry/registry-publication-action-provider/src/main/java/org/codice/ddf/registry/publication/manager/RegistryPublicationManager.java
@@ -72,6 +72,10 @@ public class RegistryPublicationManager implements EventHandler {
   }
 
   public void init() {
+    executorService.submit(this::setPublications);
+  }
+
+  public void setPublications() {
     try {
       List<Metacard> metacards =
           Security.getInstance()
@@ -99,7 +103,7 @@ public class RegistryPublicationManager implements EventHandler {
       LOGGER.debug(
           "Error reading from local catalog. Catalog is probably not up yet. Will try again in {} seconds.",
           RETRY_INTERVAL);
-      executorService.schedule(this::init, RETRY_INTERVAL, TimeUnit.SECONDS);
+      executorService.schedule(this::setPublications, RETRY_INTERVAL, TimeUnit.SECONDS);
     }
   }
 
