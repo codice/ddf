312611d6ecfafb5eaf6dba27ec824754a38bfdc9
Vina Martin <vina.martin@connexta.com>
2018-11-21 14:18:47 -0700

    DDF-4350 Add internal/system hostname and port to the installer

--- ui/packages/ui/src/main/webapp/js/models/installer/Configuration.js
+++ ui/packages/ui/src/main/webapp/js/models/installer/Configuration.js
@@ -12,7 +12,7 @@
  * <http://www.gnu.org/licenses/lgpl.html>.
  *
  **/
-/*global define, location*/
+/*global define*/
 /** Main view page for add. */
 define([
     'backbone',
@@ -78,12 +78,6 @@ define([
             this.models.forEach(function(model){
                 propertiesMap[model.get('key')] = model.get('value');
             });
-
-            var devMode = location.search.indexOf('dev=true') > -1 ? true : false;
-            if (devMode) {
-                var hostname = propertiesMap['org.codice.ddf.external.hostname'];
-                propertiesMap['org.codice.ddf.system.hostname'] = hostname;
-            }
 
             data.arguments = [propertiesMap];
             data = JSON.stringify(data);
