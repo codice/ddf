cfd9b7d92dbf719004cbc720ec224693a015848a
leo-sakh <leo.sakhvoruk@gmail.com>
2018-12-10 19:13:54 -0500

    DDF-4336: Refactored metacard interactions view (#4038)
    * DDF-4336: Refactored metacard interactions view
    
    * DDF-4336: Separated into container/presentation; added copyright
    
    * DDF-4336: Simplified region handling; misc
    
    * DDF-4336: PR comments

--- ui/packages/catalog-ui-search/src/main/webapp/component/metacard-interactions/metacard-interactions.view.js
+++ ui/packages/catalog-ui-search/src/main/webapp/component/metacard-interactions/metacard-interactions.view.js
@@ -13,332 +13,44 @@
  *
  **/
 /*global define, window*/
-const wreqr = require('../../js/wreqr.js')
 const Marionette = require('marionette')
-const _ = require('underscore')
-const $ = require('jquery')
-const template = require('./metacard-interactions.hbs')
 const CustomElements = require('../../js/CustomElements.js')
-const store = require('../../js/store.js')
-const router = require('../router/router.js')
-const user = require('../singletons/user-instance.js')
-const sources = require('../singletons/sources-instance.js')
-const Query = require('../../js/model/Query.js')
-const wkx = require('wkx')
-const CQLUtils = require('../../js/CQLUtils.js')
-const QueryConfirmationView = require('../confirmation/query/confirmation.query.view.js')
-const LoadingView = require('../loading/loading.view.js')
-const PopoutView = require('../dropdown/popout/dropdown.popout.view.js')
-const ResultAddView = require('../result-add/result-add.view.js')
-const ExportActionsView = require('../export-actions/export-actions.view.js')
 const plugin = require('plugins/metacard-interactions')
 
-module.exports = plugin(
-  Marionette.LayoutView.extend({
-    template: template,
-    tagName: CustomElements.register('metacard-interactions'),
-    className: 'composed-menu',
-    modelEvents: {
-      change: 'render',
-    },
-    regions: {
-      resultAdd: '.interaction-add',
-      resultActionsExport: '.interaction-actions-export',
-      extensions: '.interaction-extensions',
-    },
-    events: {
-      'click .interaction-add': 'handleAdd',
-      'click .interaction-hide': 'handleHide',
-      'click .interaction-show': 'handleShow',
-      'click .interaction-expand': 'handleExpand',
-      'click .interaction-share': 'handleShare',
-      'click .interaction-download': 'handleDownload',
-      'click .interaction-create-search': 'handleCreateSearch',
-      'click .metacard-interaction:not(.interaction-add)': 'handleClick',
-    },
-    ui: {},
-    initialize: function() {
-      var currentWorkspace = store.getCurrentWorkspace()
-      this.listenTo(this.model, 'change:metacard>properties', this.onRender)
-      this.listenTo(
-        user
-          .get('user')
-          .get('preferences')
-          .get('resultBlacklist'),
-        'add remove update reset',
-        this.checkIfBlacklisted
-      )
-    },
-    onRender: function() {
-      this.checkTypes()
-      this.checkIsInWorkspace()
-      this.checkIfDownloadable()
-      this.checkIfMultiple()
-      this.checkIfRouted()
-      this.checkIfBlacklisted()
-      this.checkHasLocation()
-      this.setupResultAdd()
-      this.setupResultActionsExport()
-      const extensions = this.getExtensions()
-      if (extensions !== undefined) {
-        this.extensions.show(extensions)
-      }
-    },
-    getExtensions: function() {},
-    setupResultActionsExport() {
-      this.resultActionsExport.show(
-        PopoutView.createSimpleDropdown({
-          componentToShow: ExportActionsView,
-          dropdownCompanionBehaviors: {
-            navigation: {},
-          },
-          modelForComponent: this.model.first(),
-          leftIcon: 'fa fa-external-link',
-          rightIcon: 'fa fa-chevron-down',
-          label: 'Export as',
-        })
-      )
-    },
-    setupResultAdd: function() {
-      this.resultAdd.show(
-        PopoutView.createSimpleDropdown({
-          componentToShow: ResultAddView,
-          modelForComponent: this.model,
-          leftIcon: 'fa fa-plus',
-          rightIcon: 'fa fa-chevron-down',
-          label: 'Add / Remove from List',
-        })
-      )
-    },
-    handleAdd: function(e) {
-      this.$el
-        .find('.interaction-add > *')
-        .mousedown()
-        .click()
-    },
-    handleHide: function() {
-      var preferences = user.get('user').get('preferences')
-      preferences.get('resultBlacklist').add(
-        this.model.map(function(result) {
-          return {
-            id: result
-              .get('metacard')
-              .get('properties')
-              .get('id'),
-            title: result
-              .get('metacard')
-              .get('properties')
-              .get('title'),
-          }
-        })
-      )
-      preferences.savePreferences()
-    },
-    handleShow: function() {
-      var preferences = user.get('user').get('preferences')
-      preferences.get('resultBlacklist').remove(
-        this.model.map(function(result) {
-          return result
-            .get('metacard')
-            .get('properties')
-            .get('id')
-        })
-      )
-      preferences.savePreferences()
-    },
-    handleExpand: function() {
-      var id = this.model
-        .first()
-        .get('metacard')
-        .get('properties')
-        .get('id')
-      wreqr.vent.trigger('router:navigate', {
-        fragment: 'metacards/' + id,
-        options: {
-          trigger: true,
-        },
-      })
-    },
-    handleShare: function() {},
-    handleDownload: function() {
-      this.model.forEach(function(result) {
-        var downloadUrl = result
-          .get('metacard')
-          .get('properties')
-          .get('resource-download-url')
-        if (downloadUrl !== undefined) {
-          window.open(downloadUrl)
-        }
-      })
-    },
-    handleCreateSearch: function() {
-      var locations = this.model.reduce((locationArray, model) => {
-        let location = model
-          .get('metacard')
-          .get('properties')
-          .get('location')
-        if (location) {
-          let locationGeometry = wkx.Geometry.parse(location)
-          let cqlString =
-            '(' + CQLUtils.buildIntersectCQL(locationGeometry) + ')'
-          locationArray.push(cqlString)
-        }
-        return locationArray
-      }, [])
-      if (locations.length === 0) {
-        return // shouldn't happen but just in case
-      }
-      var combinedCqlString = locations.reduce(
-        (cqlString, subCqlString, index) => {
-          if (index !== 0) {
-            cqlString = cqlString + ' OR '
-          }
-          cqlString = cqlString + subCqlString
-          return cqlString
-        },
-        ''
-      )
-      let cqlString = '(' + combinedCqlString + ')'
-      var newQuery = new Query.Model({
-        type: locations.length > 1 ? 'advanced' : 'basic',
-      })
-      var queryModel = store.getCurrentQueries()
-      newQuery.set('cql', cqlString)
-      if (queryModel.canAddQuery()) {
-        queryModel.add(newQuery)
-        store.setCurrentQuery(newQuery)
-      } else {
-        this.listenTo(
-          QueryConfirmationView.generateConfirmation({}),
-          'change:choice',
-          function(confirmation) {
-            var choice = confirmation.get('choice')
-            if (choice === true) {
-              var loadingview = new LoadingView()
-              store
-                .get('workspaces')
-                .once('sync', function(workspace, resp, options) {
-                  loadingview.remove()
-                  wreqr.vent.trigger('router:navigate', {
-                    fragment: 'workspaces/' + workspace.id,
-                    options: {
-                      trigger: true,
-                    },
-                  })
-                })
-              store.get('workspaces').createWorkspaceWithQuery(newQuery)
-            } else if (choice !== false) {
-              store.getCurrentQueries().remove(choice)
-              store.getCurrentQueries().add(newQuery)
-              store.setCurrentQuery(newQuery)
-            }
-          }.bind(this)
-        )
-      }
-    },
-    handleClick: function() {
-      this.$el.trigger('closeDropdown.' + CustomElements.getNamespace())
-    },
-    checkIsInWorkspace: function() {
-      var currentWorkspace = store.getCurrentWorkspace()
-      this.$el.toggleClass('in-workspace', Boolean(currentWorkspace))
-    },
-    checkIfMultiple: function() {
-      this.$el.toggleClass('is-multiple', Boolean(this.model.length > 1))
-    },
-    checkIfRouted: function() {
-      this.$el.toggleClass(
-        'is-routed',
-        Boolean(router.toJSON().name === 'openMetacard')
-      )
-    },
-    checkIfDownloadable: function() {
-      var downloadable = this.model.find(result => {
-        var downloadUrl = result
-          .get('metacard')
-          .get('properties')
-          .get('resource-download-url')
-        return downloadUrl !== undefined
-      })
-      this.$el.toggleClass('is-downloadable', downloadable !== undefined)
-    },
-    checkIfBlacklisted: function() {
-      var pref = user.get('user').get('preferences')
-      var blacklist = pref.get('resultBlacklist')
-      var ids = this.model.map(function(result) {
-        return result
-          .get('metacard')
-          .get('properties')
-          .get('id')
-      })
-      var isBlacklisted = false
-      ids.forEach(function(id) {
-        if (blacklist.get(id) !== undefined) {
-          isBlacklisted = true
-        }
-      })
-      this.$el.toggleClass('is-blacklisted', isBlacklisted)
-    },
-    checkHasLocation: function() {
-      var locations = this.model.reduce((locationArray, model) => {
-        let location = model
-          .get('metacard')
-          .get('properties')
-          .get('location')
-        if (location) {
-          let locationGeometry = wkx.Geometry.parse(location)
-          let cqlString =
-            '(' + CQLUtils.buildIntersectCQL(locationGeometry) + ')'
-          locationArray.push(cqlString)
-        }
-        return locationArray
-      }, [])
-      this.$el.toggleClass('has-location', locations.length > 0)
-    },
-    checkTypes: function() {
-      var types = {}
-      this.model.forEach(function(result) {
-        var tags = result
-          .get('metacard')
-          .get('properties')
-          .get('metacard-tags')
-        if (result.isWorkspace()) {
-          types.workspace = true
-        } else if (result.isResource()) {
-          types.resource = true
-        } else if (result.isRevision()) {
-          types.revision = true
-        } else if (result.isDeleted()) {
-          types.deleted = true
-        }
-        if (result.isRemote()) {
-          types.remote = true
-        }
-      })
-      this.$el.toggleClass('is-mixed', Object.keys(types).length > 1)
-      this.$el.toggleClass('is-workspace', types.workspace !== undefined)
-      this.$el.toggleClass('is-resource', types.resource !== undefined)
-      this.$el.toggleClass('is-revision', types.revision !== undefined)
-      this.$el.toggleClass('is-deleted', types.deleted !== undefined)
-      this.$el.toggleClass('is-remote', types.remote !== undefined)
-    },
-    serializeData: function() {
-      var currentWorkspace = store.getCurrentWorkspace()
-      var resultJSON, workspaceJSON
-      if (this.model) {
-        resultJSON = this.model.toJSON()
-      }
-      if (currentWorkspace) {
-        workspaceJSON = currentWorkspace.toJSON()
-      }
-      var result = resultJSON[0]
-      return {
-        remoteResourceCached:
-          result.isResourceLocal &&
-          result.metacard.properties['source-id'] !== sources.localCatalog,
-        result: resultJSON,
-        workspace: workspaceJSON,
-      }
-    },
-  })
-)
+import React from 'react'
+import InteractionsView from '../../react-component/container/metacard-interactions'
+
+const MetacardInteractionsView = Marionette.LayoutView.extend({
+  template() {
+    const props = {
+      model: this.model,
+      el: this.$el,
+      extensions: this.getExtensions(),
+      categories: this.getCategories(),
+    }
+    return <InteractionsView {...props} />
+  },
+  tagName: CustomElements.register('metacard-interactions'),
+  className: 'composed-menu',
+  modelEvents: {
+    change: 'render',
+  },
+  ui: {},
+  getExtensions() {},
+  handleShare() {},
+
+  /**
+   * Should return a list of items comprising a 'category' of action links that will be rendered in the drop-down.
+   * Ex.
+   * {`category-name`: [{
+   *  parent: `parent-css-class`,
+   *  dataHelp: `Something helpful here`,
+   *  icon: `icon class`, //should be consistent throughout the category
+   *  linkText: `Text to be rendered for the link`,
+   *  actionHandler: () => `Perform on-click`
+   * }]}
+   */
+  getCategories() {},
+})
+
+module.exports = plugin(MetacardInteractionsView)
---
+++ ui/packages/catalog-ui-search/src/main/webapp/react-component/container/metacard-interactions/index.tsx
@@ -0,0 +1,411 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ **/
+import * as React from 'react'
+import { Geometry } from 'wkx'
+
+import {
+  render as View,
+  Model as ViewModel,
+} from '../../presentation/metacard-interactions'
+
+import withListenTo, { WithBackboneProps } from '../backbone-container'
+
+import * as CqlUtils from '../../../js/CQLUtils'
+
+import * as CustomElements from '../../../js/CustomElements'
+
+import * as QueryConfirmationView from '../../../component/confirmation/query/confirmation.query.view'
+import * as LoadingView from '../../../component/loading/loading.view'
+import * as PopoutView from '../../../component/dropdown/popout/dropdown.popout.view'
+import * as ResultAddView from '../../../component/result-add/result-add.view'
+import * as ExportActionsView from '../../../component/export-actions/export-actions.view'
+
+import * as router from '../../../component/router/router'
+import * as sources from '../../../component/singletons/sources-instance'
+
+import * as store from '../../../js/store'
+import * as user from '../../../component/singletons/user-instance'
+
+import MarionetteRegionContainer from '../marionette-region-container'
+
+const Query = require('../../../js/model/Query')
+const wreqr = require('wreqr')
+
+type Props = {
+  model: {} | any
+  el: {} | any
+  extensions: {} | any
+  handleShare?: () => void
+  categories: { [key: string]: Array<{}> }
+} & WithBackboneProps
+
+type El = {
+  find: (matcher: string) => any
+  toggleClass: (key: string, flag: boolean) => void
+}
+
+type Result = {
+  get: (key: any) => any
+  isWorkspace: () => boolean
+  isResource: () => boolean
+  isRevision: () => boolean
+  isDeleted: () => boolean
+  isRemote: () => boolean
+}
+
+type Model = {
+  map: (
+    result: Result | any
+  ) =>
+    | {
+        id?: any
+        title?: any
+      }
+    | {}
+  toJSON: () => any
+  first: () => any
+  forEach: (result: Result | any) => void
+  find: (result: Result | any) => boolean
+} & Array<any>
+
+const withCloseDropdown = (
+  context: Props,
+  action: (context: Props) => void
+) => {
+  context.el.trigger(`closeDropdown.${CustomElements.getNamespace()}`)
+  action(context)
+}
+
+const handleDownload = (context: Props) => {
+  const openValidUrl = (result: Result) => {
+    const downloadUrl = result
+      .get('metacard')
+      .get('properties')
+      .get('resource-download-url')
+    downloadUrl && window.open(downloadUrl)
+  }
+
+  context.model.forEach(openValidUrl)
+}
+
+const getGeoLocations = (model: Model) =>
+  model.reduce((locationArray: Array<string>, result: Result) => {
+    const location = result
+      .get('metacard')
+      .get('properties')
+      .get('location')
+
+    if (location) {
+      const geometry = Geometry.parse(location)
+      locationArray.push(`(${CqlUtils.buildIntersectCQL(geometry)})`)
+    }
+
+    return locationArray
+  }, [])
+
+const handleCreateSearch = (context: Props) => {
+  const locations = getGeoLocations(context.model)
+
+  if (locations.length === 0) return
+
+  const locationString = `(${locations.join(` OR `)})`
+  const newQuery = new Query.Model({
+    type: locations.length > 1 ? 'advanced' : 'basic',
+  })
+  newQuery.set('cql', locationString)
+
+  const existingQuery = store.getCurrentQueries()
+
+  if (existingQuery.canAddQuery()) {
+    existingQuery.add(newQuery)
+    store.setCurrentQuery(newQuery)
+    return
+  }
+
+  context.listenTo(
+    QueryConfirmationView.generateConfirmation({}),
+    'change:choice',
+    (confirmation: any) => {
+      const choice = confirmation.get('choice')
+      if (choice === true) {
+        const loadingView = new LoadingView()
+        store.get('workspaces').once('sync', (workspace: any) => {
+          loadingView.remove()
+          wreqr.vent.trigger('router:navigate', {
+            fragment: `workspaces/${workspace.id}`,
+            options: {
+              trigger: true,
+            },
+          })
+        })
+        store.get('workspaces').createWorkspaceWithQuery(newQuery)
+      } else if (choice !== false) {
+        store.getCurrentQueries().remove(choice)
+        store.getCurrentQueries().add(newQuery)
+        store.setCurrentQuery(newQuery)
+      }
+    }
+  )
+}
+
+const handleShow = (context: Props) => {
+  const preferences = user.get('user').get('preferences')
+  const getResult = (result: Result) =>
+    result
+      .get('metacard')
+      .get('properties')
+      .get('id')
+
+  preferences.get('resultBlacklist').remove(context.model.map(getResult))
+  preferences.savePreferences()
+}
+
+const handleHide = (context: Props) => {
+  const preferences = user.get('user').get('preferences')
+  const getResult = (result: Result) => ({
+    id: result
+      .get('metacard')
+      .get('properties')
+      .get('id'),
+    title: result
+      .get('metacard')
+      .get('properties')
+      .get('title'),
+  })
+
+  preferences.get('resultBlacklist').add(context.model.map(getResult))
+  preferences.savePreferences()
+}
+
+const handleExpand = (context: Props) => {
+  const id = context.model
+    .first()
+    .get('metacard')
+    .get('properties')
+    .get('id')
+
+  wreqr.vent.trigger('router:navigate', {
+    fragment: 'metacards/' + id,
+    options: {
+      trigger: true,
+    },
+  })
+}
+
+const handleShare = (context: Props) =>
+  context.handleShare && context.handleShare()
+
+const appendCssIfNeeded = (model: Model, el: El) => {
+  const isMixed = model.reduce((accum: number, item: Result) => {
+    if (item.isRemote()) {
+      el.toggleClass('is-remote', true)
+      return ++accum
+    }
+    if (item.isWorkspace()) {
+      el.toggleClass('is-workspace', true)
+      return ++accum
+    }
+    if (item.isResource()) {
+      el.toggleClass('is-resource', true)
+      return ++accum
+    }
+    if (item.isRevision()) {
+      el.toggleClass('is-revision', true)
+      return ++accum
+    }
+    if (item.isDeleted()) {
+      el.toggleClass('is-deleted', true)
+      return ++accum
+    }
+  }, 0)
+
+  el.toggleClass('is-mixed', isMixed > 1)
+
+  const currentWorkspace = store.getCurrentWorkspace()
+  el.toggleClass('in-workspace', Boolean(currentWorkspace))
+  el.toggleClass('is-downloadable', isDownloadable(model))
+  el.toggleClass('is-multiple', model.length > 1)
+  el.toggleClass('is-routed', isRouted())
+  el.toggleClass('is-blacklisted', isBlacklisted(model))
+  el.toggleClass('has-location', hasLocation(model))
+}
+
+const isDownloadable = (model: Model): boolean =>
+  model.some((result: Result) =>
+    result
+      .get('metacard')
+      .get('properties')
+      .get('resource-download-url')
+  )
+const isRouted = (): boolean =>
+  router && router.toJSON().name === 'openMetacard'
+const isBlacklisted = (model: Model): boolean => {
+  const blacklist = user
+    .get('user')
+    .get('preferences')
+    .get('resultBlacklist')
+  return model.reduce((accum: boolean, result: Result) => {
+    const id = result
+      .get('metacard')
+      .get('properties')
+      .get('id')
+    return blacklist.get(id) !== undefined || accum
+  }, false)
+}
+
+const isRemoteResourceCached = (model: Model): boolean => {
+  if (!model) return false
+
+  const modelJson = model.toJSON()
+
+  if (!modelJson || modelJson.length <= 0) return false
+
+  return (
+    modelJson[0].isResourceLocal &&
+    modelJson[0].metacard.properties['source-id'] !== sources.localCatalog
+  )
+}
+
+const hasLocation = (model: Model): boolean => getGeoLocations(model).length > 0
+
+const createAddRemoveRegion = (model: Model) =>
+  PopoutView.createSimpleDropdown({
+    componentToShow: ResultAddView,
+    modelForComponent: model,
+    leftIcon: 'fa fa-plus',
+    rightIcon: 'fa fa-chevron-down',
+    label: 'Add / Remove from List',
+  })
+
+const createResultActionsExportRegion = (model: Model) =>
+  PopoutView.createSimpleDropdown({
+    componentToShow: ExportActionsView,
+    dropdownCompanionBehaviors: {
+      navigation: {},
+    },
+    modelForComponent: model.first(),
+    leftIcon: 'fa fa-external-link',
+    rightIcon: 'fa fa-chevron-down',
+    label: 'Export as',
+  })
+
+const defaultLinks = [
+  {
+    parent: `interaction-hide`,
+    dataHelp: `Adds to a list
+    of results that will be hidden from future searches.  To clear this list,
+    click the Settings icon, select Hidden, then choose to unhide the record.`,
+    icon: `fa fa-eye-slash`,
+    linkText: `Hide from Future Searches`,
+    actionHandler: handleHide,
+  },
+  {
+    parent: `interaction-show`,
+    dataHelp: `Removes from the
+    list of results that are hidden from future searches.`,
+    icon: `fa fa-eye`,
+    linkText: `Unhide from Future Searches`,
+    actionHandler: handleShow,
+  },
+  {
+    parent: `interaction-expand`,
+    dataHelp: `Takes you to a
+    view that only focuses on this particular result.  Bookmarking it will allow
+    you to come back to this result directly.`,
+    icon: `fa fa-expand`,
+    linkText: `Expand Metacard View`,
+    actionHandler: handleExpand,
+  },
+  {
+    parent: `interaction-share`,
+    dataHelp: `Copies the
+    URL that leads to this result directly to your clipboard.`,
+    icon: `fa fa-share-alt`,
+    linkText: `Share Metacard`,
+    actionHandler: handleShare,
+  },
+]
+
+const viewModelFromProps = (props: Props): ViewModel => {
+  const defaultCategories = [{ name: 'default', items: defaultLinks }]
+
+  if (!props.categories) return { categories: defaultCategories } as ViewModel
+
+  const categories = Object.keys(props.categories).map(key => ({
+    name: key,
+    items: props.categories[key],
+  }))
+  return { categories: [...defaultCategories, ...categories] } as ViewModel
+}
+
+class MetacardInteractions extends React.Component<Props> {
+  componentDidMount = () => {
+    appendCssIfNeeded(this.props.model, this.props.el)
+
+    const setState = (model: Model) => this.setState({ model: model })
+
+    const toggleIsBlacklisted = () =>
+      this.props.el.toggleClass(
+        'is-blacklisted',
+        isBlacklisted(this.props.model)
+      )
+
+    this.props.listenTo(
+      this.props.model,
+      'change:metacard>properties',
+      setState
+    )
+    this.props.listenTo(
+      user
+        .get('user')
+        .get('preferences')
+        .get('resultBlacklist'),
+      'add remove update reset',
+      toggleIsBlacklisted
+    )
+  }
+
+  render = () => (
+    <>
+      <MarionetteRegionContainer
+        data-help="Add the result to a list."
+        className="metacard-interaction interaction-add"
+        view={createAddRemoveRegion(this.props.model)}
+        viewOptions={{ model: this.props.model }}
+      />
+      <View
+        handleCreateSearch={() =>
+          withCloseDropdown(this.props, handleCreateSearch)
+        }
+        handleDownload={() => withCloseDropdown(this.props, handleDownload)}
+        isRemoteResourceCached={isRemoteResourceCached(this.props.model)}
+        withCloseDropdown={handler => withCloseDropdown(this.props, handler)}
+        viewModel={viewModelFromProps(this.props)}
+      />
+      <MarionetteRegionContainer
+        data-help="Opens the available actions for the item."
+        className="metacard-interaction interaction-actions-export composed-menu"
+        view={createResultActionsExportRegion(this.props.model)}
+        viewOptions={{ model: this.props.model }}
+      />
+      {this.props.extensions && (
+        <MarionetteRegionContainer
+          className="composed-menu interaction-extensions"
+          view={this.props.extensions}
+          viewOptions={{ mode: this.props.model }}
+        />
+      )}
+    </>
+  )
+}
+
+export default withListenTo(MetacardInteractions)
