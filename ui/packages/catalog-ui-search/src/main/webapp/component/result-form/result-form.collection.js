98b22f6bf8229cc7bf2b4abbc559b61ac268ac5d
david.morales <david.morales@connexta.com>
2019-01-21 12:53:27 -0700

    DDF-4463 Fix result form sorting

--- ui/packages/catalog-ui-search/src/main/webapp/component/result-form/result-form.collection.js
+++ ui/packages/catalog-ui-search/src/main/webapp/component/result-form/result-form.collection.js
@@ -66,6 +66,21 @@ module.exports = Backbone.AssociatedModel.extend({
         model: ResultForm,
         url: './internal/forms/result',
         initialize: function() {},
+        comparator: function(a, b) {
+          // Keep the new result form button at the front
+          const typeA = a.get('type')
+          const typeB = b.get('type')
+          if (typeA === 'new-result' && typeB !== 'new-result') {
+            return -1
+          }
+          if (typeA !== 'new-result' && typeB === 'new-result') {
+            return 1
+          }
+
+          const titleA = a.get('title') || ''
+          const titleB = b.get('title') || ''
+          return titleA.toLowerCase().localeCompare(titleB.toLowerCase())
+        },
       }),
     },
   ],
@@ -107,6 +122,7 @@ module.exports = Backbone.AssociatedModel.extend({
           )
         }.bind(this)
       )
+      this.get('resultForms').sort()
     }
   },
   addResultForm: function(newForm) {
