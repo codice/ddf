d51e1fb14d4de670a1dfa6640f4702d9e44b648c
Zachary Adam <zta6@pitt.edu>
2019-01-22 16:16:07 -0700

    add rounding to upload editor

--- ui/packages/catalog-ui-search/src/main/webapp/component/location-new/geo-components/wkt.js
+++ ui/packages/catalog-ui-search/src/main/webapp/component/location-new/geo-components/wkt.js
@@ -2,6 +2,8 @@ const React = require('react')
 const TextField = require('../../../react-component/text-field/index.js')
 const { Units } = require('../../../react-component/location/common.js')
 
+const { validateWkt } = require('../utils')
+
 const WKT = props => {
   const { wkt, setState } = props
 
@@ -9,10 +11,19 @@ const WKT = props => {
     <div className="input-location">
       <TextField
         value={wkt}
-        onChange={setState((draft, value) => (draft.wkt = value))}
+        onChange={setState(validateInput((draft, value) => (draft.wkt = value)))}
       />
     </div>
   )
 }
 
+function validateInput(draft, value) {
+  let result = validateWkt(value)
+  if (result.valid) {
+    return value
+  } else {
+    console.log(value)
+    return value
+  }
+}
 module.exports = WKT
