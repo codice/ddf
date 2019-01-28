cdd69d82fab829e1f7a39d52b874baae365a7226
cantstoptheunk <ryn10sng@gmail.com>
2019-01-28 11:57:22 -0700

    EMPTY search now deletes the text input area

--- ui/packages/catalog-ui-search/src/main/webapp/component/filter-comparator/filter-comparator.view.js
+++ ui/packages/catalog-ui-search/src/main/webapp/component/filter-comparator/filter-comparator.view.js
@@ -52,6 +52,9 @@ module.exports = Marionette.ItemView.extend({
     var value = $(e.currentTarget).attr('data-value')
     this.model.set('comparator', value)
     this.$el.trigger('closeDropdown.' + CustomElements.getNamespace())
+    if(this.model.attributes.comparator === 'EMPTY'){
+      $('.if-editing').toggle()
+    }
   },
   serializeData: function() {
     switch (metacardDefinitions.metacardTypes[this.model.get('type')].type) {
