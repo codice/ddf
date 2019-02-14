f5cee485c63d9499aa00e1cd2a14aa252bdda320
brjeter <brjeter@users.noreply.github.com>
2019-02-14 09:29:14 -0800

    DDF-04318 Check that the user is logged in for every Intrigue WebSocket request (#4368)
    * DDF-04318 Change WebSocketServlet to check the user's token at each request
    
    * DDF-04318 Reorder/cleanup JsonRpc methods
    
    * DDF-04318 Fix the JsonRpc implementation for handling errors
    
    * DDF-04318 Addressing comments

--- catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/ws/JsonRpc.java
+++ catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/ws/JsonRpc.java
@@ -15,11 +15,13 @@ package org.codice.ddf.catalog.ui.ws;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import ddf.security.common.audit.SecurityLogger;
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import org.codice.gsonsupport.GsonTypeAdapters.DateLongFormatTypeAdapter;
 import org.codice.gsonsupport.GsonTypeAdapters.LongDoubleTypeAdapter;
@@ -35,9 +37,11 @@ public class JsonRpc implements WebSocket {
   public static final int METHOD_NOT_FOUND = -32601;
   public static final int INVALID_PARAMS = 32602;
   public static final int INTERNAL_ERROR = -32603;
+  public static final int NOT_LOGGED_IN_ERROR = -32000;
 
   private static final String JSON_RPC = "jsonrpc";
   private static final String METHOD = "method";
+  private static final String ID = "id";
 
   private static final Gson GSON =
       new GsonBuilder()
@@ -57,14 +61,18 @@ public class JsonRpc implements WebSocket {
     return new Error(code, message);
   }
 
-  public static Error error(int code, String message, Object data) {
+  public static Error invalidParams(String message, Object params) {
+    return error(INVALID_PARAMS, String.format("Invalid parameters: %s", message), params);
+  }
+
+  private static Error error(int code, String message, Object data) {
     return new Error(code, message, data);
   }
 
   private static Map<String, Object> response(Object id, Object value) {
     Map<String, Object> response = new HashMap<>();
     response.put(JSON_RPC, VERSION);
-    response.put("id", id);
+    response.put(ID, id);
     if (value instanceof Error) {
       response.put("error", value);
     } else {
@@ -73,16 +81,12 @@ public class JsonRpc implements WebSocket {
     return response;
   }
 
-  public static Error invalid(String message) {
+  private static Error invalid(String message) {
     return invalid(message, null);
   }
 
-  public static Error invalid(String message, Object data) {
+  private static Error invalid(String message, Object data) {
     return error(INVALID_REQUEST, String.format("Invalid Request: %s", message), data);
-  }
-
-  public static Error invalidParams(String message, Object params) {
-    return error(INVALID_PARAMS, String.format("Invalid params: %s", message), params);
   }
 
   @Override
@@ -97,83 +101,127 @@ public class JsonRpc implements WebSocket {
 
   @Override
   public void onError(Session session, Throwable ex) {
-    // no action required on error
-  }
-
-  private Object exec(Map msg) {
-    if (!msg.containsKey("id")) {
-      return response(null, invalid("required key `id` missing"));
+    if (ex instanceof WebSocketAuthenticationException) {
+      SecurityLogger.audit("Received WebSockets request for user that is not logged in.");
+      handleMessage(
+          session,
+          ((WebSocketAuthenticationException) ex).getWsMessage(),
+          (message, id) -> error(NOT_LOGGED_IN_ERROR, ex.getMessage()));
+    } else {
+      // no action required
     }
-
-    Object id = msg.get("id");
-
-    if (!(id instanceof String || id instanceof Number || id == null)) {
-      return response(null, invalid("key `id` not string or number or null", id));
-    }
-
-    if (!msg.containsKey(JSON_RPC)) {
-      return response(id, invalid("required key `jsonrpc` missing"));
-    }
-
-    if (!VERSION.equals(msg.get(JSON_RPC))) {
-      return response(id, invalid("key `jsonrpc` not equal to `2.0`", msg.get(JSON_RPC)));
-    }
-
-    if (!msg.containsKey(METHOD)) {
-      return response(id, invalid("required key `method` missing"));
-    }
-
-    if (!(msg.get(METHOD) instanceof String)) {
-      return response(id, invalid("key `method` not string", msg.get(METHOD)));
-    }
-
-    String method = (String) msg.get(METHOD);
-
-    if (!methods.containsKey(method)) {
-      return response(id, error(METHOD_NOT_FOUND, String.format("method `%s` not found", method)));
-    }
-
-    Object params = msg.get("params");
-
-    if (params != null && !(params instanceof List || params instanceof Map)) {
-      return response(id, invalidParams("parameters must be a structured value", params));
-    }
-
-    try {
-      return response(id, methods.get(method).apply(params));
-    } catch (RuntimeException e) {
-      return response(id, JsonRpc.error(INTERNAL_ERROR, "Internal Error"));
-    }
-  }
-
-  private Object handleMessage(String message) {
-    Map parsed;
-
-    try {
-      parsed = GSON.fromJson(message, Map.class);
-      Map<String, Object> numberTypeFix = (Map<String, Object>) parsed;
-      if (numberTypeFix.containsKey("id")) {
-        Object id = numberTypeFix.get("id");
-        if (id instanceof Number) {
-          numberTypeFix.put("id", ((Number) id).intValue());
-        }
-      }
-    } catch (RuntimeException ex) {
-      return response(null, error(PARSE_ERROR, "Parse error", message));
-    }
-
-    return exec(parsed);
   }
 
   @Override
   public void onMessage(Session session, String message) throws IOException {
-    session.getRemote().sendStringByFuture(GSON.toJson(handleMessage(message)));
+    handleMessage(session, message, this::callMethod);
+  }
+
+  private void handleMessage(
+      Session session, String message, BiFunction<Map, Object, Object> handleFunc) {
+    Object id;
+    Object result;
+
+    try {
+      Map messageMap = parseMessage(message);
+      id = parseId(messageMap);
+      validateJsonRpcVersion(messageMap, id);
+      result = handleFunc.apply(messageMap, id);
+    } catch (JsonRpcException exception) {
+      id = exception.messageId;
+      result = exception.error;
+    }
+
+    session.getRemote().sendStringByFuture(GSON.toJson(response(id, result)));
+  }
+
+  private Map parseMessage(String message) throws JsonRpcException {
+    Map parsed;
+
+    try {
+      parsed = GSON.fromJson(message, Map.class);
+    } catch (RuntimeException ex) {
+      throw new JsonRpcException(null, error(PARSE_ERROR, "Parse error", message));
+    }
+
+    return parsed;
+  }
+
+  private Object parseId(Map message) throws JsonRpcException {
+    if (!message.containsKey(ID)) {
+      throw new JsonRpcException(null, invalid(String.format("required key `%s` missing", ID)));
+    }
+
+    Object id = message.get(ID);
+
+    if (!(id instanceof String || id instanceof Number || id == null)) {
+      throw new JsonRpcException(
+          null, invalid(String.format("key `%s` not string or number or null", ID), id));
+    } else {
+      return id;
+    }
+  }
+
+  private void validateJsonRpcVersion(Map message, Object id) throws JsonRpcException {
+    if (!message.containsKey(JSON_RPC)) {
+      throw new JsonRpcException(id, invalid(String.format("required key `%s` missing", JSON_RPC)));
+    }
+
+    if (!VERSION.equals(message.get(JSON_RPC))) {
+      throw new JsonRpcException(
+          id,
+          invalid(
+              String.format("key `%s` not equal to `%s`", JSON_RPC, VERSION),
+              message.get(JSON_RPC)));
+    }
+  }
+
+  private Object callMethod(Map message, Object id) throws JsonRpcException {
+    if (!message.containsKey(METHOD)) {
+      throw new JsonRpcException(id, invalid(String.format("required key `%s` missing", METHOD)));
+    }
+
+    if (!(message.get(METHOD) instanceof String)) {
+      throw new JsonRpcException(
+          id, invalid(String.format("key `%s` not string", METHOD), message.get(METHOD)));
+    }
+
+    String method = (String) message.get(METHOD);
+
+    if (!methods.containsKey(method)) {
+      throw new JsonRpcException(
+          id, error(METHOD_NOT_FOUND, String.format("method `%s` not found", method)));
+    }
+
+    Object params = message.get("params");
+
+    if (params != null && !(params instanceof List || params instanceof Map)) {
+      throw new JsonRpcException(
+          id, invalidParams("parameters must be a structured value", params));
+    }
+
+    try {
+      return methods.get(method).apply(params);
+    } catch (RuntimeException e) {
+      throw new JsonRpcException(id, error(INTERNAL_ERROR, "Internal Error"));
+    }
+  }
+
+  private static class JsonRpcException extends RuntimeException {
+    private final Object messageId;
+    private final Error error;
+
+    private JsonRpcException(Object messageId, Error error) {
+      this.messageId = messageId;
+      this.error = error;
+    }
   }
 
   private static class Error {
-    public final int code;
-    public final String message;
-    public final Object data;
+    private final int code;
+    private final String message;
+    private final Object data;
+    private Object id;
 
     private Error(int code, String message) {
       this(code, message, null);
