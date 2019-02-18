0bbfcf10f7d6bdba7513d2e796fd8821dadc38e5
cantstoptheunk <ryn10sng@gmail.com>
2019-02-18 08:04:27 -0700

    DDF-04398 Added new CsvMetacardTransformer and blueprint (currently able to export a .csv file)

---
+++ catalog/transformer/catalog-transformer-csv-queryresponse/src/main/java/ddf/catalog/transformer/csv/CsvMetacardTransformer.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) Codice Foundation
+ * <p/>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package ddf.catalog.transformer.csv;
+
+import static ddf.catalog.transformer.csv.common.CsvTransformer.createResponse;
+import static ddf.catalog.transformer.csv.common.CsvTransformer.writeMetacardsToCsv;
+
+import ddf.catalog.data.BinaryContent;
+import ddf.catalog.data.Metacard;
+import ddf.catalog.transform.CatalogTransformerException;
+import ddf.catalog.transform.MetacardTransformer;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CsvMetacardTransformer implements MetacardTransformer {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(CsvMetacardTransformer.class);
+
+  @Override
+  public BinaryContent transform(Metacard metacard, Map<String, Serializable> arguments)
+      throws CatalogTransformerException {
+
+    if (metacard == null) {
+      LOGGER.debug("Attempted to transform null metacard");
+      throw new CatalogTransformerException("Unable to transform null metacard");
+    }
+
+    Map<String, String> aliases =
+        (Map<String, String>) arguments.getOrDefault("aliases", new HashMap<>());
+
+    Appendable appendable =
+        writeMetacardsToCsv(
+            Collections.singletonList(metacard),
+            new ArrayList<>(metacard.getMetacardType().getAttributeDescriptors()),
+            aliases);
+    return createResponse(appendable);
+  }
+}
