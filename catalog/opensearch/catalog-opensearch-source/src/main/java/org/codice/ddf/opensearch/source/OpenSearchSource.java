f64cced65e75e5adaeb4e47eb6efa983183c7a4b
Ryan Machado <37838624+rymach@users.noreply.github.com>
2019-03-05 14:55:49 -0500

    DDF-04284 Add poll interval configuration option for OpenSearch sources

--- catalog/opensearch/catalog-opensearch-source/src/main/java/org/codice/ddf/opensearch/source/OpenSearchSource.java
+++ catalog/opensearch/catalog-opensearch-source/src/main/java/org/codice/ddf/opensearch/source/OpenSearchSource.java
@@ -277,7 +277,7 @@ public class OpenSearchSource implements FederatedSource, ConfiguredService {
             isAvailable = availabilityCheck();
           }
         },
-        0,
+        1,
         pollInterval,
         TimeUnit.MINUTES);
   }
