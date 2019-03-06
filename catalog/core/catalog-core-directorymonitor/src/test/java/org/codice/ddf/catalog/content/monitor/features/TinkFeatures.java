e678149e51d150779522d35a6a514c0f13317ec0
Jacob Beyner <bakejeyner@gmail.com>
2019-03-06 14:46:12 -0700

    DDF-4457 Fix component / i tests

---
+++ catalog/core/catalog-core-directorymonitor/src/test/java/org/codice/ddf/catalog/content/monitor/features/TinkFeatures.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.codice.ddf.catalog.content.monitor.features;
+
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
+
+import org.ops4j.pax.exam.Option;
+
+public class TinkFeatures {
+  public static Option start(String... features) {
+    return features(
+        maven()
+            .groupId("ddf.features")
+            .artifactId("security")
+            .version(System.getProperty("component.version")) // equal to ${project.version}
+            .classifier("features")
+            .type("xml"),
+        features);
+  }
+}
