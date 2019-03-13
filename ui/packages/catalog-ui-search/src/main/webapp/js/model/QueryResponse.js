3905135566cdefab56d12b5635d8ba6a25f73322
Brandan Jeter <brandanjeter@gmail.com>
2019-03-13 11:03:23 -0700

    DDF-04318 Fix the error handling in Intrigue WebSockets

--- ui/packages/catalog-ui-search/src/main/webapp/js/model/QueryResponse.js
+++ ui/packages/catalog-ui-search/src/main/webapp/js/model/QueryResponse.js
@@ -139,13 +139,22 @@ module.exports = Backbone.AssociatedModel.extend({
             handled = true
             res.options = options
             switch (res.code) {
+              case 400:
+              case 404:
+              case 500:
+                options.error({
+                  responseJSON: res,
+                })
+                break
               case -32000:
                 if (rpc !== null) {
                   rpc.close()
                   rpc = null
                 }
                 options.error({
-                  message: 'User not logged in.',
+                  responseJSON: {
+                    message: 'User not logged in.',
+                  },
                 })
                 break
               default:
