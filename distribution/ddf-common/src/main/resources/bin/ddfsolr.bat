193c64223a96125c7e4ba13695a3f19c00e1a4b4
Aaron Hoffer <github@hoffer.org>
2019-03-19 12:25:01 -0700

    DDF-04556 Generate Solr stop key  (#4563)
    * DDF-04415 Solr stop key for windows (#4433)
    
    * DDF-04556 Solr stop key
    
    * DDF-04415 Solr stop key for windows (#4433)
    
    * DDF-04556 Generate a Solr stopkey
    
    * DDF-04556 Generate Solr stopkey
     Port to master branch
    
    * DDF-04556 Generate Solr stopkey
     Cherry pick windows fixes

--- distribution/ddf-common/src/main/resources/bin/ddfsolr.bat
+++ distribution/ddf-common/src/main/resources/bin/ddfsolr.bat
@@ -1,17 +1,20 @@
 @ECHO OFF
 SETLOCAL enabledelayedexpansion
+
 SET COMMAND=%1
 SET DIRNAME=%~dp0%
 PUSHD %DIRNAME%..
 SET DDF_HOME=%CD%
 SET GET_PROPERTY=%DIRNAME%get_property.bat
+SET MAKE_KEY=%DIRNAME%makekey.bat
 SET SOLR_EXEC=%DDF_HOME%\solr\bin\solr.cmd
+SET STOPKEY_FILE=%DIRNAME%STOPKEY
 CALL %GET_PROPERTY% solr.http.port
 CALL %GET_PROPERTY% solr.http.protocol
 CALL %GET_PROPERTY% solr.mem 2g
 
 IF NOT "!solr.http.protocol!"=="http" IF NOT "!solr.http.protocol!"=="https" (
-    ECHO Unkown Solr protocol %solr.http.protocol% found in custom.system.properties file
+    ECHO Unknown Solr protocol %solr.http.protocol% found in custom.system.properties file
     ECHO Expected 'http' or 'https' - Setting to the default https
     SET solr.http.protocol=https
 )
@@ -35,17 +38,24 @@ IF "!solr.http.protocol!"=="https" (
 IF "%COMMAND%"=="" ECHO Missing command. Use start, restart, stop.
 
 IF "%COMMAND%"=="start" (
+    CALL :GEN_STOPKEY
     IF "!solr.http.protocol!"=="http" ECHO **** USING INSECURE SOLR CONFIGURATION ****
     CALL %SOLR_EXEC% start -p !solr.http.port! -m !solr.mem!
 )
 
-IF "%COMMAND%"=="restart" (
-    IF "!solr.http.protocol!"=="http" ECHO **** USING INSECURE SOLR CONFIGURATION ****
-    CALL %SOLR_EXEC% restart -p !solr.http.port! -m !solr.mem!
+
+IF "%COMMAND%"=="stop" (
+   SET /P STOP_KEY=<%STOPKEY_FILE%
+   CALL %SOLR_EXEC% stop -p !solr.http.port!
+   DEL %STOPKEY_FILE%
 )
 
-IF "%COMMAND%"=="stop" CALL %SOLR_EXEC% stop -p !solr.http.port!
+EXIT /B
 
+:GEN_STOPKEY
+REM Generate a random stop key for Solr and write it to a file
+CALL %MAKE_KEY% > %STOPKEY_FILE%
+SET /P STOP_KEY=<%STOPKEY_FILE%
 EXIT /B
 
 ENDLOCAL
