22359fcd90f34ba01fa44d27b93eb82134588a58
Joseph North <joernorth@gmail.com>
2019-03-20 15:39:57 -0700

    DDF-4652 Add coordinate order handling to HandlebarsWfsFeatureTransformer
    Previously, HandlebarsWfsFeatureTransformer would always reverse the
    coordinate order of every spatial feature when transforming them to WKT
    strings even if the coordinates were already in lon/lat order.
    
    This commit adds the ability to configure each
    HandlebarsWfsFeatureTransformer's feature coordinate order, that is, the
    coordinate order of its corresponding feature's spatial properties. The
    coordinate order will only be reversed if the feature coordinate order
    is lat/lon, since DDF expects WKT strings to be in lon/lat order.

---
+++ catalog/spatial/wfs/spatial-wfs-featuretransformer-handlebars/src/main/java/org/codice/ddf/spatial/ogc/wfs/transformer/handlebars/CoordinateOrderTransformer.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.codice.ddf.spatial.ogc.wfs.transformer.handlebars;
+
+import com.vividsolutions.jts.geom.CoordinateSequence;
+import com.vividsolutions.jts.geom.CoordinateSequenceFilter;
+
+public class CoordinateOrderTransformer implements CoordinateSequenceFilter {
+  @Override
+  public void filter(final CoordinateSequence coordinateSequence, final int i) {
+    final double x = coordinateSequence.getOrdinate(i, 0);
+    final double y = coordinateSequence.getOrdinate(i, 1);
+    coordinateSequence.setOrdinate(i, 0, y);
+    coordinateSequence.setOrdinate(i, 1, x);
+  }
+
+  @Override
+  public boolean isDone() {
+    return false;
+  }
+
+  @Override
+  public boolean isGeometryChanged() {
+    return true;
+  }
+}
