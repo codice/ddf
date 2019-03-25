5df70623f870a66d3d35321c817a1cfb99e7d5fa
andrewzimmer <39923178+andrewzimmer@users.noreply.github.com>
2019-03-25 15:51:25 -0600

    DDF-04522 migrate metacard-quality to react (#4523)

--- ui/packages/catalog-ui-search/src/main/webapp/component/tabs/metacard/tabs-metacard.js
+++ ui/packages/catalog-ui-search/src/main/webapp/component/tabs/metacard/tabs-metacard.js
@@ -16,12 +16,12 @@ const MetacardBasicView = require('../../editor/metacard-basic/metacard-basic.vi
 const MetacardAdvancedView = require('../../editor/metacard-advanced/metacard-advanced.view.js')
 const MetacardHistoryView = require('../../metacard-history/metacard-history.view.js')
 const MetacardAssociationsView = require('../../metacard-associations/metacard-associations.view.js')
-const MetacardQualityView = require('../../metacard-quality/metacard-quality.view.js')
 const MetacardPreviewView = require('../../metacard-preview/metacard-preview.view.js')
 import React from 'react'
 import MetacardOverwrite from '../../metacard-overwrite/metacard-overwrite.view.js'
 import MetacardArchive from '../../../react-component/container/metacard-archive'
 import MetacardActions from '../../../react-component/container/metacard-actions'
+import MetacardQuality from '../../../react-component/container/metacard-quality'
 
 const MetacardOverwriteView = Marionette.LayoutView.extend({
   template: () => <MetacardOverwrite />,
@@ -39,6 +39,14 @@ const MetacardActionsView = Marionette.LayoutView.extend({
   template() {
     return (
       <MetacardActions selectionInterface={this.options.selectionInterface} />
+    )
+  },
+})
+
+const MetacardQualityView = Marionette.LayoutView.extend({
+  template() {
+    return (
+      <MetacardQuality selectionInterface={this.options.selectionInterface} />
     )
   },
 })
