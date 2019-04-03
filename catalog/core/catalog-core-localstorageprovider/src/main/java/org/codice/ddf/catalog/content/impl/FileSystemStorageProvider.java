codice/ddf

commit be04274e3bd0d3a99839b487123fecc2dbb70f82

[2.13.x] DDF-4457 Encrypt file system storage (#4502)
Updated the FileSystemStorageProvider to encrypt the content it writes to disk.

catalog/core/catalog-core-localstorageprovider/src/main/java/org/codice/ddf/catalog/content/impl/FileSystemStorageProvider.java
@@ -13,7 +13,9 @@
  */
 package org.codice.ddf.catalog.content.impl;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.io.ByteSource;
+import com.google.common.io.FileBackedOutputStream;
 import ddf.catalog.Constants;
 import ddf.catalog.content.StorageException;
 import ddf.catalog.content.StorageProvider;
@@ -37,6 +39,8 @@ import ddf.catalog.data.Metacard;
 import ddf.catalog.data.impl.AttributeImpl;
 import ddf.mime.MimeTypeMapper;
 import ddf.mime.MimeTypeResolutionException;
+import ddf.security.encryption.crypter.Crypter;
+import ddf.security.encryption.crypter.Crypter.CrypterException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -50,6 +54,8 @@ import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -59,6 +65,7 @@ import java.util.stream.Collectors;
 import javax.activation.MimeType;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -78,6 +85,8 @@ public class FileSystemStorageProvider implements StorageProvider {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(FileSystemStorageProvider.class);
 
+  @VisibleForTesting static final String CRYPTER_NAME = "file-system";
+
   public static final String REF_EXT = "external-reference";
 
   /** Mapper for file extensions-to-mime types (and vice versa) */
@@ -92,9 +101,13 @@ public class FileSystemStorageProvider implements StorageProvider {
 
   private Map<String, Set<String>> updateMap = new ConcurrentHashMap<>();
 
+  private Crypter crypter;
+
   /** Default constructor, invoked by blueprint. */
   public FileSystemStorageProvider() {
     LOGGER.debug("File System Provider initializing...");
+    crypter =
+        AccessController.doPrivileged((PrivilegedAction<Crypter>) () -> new Crypter(CRYPTER_NAME));
   }
 
   @Override
@@ -388,88 +401,137 @@ public class FileSystemStorageProvider implements StorageProvider {
   }
 
   private ContentItem readContent(URI uri) throws StorageException {
-    Path file = getContentFilePath(uri);
+    Path path = getContentFilePath(uri);
 
-    if (file == null) {
+    if (path == null) {
       throw new StorageException(
           "Unable to find file for content ID: " + uri.getSchemeSpecificPart());
     }
 
-    String filename = file.getFileName().toString();
-    String extension = FilenameUtils.getExtension(filename);
-    URI reference = null;
-    // if the file is an external reference, remove the reference extension
-    // so we can get the real extension
-    if (REF_EXT.equals(extension)) {
-      extension = FilenameUtils.getExtension(FilenameUtils.removeExtension(filename));
-      try {
-        reference = new URI(new String(Files.readAllBytes(file), Charset.forName("UTF-8")));
+    String filename = path.getFileName().toString();
 
-        if (reference.getScheme() == null) {
-          file = Paths.get(reference.toASCIIString());
-        } else if (reference.getScheme().equalsIgnoreCase("file")) {
-          file = Paths.get(reference);
-        } else {
-          file = null;
-        }
-      } catch (IOException | URISyntaxException e) {
-        throw new StorageException(e);
+    // resolve external reference if necessary, determine the extension, and retrieve an
+    // InputStream to the content
+    InputStream contentInputStream;
+    String extension;
+
+    try {
+      if (REF_EXT.equals(FilenameUtils.getExtension(filename))) {
+        // remove the external reference extension so we can get the real extension
+        extension =
+            FilenameUtils.getExtension(
+                FilenameUtils.removeExtension(path.getFileName().toString()));
+        contentInputStream = getInputStreamFromReference(path);
+      } else {
+        extension = FilenameUtils.getExtension(path.getFileName().toString());
+        contentInputStream = getInputStreamFromResource(path);
       }
+    } catch (IOException e) {
+      throw new StorageException(
+          String.format("Unable to resolve InputStream given URI of %s", uri), e);
     }
 
-    if (reference != null && file != null && !file.toFile().exists()) {
-      throw new StorageException("Cannot read " + uri + ".");
-    }
+    // decrypt the content and return as a ByteSource
+    ByteSource byteSource = decryptStream(contentInputStream);
 
-    String mimeType = DEFAULT_MIME_TYPE;
+    // determine the size of the content
     long size = 0;
-    ByteSource byteSource;
 
-    try (InputStream fileInputStream =
-        file != null ? Files.newInputStream(file) : reference.toURL().openStream()) {
-      mimeType = mimeTypeMapper.guessMimeType(fileInputStream, extension);
-    } catch (MimeTypeResolutionException e) {
-      LOGGER.debug(
-          "Could not determine mime type for file extension = {}; defaulting to {}",
-          extension,
-          DEFAULT_MIME_TYPE);
-    } catch (IOException ie) {
-      LOGGER.debug(
-          "Error opening stream to external reference {}. Failing StorageProvider read.",
-          reference,
-          ie);
-      throw new StorageException("Cannot read " + reference + ".");
+    try {
+      size = byteSource.size();
+    } catch (IOException e) {
+      LOGGER.debug("Problem determining size of resource; defaulting to {}.", size, e);
     }
 
-    if (file != null) {
-      if (mimeType == null || DEFAULT_MIME_TYPE.equals(mimeType)) {
-        try {
-          mimeType = Files.probeContentType(file);
-        } catch (IOException e) {
-          LOGGER.info("Unable to determine mime type using Java Files service.", e);
-        }
-      }
-
-      LOGGER.debug("mimeType = {}", mimeType);
-      try {
-        size = Files.size(file);
-      } catch (IOException e) {
-        LOGGER.info("Unable to retrieve size of file: {}", file.toAbsolutePath().toString(), e);
-      }
-      byteSource = com.google.common.io.Files.asByteSource(file.toFile());
-    } else {
-      URI finalReference = reference;
-      byteSource =
-          new ByteSource() {
-            @Override
-            public InputStream openStream() throws IOException {
-              return finalReference.toURL().openStream();
-            }
-          };
-    }
+    // determine the MimeType of the content
+    String mimeType = determineMimeType(extension, path, byteSource);
 
     return new ContentItemImpl(
         uri.getSchemeSpecificPart(), uri.getFragment(), byteSource, mimeType, filename, size, null);
+  }
+
+  private InputStream getInputStreamFromReference(Path externalReferencePath) throws IOException {
+    URI reference;
+
+    try {
+      byte[] encryptedRefBytes = Files.readAllBytes(externalReferencePath);
+      String encryptedRefString = new String(encryptedRefBytes, Charset.forName("UTF-8"));
+
+      reference = new URI(crypter.decrypt(encryptedRefString));
+    } catch (IOException | URISyntaxException e) {
+      throw new IOException(e);
+    }
+
+    // try and represent the reference as a path
+    Path newPath = null;
+    if (reference.getScheme() == null) {
+      newPath = Paths.get(reference.toASCIIString());
+    } else if (reference.getScheme().equalsIgnoreCase("file")) {
+      newPath = Paths.get(reference);
+    }
+
+    // if the reference can be represented as a path
+    if (newPath != null) {
+      if (!newPath.toFile().exists()) {
+        throw new IOException("Cannot read " + reference + ".");
+      }
+      return getInputStreamFromResource(newPath);
+    }
+
+    return reference.toURL().openStream();
+  }
+
+  private InputStream getInputStreamFromResource(Path path) throws IOException {
+    return Files.newInputStream(path);
+  }
+
+  private String determineMimeType(String extension, Path path, ByteSource byteSource) {
+    String mimeType = DEFAULT_MIME_TYPE;
+
+    // guess MimeType
+    try {
+      mimeType = mimeTypeMapper.guessMimeType(byteSource.openStream(), extension);
+    } catch (IOException | MimeTypeResolutionException e) {
+      LOGGER.debug(
+          "Could not determine mime type for file extension = {}; defaulting to {}.",
+          extension,
+          mimeType);
+    }
+
+    // probe for MimeType
+    if (mimeType == null || DEFAULT_MIME_TYPE.equals(mimeType)) {
+      try {
+        mimeType = Files.probeContentType(path);
+      } catch (IOException e) {
+        LOGGER.debug(
+            "Could not determine mime type from file {}; defaulting to {}.", path, mimeType);
+      }
+    }
+
+    return mimeType;
+  }
+
+  private ByteSource decryptStream(InputStream contentInputStream) throws StorageException {
+    InputStream decryptedInputStream = null;
+    FileBackedOutputStream decryptedOutputStream = null;
+
+    try {
+      // do not use try with resources in order to have these InputStreams in the finally block
+      decryptedInputStream = crypter.decrypt(contentInputStream);
+      decryptedOutputStream = new FileBackedOutputStream(128);
+      IOUtils.copy(decryptedInputStream, decryptedOutputStream);
+    } catch (CrypterException | IOException e) {
+      LOGGER.debug(
+          "Error decrypting InputStream {}. Failing StorageProvider read.", contentInputStream, e);
+      throw new StorageException(
+          String.format("Cannot decrypt InputStream %s.", contentInputStream), e);
+    } finally {
+      // need to close both streams in order for IOUtils to copy properly
+      IOUtils.closeQuietly(decryptedInputStream);
+      IOUtils.closeQuietly(decryptedOutputStream);
+    }
+
+    return decryptedOutputStream.asByteSource();
   }
 
   private List<Path> listPaths(Path dir) throws IOException {
@@ -562,17 +624,17 @@ public class FileSystemStorageProvider implements StorageProvider {
       Files.createDirectories(contentDirectory);
     }
 
+    long copySize;
     Path contentItemPath =
         Paths.get(contentDirectory.toAbsolutePath().toString(), item.getFilename());
     ByteSource byteSource;
 
-    long copy;
-
     if (storeReference != null) {
-      copy = item.getSize();
+      copySize = item.getSize();
+      String encryptedReference = crypter.encrypt(storeReference);
       Files.write(
           Paths.get(contentItemPath.toString() + "." + REF_EXT),
-          storeReference.getBytes(Charset.forName("UTF-8")));
+          encryptedReference.getBytes(Charset.forName("UTF-8")));
       byteSource =
           new ByteSource() {
             @Override
@@ -581,17 +643,18 @@ public class FileSystemStorageProvider implements StorageProvider {
             }
           };
     } else {
-      try (InputStream inputStream = item.getInputStream()) {
-        copy = Files.copy(inputStream, contentItemPath);
+      try (InputStream plainInputStream = item.getInputStream();
+          InputStream encryptedInputStream = crypter.encrypt(plainInputStream)) {
+        copySize = Files.copy(encryptedInputStream, contentItemPath);
       }
       byteSource = com.google.common.io.Files.asByteSource(contentItemPath.toFile());
 
-      if (copy != item.getSize() && LOGGER.isWarnEnabled()) {
+      if (copySize < item.getSize() && LOGGER.isWarnEnabled()) {
         LOGGER.warn(
-            "Created content item {} size {} does not match expected size {} {}. "
+            "Created content item {} encrypted size {} is not greater than plain size {}.{}"
                 + "Verify filesystem and/or network integrity.",
             item.getId(),
-            copy,
+            copySize,
             item.getSize(),
             System.lineSeparator());
       }
@@ -604,7 +667,7 @@ public class FileSystemStorageProvider implements StorageProvider {
             byteSource,
             item.getMimeType().toString(),
             contentItemPath.getFileName().toString(),
-            copy,
+            copySize,
             item.getMetacard());
 
     LOGGER.trace("EXITING: generateContentFile");

platform/security/encryption/platform-security-encryption-crypter/src/main/java/ddf/security/encryption/crypter/Crypter.java
@@ -19,11 +19,18 @@ import com.google.crypto.tink.CleartextKeysetHandle;
 import com.google.crypto.tink.JsonKeysetReader;
 import com.google.crypto.tink.JsonKeysetWriter;
 import com.google.crypto.tink.KeysetHandle;
+import com.google.crypto.tink.StreamingAead;
 import com.google.crypto.tink.aead.AeadConfig;
 import com.google.crypto.tink.aead.AeadFactory;
 import com.google.crypto.tink.aead.AeadKeyTemplates;
+import com.google.crypto.tink.proto.KeyTemplate;
+import com.google.crypto.tink.streamingaead.StreamingAeadConfig;
+import com.google.crypto.tink.streamingaead.StreamingAeadFactory;
+import com.google.crypto.tink.streamingaead.StreamingAeadKeyTemplates;
 import ddf.security.SecurityConstants;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -45,8 +52,10 @@ import org.slf4j.LoggerFactory;
 public class Crypter {
   private static final Logger LOGGER = LoggerFactory.getLogger(Crypter.class);
   private static final String DEFAULT_KEYSET_FILE_NAME = "default";
+  private static final String STREAMING_KEYSET_FILE_SPECIFIER = "-streaming";
   private static final String KEYSET_FILE_EXTENSION = ".json";
   private static final int ASSOCIATED_DATA_BYTE_SIZE = 10;
+  @VisibleForTesting static final int CHUNK_SIZE = 128;
 
   private final String keysetDir =
       AccessController.doPrivileged(
@@ -57,8 +66,10 @@ public class Crypter {
               () -> System.getProperty(SecurityConstants.ASSOCIATED_DATA_PATH));
 
   @VisibleForTesting KeysetHandle keysetHandle;
+  @VisibleForTesting KeysetHandle streamingKeysetHandle;
   private byte[] associatedData;
   private Aead aead;
+  private StreamingAead streamingAead;
 
   /**
    * Read/create a specified keyset under the keyset directory.
@@ -68,18 +79,35 @@ public class Crypter {
    */
   public Crypter(String keysetFileName) throws CrypterException {
     String keysetLocation = Paths.get(keysetDir, keysetFileName + KEYSET_FILE_EXTENSION).toString();
+    String streamingKeysetLocation =
+        Paths.get(
+                keysetDir, keysetFileName + STREAMING_KEYSET_FILE_SPECIFIER + KEYSET_FILE_EXTENSION)
+            .toString();
 
     synchronized (Crypter.class) {
       File keysetFile = new File(keysetLocation);
+      File streamingKeysetFile = new File(streamingKeysetLocation);
 
       try {
         AeadConfig.register();
+        StreamingAeadConfig.register();
+
+        // aead keyset
         if (!keysetFile.exists()) {
-          keysetHandle = initKeysetHandle(keysetFile);
+          keysetHandle = initKeysetHandle(keysetFile, AeadKeyTemplates.AES128_GCM);
         } else {
           keysetHandle = readKeysetHandle(keysetFile);
         }
+        // streaming aead keyset
+        if (!streamingKeysetFile.exists()) {
+          streamingKeysetHandle =
+              initKeysetHandle(streamingKeysetFile, StreamingAeadKeyTemplates.AES128_GCM_HKDF_4KB);
+        } else {
+          streamingKeysetHandle = readKeysetHandle(streamingKeysetFile);
+        }
+
         aead = AeadFactory.getPrimitive(keysetHandle);
+        streamingAead = StreamingAeadFactory.getPrimitive(streamingKeysetHandle);
         associatedData = getAssociatedData();
       } catch (GeneralSecurityException | IOException e) {
         LOGGER.warn("Problem initializing keyset.");
@@ -95,6 +123,46 @@ public class Crypter {
   /**
    * Encrypts a plain text value using Tink.
    *
+   * @param plainBytes The value to encrypt.
+   */
+  public byte[] encrypt(byte[] plainBytes) throws CrypterException {
+    if (plainBytes == null || plainBytes.length < 1) {
+      throw new CrypterException("Bytes to encrypt cannot be null or empty.");
+    }
+    if (associatedData == null) {
+      throw new CrypterException("Associated data cannot be null.");
+    }
+
+    try {
+      return aead.encrypt(plainBytes, associatedData);
+    } catch (GeneralSecurityException e) {
+      throw new CrypterException("Problem encrypting.", e);
+    }
+  }
+
+  /**
+   * Decrypts a plain text value using Tink.
+   *
+   * @param encryptedBytes The value to decrypt.
+   */
+  public byte[] decrypt(byte[] encryptedBytes) throws CrypterException {
+    if (encryptedBytes == null || encryptedBytes.length < 1) {
+      throw new CrypterException("Bytes to decrypt cannot be null or empty.");
+    }
+    if (associatedData == null) {
+      throw new CrypterException("Associated data cannot be null.");
+    }
+
+    try {
+      return aead.decrypt(encryptedBytes, associatedData);
+    } catch (GeneralSecurityException | NullPointerException e) {
+      throw new CrypterException("Problem decrypting.", e);
+    }
+  }
+
+  /**
+   * Encrypts a plain text value using Tink.
+   *
    * @param plainTextValue The value to encrypt.
    */
   public String encrypt(String plainTextValue) throws CrypterException {
@@ -104,11 +172,11 @@ public class Crypter {
     if (associatedData == null) {
       throw new CrypterException("Associated data cannot be null.");
     }
+
     try {
       byte[] encryptedBytes = aead.encrypt(plainTextValue.getBytes(), associatedData);
       return Base64.getEncoder().encodeToString(encryptedBytes);
     } catch (GeneralSecurityException e) {
-      LOGGER.debug("Problem encrypting.", e);
       throw new CrypterException("Problem encrypting.", e);
     }
   }
@@ -125,16 +193,85 @@ public class Crypter {
     if (associatedData == null) {
       throw new CrypterException("Associated data cannot be null.");
     }
+
     try {
       byte[] encryptedBytes = Base64.getDecoder().decode(encryptedValue);
       return new String(aead.decrypt(encryptedBytes, associatedData));
     } catch (GeneralSecurityException | NullPointerException e) {
-      LOGGER.debug("Problem decrypting.", e);
       throw new CrypterException("Problem decrypting.", e);
     }
   }
 
-  private KeysetHandle initKeysetHandle(File keysetFile)
+  /**
+   * Encrypts a plain InputStream using Tink.
+   *
+   * @param plainInputStream The InputStream to encrypt.
+   */
+  public InputStream encrypt(InputStream plainInputStream) throws CrypterException {
+    if (associatedData == null) {
+      throw new CrypterException("Associated data cannot be null.");
+    }
+    try {
+      if (plainInputStream == null || plainInputStream.available() < 1) {
+        throw new CrypterException("Plain InputStream cannot be null or empty.");
+      }
+    } catch (IOException e) {
+      throw new CrypterException("Problem reading data from plain InputStream.", e);
+    }
+
+    File tmpFile;
+    try {
+      tmpFile = File.createTempFile("encryption", ".tmp");
+    } catch (IOException e) {
+      throw new CrypterException("Unable to create temporary file for encryption.");
+    }
+
+    try (FileOutputStream fileOutputStream = new FileOutputStream(tmpFile);
+        OutputStream encryptedOutputStream =
+            streamingAead.newEncryptingStream(fileOutputStream, associatedData)) {
+      byte[] byteBuffer = new byte[CHUNK_SIZE];
+      while (plainInputStream.available() > 0) {
+        int bytesRead = plainInputStream.read(byteBuffer);
+        if (bytesRead < 1) {
+          throw new IOException(
+              String.format("Incorrect # of bytes read from plain InputStream: %d.", bytesRead));
+        }
+        encryptedOutputStream.write(byteBuffer);
+      }
+      encryptedOutputStream.close(); // need to close it here in order for it to flush
+      return new FileInputStream(tmpFile);
+    } catch (GeneralSecurityException | IOException e) {
+      throw new CrypterException("Problem encrypting.", e);
+    } finally {
+      tmpFile.deleteOnExit();
+    }
+  }
+
+  /**
+   * Decrypts an encrypted InputStream using Tink.
+   *
+   * @param encryptedInputStream The InputStream to decrypt.
+   */
+  public InputStream decrypt(InputStream encryptedInputStream) throws CrypterException {
+    if (associatedData == null) {
+      throw new CrypterException("Associated data cannot be null.");
+    }
+    try {
+      if (encryptedInputStream == null || encryptedInputStream.available() < 1) {
+        throw new CrypterException("Encrypted InputStream cannot be null or empty.");
+      }
+    } catch (IOException e) {
+      throw new CrypterException("Problem reading data from encrypted InputStream.", e);
+    }
+
+    try {
+      return streamingAead.newDecryptingStream(encryptedInputStream, associatedData);
+    } catch (GeneralSecurityException | IOException e) {
+      throw new CrypterException("Problem decrypting.", e);
+    }
+  }
+
+  private KeysetHandle initKeysetHandle(File keysetFile, KeyTemplate keyType)
       throws IOException, GeneralSecurityException {
 
     if (!keysetFile.getParentFile().exists()) {
@@ -142,15 +279,24 @@ public class Crypter {
           (PrivilegedAction<Boolean>) () -> keysetFile.getParentFile().mkdirs());
     }
 
+    KeysetHandle keysetHandle = KeysetHandle.generateNew(keyType);
+
     try (OutputStream keysetFileOutputStream = getKeysetOutputStream(keysetFile)) {
       if (keysetFileOutputStream == null) {
         throw new IOException("Problem opening keyset file output stream.");
       }
-      KeysetHandle keysetHandle = KeysetHandle.generateNew(AeadKeyTemplates.AES128_GCM);
       CleartextKeysetHandle.write(
           keysetHandle, JsonKeysetWriter.withOutputStream(keysetFileOutputStream));
-      return keysetHandle;
     }
+
+    try (OutputStream keysetFileOutputStream = getKeysetOutputStream(keysetFile)) {
+      if (keysetFileOutputStream == null) {
+        throw new IOException("Problem opening keyset file output stream.");
+      }
+      CleartextKeysetHandle.write(
+          keysetHandle, JsonKeysetWriter.withOutputStream(keysetFileOutputStream));
+    }
+    return keysetHandle;
   }
 
   private OutputStream getKeysetOutputStream(File keysetFile) {

catalog/core/catalog-core-directorymonitor/pom.xml
@@ -210,6 +210,12 @@
             <scope>test</scope>
         </dependency>
         <dependency>
+            <groupId>org.ops4j.pax.url</groupId>
+            <artifactId>pax-url-wrap</artifactId>
+            <version>${pax.url.version}</version>
+            <scope>test</scope>
+        </dependency>
+        <dependency>
             <groupId>javax.inject</groupId>
             <artifactId>javax.inject</artifactId>
             <version>1</version>
@@ -271,6 +277,14 @@
             <groupId>org.apache.camel.karaf</groupId>
             <artifactId>apache-camel</artifactId>
             <version>${camel.version}</version>
+            <classifier>features</classifier>
+            <type>xml</type>
+            <scope>test</scope>
+        </dependency>
+        <dependency>
+            <groupId>ddf.features</groupId>
+            <artifactId>test-utilities</artifactId>
+            <version>${project.version}</version>
             <classifier>features</classifier>
             <type>xml</type>
             <scope>test</scope>

catalog/core/catalog-core-directorymonitor/src/test/java/org/codice/ddf/catalog/content/monitor/ContentDirectoryMonitorIT.java
@@ -79,6 +79,7 @@ import org.codice.ddf.catalog.content.monitor.configurators.KeystoreTruststoreCo
 import org.codice.ddf.catalog.content.monitor.features.CamelFeatures;
 import org.codice.ddf.catalog.content.monitor.features.CxfFeatures;
 import org.codice.ddf.catalog.content.monitor.features.KarafSpringFeatures;
+import org.codice.ddf.catalog.content.monitor.features.TinkFeatures;
 import org.codice.ddf.catalog.content.monitor.util.BundleInfo;
 import org.codice.ddf.test.common.ComponentTestRunner;
 import org.codice.ddf.test.common.annotations.AfterExam;
@@ -246,6 +247,7 @@ public class ContentDirectoryMonitorIT extends AbstractComponentTest {
             "cxf-ws-security",
             "cxf-frontend-javascript",
             "cxf-jaxrs"),
+        TinkFeatures.start("tink-test-dependencies"),
         keystoreAndTruststoreConfig(),
         initContentDirectoryMonitorConfig(),
         streamBundle(

commit 9298793adb03f7c33a574d5bfaac942358d0c1e5

DDF-4457 Fixes file system encryption issues introduced by #4502 (#4607) - Fixes issues with encrypted input stream breaking downstream tests - Fixes issues with writing encrypted resources

catalog/core/catalog-core-localstorageprovider/src/main/java/org/codice/ddf/catalog/content/impl/FileSystemStorageProvider.java
@@ -41,6 +41,7 @@ import ddf.mime.MimeTypeMapper;
 import ddf.mime.MimeTypeResolutionException;
 import ddf.security.encryption.crypter.Crypter;
 import ddf.security.encryption.crypter.Crypter.CrypterException;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -624,13 +625,13 @@ public class FileSystemStorageProvider implements StorageProvider {
       Files.createDirectories(contentDirectory);
     }
 
+    long itemSize = item.getSize();
     long copySize;
     Path contentItemPath =
         Paths.get(contentDirectory.toAbsolutePath().toString(), item.getFilename());
     ByteSource byteSource;
 
     if (storeReference != null) {
-      copySize = item.getSize();
       String encryptedReference = crypter.encrypt(storeReference);
       Files.write(
           Paths.get(contentItemPath.toString() + "." + REF_EXT),
@@ -639,7 +640,8 @@ public class FileSystemStorageProvider implements StorageProvider {
           new ByteSource() {
             @Override
             public InputStream openStream() throws IOException {
-              return new URL(storeReference).openStream();
+              InputStream referenceInputStream = new URL(storeReference).openStream();
+              return crypter.decrypt(referenceInputStream);
             }
           };
     } else {
@@ -647,9 +649,16 @@ public class FileSystemStorageProvider implements StorageProvider {
           InputStream encryptedInputStream = crypter.encrypt(plainInputStream)) {
         copySize = Files.copy(encryptedInputStream, contentItemPath);
       }
-      byteSource = com.google.common.io.Files.asByteSource(contentItemPath.toFile());
+      byteSource =
+          new ByteSource() {
+            @Override
+            public InputStream openStream() throws IOException {
+              InputStream fileInputStream = new FileInputStream(contentItemPath.toFile());
+              return crypter.decrypt(fileInputStream);
+            }
+          };
 
-      if (copySize < item.getSize() && LOGGER.isWarnEnabled()) {
+      if (copySize < itemSize && LOGGER.isWarnEnabled()) {
         LOGGER.warn(
             "Created content item {} encrypted size {} is not greater than plain size {}.{}"
                 + "Verify filesystem and/or network integrity.",
@@ -667,7 +676,7 @@ public class FileSystemStorageProvider implements StorageProvider {
             byteSource,
             item.getMimeType().toString(),
             contentItemPath.getFileName().toString(),
-            copySize,
+            itemSize,
             item.getMetacard());
 
     LOGGER.trace("EXITING: generateContentFile");

platform/security/encryption/platform-security-encryption-crypter/src/main/java/ddf/security/encryption/crypter/Crypter.java
@@ -55,7 +55,7 @@ public class Crypter {
   private static final String STREAMING_KEYSET_FILE_SPECIFIER = "-streaming";
   private static final String KEYSET_FILE_EXTENSION = ".json";
   private static final int ASSOCIATED_DATA_BYTE_SIZE = 10;
-  @VisibleForTesting static final int CHUNK_SIZE = 128;
+  @VisibleForTesting static final int CHUNK_SIZE = 256;
 
   private final String keysetDir =
       AccessController.doPrivileged(
@@ -229,14 +229,18 @@ public class Crypter {
     try (FileOutputStream fileOutputStream = new FileOutputStream(tmpFile);
         OutputStream encryptedOutputStream =
             streamingAead.newEncryptingStream(fileOutputStream, associatedData)) {
+
       byte[] byteBuffer = new byte[CHUNK_SIZE];
-      while (plainInputStream.available() > 0) {
+      int availableBytes = getAvailableBytesLessThanChunkSize(plainInputStream);
+
+      while (availableBytes > 0) {
         int bytesRead = plainInputStream.read(byteBuffer);
         if (bytesRead < 1) {
           throw new IOException(
               String.format("Incorrect # of bytes read from plain InputStream: %d.", bytesRead));
         }
-        encryptedOutputStream.write(byteBuffer);
+        encryptedOutputStream.write(byteBuffer, 0, availableBytes);
+        availableBytes = getAvailableBytesLessThanChunkSize(plainInputStream);
       }
       encryptedOutputStream.close(); // need to close it here in order for it to flush
       return new FileInputStream(tmpFile);
@@ -269,6 +273,11 @@ public class Crypter {
     } catch (GeneralSecurityException | IOException e) {
       throw new CrypterException("Problem decrypting.", e);
     }
+  }
+
+  private int getAvailableBytesLessThanChunkSize(InputStream inputStream) throws IOException {
+    int available = inputStream.available();
+    return available > CHUNK_SIZE ? CHUNK_SIZE : available;
   }
 
   private KeysetHandle initKeysetHandle(File keysetFile, KeyTemplate keyType)