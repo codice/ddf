388bdbaefeb89e34bc2f9787a1f727d300a2f705
Aaron Hoffer <github@hoffer.org>
2019-04-09 10:41:14 -0700

    DDF-4570-sync Installer calls solr password updater directly   Added skeleton of new insecure default validator

---
+++ platform/admin/core/admin-core-insecuredefaults/src/main/java/org/codice/ddf/admin/insecure/defaults/service/SolrPasswordValidator.java
@@ -0,0 +1,11 @@
+package org.codice.ddf.admin.insecure.defaults.service;
+
+import java.util.List;
+
+public class SolrPasswordValidator implements Validator {
+
+  @Override
+  public List<Alert> validate() {
+    return null;
+  }
+}
