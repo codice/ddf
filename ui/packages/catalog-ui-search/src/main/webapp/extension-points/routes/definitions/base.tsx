569eb830d9a38c442e7350709f068857c095d621
Ryan Machado <37838624+rymach@users.noreply.github.com>
2019-04-23 19:24:50 -0400

    DDF-04746 format - add missing license to js files

--- ui/packages/catalog-ui-search/src/main/webapp/extension-points/routes/definitions/base.tsx
+++ ui/packages/catalog-ui-search/src/main/webapp/extension-points/routes/definitions/base.tsx
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ **/
 export const home = require('!./routes-loader!./base/home.js')
 export const newForm = require('!./routes-loader!./base/newForm.js')
 export const openAbout = require('!./routes-loader!./base/openAbout.js')
