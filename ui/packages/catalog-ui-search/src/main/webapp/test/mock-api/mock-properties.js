b06b098baeca9b417520d823e6676efcf2c3f0c3
Dimitry Sherman <1082205+dimitry-sherman@users.noreply.github.com>
2019-05-06 19:36:55 -0700

    DDF-04410 adds MapSetting unit tests, network call mocks(#4777)

---
+++ ui/packages/catalog-ui-search/src/main/webapp/test/mock-api/mock-properties.js
@@ -0,0 +1,26 @@
+const properties = require('../../js/properties')
+const api = require('./index')
+const oldInit = properties.init
+
+const mock = () => {
+  properties.init = function() {
+    const data = api('./internal/config')
+    const uiConfig = api('./internal/platform/config/ui')
+    // use this function to initialize variables that rely on others
+    var props = this
+    props = _.extend(props, data)
+    props.ui = uiConfig
+    this.handleEditing()
+    this.handleFeedback()
+    this.handleExperimental()
+    this.handleUpload()
+    this.handleListTemplates()
+  }
+  properties.init()
+}
+
+const unmock = () => {
+  properties.init = oldInit
+}
+
+export { mock, unmock }
