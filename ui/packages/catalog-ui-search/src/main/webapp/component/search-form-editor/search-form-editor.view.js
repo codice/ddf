0fc0d45a1194b27e96d62d7dc1b34312911ea949
Gabriel Vera <36056781+gjvera@users.noreply.github.com>
2019-05-16 15:36:30 -0700

    DDF-04834 Make Geos load in search form editor (#4835)

--- ui/packages/catalog-ui-search/src/main/webapp/component/search-form-editor/search-form-editor.view.js
+++ ui/packages/catalog-ui-search/src/main/webapp/component/search-form-editor/search-form-editor.view.js
@@ -106,8 +106,13 @@ module.exports = Marionette.LayoutView.extend({
     this.map.show(
       new MapView({
         selectionInterface: new SelectionInterface(),
+        onMapLoaded: olMap => {
+          this.showQueryForm(collection, id)
+        },
       })
     )
+  },
+  showQueryForm(collection, id) {
     this.editor.show(
       new QueryAdvanced({
         model: this.model,
