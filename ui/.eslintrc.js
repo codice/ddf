558f69658e41055591c0dafa1357699f8b485074
Ryan Machado <37838624+rymach@users.noreply.github.com>
2019-06-04 21:27:40 -0400

    DDF-04746 enable eslint recommended rules. rules we don't already follow have been disabled as needed. this change is still beneficial because it ensures we don't break any new rules and provides a list of rule violations we should resolve

---
+++ ui/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "extends": ["eslint:recommended", "plugin:react/recommended"],
+    rules: {
+        // TODO turn these recommended rules back on
+
+        // eslint:recommended
+        "no-extra-semi" : "off",
+        "no-undef" : "off",
+        "no-unused-vars" : "off",
+        "no-redeclare" : "off",
+        "no-inner-declarations" : "off",
+        "no-case-declarations" : "off",
+        "no-empty" : "off",
+        "no-dupe-keys" : "off",
+        "no-useless-escape" : "off",
+        "no-unreachable" : "off",
+        "no-fallthrough" : "off",
+
+        // plugin:react/recommended
+        "react/prop-types" : "off",
+        "react/display-name" : "off",
+        "react/no-unescaped-entities" : "off",
+        "react/jsx-key" : "off",
+        "react/no-render-return-value" : "off",
+        "react/no-deprecated" : "off",
+        "react/no-find-dom-node" : "off",
+    }
+};
