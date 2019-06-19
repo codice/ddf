6d92947e6ca6cbdf92789894451908e6dfaea17b
Blake Thomson <blake.thomson@connexta.com>
2019-06-19 15:15:19 -0700

    DDF-4982 Use declared font size

--- ui/packages/catalog-ui-search/src/main/webapp/component/search-form/search-form-presentation.js
+++ ui/packages/catalog-ui-search/src/main/webapp/component/search-form/search-form-presentation.js
@@ -2,7 +2,7 @@ import styled from '../../react-component/styles/styled-components'
 
 export const TabMessage = styled.div`
   text-align: center;
-  font-size: 20px;
+  font-size: ${props => props.theme.largeFontSize};
   padding: 15px;
   border: none !important;
 `
