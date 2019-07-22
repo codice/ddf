27e9c8cb699f4c91edf7f4ad40a831ea89b691b2
Ryan Machado <37838624+rymach@users.noreply.github.com>
2019-07-22 19:36:40 -0400

    DDF-04746 remove unused method

--- ui/packages/ui/src/main/webapp/components/features/features.view.js
+++ ui/packages/ui/src/main/webapp/components/features/features.view.js
@@ -65,19 +65,6 @@ define([
     focus: function() {
       this.$el.find('> .features-header input').focus()
     },
-    getFeatureView: function(options) {
-      if (options.collection && options.collection.length) {
-        // eslint-disable-next-line no-undef
-        return new FeaturesView(options)
-      }
-      // eslint-disable-next-line no-undef
-      return new EmptyView.view({
-        message:
-          'No features are available for the "' +
-          this.appName +
-          '" application.',
-      })
-    },
     onFeatureAction: function(model) {
       var self = this
       var status = model.get('status')
