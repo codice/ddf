371be411e959621a5171bb0220cee725bb7dcf2a
Adam Dimka <adam.dimka@connexta.com>
2019-07-24 14:18:22 -0700

    Added package root index.tsx

---
+++ ui/packages/catalog-ui-search/index.tsx
@@ -0,0 +1,7 @@
+import { default as MarionetteRegionContainer } from './src/main/webapp/react-component/marionette-region-container';
+import { default as LoadingCompanion } from './src/main/webapp/react-component/loading-companion'
+
+export {
+  MarionetteRegionContainer,
+  LoadingCompanion,
+}
