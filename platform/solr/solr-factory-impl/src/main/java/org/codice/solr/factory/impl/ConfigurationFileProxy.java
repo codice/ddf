bf58fede139fbc335593c99099e05d72e5036fae
Joseph North <joernorth@gmail.com>
2019-08-15 16:41:24 -0700

    DDF-5215 Upgrade Solr to 7.7.2
    - Remove the sole usage of StandardFilterFactory as it is a no-op and
      was deprecated in Solr 7.5.
    - Update docs to specify the correct Solr directories in which to place
      the jts-core and solr-xpath jars.
    - Update the ddf-solr Docker image to copy the jts-core jar to the
      correct Solr directory.
    - Update the solr-distro assembly file to copy the jts-core jar to the
      correct Solr directory.
    - Delete solrconfig-inmemory.xml as it is unused.

--- platform/solr/solr-factory-impl/src/main/java/org/codice/solr/factory/impl/ConfigurationFileProxy.java
+++ platform/solr/solr-factory-impl/src/main/java/org/codice/solr/factory/impl/ConfigurationFileProxy.java
@@ -52,7 +52,6 @@ public class ConfigurationFileProxy {
               "schema.xml",
               "solr.xml",
               "solrconfig.xml",
-              "solrconfig-inmemory.xml",
               "stopwords.txt",
               "stopwords_en.txt",
               "synonyms.txt"));
