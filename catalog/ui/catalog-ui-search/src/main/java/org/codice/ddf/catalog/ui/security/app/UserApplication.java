3fa0fafa6a7b778d4bbdbcb610595ae27fa1d855
Adam Brooks <adam.brooks@connexta.com>
2019-09-04 10:15:59 -0700

    DDF-5312 Read and write from notifications core

--- catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/security/app/UserApplication.java
+++ catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/security/app/UserApplication.java
@@ -22,11 +22,13 @@ import static spark.Spark.put;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import ddf.catalog.filter.FilterBuilder;
 import ddf.security.Subject;
 import ddf.security.SubjectIdentity;
 import ddf.security.SubjectUtils;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Collections;
 import java.util.HashMap;
@@ -34,6 +36,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 import org.apache.commons.lang.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.codice.ddf.catalog.ui.metacard.EntityTooLargeException;
@@ -44,6 +47,8 @@ import org.codice.ddf.persistence.PersistentItem;
 import org.codice.ddf.persistence.PersistentStore;
 import org.codice.ddf.persistence.PersistentStore.PersistenceType;
 import org.codice.gsonsupport.GsonTypeAdapters.LongDoubleTypeAdapter;
+import org.geotools.filter.text.ecql.ECQL;
+import org.opengis.filter.Filter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.servlet.SparkApplication;
@@ -51,6 +56,8 @@ import spark.servlet.SparkApplication;
 public class UserApplication implements SparkApplication {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(UserApplication.class);
+
+  private FilterBuilder filterBuilder;
 
   private static final Gson GSON =
       new GsonBuilder()
@@ -65,10 +72,14 @@ public class UserApplication implements SparkApplication {
   private final SubjectIdentity subjectIdentity;
 
   public UserApplication(
-      EndpointUtil util, PersistentStore persistentStore, SubjectIdentity subjectIdentity) {
+      EndpointUtil util,
+      PersistentStore persistentStore,
+      SubjectIdentity subjectIdentity,
+      FilterBuilder filterBuilder) {
     this.util = util;
     this.persistentStore = persistentStore;
     this.subjectIdentity = subjectIdentity;
+    this.filterBuilder = filterBuilder;
   }
 
   @Override
@@ -103,6 +114,26 @@ public class UserApplication implements SparkApplication {
           setUserPreferences(subject, preferences);
 
           return preferences;
+        },
+        util::getJson);
+
+    put(
+        "/user/notifications",
+        APPLICATION_JSON,
+        (req, res) -> {
+          Subject subject = (Subject) SecurityUtils.getSubject();
+
+          if (subject.isGuest()) {
+            res.status(401);
+            return ImmutableMap.of("message", "Guest cannot save notifications.");
+          }
+
+          Map<String, List<String>> notification =
+              GSON.fromJson(util.safeGetBody(req), MAP_STRING_TO_OBJECT_TYPE);
+
+          deleteNotifications(notification.get("alerts"));
+
+          return "";
         },
         util::getJson);
 
@@ -152,7 +183,10 @@ public class UserApplication implements SparkApplication {
       if (preferencesList.size() == 1) {
         byte[] json = (byte[]) preferencesList.get(0).get("preferences_json_bin");
 
-        return GSON.fromJson(new String(json, Charset.defaultCharset()), MAP_STRING_TO_OBJECT_TYPE);
+        Map<String, Object> attributes =
+            GSON.fromJson(new String(json, Charset.defaultCharset()), MAP_STRING_TO_OBJECT_TYPE);
+        attributes.put("alerts", getSubjectNotifications(subject));
+        return attributes;
       }
     } catch (PersistenceException e) {
       LOGGER.info(
@@ -162,6 +196,27 @@ public class UserApplication implements SparkApplication {
     }
 
     return Collections.emptyMap();
+  }
+
+  private List getSubjectNotifications(Subject subject) {
+    String userid = subjectIdentity.getUniqueIdentifier(subject);
+    try {
+      String filter = String.format("user = '%s'", userid);
+      List<Map<String, Object>> notificationsList =
+          persistentStore.get(PersistenceType.NOTIFICATION_TYPE.toString(), filter);
+      return notificationsList
+          .stream()
+          .map(
+              json ->
+                  GSON.fromJson((String) json.get("notification_txt"), MAP_STRING_TO_OBJECT_TYPE))
+          .collect(Collectors.toList());
+    } catch (PersistenceException e) {
+      LOGGER.info(
+          "PersistenceException while trying to retrieve persisted notifications for user {}",
+          userid,
+          e);
+    }
+    return Collections.emptyList();
   }
 
   private Map<String, Object> getSubjectAttributes(Subject subject) {
@@ -182,5 +237,23 @@ public class UserApplication implements SparkApplication {
     }
 
     return ImmutableMap.<String, Object>builder().putAll(required).put("email", email).build();
+  }
+
+  private void deleteNotifications(List<String> ids) {
+    if (ids.isEmpty()) {
+      return;
+    }
+    List<Filter> idsToDelete = new ArrayList<>();
+    ids.forEach(id -> idsToDelete.add(filterBuilder.attribute("id").is().equalTo().text(id)));
+    try {
+      persistentStore.delete(
+          PersistenceType.NOTIFICATION_TYPE.toString(),
+          ECQL.toCQL(filterBuilder.anyOf(idsToDelete)));
+    } catch (PersistenceException e) {
+      LOGGER.info(
+          "PersistenceException while trying to delete persisted notifications with ids {} ",
+          ids,
+          e);
+    }
   }
 }
