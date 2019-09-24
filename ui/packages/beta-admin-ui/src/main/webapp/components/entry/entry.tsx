56231c4d8c7faf7f4fb77c8c0db774caea2b9be0
andrewkfiedler <andrewkfiedler@gmail.com>
2019-09-24 13:45:36 -0700

    DDF-5371 Adds beta-admin-ui
     - Fixes a few typescript issues we had.
     - Adds a beta-admin-ui (dependency on @connexta/kanri).
     - Leaves the old admin ui around, so that people can continue to use that one if they choose.
     - Adds a link in the old admin ui so people can find the beta admin ui.

---
+++ ui/packages/beta-admin-ui/src/main/webapp/components/entry/entry.tsx
@@ -0,0 +1,4 @@
+import { entry } from '@connexta/kanri/src/main/webapp/components/entry/entry'
+import { hot } from 'react-hot-loader/root'
+entry()
+export default hot(() => {}) // just to enable hot reloading
---
+++ ui/packages/beta-admin-ui/src/main/webapp/index.js
@@ -0,0 +1,3 @@
+import Index from '@connexta/kanri/src/main/webapp/index.html'
+
+export default Index
