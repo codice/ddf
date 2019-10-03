7526dc0f3fb5ff271b0bf1b9f2d5eb1deb86828f
Rick Larsen <rick.larsen@connexta.com>
2019-10-03 12:36:00 -0700

    DDF-04759 edits to Documentation-version page and copy edits

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var getLatestStableVersion = function(callback) {
     var versionContent = $("#stable-version");
+    var versionContentNoLink = $('#stable-version-no-link')
 
     $.ajax({
         url: "https://api.github.com/repos/codice/ddf/releases",
@@ -14,7 +15,9 @@ var getLatestStableVersion = function(callback) {
             var dateStr = currentVersion.published_at.slice(0, 10);
             var html = "<a href='" + currentVersion.html_url + "' target='_blank'>" + currentVersion.tag_name.toUpperCase() + "</a>" +
                 " released on " + dateStr;
+            var htmlNoLink = currentVersion.tag_name.toUpperCase() + " released on " + dateStr;
             versionContent.html(html);
+            versionContentNoLink.html(htmlNoLink);
             
             var downloadButton = $("#download-button");
             var buttonHref = currentVersion.html_url
@@ -64,8 +67,7 @@ var setPreviousVersionList = function(data) {
         data.forEach(element => {
             href = element.html_url
             dateString = element.published_at.slice(0, 10)
-            versionName = element.name.slice(4)
-
+            versionName = element.name.split(/[A-z]+-|[A-z]+\ /).filter(function(value){ return value !== "";})
             html = `<tr>\n<td>${versionName}</td>\n<td>${dateString}</td>\n<td class='text-center'><a href='http://artifacts.codice.org//service/local/artifact/maven/redirect?g=ddf&a=docs&v=${versionName}&r=public&c=documentation&e=html' target='_blank'>html</a></td>\n<td class='text-center'><a href='http://artifacts.codice.org//service/local/artifact/maven/content?g=ddf&a=docs&v=${versionName}&r=public&c=documentation&e=pdf' target='_blank'>pdf</a></td>\n</tr>`
 
             versionHistoryTable.prepend(html);
