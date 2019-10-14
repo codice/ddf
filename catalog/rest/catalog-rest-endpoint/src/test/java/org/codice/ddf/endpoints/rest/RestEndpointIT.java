037c84fece1222e60423dff4c62194d1eebda329
Aaron Ilovici <aaron.ilovici@connexta.com>
2019-10-14 15:00:13 -0400

    Updating based on build and test failures

--- catalog/rest/catalog-rest-endpoint/src/test/java/org/codice/ddf/endpoints/rest/RestEndpointIT.java
+++ catalog/rest/catalog-rest-endpoint/src/test/java/org/codice/ddf/endpoints/rest/RestEndpointIT.java
@@ -82,203 +82,203 @@ import org.slf4j.LoggerFactory;
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
 public class RestEndpointIT extends AbstractComponentTest {
-  private static final Logger LOGGER = LoggerFactory.getLogger(RestEndpointIT.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestEndpointIT.class);
 
-  private static UrlBuilder restEndpointUrlBuilder;
+    private static UrlBuilder restEndpointUrlBuilder;
 
-  @Rule public PaxExamRule paxExamRule = new PaxExamRule(this);
+    @Rule public PaxExamRule paxExamRule = new PaxExamRule(this);
 
-  @Rule
-  public final ServiceRegistrationRule serviceRegistrationRule = new ServiceRegistrationRule();
+    @Rule
+    public final ServiceRegistrationRule serviceRegistrationRule = new ServiceRegistrationRule();
 
-  @Inject ConfigurationAdmin configAdmin;
+    @Inject ConfigurationAdmin configAdmin;
 
-  @Inject private BundleContext bundleContext;
+    @Inject private BundleContext bundleContext;
 
-  @MockOsgiService private CatalogFramework catalogFramework;
+    @MockOsgiService private CatalogFramework catalogFramework;
 
-  @MockOsgiService private AttachmentParser attachmentParser;
+    @MockOsgiService private AttachmentParser attachmentParser;
 
-  @MockOsgiService private MimeTypeToTransformerMapper mimeTypeToTransformerMapper;
+    @MockOsgiService private MimeTypeToTransformerMapper mimeTypeToTransformerMapper;
 
-  @MockOsgiService private AttributeRegistry attributeRegistry;
+    @MockOsgiService private AttributeRegistry attributeRegistry;
 
-  @MockOsgiService(answer = Answers.RETURNS_DEEP_STUBS)
-  private FilterBuilder filterBuilder;
+    @MockOsgiService(answer = Answers.RETURNS_DEEP_STUBS)
+    private FilterBuilder filterBuilder;
 
-  @MockOsgiService(properties = {@Property(key = "name", value = "tikaMimeTypeResolver")})
-  private MimeTypeResolver mimeTypeResolver;
+    @MockOsgiService(properties = {@Property(key = "name", value = "tikaMimeTypeResolver")})
+    private MimeTypeResolver mimeTypeResolver;
 
-  @MockOsgiService private MimeTypeMapper mimeTypeMapper;
+    @MockOsgiService private MimeTypeMapper mimeTypeMapper;
 
-  @MockOsgiService(properties = {@Property(key = "id", value = "uuidGenerator")})
-  private UuidGenerator uuidGenerator;
+    @MockOsgiService(properties = {@Property(key = "id", value = "uuidGenerator")})
+    private UuidGenerator uuidGenerator;
 
-  @BeforeExam
-  public void setupClass() {
-    LOGGER.trace("setupClass");
+    @BeforeExam
+    public void setupClass() {
+        LOGGER.trace("setupClass");
 
-    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 
-    restEndpointUrlBuilder =
-        UrlBuilder.fromSystemProperties(
-                SystemBaseUrl.EXTERNAL_HOST,
-                SystemBaseUrl.EXTERNAL_HTTP_PORT,
-                SystemBaseUrl.EXTERNAL_HTTPS_PORT,
-                SystemBaseUrl.INTERNAL_ROOT_CONTEXT)
-            .add("/catalog");
-  }
+        restEndpointUrlBuilder =
+                UrlBuilder.fromSystemProperties(
+                        SystemBaseUrl.EXTERNAL_HOST,
+                        SystemBaseUrl.EXTERNAL_HTTP_PORT,
+                        SystemBaseUrl.EXTERNAL_HTTPS_PORT,
+                        SystemBaseUrl.INTERNAL_ROOT_CONTEXT)
+                        .add("/catalog");
+    }
 
-  @Before
-  public void setup() {
-    LOGGER.trace("setup");
+    @Before
+    public void setup() {
+        LOGGER.trace("setup");
 
-    waitForAllBundlesActive();
-    waitForRestEndpoint();
-  }
+        waitForAllBundlesActive();
+        waitForRestEndpoint();
+    }
 
-  @Test
-  public void testGetWithMetacardIdOnly() throws Exception {
-    String metacardId = "123";
-    String contentType = "text/plain";
-    String contentText = "Content";
-    byte[] contentBytes = contentText.getBytes();
+    @Test
+    public void testGetWithMetacardIdOnly() throws Exception {
+        String metacardId = "123";
+        String contentType = "text/plain";
+        String contentText = "Content";
+        byte[] contentBytes = contentText.getBytes();
 
-    Filter filter = mock(Filter.class);
-    QueryResponse queryResponse = mock(QueryResponse.class);
-    Result result = mock(Result.class);
-    Metacard metacard = mock(Metacard.class);
-    BinaryContent content = mock(BinaryContent.class);
+        Filter filter = mock(Filter.class);
+        QueryResponse queryResponse = mock(QueryResponse.class);
+        Result result = mock(Result.class);
+        Metacard metacard = mock(Metacard.class);
+        BinaryContent content = mock(BinaryContent.class);
 
-    given(filterBuilder.attribute(Metacard.ID).is().equalTo().text(metacardId)).willReturn(filter);
-    given(catalogFramework.query(any(QueryRequest.class), any(FederationStrategy.class)))
-        .willReturn(queryResponse);
-    given(queryResponse.getResults()).willReturn(ImmutableList.of(result));
-    given(result.getMetacard()).willReturn(metacard);
-    given(catalogFramework.transform(same(metacard), eq("xml"), any(Map.class)))
-        .willReturn(content);
-    given(content.getInputStream()).willReturn(new ByteArrayInputStream(contentBytes));
-    given(content.getMimeTypeValue()).willReturn(contentType);
-    given(content.getSize()).willReturn((long) contentBytes.length);
+        given(filterBuilder.attribute(Metacard.ID).is().equalTo().text(metacardId)).willReturn(filter);
+        given(catalogFramework.query(any(QueryRequest.class), any(FederationStrategy.class)))
+                .willReturn(queryResponse);
+        given(queryResponse.getResults()).willReturn(ImmutableList.of(result));
+        given(result.getMetacard()).willReturn(metacard);
+        given(catalogFramework.transform(same(metacard), eq("xml"), any(Map.class)))
+                .willReturn(content);
+        given(content.getInputStream()).willReturn(new ByteArrayInputStream(contentBytes));
+        given(content.getMimeTypeValue()).willReturn(contentType);
+        given(content.getSize()).willReturn((long) contentBytes.length);
 
-    when()
-        .get(restEndpointUrlBuilder.add(metacardId).build())
-        .then()
-        .assertThat()
-        .statusCode(200)
-        .header(HttpHeaders.CONTENT_LENGTH, is(String.valueOf(contentBytes.length)))
-        .header(HttpHeaders.ACCEPT_RANGES, "bytes")
-        .contentType(contentType)
-        .body(is(contentText));
-  }
+        when()
+                .get(restEndpointUrlBuilder.add(metacardId).build())
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .header(HttpHeaders.CONTENT_LENGTH, is(String.valueOf(contentBytes.length)))
+                .header(HttpHeaders.ACCEPT_RANGES, "bytes")
+                .contentType(contentType)
+                .body(is(contentText));
+    }
 
-  @Test
-  public void testGetInvalidMetacardId() throws Exception {
-    String metacardId = "123";
+    @Test
+    public void testGetInvalidMetacardId() throws Exception {
+        String metacardId = "123";
 
-    Filter filter = mock(Filter.class);
-    QueryResponse queryResponse = mock(QueryResponse.class);
+        Filter filter = mock(Filter.class);
+        QueryResponse queryResponse = mock(QueryResponse.class);
 
-    given(filterBuilder.attribute(Metacard.ID).is().equalTo().text(metacardId)).willReturn(filter);
-    given(catalogFramework.query(any(QueryRequest.class), any(FederationStrategy.class)))
-        .willReturn(queryResponse);
-    given(queryResponse.getResults()).willReturn(ImmutableList.of());
+        given(filterBuilder.attribute(Metacard.ID).is().equalTo().text(metacardId)).willReturn(filter);
+        given(catalogFramework.query(any(QueryRequest.class), any(FederationStrategy.class)))
+                .willReturn(queryResponse);
+        given(queryResponse.getResults()).willReturn(ImmutableList.of());
 
-    when()
-        .get(restEndpointUrlBuilder.add(metacardId).build())
-        .then()
-        .assertThat()
-        .statusCode(404)
-        .body(containsString("Unable to retrieve requested metacard."));
-  }
+        when()
+                .get(restEndpointUrlBuilder.add(metacardId).build())
+                .then()
+                .assertThat()
+                .statusCode(404)
+                .body(containsString("Unable to retrieve requested metacard."));
+    }
 
-  @Override
-  protected ApplicationOptions getApplicationOptions(PortFinder portFinder) {
-    return new DdfComponentOptions(portFinder) {
+    @Override
+    protected ApplicationOptions getApplicationOptions(PortFinder portFinder) {
+        return new DdfComponentOptions(portFinder) {
 
-      @Override
-      protected BundleOption getBundleOptions() {
-        return super.getBundleOptions()
-            .add("org.bouncycastle", "bcprov-jdk15on")
-            .add("ddf.catalog.transformer", "catalog-transformer-attribute")
-            .add("ddf.catalog.core", "catalog-core-attachment")
-            .add("ddf.catalog.rest", "catalog-rest-api")
-            .add("ddf.catalog.rest", "catalog-rest-service-impl")
-            .add("ddf.catalog.rest", "catalog-rest-impl");
-      }
+            @Override
+            protected BundleOption getBundleOptions() {
+                return super.getBundleOptions()
+                        .add("org.bouncycastle", "bcprov-jdk15on")
+                        .add("ddf.catalog.transformer", "catalog-transformer-attribute")
+                        .add("ddf.catalog.core", "catalog-core-attachment")
+                        .add("ddf.catalog.rest", "catalog-rest-api")
+                        .add("ddf.catalog.rest", "catalog-rest-service-impl")
+                        .add("ddf.catalog.rest", "catalog-rest-impl");
+            }
 
-      @Override
-      protected FeatureOption getFeatureOptions() {
-        final String[] springFeatures = {
-          "spring-instrument", "spring-jms", "spring-test", "spring-web"
+            @Override
+            protected FeatureOption getFeatureOptions() {
+                final String[] springFeatures = {
+                        "spring-instrument", "spring-jms", "spring-test", "spring-web"
+                };
+
+                final String[] cxfFeatures = {"cxf", "cxf-commands"};
+                final String[] utilitiesFeatures = {"action-core-impl"};
+                final String[] kernelFeatures = {"apache-commons, guava"};
+
+                return super.getFeatureOptions()
+                        .addFeatures("org.apache.karaf.features", "spring", springFeatures)
+                        .addFeatures("org.apache.cxf.karaf", "apache-cxf", cxfFeatures)
+                        .addFeatures("ddf.features", "utilities", utilitiesFeatures)
+                        .addFeatures("ddf.features", "kernel", kernelFeatures)
+                        .addFeatureFrom("ddf.features", "test-utilities", "features", "rest-assured")
+                        .addFeatureFrom(
+                                "ddf.platform.util", "util-uuidgenerator-api", "feature", "uuidgenerator-api")
+                        .addFeatureFrom("ddf.mime.core", "mime-core-api", "feature", "mime-core-api-only")
+                        .addFeatureFrom(
+                                "ddf.catalog.core", "catalog-core-api", "feature", "catalog-core-api-only");
+            }
+
+            @Override
+            public Option get() {
+                return CoreOptions.composite(
+                        super.get(),
+                        CoreOptions.bootClasspathLibraries(
+                                new BootClasspathLibraryOption(
+                                        CoreOptions.maven("javax.annotation", "javax.annotation-api", "1.3"))),
+                        KarafDistributionOption.editConfigurationFilePut(
+                                "etc/custom.properties",
+                                "org.osgi.framework.system.packages.extra",
+                                new StringBuilder()
+                                        .append("javax.annotation;version=1.0.0,")
+                                        .append("javax.annotation;version=1.3.2,")
+                                        .append("javax.annotation;version=3.0.2")
+                                        .toString()));
+            }
         };
+    }
 
-        final String[] cxfFeatures = {"cxf", "cxf-commands"};
-        final String[] utilitiesFeatures = {"action-core-impl"};
-        final String[] kernelFeatures = {"apache-commons, guava"};
+    private void waitForRestEndpoint() {
+        LOGGER.trace("waitForRestEndpoint");
+        with()
+                .pollInterval(FIVE_HUNDRED_MILLISECONDS)
+                .await("REST Endpoint Available")
+                .atMost(TEN_SECONDS)
+                .until(
+                        () -> {
+                            try {
+                                when().head(restEndpointUrlBuilder.build()).then().assertThat().statusCode(200);
+                            } catch (Throwable e) {
+                                return false;
+                            }
 
-        return super.getFeatureOptions()
-            .addFeatures("org.apache.karaf.features", "spring", springFeatures)
-            .addFeatures("org.apache.cxf.karaf", "apache-cxf", cxfFeatures)
-            .addFeatures("ddf.features", "utilities", utilitiesFeatures)
-            .addFeatures("ddf.features", "kernel", kernelFeatures)
-            .addFeatureFrom("ddf.features", "test-utilities", "features", "rest-assured")
-            .addFeatureFrom(
-                "ddf.platform.util", "util-uuidgenerator-api", "feature", "uuidgenerator-api")
-            .addFeatureFrom("ddf.mime.core", "mime-core-api", "feature", "mime-core-api-only")
-            .addFeatureFrom(
-                "ddf.catalog.core", "catalog-core-api", "feature", "catalog-core-api-only");
-      }
+                            return true;
+                        });
+    }
 
-      @Override
-      public Option get() {
-        return CoreOptions.composite(
-            super.get(),
-            CoreOptions.bootClasspathLibraries(
-                new BootClasspathLibraryOption(
-                    CoreOptions.maven("javax.annotation", "javax.annotation-api", "1.3"))),
-            KarafDistributionOption.editConfigurationFilePut(
-                "etc/custom.properties",
-                "org.osgi.framework.system.packages.extra",
-                new StringBuilder()
-                    .append("javax.annotation;version=1.0.0,")
-                    .append("javax.annotation;version=1.3.2,")
-                    .append("javax.annotation;version=3.0.2")
-                    .toString()));
-      }
-    };
-  }
-
-  private void waitForRestEndpoint() {
-    LOGGER.trace("waitForRestEndpoint");
-    with()
-        .pollInterval(FIVE_HUNDRED_MILLISECONDS)
-        .await("REST Endpoint Available")
-        .atMost(TEN_SECONDS)
-        .until(
-            () -> {
-              try {
-                when().head(restEndpointUrlBuilder.build()).then().assertThat().statusCode(200);
-              } catch (Throwable e) {
-                return false;
-              }
-
-              return true;
-            });
-  }
-
-  // TODO (DDF-3547) - Change to make sure all the bundles of the feature under test are active
-  // and move to common class.
-  private void waitForAllBundlesActive() {
-    LOGGER.trace("waitForAllBundlesActive");
-    with()
-        .pollInterval(TWO_HUNDRED_MILLISECONDS)
-        .await("All bundles active")
-        .atMost(TEN_SECONDS)
-        .until(
-            () ->
-                Arrays.stream(bundleContext.getBundles())
-                    .allMatch(
-                        b -> (b.getState() == Bundle.ACTIVE) || (b.getState() == Bundle.RESOLVED)));
-  }
+    // TODO (DDF-3547) - Change to make sure all the bundles of the feature under test are active
+    // and move to common class.
+    private void waitForAllBundlesActive() {
+        LOGGER.trace("waitForAllBundlesActive");
+        with()
+                .pollInterval(TWO_HUNDRED_MILLISECONDS)
+                .await("All bundles active")
+                .atMost(TEN_SECONDS)
+                .until(
+                        () ->
+                                Arrays.stream(bundleContext.getBundles())
+                                        .allMatch(
+                                                b -> (b.getState() == Bundle.ACTIVE) || (b.getState() == Bundle.RESOLVED)));
+    }
 }
