26df27b978b743295ec7663a8d24bcc73d45203d
Zachary Adam <zta6@pitt.edu>
2019-10-22 13:38:38 -0600

    DDF-5373 Remove manual metacard creation view (#5375) (#5499)

--- ui/packages/catalog-ui-search/src/main/webapp/component/tabs/list-add/tabs-list-add.js
+++ ui/packages/catalog-ui-search/src/main/webapp/component/tabs/list-add/tabs-list-add.js
@@ -15,13 +15,11 @@
 
 const Tabs = require('../tabs')
 const IngestView = require('../../ingest/ingest.view.js')
-const BuilderView = require('../../builder/builder.view.js')
 
 module.exports = Tabs.extend({
   defaults: {
     tabs: {
       Import: IngestView,
-      Manual: BuilderView,
     },
   },
   initialize(options) {},
