ef759360009213318e2c2763efeb576aadce6479
Cassandra Bailey <cassandra.bailey@connexta.com>
2019-12-02 08:16:46 -0700

    [2.19.x]  DDF-5570 search error msg (#5571)
    * DDF-5570 call new validate function to pass specific error messages
    
    * DDF-5570 remove debugging statement
    
    * DDF-5570 call new validate function and incorporate its logic into existing functions
    
    * DDF-5570 change error msg to array
    
    * DDF-5570 refactor common code and fix message concat
    
    * DDF-5570 refactor validate function
    
    * DDF-5570 refactor invalidsearchformmessage array declaration
    
    * DDF-5570 remove unnecessary const declaration and specify type for error message array
    
    * DDF-5570 refactor validate to build title and body per UX
    
    * DDF-5570 fix text alignment and remove extra wording per uX
    
    * DDF-5570 formatting
    
    * DDF-5570 fix capitalization
    
    * DDF-5570 syntax fixes
    
    * DDF-5570 formatting
    
    * DDF-5570 add handling for query adhoc

---
+++ ui/packages/catalog-ui-search/src/main/webapp/react-component/utils/validation/validation.tsx
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ **/
+
+import { InvalidSearchFormMessage } from '../../../component/announcement/CommonMessages'
+const announcement = require('../../../component/announcement/index.jsx')
+
+export function validate(errors: any) {
+  if (errors.length === 0) {
+    return true
+  }
+  let searchErrorMessage = JSON.parse(JSON.stringify(InvalidSearchFormMessage))
+  if (errors.length > 1) {
+    let msg = searchErrorMessage.message
+    searchErrorMessage.title =
+      'Your search cannot be run due to multiple errors'
+    const formattedErrors = errors.map(
+      (error: any) => `\u2022 ${error.title}: ${error.body}`
+    )
+    searchErrorMessage.message = msg.concat(formattedErrors)
+  } else {
+    const error = errors[0]
+    searchErrorMessage.title = error.title
+    searchErrorMessage.message = error.body
+  }
+  announcement.announce(searchErrorMessage)
+  return false
+}
