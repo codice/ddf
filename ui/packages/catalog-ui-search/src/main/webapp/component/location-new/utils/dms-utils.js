c6a63ebc51279e63197438d7b9e3eff91d5f3a30
Cassandra Bailey <cassandra.bailey@connexta.com>
2019-12-06 14:22:43 -0700

    DDF-5672 fix dms lat/lon entry UX

--- ui/packages/catalog-ui-search/src/main/webapp/component/location-new/utils/dms-utils.js
+++ ui/packages/catalog-ui-search/src/main/webapp/component/location-new/utils/dms-utils.js
@@ -15,7 +15,6 @@
 const wkx = require('wkx')
 const { computeCircle, toKilometers } = require('./geo-helper')
 const errorMessages = require('./errors')
-
 const dmsRegex = new RegExp('^([0-9]*)°([0-9]*)\'([0-9]*\\.?[0-9]*)"$')
 const minimumDifference = 0.0001
 
