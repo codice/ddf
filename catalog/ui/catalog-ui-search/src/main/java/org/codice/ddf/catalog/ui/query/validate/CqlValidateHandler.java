e5243ee9cda979ae0b713e009ddb78c1cec9302e
david.morales <david.morales@connexta.com>
2019-12-06 08:52:22 -0700

    DDF-5627 Factor out CqlRequestParser

--- catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/query/validate/CqlValidateHandler.java
+++ catalog/ui/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/query/validate/CqlValidateHandler.java
@@ -1,22 +1,27 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
 package org.codice.ddf.catalog.ui.query.validate;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import ddf.catalog.CatalogFramework;
-import ddf.catalog.filter.FilterBuilder;
+import ddf.catalog.operation.QueryRequest;
 import ddf.catalog.validation.QueryValidator;
 import ddf.catalog.validation.violation.QueryValidationViolation;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-import org.codice.ddf.catalog.ui.query.cql.CqlRequest;
-import org.codice.ddf.catalog.ui.util.EndpointUtil;
-import org.codice.gsonsupport.GsonTypeAdapters.DateLongFormatTypeAdapter;
-import org.codice.gsonsupport.GsonTypeAdapters.LongDoubleTypeAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Request;
@@ -27,37 +32,17 @@ public class CqlValidateHandler implements Route {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(CqlValidateHandler.class);
 
-  private static final Gson GSON =
-      new GsonBuilder()
-          .disableHtmlEscaping()
-          .serializeNulls()
-          .registerTypeAdapterFactory(LongDoubleTypeAdapter.FACTORY)
-          .registerTypeAdapter(Date.class, new DateLongFormatTypeAdapter())
-          .create();
-
   private QueryValidatorsById queryValidatorsById;
 
-  private CatalogFramework catalogFramework;
+  private CqlRequestParser parser;
 
-  private FilterBuilder filterBuilder;
-
-  private EndpointUtil util;
-
-  // TODO remove dependency on endpoint util?
-  public CqlValidateHandler(
-      QueryValidatorsById queryValidatorsById,
-      CatalogFramework catalogFramework,
-      FilterBuilder filterBuilder,
-      EndpointUtil util) {
+  public CqlValidateHandler(QueryValidatorsById queryValidatorsById, CqlRequestParser parser) {
     this.queryValidatorsById = queryValidatorsById;
-    this.catalogFramework = catalogFramework;
-    this.filterBuilder = filterBuilder;
-    this.util = util;
+    this.parser = parser;
   }
 
   @Override
   public Object handle(Request request, Response response) throws Exception {
-    CqlRequest cqlRequest = GSON.fromJson(util.safeGetBody(request), CqlRequest.class);
     String validatorId = request.params(":validatorId");
 
     QueryValidator validator = queryValidatorsById.get(validatorId);
@@ -72,8 +57,9 @@ public class CqlValidateHandler implements Route {
           Collections.emptyList());
     }
 
-    Set<QueryValidationViolation> violations =
-        validator.validate(cqlRequest.createQueryRequest(catalogFramework.getId(), filterBuilder));
+    QueryRequest queryRequest = parser.parse(request);
+    Set<QueryValidationViolation> violations = validator.validate(queryRequest);
+
     List<Map<String, Object>> violationResponses =
         violations
             .stream()
--- catalog/ui/catalog-ui-search/src/test/java/org/codice/ddf/catalog/ui/query/validate/CqlValidateHandlerTest.java
+++ catalog/ui/catalog-ui-search/src/test/java/org/codice/ddf/catalog/ui/query/validate/CqlValidateHandlerTest.java
@@ -1,11 +1,155 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
 package org.codice.ddf.catalog.ui.query.validate;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import ddf.catalog.filter.FilterBuilder;
+import ddf.catalog.filter.proxy.builder.GeotoolsFilterBuilder;
+import ddf.catalog.operation.QueryRequest;
+import ddf.catalog.operation.impl.QueryImpl;
+import ddf.catalog.operation.impl.QueryRequestImpl;
+import ddf.catalog.validation.QueryValidator;
+import ddf.catalog.validation.impl.violation.QueryValidationViolationImpl;
+import ddf.catalog.validation.violation.QueryValidationViolation;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import org.junit.Test;
+import org.opengis.filter.Filter;
+import spark.Request;
+import spark.Response;
 
 public class CqlValidateHandlerTest {
 
   private CqlValidateHandler cqlValidateHandler;
 
+  private CqlRequestParser parser = mock(CqlRequestParser.class);
+
+  private QueryValidatorsById queryValidatorsById = mock(QueryValidatorsById.class);
+
+  private FilterBuilder filterBuilder = new GeotoolsFilterBuilder();
+
   @Test
-  public void test() {}
+  public void testHandleUnknownValidatorId() throws Exception {
+    when(queryValidatorsById.get(any())).thenReturn(null);
+    QueryRequest queryRequest =
+        createQueryRequest(filterBuilder.attribute("attr").is().text("val"), "src");
+    when(parser.parse(any())).thenReturn(queryRequest);
+    Request request = mock(Request.class);
+    when(request.params(":validatorId")).thenReturn("invalidId");
+    Response response = mock(Response.class);
+
+    cqlValidateHandler = new CqlValidateHandler(queryValidatorsById, parser);
+    Object objResponse = cqlValidateHandler.handle(request, response);
+
+    verify(response).status(404);
+    Map<String, Object> jsonResponse = (Map<String, Object>) objResponse;
+    assertThat(jsonResponse.get("error"), is("No validator matching id invalidId"));
+    assertThat((List<Object>) jsonResponse.get("validationViolations"), hasSize(0));
+  }
+
+  @Test
+  public void testHandleOneValidatorOneViolation() throws Exception {
+    QueryValidationViolation violation =
+        new QueryValidationViolationImpl(
+            QueryValidationViolation.Severity.ERROR,
+            "there was an error",
+            ImmutableMap.of("k", "v"));
+    QueryValidator validator = mock(QueryValidator.class);
+    when(validator.validate(any())).thenReturn(ImmutableSet.of(violation));
+    when(validator.getValidatorId()).thenReturn("id");
+    when(queryValidatorsById.get("id")).thenReturn(validator);
+    QueryRequest queryRequest = mock(QueryRequest.class);
+    when(parser.parse(any())).thenReturn(queryRequest);
+    Request request = mock(Request.class);
+    when(request.params(":validatorId")).thenReturn("id");
+    Response response = mock(Response.class);
+
+    cqlValidateHandler = new CqlValidateHandler(queryValidatorsById, parser);
+    Object objResponse = cqlValidateHandler.handle(request, response);
+
+    Map<String, Object> jsonResponse = (Map<String, Object>) objResponse;
+    List<Object> violations = (List<Object>) jsonResponse.get("validationViolations");
+    assertThat(violations, hasSize(1));
+    assertViolation(
+        (Map<String, Object>) violations.get(0),
+        QueryValidationViolation.Severity.ERROR,
+        "there was an error",
+        ImmutableMap.of("k", "v"),
+        "id");
+  }
+
+  @Test
+  public void testHandleOneValidatorTwoViolations() throws Exception {
+    QueryValidationViolation violation1 =
+        new QueryValidationViolationImpl(
+            QueryValidationViolation.Severity.ERROR, "ONE", ImmutableMap.of("k1", "v1"));
+    QueryValidationViolation violation2 =
+        new QueryValidationViolationImpl(
+            QueryValidationViolation.Severity.WARNING, "TWO", ImmutableMap.of("k2", "v2"));
+    QueryValidator validator = mock(QueryValidator.class);
+    when(validator.validate(any())).thenReturn(ImmutableSet.of(violation1, violation2));
+    when(validator.getValidatorId()).thenReturn("id");
+    when(queryValidatorsById.get("id")).thenReturn(validator);
+    QueryRequest queryRequest = mock(QueryRequest.class);
+    when(parser.parse(any())).thenReturn(queryRequest);
+    Request request = mock(Request.class);
+    when(request.params(":validatorId")).thenReturn("id");
+    Response response = mock(Response.class);
+
+    cqlValidateHandler = new CqlValidateHandler(queryValidatorsById, parser);
+    Object objResponse = cqlValidateHandler.handle(request, response);
+
+    Map<String, Object> jsonResponse = (Map<String, Object>) objResponse;
+    List<Object> violations = (List<Object>) jsonResponse.get("validationViolations");
+    assertThat(violations, hasSize(2));
+    assertViolation(
+        (Map<String, Object>) violations.get(0),
+        QueryValidationViolation.Severity.ERROR,
+        "ONE",
+        ImmutableMap.of("k1", "v1"),
+        "id");
+    assertViolation(
+        (Map<String, Object>) violations.get(1),
+        QueryValidationViolation.Severity.WARNING,
+        "TWO",
+        ImmutableMap.of("k2", "v2"),
+        "id");
+  }
+
+  private void assertViolation(
+      Map<String, Object> actualViolation,
+      QueryValidationViolation.Severity severity,
+      String message,
+      Map<String, Object> extras,
+      String validatorId) {
+    assertThat(actualViolation.get("severity"), is(severity));
+    assertThat(actualViolation.get("message"), is(message));
+    assertThat(actualViolation.get("extraData"), is(extras));
+    assertThat(actualViolation.get("type"), is(validatorId));
+  }
+
+  private QueryRequest createQueryRequest(Filter filter, String... sourceIds) {
+    return new QueryRequestImpl(new QueryImpl(filter), Arrays.asList(sourceIds));
+  }
 }
