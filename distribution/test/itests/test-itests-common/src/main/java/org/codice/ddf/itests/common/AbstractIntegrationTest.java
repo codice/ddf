dd0e75f14f7082de90b78f13ccf69ff5b59f2ab3
Shaun Morris <shaun.morris@connexta.com>
2020-01-15 17:05:18 -0700

    DDF-5777 updated repo references to use https

--- distribution/test/itests/test-itests-common/src/main/java/org/codice/ddf/itests/common/AbstractIntegrationTest.java
+++ distribution/test/itests/test-itests-common/src/main/java/org/codice/ddf/itests/common/AbstractIntegrationTest.java
@@ -563,16 +563,16 @@ public abstract class AbstractIntegrationTest {
         editConfigurationFilePut(
             "etc/org.ops4j.pax.url.mvn.cfg",
             "org.ops4j.pax.url.mvn.repositories",
-            "http://repo1.maven.org/maven2@id=central,"
-                + "http://oss.sonatype.org/content/repositories/snapshots@snapshots@noreleases@id=sonatype-snapshot,"
-                + "http://oss.sonatype.org/content/repositories/ops4j-snapshots@snapshots@noreleases@id=ops4j-snapshot,"
-                + "http://repository.apache.org/content/groups/snapshots-group@snapshots@noreleases@id=apache,"
-                + "http://svn.apache.org/repos/asf/servicemix/m2-repo@id=servicemix,"
-                + "http://repository.springsource.com/maven/bundles/release@id=springsource,"
-                + "http://repository.springsource.com/maven/bundles/external@id=springsourceext,"
-                + "http://oss.sonatype.org/content/repositories/releases/@id=sonatype,"
-                + "http://artifacts.codice.org/content/repositories/releases@id=codice-releases,"
-                + "http://artifacts.codice.org/content/repositories/thirdparty@id=codice-thirdparty"),
+            "https://repo1.maven.org/maven2@id=central,"
+                + "https://oss.sonatype.org/content/repositories/snapshots@snapshots@noreleases@id=sonatype-snapshot,"
+                + "https://oss.sonatype.org/content/repositories/ops4j-snapshots@snapshots@noreleases@id=ops4j-snapshot,"
+                + "https://repository.apache.org/content/groups/snapshots-group@snapshots@noreleases@id=apache,"
+                + "https://svn.apache.org/repos/asf/servicemix/m2-repo@id=servicemix,"
+                + "https://repository.springsource.com/maven/bundles/release@id=springsource,"
+                + "https://repository.springsource.com/maven/bundles/external@id=springsourceext,"
+                + "https://oss.sonatype.org/content/repositories/releases/@id=sonatype,"
+                + "https://artifacts.codice.org/content/repositories/releases@id=codice-releases,"
+                + "https://artifacts.codice.org/content/repositories/thirdparty@id=codice-thirdparty"),
         when(System.getProperty(MVN_LOCAL_REPO) != null)
             .useOptions(
                 editConfigurationFilePut(
