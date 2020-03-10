0d91bb7cb7b5bd126d97497bae1bf4b7f4fc378d
Cassandra Bailey <cassandra.bailey@connexta.com>
2020-03-10 08:13:00 -0600

    DDF-UI-112 add linestring case to opensearch

--- catalog/opensearch/catalog-opensearch-source/src/main/java/org/codice/ddf/opensearch/source/OpenSearchFilterVisitor.java
+++ catalog/opensearch/catalog-opensearch-source/src/main/java/org/codice/ddf/opensearch/source/OpenSearchFilterVisitor.java
@@ -15,6 +15,7 @@ package org.codice.ddf.opensearch.source;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
@@ -492,6 +493,9 @@ public class OpenSearchFilterVisitor extends DefaultFilterVisitor {
       openSearchFilterVisitorObject.addGeometrySearch(polygon);
     } else if (geometryExpression instanceof MultiPolygon) {
       Geometry polygon = ((MultiPolygon) geometryExpression);
+      openSearchFilterVisitorObject.addGeometrySearch(polygon);
+    } else if (geometryExpression instanceof LineString) {
+      Geometry polygon = ((LineString) geometryExpression);
       openSearchFilterVisitorObject.addGeometrySearch(polygon);
     } else {
       LOGGER.debug("Unsupported filter constraint");
