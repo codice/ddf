8df54f621bc54e7f2faaafcdd6dcd596dbeefd07
Cassandra Bailey <cassandra.bailey@connexta.com>
2020-07-14 11:33:44 -0600

    DDF-UI-248 remove unused file

--- ui/packages/catalog-ui-search/src/main/webapp/js/workers/shared-worker.js
+++
@@ -1,7 +0,0 @@
-// var EventSource = require('eventsource')
-// const REQUEST_HEADER = 'XMLHttpRequest'
-// const HEADERS = {
-//   'X-Requested-With': REQUEST_HEADER,
-// }
-
-console.log('Inside worker')
