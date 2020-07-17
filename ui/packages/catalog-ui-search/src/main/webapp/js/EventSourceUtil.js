c3be84ba30bab8a1304063feccb60be93085e3b3
Cassandra Bailey <cassandra.bailey@connexta.com>
2020-07-16 18:31:28 -0600

    DDF-UI-248 formatting

--- ui/packages/catalog-ui-search/src/main/webapp/js/EventSourceUtil.js
+++ ui/packages/catalog-ui-search/src/main/webapp/js/EventSourceUtil.js
@@ -2,9 +2,11 @@ var EventSource = require('eventsource')
 const Common = require('./Common.js')
 
 const REQUEST_HEADER = 'XMLHttpRequest'
+const REQUEST_PATH = './internal/events/'
 const HEADERS = {
   'X-Requested-With': REQUEST_HEADER,
 }
+
 import { EventType } from '../react-component/utils/event'
 
 var source
@@ -42,34 +44,24 @@ module.exports = {
 
     const { onMessage } = handlers
     if (source) {
-      console.log('IF on message')
-      console.log('id: ', id)
       source.addEventListener(type, event => {
         onMessage(event)
       })
     } else {
       id = Common.generateUUID()
-      source = new EventSource('./internal/events/' + id, {
+      source = new EventSource(REQUEST_PATH + id, {
         withCredentials: true,
         credentials: 'same-origin',
         headers: HEADERS,
       })
       source.addEventListener(type, event => {
-        console.log('ELSE on message')
-        console.log('id: ', id)
         onMessage(event)
       })
-      source.onerror = event => {
-        console.log('on error')
-        console.log(event)
-        console.log('id: ', id)
+      source.onerror = () => {
         source.close()
         source = null
       }
-      source.addEventListener(EventType.Close, event => {
-        console.log('on close')
-        console.log(event)
-        console.log('id: ', id)
+      source.addEventListener(EventType.Close, () => {
         source.close()
         source = null
       })
