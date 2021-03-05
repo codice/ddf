806ffe8ec1bfad3d8c38395d46c813497782c772
Zachary Adam <zta6@pitt.edu>
2021-03-04 18:32:51 -0700

    change file rollover size

--- distribution/ddf-common/src/main/resources/bin/setenv.bat
+++ distribution/ddf-common/src/main/resources/bin/setenv.bat
@@ -84,5 +84,5 @@ rem set JAVA_ERROR_OPTS=-XX:OnOutOfMemoryError=%DDF_ON_ERROR%%%p -XX:OnError=%DD
 set JAVA_ERROR_OPTS=-XX:OnOutOfMemoryError=%DDF_ON_ERROR% -XX:OnError=%DDF_ON_ERROR%
 set KARAF_OPTS=-Dfile.encoding=UTF8
 rem Java garbage collection logging options
-set GC_OPTS=-Xlog:gc=info:file=\"%DDF_HOME%data/log/gc.log\":time,uptime,level,tags:filecount=5,filesize=100m 
+set GC_OPTS=-Xlog:gc=info:file=\"%DDF_HOME%data/log/gc.log\":time,uptime,level,tags:filecount=5,filesize=50m 
 set JAVA_OPTS=-Xms2g -Xmx6g -Dderby.system.home="%DDF_HOME%\data\derby" -Dderby.storage.fileSyncTransactionLog=true -Dfile.encoding=UTF8 -Dddf.home=%DDF_HOME% -Dddf.home.perm=%DDF_HOME_PERM% -XX:+DisableAttachMechanism %JAVA_ERROR_OPTS% %GC_OPTS%
