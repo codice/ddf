da2ffd937c2dbfe48a0720f5a08aa10bb52405c0
Andrew Zimmerman <andrew.zimmerman@connexta.com>
2021-03-26 13:02:33 -0600

    format

--- ui/packages/catalog-ui-search/src/main/webapp/component/query-add/query-add.view.js
+++ ui/packages/catalog-ui-search/src/main/webapp/component/query-add/query-add.view.js
@@ -64,12 +64,16 @@ module.exports = Marionette.LayoutView.extend({
       .get('content')
       .get('currentWorkspace')
       .get('id')
-    this.listenTo(store.get('content'), 'change:currentWorkspace', workspace => {
-      if (this.currentWorkspaceId !== workspace.get('id')) {
-        this.currentWorkspaceId = workspace.get('id')
-        this.cancel()
+    this.listenTo(
+      store.get('content'),
+      'change:currentWorkspace',
+      workspace => {
+        if (this.currentWorkspaceId !== workspace.get('id')) {
+          this.currentWorkspaceId = workspace.get('id')
+          this.cancel()
+        }
       }
-    })
+    )
   },
   updateCurrentQuery(currentQuerySettings) {
     if (
