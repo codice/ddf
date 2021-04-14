97aa7b28a9ba9ac8f66f05929e7a376ecaa5f77f
abel-connexta <65194214+abel-connexta@users.noreply.github.com>
2021-04-14 11:13:15 -0600

    Upgrade to Camel 3.8.0 (#6549)
    * Upgrade to Camel 3.8.0
    
    * update camel dependency versions

--- catalog/core/catalog-core-camelcomponent/src/main/java/ddf/camel/component/catalog/CatalogEndpoint.java
+++ catalog/core/catalog-core-camelcomponent/src/main/java/ddf/camel/component/catalog/CatalogEndpoint.java
@@ -28,7 +28,9 @@ import org.apache.camel.ExchangePattern;
 import org.apache.camel.MultipleConsumersSupport;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
+import org.apache.camel.spi.UriParam;
 import org.apache.camel.support.DefaultEndpoint;
+import org.apache.camel.support.SynchronousDelegateProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -60,6 +62,12 @@ public class CatalogEndpoint extends DefaultEndpoint implements MultipleConsumer
   private final CatalogFramework catalogFramework;
 
   private final MimeTypeMapper mimeTypeMapper;
+
+  @UriParam(
+      defaultValue = "false",
+      label = "producer,advanced",
+      description = "Sets whether synchronous processing should be strictly used")
+  private boolean synchronous;
 
   /**
    * Constructs a CatalogEndpoint for the specified custom <code>catalog</code> component.
@@ -141,6 +149,10 @@ public class CatalogEndpoint extends DefaultEndpoint implements MultipleConsumer
       LOGGER.debug("Unable to create producer for context path [{}]", contextPath);
       throw new IllegalArgumentException(
           "Unable to create producer for context path [" + contextPath + "]");
+    }
+
+    if (isSynchronous()) {
+      return new SynchronousDelegateProducer(producer);
     }
 
     return producer;
@@ -230,5 +242,13 @@ public class CatalogEndpoint extends DefaultEndpoint implements MultipleConsumer
   @Override
   public int hashCode() {
     return Objects.hash(super.hashCode(), transformerId, contextPath, mimeType);
+  }
+
+  public boolean isSynchronous() {
+    return synchronous;
+  }
+
+  public void setSynchronous(boolean synchronous) {
+    this.synchronous = synchronous;
   }
 }
