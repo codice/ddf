86643293b70f3e0e5e5770eb7dd6b473e71817b9
Joseph North <joernorth@gmail.com>
2021-06-23 18:16:24 -0500

    CsvMetacardTransformer excludes binary and object attributes (#6592)
    CsvMetacardTransformer now uses the same transformation logic as
    CsvQueryResponseTransformer, which correctly excludes binary and object
    attributes. CsvMetacardTransformer now also supports column sorting and
    hiding.

--- catalog/transformer/catalog-transformer-csv-queryresponse/src/main/java/ddf/catalog/transformer/csv/CsvMetacardTransformer.java
+++ catalog/transformer/catalog-transformer-csv-queryresponse/src/main/java/ddf/catalog/transformer/csv/CsvMetacardTransformer.java
@@ -13,25 +13,15 @@
  */
 package ddf.catalog.transformer.csv;
 
-import static ddf.catalog.transformer.csv.common.CsvTransformer.createResponse;
-import static ddf.catalog.transformer.csv.common.CsvTransformer.writeMetacardsToCsv;
+import static java.util.Collections.singletonList;
 
-import ddf.catalog.data.AttributeDescriptor;
 import ddf.catalog.data.BinaryContent;
 import ddf.catalog.data.Metacard;
 import ddf.catalog.transform.CatalogTransformerException;
 import ddf.catalog.transform.MetacardTransformer;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,33 +38,7 @@ public class CsvMetacardTransformer implements MetacardTransformer {
       throw new CatalogTransformerException("Unable to transform null metacard");
     }
 
-    Map<String, String> aliases =
-        (Map<String, String>)
-            arguments.getOrDefault(CsvQueryResponseTransformer.COLUMN_ALIAS_KEY, new HashMap<>());
-    List<String> attributes = getColumnOrder(arguments);
-
-    List<AttributeDescriptor> allAttributes =
-        new ArrayList<AttributeDescriptor>(metacard.getMetacardType().getAttributeDescriptors());
-    List<AttributeDescriptor> descriptors =
-        CollectionUtils.isEmpty(attributes)
-            ? allAttributes
-            : allAttributes
-                .stream()
-                .filter(attr -> attributes.contains(attr.getName()))
-                .collect(Collectors.toList());
-    Appendable appendable =
-        writeMetacardsToCsv(Collections.singletonList(metacard), descriptors, aliases);
-    return createResponse(appendable);
-  }
-
-  private List<String> getColumnOrder(Map<String, Serializable> arguments) {
-    Object columnOrder = arguments.get(CsvQueryResponseTransformer.COLUMN_ORDER_KEY);
-    if (columnOrder instanceof String && StringUtils.isNotBlank((String) columnOrder)) {
-      return Arrays.asList(((String) columnOrder).split(","));
-    }
-    return Optional.ofNullable(columnOrder)
-        .filter(value -> value instanceof List)
-        .map(value -> (List<String>) value)
-        .orElse(new ArrayList<>());
+    final List<Metacard> metacards = singletonList(metacard);
+    return CsvTransformerSupport.transformWithArguments(metacards, arguments);
   }
 }
