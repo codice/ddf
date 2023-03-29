f5133fc8492216c58d494c9cb381aaf657c9a573
Glen Hein <glen.hein@connexta.com>
2023-03-29 06:26:04 -0700

    New preferences storage solution

---
+++ platform/preferences/preferences-api/src/main/java/org/codice/ddf/preferences/PostRetrievalPlugin.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package org.codice.ddf.preferences;
+
+import java.util.Map;
+
+public interface PostRetrievalPlugin {
+
+  /**
+   * Implementations may modify the preferences map.
+   *
+   * @param preferences map of preference settings
+   */
+  void process(Map<String, Object> preferences);
+}
