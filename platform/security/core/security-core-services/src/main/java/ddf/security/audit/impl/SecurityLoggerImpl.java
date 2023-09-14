d268e1fcc277e324178f5e007792ad18dd028953
Glen Hein <glen.hein@octo.us>
2023-09-14 12:49:38 -0700

    fix star import

--- platform/security/core/security-core-services/src/main/java/ddf/security/audit/impl/SecurityLoggerImpl.java
+++ platform/security/core/security-core-services/src/main/java/ddf/security/audit/impl/SecurityLoggerImpl.java
@@ -21,6 +21,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.security.AccessController;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
