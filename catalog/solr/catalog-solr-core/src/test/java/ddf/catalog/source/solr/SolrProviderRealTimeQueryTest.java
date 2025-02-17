7662a746cb9d452ac6e20c0c7b3f95a5bfd2971e
Alexa Bird <alexa.bird@octo.us>
2025-02-18 06:42:21 +1100

    [2.29.x] Upgrade Solr 9.2.1 & Zookeeper 3.8.4 (#6845)
    * Upgrade to Solr 9.1
    
    * Upgrade to Solr 9.1.1
    
    * Upgrade zookeeper
    
    * Upgrade to Solr 9.2.1
    
    * Fix jetty version conflicts
    
    * Add the jetty-alpn-java-client bundle to solr feature
    
    ---------
    
    Co-authored-by: Phillip Klinefelter <phillip.klinefelter@gmail.com>

--- catalog/solr/catalog-solr-core/src/test/java/ddf/catalog/source/solr/SolrProviderRealTimeQueryTest.java
+++ catalog/solr/catalog-solr-core/src/test/java/ddf/catalog/source/solr/SolrProviderRealTimeQueryTest.java
@@ -77,6 +77,7 @@ public class SolrProviderRealTimeQueryTest {
   public static void beforeClass() throws Exception {
     Path solrDataPath = Paths.get("target/test-classes/realtime");
     System.setProperty("ddf.home", solrDataPath.toString());
+    System.setProperty("solr.install.dir", solrDataPath.toString());
 
     ConfigurationStore store = ConfigurationStore.getInstance();
     // Set to false for real time query by ID tests
@@ -107,6 +108,9 @@ public class SolrProviderRealTimeQueryTest {
 
     System.setProperty("jetty.testMode", "true");
     System.setProperty("jute.maxbuffer", "20000000"); // windows solution
+
+    System.setProperty("zookeeper.4lw.commands.whitelist", "*");
+    System.setProperty("solr.allowPaths", "*");
 
     miniSolrCloud =
         new MiniSolrCloudCluster.Builder(1, baseDir.getRoot().toPath())
@@ -155,10 +159,13 @@ public class SolrProviderRealTimeQueryTest {
   @AfterClass
   public static void afterClass() throws Exception {
     System.clearProperty("ddf.home");
+    System.clearProperty("solr.install.dir");
     System.clearProperty("pkiHandlerPrivateKeyPath");
     System.clearProperty("pkiHandlerPublicKeyPath");
     System.clearProperty("jetty.testMode");
     System.clearProperty("jute.maxbuffer");
+    System.clearProperty("zookeeper.4lw.commands.whitelist");
+    System.clearProperty("solr.allowPaths");
     System.clearProperty("solr.cloud.shardCount");
     System.clearProperty("solr.cloud.replicationFactor");
     System.clearProperty("solr.cloud.zookeeper.chroot");
